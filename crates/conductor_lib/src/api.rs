3801e965ef4ee2800692c518a699fe7b66575ac6
Michael Dougherty <maackle.d@gmail.com>
2020-02-15 21:32:41 -0800

    Cumbersome workaround to mockall + async_trait

--- crates/conductor_lib/src/api.rs
+++ crates/conductor_lib/src/api.rs
@@ -4,13 +4,38 @@ use crate::{
 };
 use async_trait::async_trait;
 use futures::sink::SinkExt;
+use mockall::mock;
 use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-use std::sync::Arc;
+use std::{pin::Pin, sync::Arc};
 use sx_core::{
-    cell::{autonomic::AutonomicCue, CellId, Cell},
+    cell::{autonomic::AutonomicCue, Cell, CellId},
     nucleus::{ZomeInvocation, ZomeInvocationResult},
 };
 use sx_types::{error::SkunkResult, prelude::*, shims::*, signature::Signature};
+
+#[async_trait(?Send)]
+pub trait ConductorApiInternalT {
+    async fn invoke_zome(
+        &self,
+        cell: Cell,
+        invocation: ZomeInvocation,
+    ) -> ConductorResult<ZomeInvocationResult>;
+
+    async fn network_send(&self, message: Lib3hClientProtocol) -> ConductorResult<()>;
+
+    async fn network_request(
+        &self,
+        _message: Lib3hClientProtocol,
+    ) -> ConductorResult<Lib3hServerProtocol>;
+
+    async fn autonomic_cue(&self, cue: AutonomicCue) -> ConductorResult<()>;
+
+    async fn crypto_sign(&self, _payload: String) -> ConductorResult<Signature>;
+
+    async fn crypto_encrypt(&self, _payload: String) -> ConductorResult<String>;
+
+    async fn crypto_decrypt(&self, _payload: String) -> ConductorResult<String>;
+}
 
 #[derive(Clone)]
 pub struct ConductorApiExternal {
@@ -35,49 +60,44 @@ impl ConductorApiInternal {
     }
 }
 
-type ConductorImmutable<'c> = RwLockReadGuard<'c, Conductor>;
-type ConductorMutable<'c> = RwLockWriteGuard<'c, Conductor>;
-
-impl ConductorApiInternal {
-    pub async fn invoke_zome(
+#[async_trait(?Send)]
+impl ConductorApiInternalT for ConductorApiInternal {
+    async fn invoke_zome(
         &self,
         cell: Cell,
         invocation: ZomeInvocation,
-    ) -> ConductorResult<ZomeInvocationResult>
-    {
+    ) -> ConductorResult<ZomeInvocationResult> {
         Ok(cell.invoke_zome(invocation).await?)
     }
 
-    pub async fn network_send(&self, message: Lib3hClientProtocol) -> ConductorResult<()>
-    {
+    async fn network_send(&self, message: Lib3hClientProtocol) -> ConductorResult<()> {
         let mut tx = self.lock.read().tx_network().clone();
         tx.send(message).await.map_err(|e| e.to_string().into())
     }
 
-    pub async fn network_request(
+    async fn network_request(
         &self,
         _message: Lib3hClientProtocol,
-    ) -> ConductorResult<Lib3hServerProtocol>
-    {
+    ) -> ConductorResult<Lib3hServerProtocol> {
         unimplemented!()
     }
 
-    pub async fn autonomic_cue(&self, cue: AutonomicCue) -> ConductorResult<()> {
+    async fn autonomic_cue(&self, cue: AutonomicCue) -> ConductorResult<()> {
         let conductor = self.lock.write();
         let cell = conductor.cell_by_id(&self.cell_id)?;
         let _ = cell.handle_autonomic_process(cue.into()).await;
         Ok(())
     }
 
-    pub async fn crypto_sign(&self, _payload: String) -> ConductorResult<Signature> {
+    async fn crypto_sign(&self, _payload: String) -> ConductorResult<Signature> {
         unimplemented!()
     }
 
-    pub async fn crypto_encrypt(&self, _payload: String) -> ConductorResult<String> {
+    async fn crypto_encrypt(&self, _payload: String) -> ConductorResult<String> {
         unimplemented!()
     }
 
-    pub async fn crypto_decrypt(&self, _payload: String) -> ConductorResult<String> {
+    async fn crypto_decrypt(&self, _payload: String) -> ConductorResult<String> {
         unimplemented!()
     }
 }
@@ -96,8 +116,55 @@ impl ConductorApiExternal {
     }
 }
 
+
+// See https://github.com/asomers/mockall/issues/75
+
+#[async_trait]
+pub trait TestT {
+    async fn invoke_zome(
+        &self, cell: Cell, invocation: ZomeInvocation
+    ) -> ConductorResult<ZomeInvocationResult>;
+}
+
+mock! {
+    pub Imp {
+        fn sync_invoke_zome(
+            &self, cell: Cell, invocation: ZomeInvocation
+        ) -> ConductorResult<ZomeInvocationResult>;
+    }
+}
+
+#[async_trait]
+impl TestT for MockImp {
+    async fn invoke_zome(
+        &self, cell: Cell, invocation: ZomeInvocation
+    ) -> ConductorResult<ZomeInvocationResult> {
+        self.sync_invoke_zome(cell, invocation)
+    }
+}
+
+
+
+// macro_rules! async_return_type {
+//     ($t:ty) => {
+//         Pin<Box<dyn std::future::Future<Output = $t> >>
+//     }
+// }
+
+// macro_rules! async_return_val {
+//     ($v:expr) => {
+//         Box::pin( async { $v })
+//     };
+//     ($v:block) => {
+//         Box::pin( async { $v })
+//     }
+// }
+
+
+
+
 //////////////////////////////////////////////////////////////////////////////////
-///
+/// Unused ideas from a while ago
 ///
 
 /// The set of messages that a conductor understands how to handle
