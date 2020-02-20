124589a6bde629976811d30a12ae6de2b24b76f5
Michael Dougherty <maackle.d@gmail.com>
2020-02-19 19:16:51 -0800

    Musings on Workflows

--- crates/cell/workflow.rs
+++ crates/cell/workflow.rs
@@ -8,3 +8,28 @@ struct CellContext {
   conductor_api: ConductorApiRef  // cheaply clonable refernce to something that allows us to send data to conductor from anywhere
   // stuff goes here.
 }
+
+pub fn finish_workflow(result: WorkflowResult) {
+  info!("finishing workflow {}", ((result.name)));
+
+  finalize_workspace(result.workspace)
+  // actua
+}
+
+
+struct WorkflowResult {
+  workspace: Workspace,
+  triggers: Vec<WorkflowTrigger>,
+}
+
+enum Trigger {
+  Workflow,
+  Callback,
+  Signal,
+}
+
+enum TriggerDelay {
+  Immediate
+}
+
+struct Workspace;
