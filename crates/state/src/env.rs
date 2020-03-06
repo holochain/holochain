372fd5a7b70b409c62992096108b4a4c8477fd64
Michael Dougherty <maackle.d@gmail.com>
2020-03-06 00:16:20 -0800

    Split flags into default and required

--- crates/state/src/env.rs
+++ crates/state/src/env.rs
@@ -18,18 +18,25 @@ const MAX_DBS: u32 = 32;
 
 lazy_static! {
     static ref ENVIRONMENTS: RwLock<HashMap<PathBuf, Environment>> = RwLock::new(HashMap::new());
-    static ref DB_MANAGERS: RwLock<HashMap<PathBuf, Arc<DbManager>>> =
-        RwLock::new(HashMap::new());
+    static ref DB_MANAGERS: RwLock<HashMap<PathBuf, Arc<DbManager>>> = RwLock::new(HashMap::new());
+}
+
+fn default_flags() -> EnvironmentFlags {
+    // The flags WRITE_MAP and MAP_ASYNC make writes waaaaay faster by async writing to disk rather than blocking
+    // There is some loss of data integrity guarantees that comes with this.
+    EnvironmentFlags::WRITE_MAP | EnvironmentFlags::MAP_ASYNC
 }
 
 #[cfg(feature = "lmdb_no_tls")]
-fn default_flags() -> EnvironmentFlags {
-    EnvironmentFlags::WRITE_MAP | EnvironmentFlags::MAP_ASYNC | EnvironmentFlags::NO_TLS
+fn required_flags() -> EnvironmentFlags {
+    // NO_TLS associates read slots with the transaction object instead of the thread, which is crucial for us
+    // so we can have multiple read transactions per thread (since futures can run on any thread)
+    EnvironmentFlags::NO_TLS
 }
 
 #[cfg(not(feature = "lmdb_no_tls"))]
-fn default_flags() -> EnvironmentFlags {
-    EnvironmentFlags::WRITE_MAP | EnvironmentFlags::MAP_ASYNC
+fn required_flags() -> EnvironmentFlags {
+    EnvironmentFlags::default()
 }
 
 /// A standard way to create a representation of an LMDB environment suitable for Holochain
@@ -58,15 +65,10 @@ fn rkv_builder(
             .set_map_size(initial_map_size.unwrap_or(DEFAULT_INITIAL_MAP_SIZE))
             // max number of DBs in this environment
             .set_max_dbs(MAX_DBS)
-            // The flags WRITE_MAP and MAP_ASYNC make writes waaaaay faster by async writing to disk rather than blocking
-            // There is some loss of data integrity guarantees that comes with this.
-            // NO_TLS associates read slots with the transaction object instead of the thread, which is crucial for us
-            // so we can have multiple read transactions per thread (since futures can run on any thread)
-            .set_flags(flags.unwrap_or_else(default_flags));
+            .set_flags(flags.unwrap_or_else(default_flags) | required_flags());
         Rkv::from_env(path, env_builder)
     }
 }
-
 
 /// The canonical representation of a reference to a (singleton) LMDB environment.
 /// The wrapper contains methods for managing transactions and database connections,
@@ -130,9 +132,9 @@ impl Environment {
 
     pub fn dbs(&self) -> WorkspaceResult<Arc<DbManager>> {
         let mut map = DB_MANAGERS.write();
-        let dbs = map.entry(self.0.as_ref().path().into()).or_insert_with(|| {
-            Arc::new(DbManager::new(self.clone()).expect("TODO"))
-        });
+        let dbs = map
+            .entry(self.0.as_ref().path().into())
+            .or_insert_with(|| Arc::new(DbManager::new(self.clone()).expect("TODO")));
         Ok(dbs.clone())
         // DbManager::new(self.env())
     }
