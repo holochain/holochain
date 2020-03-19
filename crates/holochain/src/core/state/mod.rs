f403e4329e6728cea20037dff36ded8e85666c94
Michael Dougherty <maackle.d@gmail.com>
2020-03-19 09:13:22 -0700

    Add ASCII diagram of BufferedStore composition

--- crates/holochain/src/core/state/mod.rs
+++ crates/holochain/src/core/state/mod.rs
@@ -7,7 +7,30 @@
 //! - [SourceChainBuffer]: the union of a [ChainCasBuffer] and a [ChainSequenceBuffer], which fully represents a source chain
 //! - [CasMetaBuffer]: (*unimplemented*) Uses a KvvBuffer to represent EAV-like relationships between CAS entries
 //! - [Cascade]: (*unimplemented*) Unifies two [ChainCasBuffer] and two [CasMetaBuffer] references (one of each is a cache) in order to perform the complex metadata-aware queries for getting entries and links, including CRUD resolution
-
+//!
+//! The follow diagram shows the composition hierarchy.
+//! The arrows mean "contains at least one of".
+//!
+//! ```
+//!               Cascade         SourceChain
+//!                  |                 |
+//!                  |                 V
+//!                  |           SourceChainBuf
+//!                  |                 |
+//!                  |                 |
+//!            +----------+      +-----+------+
+//!            |          |      |            |
+//!            |          V      V            |
+//!            V         ChainCasBuf          V
+//!        CasMetaBuf         |        ChainSequenceBuf
+//!            |              V               |
+//!            |           CasBuf             |
+//!            |              |               |
+//!            V              V               V
+//!         KvvBuf          KvBuf          IntKvBuf
+//!
+//! source: https://textik.com/#d7907793784e17e9
+//! ```
 
 pub mod cascade;
 pub mod chain_cas;
