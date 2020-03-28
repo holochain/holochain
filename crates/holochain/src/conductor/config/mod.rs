ce33fc30f112fd58f8ce3b7551bd5f9019080e59
Eric Harris-Braun <eric@harris-braun.com>
2020-03-28 15:57:37 -0400

    added tests for config

--- crates/holochain/src/conductor/config/mod.rs
+++ crates/holochain/src/conductor/config/mod.rs
@@ -1,19 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 mod dpki_config;
-mod logger_config;
+//mod logger_config;
 mod network_config;
 mod passphrase_service_config;
-mod signal_config;
+//mod signal_config;
 use super::{
     error::{ConductorError, ConductorResult},
     paths::EnvironmentRootPath,
 };
 use dpki_config::DpkiConfig;
-use logger_config::LoggerConfig;
+//use logger_config::LoggerConfig;
 use network_config::NetworkConfig;
 use passphrase_service_config::PassphraseServiceConfig;
-use signal_config::SignalConfig;
+//use signal_config::SignalConfig;
 use std::path::Path;
 
 #[derive(Deserialize, Serialize, Default, Debug, PartialEq)]
@@ -23,7 +23,7 @@ pub struct ConductorConfig {
     pub environment_path: EnvironmentRootPath,
 
     /// Configures how logging should behave. Optional.
-    pub logger: LoggerConfig,
+    //pub logger: LoggerConfig,
 
     /// Config options for the network module. Optional.
     pub network: Option<NetworkConfig>,
@@ -51,7 +51,7 @@ pub struct ConductorConfig {
     pub dpki: Option<DpkiConfig>,
 
     /// Which signals to emitproject_root
-    pub signals: SignalConfig,
+    //pub signals: SignalConfig,
 
     /// Configure how the conductor should prompt the user for the passphrase to lock/unlock keystores.
     /// The conductor is independent of the specialized implementation of the trait
@@ -60,15 +60,85 @@ pub struct ConductorConfig {
     pub passphrase_service: PassphraseServiceConfig,
 }
 
+/// helper fnction function to load a `Config` from a toml string.
+pub fn config_from_toml<'a, T>(toml: &'a str) -> ConductorResult<T>
+where
+    T: Deserialize<'a>,
+{
+    toml::from_str::<T>(toml).map_err(|e| ConductorError::DeserializationError(e))
+}
+
 impl ConductorConfig {
+    /// create a ConductorConfig struct from a toml file path
     pub fn load_toml(path: &Path) -> ConductorResult<ConductorConfig> {
         // let path_buf: &Path = path.into();
-        let content_toml = std::fs::read_to_string(path).map_err(|err| match err {
+        let config_toml = std::fs::read_to_string(path).map_err(|err| match err {
             e @ std::io::Error { .. } if e.kind() == std::io::ErrorKind::NotFound => {
                 ConductorError::ConfigMissing(path.into())
             }
             _ => err.into(),
         })?;
-        Ok(toml::from_str(&content_toml)?)
+        config_from_toml(&config_toml)
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_config_load_toml() {
+        let bad_path = Path::new("fake");
+        let result = ConductorConfig::load_toml(bad_path);
+        assert_eq!(
+            "Err(ConfigMissing(\"fake\"))".to_string(),
+            format!("{:?}", result)
+        );
+
+        // successfull load test in conductor/ineractive
+    }
+
+    #[test]
+    fn test_config_bad_toml() {
+        let result: ConductorResult<ConductorConfig> = config_from_toml("this isn't toml");
+        assert_eq!("Err(DeserializationError(Error { inner: ErrorInner { kind: Wanted { expected: \"an equals\", found: \"an identifier\" }, line: Some(0), col: 5, at: Some(5), message: \"\", key: [] } }))".to_string(), format!("{:?}", result));
+    }
+
+    #[test]
+    fn test_config_complete_minimal_config() {
+        let toml = r#"
+    environment_path = "/path/to/env"
+
+    [passphrase_service]
+    type = "cmd"
+    "#;
+        let result: ConductorResult<ConductorConfig> = config_from_toml(toml);
+        assert_eq!("Ok(ConductorConfig { environment_path: EnvironmentRootPath(\"/path/to/env\"), network: None, signing_service_uri: None, encryption_service_uri: None, decryption_service_uri: None, dpki: None, passphrase_service: Cmd })", format!("{:?}", result));
+    }
+
+    #[test]
+    fn test_config_complete_config() {
+        let toml = r#"
+    environment_path = "/path/to/env"
+
+    [passphrase_service]
+    type = "cmd"
+
+    [network]
+    type = "sim2h"
+    url = "ws://localhost:9000"
+
+    encryption_service_uri = "ws://localhost:9001"
+    decryption_service_uri = "ws://localhost:9002"
+    signing_service_uri = "ws://localhost:9003"
+
+    [dpki]
+    instance_id = "some_id"
+    init_params = "some_params"
+
+    "#;
+        let result: ConductorResult<ConductorConfig> = config_from_toml(toml);
+        assert_eq!("Ok(ConductorConfig { environment_path: EnvironmentRootPath(\"/path/to/env\"), network: Some(Sim2h { url: \"ws://localhost:9000/\" }), signing_service_uri: None, encryption_service_uri: None, decryption_service_uri: None, dpki: Some(DpkiConfig { instance_id: \"some_id\", init_params: \"some_params\" }), passphrase_service: Cmd })", format!("{:?}", result));
     }
 }
