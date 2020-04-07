45b97f6d6aa61bd91b63932240558b6e5f16f19e
Michael Dougherty <maackle.d@gmail.com>
2020-04-06 17:15:36 -0700

    Add deny and immediate allow for better optics on doc status

--- crates/keystore/src/lib.rs
+++ crates/keystore/src/lib.rs
@@ -1,5 +1,8 @@
-// FIXME: uncomment this deny [TK-01128]
-// #![deny(missing_docs)]
+#![deny(missing_docs)]
 
+// FIXME: remove these allows as docs get written
+
+#[allow(missing_docs)]
 pub mod keystore;
+#[allow(missing_docs)]
 pub mod passphrase_manager;
