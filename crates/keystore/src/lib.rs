144402f42ab6716c15966756bd899a19a4465001
Michael Dougherty <maackle.d@gmail.com>
2020-04-06 17:18:26 -0700

    Revert "Add deny and immediate allow for better optics on doc status"
    This reverts commit 45b97f6d6aa61bd91b63932240558b6e5f16f19e.

--- crates/keystore/src/lib.rs
+++ crates/keystore/src/lib.rs
@@ -1,8 +1,5 @@
-#![deny(missing_docs)]
+// FIXME: uncomment this deny [TK-01128]
+// #![deny(missing_docs)]
 
-// FIXME: remove these allows as docs get written
-
-#[allow(missing_docs)]
 pub mod keystore;
-#[allow(missing_docs)]
 pub mod passphrase_manager;
