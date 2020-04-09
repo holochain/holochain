a59a0b40f432381fb5fbb9455df0bff85bd556f5
timotree3 <timorcb@gmail.com>
2020-04-09 15:44:02 -0400

    test_utils_wasm: Run cargo fmt now that it's in the workspace

--- test_utils/wasm/src/lib.rs
+++ test_utils/wasm/src/lib.rs
@@ -8,12 +8,22 @@ pub enum TestWasm {
 
 impl From<TestWasm> for DnaWasm {
     fn from(test_wasm: TestWasm) -> DnaWasm {
-        DnaWasm::from(
-        match test_wasm {
-            TestWasm::Debug => include_bytes!(concat!(env!("OUT_DIR"), "/wasm32-unknown-unknown/release/test_wasm_debug.wasm")).to_vec(),
-            TestWasm::Foo => include_bytes!(concat!(env!("OUT_DIR"), "/wasm32-unknown-unknown/release/test_wasm_foo.wasm")).to_vec(),
-            TestWasm::Imports => include_bytes!(concat!(env!("OUT_DIR"), "/wasm32-unknown-unknown/release/test_wasm_imports.wasm")).to_vec(),
-        }
-    )
+        DnaWasm::from(match test_wasm {
+            TestWasm::Debug => include_bytes!(concat!(
+                env!("OUT_DIR"),
+                "/wasm32-unknown-unknown/release/test_wasm_debug.wasm"
+            ))
+            .to_vec(),
+            TestWasm::Foo => include_bytes!(concat!(
+                env!("OUT_DIR"),
+                "/wasm32-unknown-unknown/release/test_wasm_foo.wasm"
+            ))
+            .to_vec(),
+            TestWasm::Imports => include_bytes!(concat!(
+                env!("OUT_DIR"),
+                "/wasm32-unknown-unknown/release/test_wasm_imports.wasm"
+            ))
+            .to_vec(),
+        })
     }
 }
