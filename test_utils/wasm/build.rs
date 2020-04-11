67e4f4543c22bbd183d8d15fb6b3aec8b8a3e394
Timo <timorcb@gmail.com>
2020-04-11 11:39:13 -0400

    test_utils_wasm: build.rs: Apply suggestions from code review
    test_utils_wasm: build.rs: Move `out_dir` outside of loop

--- test_utils/wasm/build.rs
+++ test_utils/wasm/build.rs
@@ -1,17 +1,28 @@
 use std::path::Path;
 
 fn main() {
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+
     // force a rebuild every time because:
     // - the wasm .rs files changing doesn't trigger a rebuild here
     // - any shared dependency with core can be changed and that won't trigger a rebuild
     // the alternative is to recurse over all the files in the dependencies and wasm
     // crates and rerun-if-changed on each of those
     // if you want to implement that, go for it :)
-    println!("cargo:rerun-if-changed=file-not-exists-forces-rebuild");
+    let hacky_file_name = "__wasm_test_utils_non-existent_file";
+    let hacky_file_not_found = match std::fs::metadata(hacky_file_name) {
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => true,
+        _ => false,
+    };
+    assert!(
+        hacky_file_not_found,
+        "hack: {} must not exist in package directory for build to continue",
+        hacky_file_name
+    );
 
-    for m in vec!["debug", "foo", "imports"] {
+    println!("cargo:rerun-if-changed={}", hacky_file_name);
 
-        let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    for &m in ["debug", "foo", "imports"].iter() {
         let cargo_toml = Path::new(m).join("Cargo.toml");
 
         let cargo_command = std::env::var_os("CARGO");
@@ -27,7 +38,7 @@ fn main() {
             .arg("--release")
             .arg("--target")
             .arg("wasm32-unknown-unknown")
-            .env("CARGO_TARGET_DIR", out_dir)
+            .env("CARGO_TARGET_DIR", &out_dir)
             .status()
             .unwrap();
 
