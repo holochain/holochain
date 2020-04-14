6a4014f264639b557153d02bbc8eeba3d9b12f78
timotree3 <timorcb@gmail.com>
2020-04-14 16:41:02 -0400

    crypto: Remove and inline remaining use of `safe_mem_copy`

--- crates/crypto/src/bytes/safe_mem_copy.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-pub(crate) fn safe_mem_copy(dst: &mut [u8], dst_offset: usize, src: &[u8]) -> CryptoResult<()> {
-    let dst_subslice = dst
-        .get_mut(dst_offset..dst_offset + src.len())
-        .ok_or(CryptoError::WriteOverflow)?;
-    dst_subslice.copy_from_slice(src);
-    Ok(())
-}
