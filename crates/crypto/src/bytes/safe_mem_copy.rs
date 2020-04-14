04a91be09f2c5785cf7b46f9db9abf9fd8518eec
timotree3 <timorcb@gmail.com>
2020-04-14 16:37:28 -0400

    crypto: Use `[T]::copy_from_slice` for safe and efficient memcpy
    See the docs for `copy_from_slice`: https://doc.rust-lang.org/std/primitive.slice.html#method.copy_from_slice
    
    It is guarenteed to be a `memcpy`. In fact, it will likely be faster than the
    previous unsafe implementation because it uses `copy_non_overlapping`.

--- crates/crypto/src/bytes/safe_mem_copy.rs
+++ crates/crypto/src/bytes/safe_mem_copy.rs
@@ -1,17 +1,9 @@
-//! rust doesn't provide a fast binary copy function
-
 use crate::*;
 
 pub(crate) fn safe_mem_copy(dst: &mut [u8], dst_offset: usize, src: &[u8]) -> CryptoResult<()> {
-    if dst_offset + src.len() > dst.len() {
-        return Err(CryptoError::WriteOverflow);
-    }
-
-    // INVARIANTS:
-    //   - we cannot write beyond the end of src - ensured by check above
-    unsafe {
-        std::ptr::copy(src.as_ptr(), dst.as_mut_ptr().add(dst_offset), src.len());
-    }
-
+    let dst_subslice = dst
+        .get_mut(dst_offset..dst_offset + src.len())
+        .ok_or(CryptoError::WriteOverflow)?;
+    dst_subslice.copy_from_slice(src);
     Ok(())
 }
