655ab44655bba1bec4596f03f2b3fa0198fb48a0
David Meister <thedavidmeister@gmail.com>
2020-04-15 22:31:20 +0400

    docker push

---
+++ docker/push.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+owner=holochain
+repo=holochain-2020
+box=${1:-sim2h_server}
+branch=${2:-$( git rev-parse --abbrev-ref HEAD )}
+tag="$owner/$repo:$box.$branch"
+
+docker push "holochain/holochain-2020:$box.$branch"
+
+#
+#while read region; do
+# docker tag $tag "024992937548.dkr.ecr.$region.amazonaws.com/$tag"
+# docker push "024992937548.dkr.ecr.$region.amazonaws.com/$tag"
+#done < $dir/aws-regions.txt
