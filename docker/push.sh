f1ee33e8df9e046da9a1171d01064acd4e1943bb
David Meister <thedavidmeister@gmail.com>
2020-04-16 11:16:05 +0400

    cleanup scheduling of docker builds and lint some comments

--- docker/push.sh
+++ docker/push.sh
@@ -10,9 +10,3 @@ branch=${2:-$( git rev-parse --abbrev-ref HEAD )}
 tag="$owner/$repo:$box.$branch"
 
 docker push "holochain/holochain-2020:$box.$branch"
-
-#
-#while read region; do
-# docker tag $tag "024992937548.dkr.ecr.$region.amazonaws.com/$tag"
-# docker push "024992937548.dkr.ecr.$region.amazonaws.com/$tag"
-#done < $dir/aws-regions.txt
