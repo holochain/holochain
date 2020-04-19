0096190ab37045dbb23a0097cdd7ce3dcfa5153e
Michael Dougherty <maackle.d@gmail.com>
2020-04-19 15:59:44 -0700

    Remove all SkunkError variants other than Todo

--- crates/types/src/error.rs
+++ crates/types/src/error.rs
@@ -5,7 +5,6 @@
 //! As we decide which previous code to use, we should port those error types
 //! over to the appropriate error type in this crate.
 
-use serde_json::Error as SerdeError;
 use std::fmt;
 use thiserror::Error;
 
@@ -15,32 +14,19 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum SkunkError {
     Todo(String),
-    NoneError,
-    IoError(#[from] std::io::Error),
-    HcidError(#[from] hcid::HcidError),
-    SerdeError(#[from] SerdeError),
-    // SerializedBytesError(#[from] SerializedBytesError),
-    // LocksmithError(#[from] holochain_locksmith::LocksmithError),
 }
 
 impl fmt::Display for SkunkError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             SkunkError::Todo(reason) => write!(f, "{}", reason),
-            _ => write!(f, "{:?}", self),
         }
     }
 }
 
 impl PartialEq for SkunkError {
     fn eq(&self, other: &Self) -> bool {
-        use SkunkError::*;
         match (self, other) {
-            (Todo(a), Todo(b)) => a == b,
-            (IoError(a), IoError(b)) => a.to_string() == b.to_string(),
-            (HcidError(a), HcidError(b)) => a.to_string() == b.to_string(),
-            (SerdeError(a), SerdeError(b)) => a.to_string() == b.to_string(),
-            // (SerializedBytesError(a), SerializedBytesError(b)) => a == b,
             _ => false,
         }
     }
