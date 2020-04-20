2cee43f0b565d77aed26c9b202759a98fc025dfe
David Meister <thedavidmeister@gmail.com>
2020-04-20 14:18:47 +0400

    tests and docs for random bytes

--- crates/holochain/src/core/ribosome/random_bytes.rs
+++ crates/holochain/src/core/ribosome/random_bytes.rs
@@ -7,6 +7,8 @@ use std::sync::Arc;
 use sx_zome_types::RandomBytesInput;
 use sx_zome_types::RandomBytesOutput;
 
+/// generate n crypto secure random bytes from the system
+/// may also be called directly by things other than the `random_bytes` ribosome invocation
 pub fn csprng_bytes(n: usize) -> Result<Vec<u8>, ring::error::Unspecified> {
     let rng = rand::SystemRandom::new();
     let mut bytes = vec![0; n];
@@ -16,6 +18,7 @@ pub fn csprng_bytes(n: usize) -> Result<Vec<u8>, ring::error::Unspecified> {
     Ok(bytes)
 }
 
+/// thin wrapper around csprng to expose it to the ribosome
 pub fn random_bytes(
     _ribosome: Arc<WasmRibosome>,
     _host_context: Arc<HostContext>,
@@ -28,10 +31,33 @@ pub fn random_bytes(
 pub mod wasm_test {
     use sx_zome_types::RandomBytesInput;
     use sx_zome_types::RandomBytesOutput;
+    use crate::core::ribosome::random_bytes::csprng_bytes;
 
     #[test]
     fn invoke_import_random_bytes_test() {
-        let _: RandomBytesOutput =
-            crate::call_test_ribosome!("imports", "random_bytes", RandomBytesInput::new(64));
+        // generate random bytes through a ribosome invocation
+        let invoke_closure = |i| {
+            let output: RandomBytesOutput = crate::call_test_ribosome!("imports", "random_bytes", RandomBytesInput::new(i));
+            output.into_inner()
+        };
+        // generate random bytes through a direct function call
+        let direct_closure = |i| {
+            csprng_bytes(i).unwrap()
+        };
+
+        let i = 64;
+        macro_rules! do_test {
+            ( $( $c:ident ),* ) => {
+                $(
+                    let o = $c(i);
+                    // number of bytes needs to match input
+                    assert!(o.len() == i);
+                    // bytes need to be filled correctly (not left as default 0 bytes)
+                    assert_ne!(vec![0; i], o);
+                )*
+            };
+        }
+        do_test!(invoke_closure, direct_closure);
+
     }
 }
