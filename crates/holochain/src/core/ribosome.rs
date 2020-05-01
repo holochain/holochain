73fd0f785b0abd8f819eaad63323a0b9bb9b6b95
David Braden <neonphog@gmail.com>
2020-05-01 13:01:37 -0600

    Update crates/holochain/src/core/ribosome.rs
    Co-authored-by: Timo <timorcb@gmail.com>

--- crates/holochain/src/core/ribosome.rs
+++ crates/holochain/src/core/ribosome.rs
@@ -90,7 +90,7 @@ pub trait RibosomeT: Sized {
 pub struct WasmRibosome {
     /// NOTE - Currently taking a full DnaFile here.
     ///      - It would be an optimization to pre-ensure the WASM bytecode
-    ///      - is already in the wasm cache, and only include the Dna portion
+    ///      - is already in the wasm cache, and only include the DnaDef portion
     ///      - here in the ribosome.
     dna_file: DnaFile,
 }
