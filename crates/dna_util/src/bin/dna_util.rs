c3c2d73e4477ed6a7d1bbf32ea07c01702ae4122
neonphog <neonphog@gmail.com>
2020-05-01 14:33:41 -0600

    checkpoint

---
+++ crates/dna_util/src/bin/dna_util.rs
@@ -0,0 +1,29 @@
+#![deny(missing_docs)]
+//! Binary `dna_util` command executable.
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "dna_util", about = "Holochain DnaFile Utility.")]
+struct Opt {
+    /// Extract a DnaFile into a Dna Working Directory
+    #[structopt(short = "e", long)]
+    extract: Option<std::path::PathBuf>,
+
+    /// Compile a Dna Working Directory into a DnaFile
+    #[structopt(short = "c", long)]
+    compile: Option<std::path::PathBuf>,
+}
+
+/// Main `dna_util` executable entrypoint.
+#[tokio::main(threaded_scheduler)]
+pub async fn main() {
+    let opt = Opt::from_args();
+
+    if opt.extract.is_none() && opt.compile.is_none() {
+        Opt::clap().print_long_help().unwrap();
+        return;
+    }
+
+    println!("yo");
+}
---
+++ crates/dna_util/src/lib.rs
@@ -0,0 +1,3 @@
+#![deny(missing_docs)]
+//! DnaFile Utilities
+
