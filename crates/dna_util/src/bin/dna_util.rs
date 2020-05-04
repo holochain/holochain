efc9cc468c981d3bbb1fefb4ff899cc4e70328b9
neonphog <neonphog@gmail.com>
2020-05-04 13:58:21 -0600

    integrate dna_util binary

--- crates/dna_util/src/bin/dna_util.rs
+++ crates/dna_util/src/bin/dna_util.rs
@@ -6,11 +6,15 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "dna_util", about = "Holochain DnaFile Utility.")]
 struct Opt {
-    /// Extract a DnaFile into a Dna Working Directory
+    /// Extract a DnaFile into a Dna Working Directory.
+    ///
+    /// (`dna_util -e my-dna.dna.gz` creates dir `my-dna.dna_work_dir`)
     #[structopt(short = "e", long)]
     extract: Option<std::path::PathBuf>,
 
-    /// Compile a Dna Working Directory into a DnaFile
+    /// Compile a Dna Working Directory into a DnaFile.
+    ///
+    /// (`dna_util -c my-dna.dna_work_dir` creates file `my-dna.dna.gz`)
     #[structopt(short = "c", long)]
     compile: Option<std::path::PathBuf>,
 }
@@ -21,8 +25,33 @@ pub async fn main() {
     let opt = Opt::from_args();
 
     if opt.extract.is_none() && opt.compile.is_none() {
-        Opt::clap().print_help().unwrap();
+        eprintln!("INPUT ERROR: no command selected.\n");
+        Opt::clap().print_long_help().unwrap();
         return;
+    }
+
+    let mut exclusive = 0;
+
+    if opt.extract.is_some() {
+        exclusive += 1;
+    }
+
+    if opt.compile.is_some() {
+        exclusive += 1;
+    }
+
+    if exclusive > 1 {
+        eprintln!("INPUT ERROR: 'extract' and 'compile' commands are exclusive.\n");
+        Opt::clap().print_long_help().unwrap();
+        return;
+    }
+
+    if let Some(extract) = opt.extract {
+        dna_util::extract(extract).await.unwrap();
+    }
+
+    if let Some(compile) = opt.compile {
+        dna_util::compile(compile).await.unwrap();
     }
 
     println!("yo");
--- crates/dna_util/src/lib.rs
+++ crates/dna_util/src/lib.rs
@@ -2,8 +2,8 @@
 //! DnaFile Utilities
 
 use holochain_serialized_bytes::prelude::*;
-use holochain_types::dna::{DnaDef, DnaFile};
-use std::{collections::BTreeMap, convert::TryFrom};
+use holochain_types::dna::{wasm::DnaWasm, zome::Zome, DnaDef, DnaFile};
+use std::collections::BTreeMap;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 /// DnaUtilError type.
@@ -33,15 +33,30 @@ pub enum DnaUtilError {
 /// DnaUtil Result type.
 pub type DnaUtilResult<T> = Result<T, DnaUtilError>;
 
-/// Extract a DnaFile into a Dna Working Directory
-pub async fn extract(dna_file_path: impl AsRef<std::path::Path>) -> DnaUtilResult<()> {
-    let dna_file_path = dna_file_path.as_ref().canonicalize()?;
-    if !dna_file_path.to_string_lossy().ends_with(".dna.gz") {
-        return Err(DnaUtilError::InvalidInput(format!(
-            "bad extract path, dna files must end with '.dna.gz': {}",
-            dna_file_path.display()
-        )));
+/// internal convert between dna_file_path and dna_work_dir
+fn dna_file_path_convert(
+    dna_file_path: impl AsRef<std::path::Path>,
+    to_work_dir: bool,
+) -> DnaUtilResult<std::path::PathBuf> {
+    let dna_file_path = dna_file_path.as_ref();
+
+    let tmp_lossy = dna_file_path.to_string_lossy();
+    if to_work_dir {
+        if !tmp_lossy.ends_with(".dna.gz") {
+            return Err(DnaUtilError::InvalidInput(format!(
+                "bad extract path, dna files must end with '.dna.gz': {}",
+                dna_file_path.display()
+            )));
+        }
+    } else {
+        if !tmp_lossy.ends_with(".dna_work_dir") {
+            return Err(DnaUtilError::InvalidInput(format!(
+                "bad compile path, work dirs must end with '.dna_work_dir': {}",
+                dna_file_path.display()
+            )));
+        }
     }
+
     let filename = dna_file_path
         .file_name()
         .ok_or_else(|| {
@@ -51,10 +66,23 @@ pub async fn extract(dna_file_path: impl AsRef<std::path::Path>) -> DnaUtilResul
             ))
         })?
         .to_string_lossy();
-    let filename_base = &filename[..filename.len() - 7];
-    let dirname = format!("{}.dna_work_dir", filename_base);
-    let mut dir = dna_file_path.clone();
-    dir.set_file_name(dirname);
+    let new_name = if to_work_dir {
+        let filename_base = &filename[..filename.len() - 7];
+        format!("{}.dna_work_dir", filename_base)
+    } else {
+        let filename_base = &filename[..filename.len() - 13];
+        format!("{}.dna.gz", filename_base)
+    };
+    let mut dir = std::path::PathBuf::new();
+    dir.push(dna_file_path);
+    dir.set_file_name(new_name);
+    Ok(dir)
+}
+
+/// Extract a DnaFile into a Dna Working Directory
+pub async fn extract(dna_file_path: impl AsRef<std::path::Path>) -> DnaUtilResult<()> {
+    let dna_file_path = dna_file_path.as_ref().canonicalize()?;
+    let dir = dna_file_path_convert(&dna_file_path, true)?;
     tokio::fs::create_dir_all(&dir).await?;
 
     let mut file = tokio::fs::File::open(dna_file_path).await?;
@@ -71,13 +99,36 @@ pub async fn extract(dna_file_path: impl AsRef<std::path::Path>) -> DnaUtilResul
         wasm_file.write_all(&wasm.code()).await?;
     }
 
-    let dna_json: DnaDefJson = dna_file.dna().clone().try_into()?;
+    let dna_json = DnaDefJson::from_dna_def(dna_file.dna())?;
     let dna_json = serde_json::to_string_pretty(&dna_json)?;
 
     let mut json_filename = dir.clone();
     json_filename.push("dna.json");
     let mut json_file = tokio::fs::File::create(json_filename).await?;
     json_file.write_all(dna_json.as_bytes()).await?;
+
+    Ok(())
+}
+
+/// Compile a Dna Working Directory into a DnaFile
+pub async fn compile(dna_work_dir: impl AsRef<std::path::Path>) -> DnaUtilResult<()> {
+    let dna_work_dir = dna_work_dir.as_ref().canonicalize()?;
+    let dna_file_path = dna_file_path_convert(&dna_work_dir, false)?;
+
+    let mut json_filename = dna_work_dir.clone();
+    json_filename.push("dna.json");
+    let mut json_file = tokio::fs::File::open(json_filename).await?;
+
+    let mut json_data = Vec::new();
+    json_file.read_to_end(&mut json_data).await?;
+
+    let json_file: DnaDefJson = serde_json::from_slice(&json_data)?;
+
+    let dna_file_content = json_file.compile_dna_file(&dna_work_dir).await?;
+    let dna_file_content = dna_file_content.as_file_content().await?;
+
+    let mut dna_file = tokio::fs::File::create(dna_file_path).await?;
+    dna_file.write_all(&dna_file_content).await?;
 
     Ok(())
 }
@@ -103,11 +154,9 @@ struct DnaDefJson {
     pub zomes: BTreeMap<String, ZomeJson>,
 }
 
-impl TryFrom<DnaDef> for DnaDefJson {
-    type Error = SerializedBytesError;
-
-    fn try_from(dna: DnaDef) -> Result<Self, SerializedBytesError> {
-        let properties: JsonValueDecodeHelper = dna.properties.try_into()?;
+impl DnaDefJson {
+    pub fn from_dna_def(dna: &DnaDef) -> DnaUtilResult<DnaDefJson> {
+        let properties: JsonValueDecodeHelper = dna.properties.clone().try_into()?;
         let mut zomes = BTreeMap::new();
         for zome_name in dna.zomes.keys() {
             let zome_file = format!("./{}.wasm", zome_name);
@@ -119,11 +168,49 @@ impl TryFrom<DnaDef> for DnaDefJson {
             );
         }
         Ok(Self {
-            name: dna.name,
-            uuid: dna.uuid,
+            name: dna.name.clone(),
+            uuid: dna.uuid.clone(),
             properties: properties.0,
             zomes,
         })
+    }
+
+    pub async fn compile_dna_file(
+        &self,
+        work_dir: impl AsRef<std::path::Path>,
+    ) -> DnaUtilResult<DnaFile> {
+        let mut work_dir_z = std::path::PathBuf::new();
+        work_dir_z.push(work_dir.as_ref());
+
+        let properties: SerializedBytes =
+            JsonValueDecodeHelper(self.properties.clone()).try_into()?;
+
+        let mut zomes = BTreeMap::new();
+        let mut wasm_list = Vec::new();
+
+        for (zome_name, zome) in self.zomes.iter() {
+            let mut zome_file_path = work_dir_z.clone();
+            zome_file_path.push(&zome.wasm_path);
+
+            let mut zome_content = Vec::new();
+            let mut zome_file = tokio::fs::File::open(zome_file_path).await?;
+            zome_file.read_to_end(&mut zome_content).await?;
+
+            let wasm: DnaWasm = zome_content.into();
+            let wasm_hash = holo_hash::WasmHash::with_data(&wasm.code()).await;
+            let wasm_hash: holo_hash_core::WasmHash = wasm_hash.into();
+            zomes.insert(zome_name.clone(), Zome { wasm_hash });
+            wasm_list.push(wasm);
+        }
+
+        let dna = DnaDef {
+            name: self.name.clone(),
+            uuid: self.uuid.clone(),
+            properties,
+            zomes,
+        };
+
+        Ok(DnaFile::new(dna, wasm_list).await?)
     }
 }
 
@@ -132,9 +219,8 @@ mod tests {
     use super::*;
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_extract() {
+    async fn test_extract_then_compile() {
         let tmp_dir = tempdir::TempDir::new("dna_util_test").unwrap();
-        println!("HANAH: {:?}", tmp_dir.path());
 
         let dna_file = holochain_types::test_utils::fake_dna_zomes(
             "bla",
@@ -153,6 +239,23 @@ mod tests {
                 .unwrap();
         }
 
-        extract(dna_filename).await.unwrap();
+        extract(&dna_filename).await.unwrap();
+
+        tokio::fs::remove_file(&dna_filename).await.unwrap();
+
+        compile(tmp_dir.path().join("test-dna.dna_work_dir"))
+            .await
+            .unwrap();
+
+        let mut dna_file_reader = tokio::fs::File::open(&dna_filename).await.unwrap();
+        let mut dna_file_content = Vec::new();
+        dna_file_reader
+            .read_to_end(&mut dna_file_content)
+            .await
+            .unwrap();
+
+        let dna_file2 = DnaFile::from_file_content(&dna_file_content).await.unwrap();
+
+        assert_eq!(dna_file, dna_file2);
     }
 }
