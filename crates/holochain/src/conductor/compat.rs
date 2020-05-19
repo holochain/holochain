4d59b75ea1d9314a0f73daed95c8d27dc03d331f
timotree3 <timorcb@gmail.com>
2020-05-19 16:24:47 -0400

    core: compat: Remove glob import

--- crates/holochain/src/conductor/compat.rs
+++ crates/holochain/src/conductor/compat.rs
@@ -38,8 +38,6 @@ pub async fn load_conductor_from_legacy_config(
     builder: ConductorBuilder,
     agent_pubkey: AgentPubKey,
 ) -> Result<ConductorHandle, CompatConfigError> {
-    use CompatConfigError::*;
-
     let config = config_from_legacy(&legacy);
 
     let conductor: ConductorHandle = builder.config(config).build().await?;
@@ -67,16 +65,18 @@ pub async fn load_conductor_from_legacy_config(
     let mut cell_ids = vec![];
     for i in legacy.instances.clone() {
         // NB: disregarding agent config for now, using a hard-coded pre-made one
-        let dna_config = legacy
-            .dna_by_id(&i.dna)
-            .ok_or_else(|| BrokenReference(format!("No DNA for id: {}", i.dna)))?;
+        let dna_config = legacy.dna_by_id(&i.dna).ok_or_else(|| {
+            CompatConfigError::BrokenReference(format!("No DNA for id: {}", i.dna))
+        })?;
         // make sure we have installed this DNA
         let dna_hash = dna_hashes
             .get(&dna_key(
                 &PathBuf::from(dna_config.file.clone()),
                 &dna_config.uuid,
             ))
-            .ok_or_else(|| BrokenReference(format!("No DNA for path: {}", dna_config.file)))?
+            .ok_or_else(|| {
+                CompatConfigError::BrokenReference(format!("No DNA for path: {}", dna_config.file))
+            })?
             .clone();
         let cell_id = CellId::new(dna_hash, agent_pubkey.clone());
         cell_ids.push((cell_id, None));
