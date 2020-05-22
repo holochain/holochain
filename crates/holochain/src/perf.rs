069f89fdb38cb46a063c764075d822924d8b45b8
Michael Dougherty <maackle.d@gmail.com>
2020-05-22 13:36:12 -0700

    Clearer debug statement

--- crates/holochain/src/perf.rs
+++ crates/holochain/src/perf.rs
@@ -56,12 +56,13 @@ macro_rules! end_hard_timeout {
             .unwrap()
                 - i128::try_from($t0.as_nanos()).unwrap();
 
-            dbg!(format!(
+            let timeout_check = format!(
                 "{}: {} <= {}?",
                 stringify!($t0),
                 hard_timeout_nanos,
                 $timeout
-            ));
+            );
+            dbg!(timeout_check);
 
             if hard_timeout_nanos > $timeout {
                 panic!(format!(
