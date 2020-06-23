c53bd08a8d706668f2c3ce36fb37ff5a5dce5a36
David Meister <thedavidmeister@gmail.com>
2020-06-23 06:40:58 +0400

    alias bytebuf for random bytes output

--- crates/zome_types/src/bytes.rs
+++ crates/zome_types/src/bytes.rs
@@ -1,19 +1,5 @@
 //! represent arbitrary bytes (not serialized)
 //! e.g. totally random crypto bytes from random_bytes
 
-#[derive(serde::Deserialize, serde::Serialize, Clone, Debug, PartialEq)]
-#[repr(transparent)]
-/// a transparent newtype to wrap Vec<u8> so we can name a vector of bytes "Bytes" in the compiler
-pub struct Bytes(#[serde(with = "serde_bytes")] Vec<u8>);
-
-impl From<Vec<u8>> for Bytes {
-    fn from(v: Vec<u8>) -> Self {
-        Self(v)
-    }
-}
-
-impl AsRef<[u8]> for Bytes {
-    fn as_ref(&self) -> &[u8] {
-        &self.0
-    }
-}
+/// simply alias whatever serde bytes is already doing for Vec<u8>
+pub type Bytes = serde_bytes::ByteBuf;
