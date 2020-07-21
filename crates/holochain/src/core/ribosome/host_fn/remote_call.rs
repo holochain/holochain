50b2dee1c0424edb6d5a71a0542732f1b00e32d4
David Meister <thedavidmeister@gmail.com>
2020-07-21 20:56:44 +0400

    wip on remote call in ribosome

--- crates/holochain/src/core/ribosome/host_fn/remote_call.rs
+++ crates/holochain/src/core/ribosome/host_fn/remote_call.rs
@@ -3,12 +3,30 @@ use crate::core::ribosome::wasm_ribosome::WasmRibosome;
 use crate::core::ribosome::CallContext;
 use holochain_zome_types::RemoteCallInput;
 use holochain_zome_types::RemoteCallOutput;
+use holochain_wasmer_host::prelude::SerializedBytes;
 use std::sync::Arc;
+
+const REMOTE_CALL_TIMEOUT: u32 = 10_000;
 
 pub fn remote_call(
     _ribosome: Arc<WasmRibosome>,
-    _call_context: Arc<CallContext>,
-    _input: RemoteCallInput,
+    call_context: Arc<CallContext>,
+    input: RemoteCallInput,
 ) -> RibosomeResult<RemoteCallOutput> {
-    unimplemented!();
+
+    let result: SerializedBytes = tokio_safe_block_on::tokio_safe_block_on(
+        async move {
+            let network = call_context.host_access().network();
+            network.remote_call(
+                input.to_agent(),
+                input.zome_name(),
+                input.fn_name(),
+                input.cap(),
+                input.request(),
+            ).await
+        },
+        std::time::Duration::from_millis(REMOTE_CALL_TIMEOUT),
+    )??;
+
+    Ok(RemoteCallOutput::new(result))
 }
