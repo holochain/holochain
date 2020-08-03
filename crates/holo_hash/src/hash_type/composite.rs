af5bf58f9fe7a09cce957b8e1cee4e3f3710ba9e
Michael Dougherty <maackle.d@gmail.com>
2020-08-03 11:10:53 -0700

    Disambiguate composite serialization with from/into intermediate form

--- crates/holo_hash/src/hash_type/composite.rs
+++ crates/holo_hash/src/hash_type/composite.rs
@@ -4,6 +4,7 @@ use super::*;
 #[derive(
     Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, serde::Deserialize, serde::Serialize,
 )]
+#[serde(from = "EntrySerial", into = "EntrySerial")]
 pub enum Entry {
     /// The hash of an Entry of EntryType::Agent
     Agent,
@@ -27,6 +28,7 @@ impl HashType for Entry {
 #[derive(
     Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, serde::Deserialize, serde::Serialize,
 )]
+#[serde(from = "AnyDhtSerial", into = "AnyDhtSerial")]
 pub enum AnyDht {
     /// The hash of an Entry
     Entry(Entry),
@@ -43,5 +45,57 @@ impl HashType for AnyDht {
     }
     fn hash_name(self) -> &'static str {
         "AnyDhtHash"
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+enum EntrySerial {
+    /// The hash of an Entry of EntryType::Agent
+    Agent(Agent),
+    /// The hash of any other EntryType
+    Content(Content),
+}
+
+impl From<Entry> for EntrySerial {
+    fn from(t: Entry) -> Self {
+        match t {
+            Entry::Agent => EntrySerial::Agent(Agent),
+            Entry::Content => EntrySerial::Content(Content),
+        }
+    }
+}
+
+impl From<EntrySerial> for Entry {
+    fn from(t: EntrySerial) -> Self {
+        match t {
+            EntrySerial::Agent(_) => Entry::Agent,
+            EntrySerial::Content(_) => Entry::Content,
+        }
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+enum AnyDhtSerial {
+    /// The hash of an Entry of EntryType::Agent
+    Header(Header),
+    /// The hash of any other EntryType
+    Entry(Entry),
+}
+
+impl From<AnyDht> for AnyDhtSerial {
+    fn from(t: AnyDht) -> Self {
+        match t {
+            AnyDht::Header => AnyDhtSerial::Header(Header),
+            AnyDht::Entry(e) => AnyDhtSerial::Entry(e),
+        }
+    }
+}
+
+impl From<AnyDhtSerial> for AnyDht {
+    fn from(t: AnyDhtSerial) -> Self {
+        match t {
+            AnyDhtSerial::Header(_) => AnyDht::Header,
+            AnyDhtSerial::Entry(e) => AnyDht::Entry(e),
+        }
     }
 }
