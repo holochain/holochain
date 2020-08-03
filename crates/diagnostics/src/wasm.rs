2267833676457c4ac916c357492b33c39bc94a81
Michael Dougherty <maackle.d@gmail.com>
2020-08-02 19:03:48 -0700

    Fix names

--- crates/diagnostics/src/wasm.rs
+++ crates/diagnostics/src/wasm.rs
@@ -7,8 +7,8 @@ pub async fn dump_wasm_state(env: EnvironmentWrite) -> anyhow::Result<()> {
     let r = g.reader()?;
 
     dump_kv(&r, "wasm", env.get_db(&WASM)?)?;
-    dump_kv(&r, "wasm", env.get_db(&DNA_DEF)?)?;
-    dump_kv(&r, "wasm", env.get_db(&ENTRY_DEF)?)?;
+    dump_kv(&r, "dna defs", env.get_db(&DNA_DEF)?)?;
+    dump_kv(&r, "entry defs", env.get_db(&ENTRY_DEF)?)?;
 
     Ok(())
 }
