e0a3f732d99af26111383995b2acc10df02e19df
Michael Dougherty <maackle.d@gmail.com>
2020-08-05 16:18:59 -0700

    Revert "Add wasmer_guest patch everywhere"
    This reverts commit 1efa2a7cfc35967c68ce64f7207142c2adaa4cd3.

--- crates/holo_hash/src/ser.rs
+++ crates/holo_hash/src/ser.rs
@@ -4,7 +4,7 @@ use holochain_serialized_bytes::{SerializedBytes, SerializedBytesError, UnsafeBy
 impl<T: HashType> std::convert::TryFrom<&HoloHash<T>> for SerializedBytes {
     type Error = SerializedBytesError;
     fn try_from(t: &HoloHash<T>) -> std::result::Result<SerializedBytes, SerializedBytesError> {
-        match holochain_serialized_bytes::encode(t) {
+        match rmp_serde::to_vec_named(t) {
             Ok(v) => Ok(SerializedBytes::from(UnsafeBytes::from(v))),
             Err(e) => Err(SerializedBytesError::ToBytes(e.to_string())),
         }
@@ -21,7 +21,7 @@ impl<T: HashType> std::convert::TryFrom<HoloHash<T>> for SerializedBytes {
 impl<T: HashType> std::convert::TryFrom<SerializedBytes> for HoloHash<T> {
     type Error = SerializedBytesError;
     fn try_from(sb: SerializedBytes) -> std::result::Result<HoloHash<T>, SerializedBytesError> {
-        match holochain_serialized_bytes::decode(sb.bytes()) {
+        match rmp_serde::from_read_ref(sb.bytes()) {
             Ok(v) => Ok(v),
             Err(e) => Err(SerializedBytesError::FromBytes(e.to_string())),
         }
