c3d49626c267393314407da4a6d58d7922725155
David Meister <thedavidmeister@gmail.com>
2020-08-10 13:17:37 +0400

    remove statics in benches

--- crates/holochain/benches/bench.rs
+++ crates/holochain/benches/bench.rs
@@ -1,70 +1,23 @@
 use ::fixt::prelude::*;
-// use criterion::BatchSize;
 use criterion::BenchmarkId;
 use criterion::Throughput;
 use criterion::{criterion_group, criterion_main, Criterion};
 use hdk3::prelude::*;
 use holo_hash::fixt::AgentPubKeyFixturator;
-use holochain::core::ribosome::wasm_ribosome::WasmRibosome;
 use holochain::core::ribosome::RibosomeT;
 use holochain::core::ribosome::ZomeCallInvocation;
-use holochain_types::cell::CellId;
 use holochain_types::fixt::CapSecretFixturator;
 use holochain_wasm_test_utils::TestWasm;
-// use holochain_wasmer_host::prelude::*;
 use holochain_zome_types::HostInput;
-use once_cell::sync::Lazy;
-
-// let's register a lazy static tokio runtime
-static TOKIO: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
-    tokio::runtime::Builder::new()
-        .threaded_scheduler()
-        .build()
-        .unwrap()
-});
-
-static RIBOSOME: Lazy<WasmRibosome> = Lazy::new(|| {
-    let mut ribosome_fixturator =
-        holochain::fixt::WasmRibosomeFixturator::new(holochain::fixt::curve::Zomes(vec![
-            TestWasm::Bench.into(),
-        ]));
-    ribosome_fixturator.next().unwrap()
-});
-
-static CELL_ID: Lazy<CellId> = Lazy::new(|| {
-    holochain_types::cell::CellIdFixturator::new(fixt::Unpredictable)
-        .next()
-        .unwrap()
-});
-
-static CAP: Lazy<CapSecret> = Lazy::new(|| CapSecretFixturator::new(Unpredictable).next().unwrap());
-
-static AGENT_KEY: Lazy<AgentPubKey> =
-    Lazy::new(|| AgentPubKeyFixturator::new(Unpredictable).next().unwrap());
-
-// static ZOME_CALL_INVOCATION: Lazy<ZomeCallInvocation> = Lazy::new(|| {
-//     let mut cell_id_fixturator = holochain_types::cell::CellIdFixturator::new(fixt::Unpredictable);
-//     let mut cap_secret_fixturator = CapSecretFixturator::new(Unpredictable);
-//     let mut agent_key_fixturator = AgentPubKeyFixturator::new(Unpredictable);
-//
-//     ZomeCallInvocation {
-//         cell_id: cell_id_fixturator.next().unwrap(),
-//         zome_name: TestWasm::Bench.into(),
-//         cap: cap_secret_fixturator.next().unwrap(),
-//         fn_name: "echo_bytes".into(),
-//         payload: HostInput::new(().try_into().unwrap()),
-//         provenance: agent_key_fixturator.next().unwrap(),
-//     }
-// });
 
 pub fn wasm_call_n(c: &mut Criterion) {
     let mut group = c.benchmark_group("wasm_call_n");
 
     for n in vec![
         // 1 byte
-        // 1,     // 1 kb
+        1,     // 1 kb
         1_000, // 1 mb
-              // 1_000_000,
+        1_000_000,
     ] {
         group.throughput(Throughput::Bytes(n as _));
 
@@ -78,28 +31,42 @@ pub fn wasm_call_n(c: &mut Criterion) {
             let mut host_access_fixturator =
                 holochain::fixt::ZomeCallHostAccessFixturator::new(fixt::Unpredictable);
 
-            TOKIO.enter(move || {
-                let ha = host_access_fixturator.next().unwrap();
-                b.iter(|| {
-                    let i = ZomeCallInvocation {
-                        cell_id: CELL_ID.clone(),
-                        zome_name: TestWasm::Bench.into(),
-                        cap: CAP.clone(),
-                        fn_name: "echo_bytes".into(),
-                        payload: HostInput::new(sb.clone()),
-                        provenance: AGENT_KEY.clone(),
-                    };
-                    RIBOSOME
-                        .clone()
-                        .maybe_call(
-                            ha.clone().into(),
-                            &i,
-                            &i.zome_name.clone(),
-                            i.fn_name.clone(),
-                        )
+            tokio::runtime::Builder::new()
+                .threaded_scheduler()
+                .build()
+                .unwrap()
+                .enter(move || {
+                    let ribosome = holochain::fixt::WasmRibosomeFixturator::new(
+                        holochain::fixt::curve::Zomes(vec![TestWasm::Bench.into()]),
+                    )
+                    .next()
+                    .unwrap();
+                    let cell_id = holochain_types::cell::CellIdFixturator::new(fixt::Unpredictable)
+                        .next()
                         .unwrap();
+                    let cap = CapSecretFixturator::new(Unpredictable).next().unwrap();
+                    let ha = host_access_fixturator.next().unwrap();
+                    let agent_key = AgentPubKeyFixturator::new(Unpredictable).next().unwrap();
+                    b.iter(|| {
+                        let i = ZomeCallInvocation {
+                            cell_id: cell_id.clone(),
+                            zome_name: TestWasm::Bench.into(),
+                            cap: cap.clone(),
+                            fn_name: "echo_bytes".into(),
+                            payload: HostInput::new(sb.clone()),
+                            provenance: agent_key.clone(),
+                        };
+                        ribosome
+                            .clone()
+                            .maybe_call(
+                                ha.clone().into(),
+                                &i,
+                                &i.zome_name.clone(),
+                                i.fn_name.clone(),
+                            )
+                            .unwrap();
+                    });
                 });
-            });
         });
     }
 
