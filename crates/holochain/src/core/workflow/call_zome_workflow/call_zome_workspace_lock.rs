0663db0f9a35367d1c2abf1b2672217e41b6fe16
Michael Dougherty <maackle.d@gmail.com>
2020-09-02 13:41:26 -0700

    Remove null fn

--- crates/holochain/src/core/workflow/call_zome_workflow/call_zome_workspace_lock.rs
+++ crates/holochain/src/core/workflow/call_zome_workflow/call_zome_workspace_lock.rs
@@ -10,17 +10,6 @@ impl CallZomeWorkspaceLock {
     pub fn new(workspace: CallZomeWorkspace) -> Self {
         Self(Arc::new(RwLock::new(workspace)))
     }
-
-    /// ABSOLUTE HACK.
-    /// At one point we had an unsafe mechanism for erasing the lifetime of an
-    /// LMDB reader, which involved using a raw pointer. This function was
-    /// implemented such that the pointer was set null. Then, a lot of test
-    /// logic was built on that foundation.
-    pub unsafe fn null() -> Self {
-        let workspace: CallZomeWorkspace =
-            std::mem::transmute([0 as u8; std::mem::size_of::<CallZomeWorkspace>()]);
-        Self::new(workspace)
-    }
 }
 
 impl From<CallZomeWorkspace> for CallZomeWorkspaceLock {
