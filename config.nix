3783bbeffe78f052a48f27d71bfb6ca674273451
Eric Harris-Braun <eric@harris-braun.com>
2020-09-14 17:46:51 -0400

    update urls

--- config.nix
+++ config.nix
@@ -93,7 +93,7 @@ See:
    owner = "holochain";
 
    # repository name on github that release are deployed to
-   repo = "holochain-2020";
+   repo = "holochain";
 
    # canonical local upstream name as per `git remote -v`
    upstream = "origin";
