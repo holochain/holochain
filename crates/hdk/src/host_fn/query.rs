a96c184777f199d0eb174f538d3ed90841e20eb2
David Meister <thedavidmeister@gmail.com>
2020-09-14 14:46:30 +0400

    query docs

--- crates/hdk/src/host_fn/query.rs
+++ crates/hdk/src/host_fn/query.rs
@@ -1,4 +1,10 @@
-// @todo document this
+/// Walks the source chain in reverse (latest to oldest) filtering by header and/or entry type
+///
+/// Given a header and entry type, returns a Vec<HeaderHash>
+///
+/// @todo document this better with examples
+/// @todo do we want to return elements rather than hashes?
+/// @todo implement cap grant/claim usage in terms of query
 #[macro_export]
 macro_rules! query {
     ( $base:expr ) => {{
