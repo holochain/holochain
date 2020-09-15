35f8d022c849f3056edcf17e6c72074550811c6d
David Meister <thedavidmeister@gmail.com>
2020-09-15 10:53:01 +0400

    Update crates/types/src/header/error.rs
    Co-authored-by: Michael Dougherty <michael.dougherty@holo.host>

--- crates/types/src/header/error.rs
+++ crates/types/src/header/error.rs
@@ -2,6 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum HeaderError {
-    #[error("Tried to create a NewEntryHeader with a type that isn't an Create or Update")]
+    #[error("Tried to create a NewEntryHeader with a type that isn't a Create or Update")]
     NotNewEntry,
 }
