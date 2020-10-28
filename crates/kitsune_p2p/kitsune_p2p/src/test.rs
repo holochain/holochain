05a819f3cbf0d24ada458efe85ae27efb975e551
neonphog <neonphog@gmail.com>
2020-10-28 16:17:54 -0600

    use test harness for kitsune tests

--- crates/kitsune_p2p/kitsune_p2p/src/test.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/test.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::{event::*, test_util::*, types::actor::KitsuneP2pSender, *};
-    use futures::future::FutureExt;
+    use crate::{test_util::*, types::actor::KitsuneP2pSender, *};
     use ghost_actor::{dependencies::tracing, GhostControlSender};
     use std::sync::Arc;
 
@@ -30,168 +29,81 @@ mod tests {
     }
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_request_workflow() {
-        let space1: Arc<KitsuneSpace> = TestVal::test_val();
-        let a1: Arc<KitsuneAgent> = TestVal::test_val();
+    async fn test_request_workflow() -> Result<(), KitsuneP2pError> {
+        let (harness, _evt) = spawn_test_harness_quic().await?;
+        let space = harness.add_space().await?;
+        let (a1, p2p) = harness.add_direct_agent("DIRECT".into()).await?;
+        // TODO when networking works, just add_*_agent again...
+        // but for now, we need the two agents to be on the same node:
         let a2: Arc<KitsuneAgent> = TestVal::test_val();
+        p2p.join(space.clone(), a2.clone()).await?;
 
-        let (p2p, mut evt) = spawn_kitsune_p2p(KitsuneP2pConfig::default())
-            .await
-            .unwrap();
-
-        let space1_clone = space1.clone();
-        let a2_clone = a2.clone();
-        let r_task = tokio::task::spawn(async move {
-            use tokio::stream::StreamExt;
-            while let Some(evt) = evt.next().await {
-                use KitsuneP2pEvent::*;
-                match evt {
-                    Call {
-                        respond,
-                        space,
-                        to_agent,
-                        payload,
-                        ..
-                    } => {
-                        if space != space1_clone {
-                            panic!("unexpected space");
-                        }
-                        if to_agent != a2_clone {
-                            panic!("unexpected agent");
-                        }
-                        if &*payload != b"hello" {
-                            panic!("unexpected request");
-                        }
-                        respond.r(Ok(async move { Ok(b"echo: hello".to_vec()) }
-                            .boxed()
-                            .into()));
-                    }
-                    _ => (),
-                }
-            }
-        });
-
-        p2p.join(space1.clone(), a1.clone()).await.unwrap();
-        p2p.join(space1.clone(), a2.clone()).await.unwrap();
-
-        let res = p2p
-            .rpc_single(space1, a2, a1, b"hello".to_vec())
-            .await
-            .unwrap();
+        let res = p2p.rpc_single(space, a2, a1, b"hello".to_vec()).await?;
         assert_eq!(b"echo: hello".to_vec(), res);
 
-        p2p.ghost_actor_shutdown().await.unwrap();
-        r_task.await.unwrap();
+        harness.ghost_actor_shutdown().await?;
+        Ok(())
     }
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_broadcast_workflow() {
-        let space1: Arc<KitsuneSpace> = TestVal::test_val();
-        let a1: Arc<KitsuneAgent> = TestVal::test_val();
+    async fn test_broadcast_workflow() -> Result<(), KitsuneP2pError> {
+        let (harness, evt) = spawn_test_harness_quic().await?;
+        let mut rcv = evt.receive();
+
+        let space = harness.add_space().await?;
+        let (a1, p2p) = harness.add_direct_agent("DIRECT".into()).await?;
+        // TODO when networking works, just add_*_agent again...
+        // but for now, we need the two agents to be on the same node:
         let a2: Arc<KitsuneAgent> = TestVal::test_val();
+        p2p.join(space.clone(), a2.clone()).await?;
         let a3: Arc<KitsuneAgent> = TestVal::test_val();
+        p2p.join(space.clone(), a3.clone()).await?;
 
-        let (p2p, mut evt) = spawn_kitsune_p2p(KitsuneP2pConfig::default())
-            .await
-            .unwrap();
+        p2p.notify_multi(actor::NotifyMulti {
+            space: space,
+            from_agent: a1,
+            // this is just a dummy value right now
+            basis: TestVal::test_val(),
+            remote_agent_count: Some(42),
+            timeout_ms: Some(40),
+            payload: b"test-broadcast".to_vec(),
+        })
+        .await?;
 
-        let recv_count = Arc::new(std::sync::atomic::AtomicU8::new(0));
+        harness.ghost_actor_shutdown().await?;
 
-        let space1_clone = space1.clone();
-        let recv_count_clone = recv_count.clone();
-        let r_task = tokio::task::spawn(async move {
-            use tokio::stream::StreamExt;
-            while let Some(evt) = evt.next().await {
-                use KitsuneP2pEvent::*;
-                match evt {
-                    Notify {
-                        respond,
-                        space,
-                        payload,
-                        ..
-                    } => {
-                        if space != space1_clone {
-                            panic!("unexpected space");
-                        }
-                        if &*payload != b"test-broadcast" {
-                            panic!("unexpected request");
-                        }
-                        respond.r(Ok(async move { Ok(()) }.boxed().into()));
-                        recv_count_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-                    }
-                    _ => (),
-                }
+        let mut recv_count = 0_usize;
+        while let Some(evt) = tokio::stream::StreamExt::next(&mut rcv).await {
+            if &**evt.nick != "DIRECT" {
+                continue;
             }
-        });
+            if let test_util::HarnessEventType::Notify { payload, .. } = &evt.ty {
+                assert_eq!(&**payload, "test-broadcast");
+                recv_count += 1;
+            }
+        }
 
-        p2p.join(space1.clone(), a1.clone()).await.unwrap();
-        p2p.join(space1.clone(), a2.clone()).await.unwrap();
-        p2p.join(space1.clone(), a3.clone()).await.unwrap();
+        assert_eq!(3, recv_count);
 
-        let res = p2p
-            .notify_multi(actor::NotifyMulti {
-                space: space1,
-                from_agent: a1,
-                // this is just a dummy value right now
-                basis: TestVal::test_val(),
-                remote_agent_count: Some(42),
-                timeout_ms: Some(40),
-                payload: b"test-broadcast".to_vec(),
-            })
-            .await
-            .unwrap();
-
-        assert_eq!(3, res);
-        assert_eq!(3, recv_count.load(std::sync::atomic::Ordering::SeqCst));
-
-        p2p.ghost_actor_shutdown().await.unwrap();
-        r_task.await.unwrap();
+        Ok(())
     }
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_multi_request_workflow() {
-        let space1: Arc<KitsuneSpace> = TestVal::test_val();
-        let a1: Arc<KitsuneAgent> = TestVal::test_val();
+    async fn test_multi_request_workflow() -> Result<(), KitsuneP2pError> {
+        let (harness, _evt) = spawn_test_harness_quic().await?;
+
+        let space = harness.add_space().await?;
+        let (a1, p2p) = harness.add_direct_agent("DIRECT".into()).await?;
+        // TODO when networking works, just add_*_agent again...
+        // but for now, we need the two agents to be on the same node:
         let a2: Arc<KitsuneAgent> = TestVal::test_val();
+        p2p.join(space.clone(), a2.clone()).await?;
         let a3: Arc<KitsuneAgent> = TestVal::test_val();
-
-        let (p2p, mut evt) = spawn_kitsune_p2p(KitsuneP2pConfig::default())
-            .await
-            .unwrap();
-
-        let space1_clone = space1.clone();
-        let r_task = tokio::task::spawn(async move {
-            use tokio::stream::StreamExt;
-            while let Some(evt) = evt.next().await {
-                use KitsuneP2pEvent::*;
-                match evt {
-                    Call {
-                        respond,
-                        space,
-                        payload,
-                        ..
-                    } => {
-                        if space != space1_clone {
-                            panic!("unexpected space");
-                        }
-                        let payload = String::from_utf8_lossy(&payload);
-                        assert_eq!(&payload, "test-multi-request");
-                        respond.r(Ok(async move { Ok(b"echo: test-multi-request".to_vec()) }
-                            .boxed()
-                            .into()));
-                    }
-                    _ => (),
-                }
-            }
-        });
-
-        p2p.join(space1.clone(), a1.clone()).await.unwrap();
-        p2p.join(space1.clone(), a2.clone()).await.unwrap();
-        p2p.join(space1.clone(), a3.clone()).await.unwrap();
+        p2p.join(space.clone(), a3.clone()).await?;
 
         let res = p2p
             .rpc_multi(actor::RpcMulti {
-                space: space1,
+                space: space,
                 from_agent: a1.clone(),
                 // this is just a dummy value right now
                 basis: TestVal::test_val(),
@@ -204,6 +116,8 @@ mod tests {
             .await
             .unwrap();
 
+        harness.ghost_actor_shutdown().await?;
+
         assert_eq!(1, res.len());
         for r in res {
             let data = String::from_utf8_lossy(&r.response);
@@ -211,50 +125,19 @@ mod tests {
             assert!(r.agent == a2 || r.agent == a3);
         }
 
-        p2p.ghost_actor_shutdown().await.unwrap();
-        r_task.await.unwrap();
+        Ok(())
     }
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_single_agent_multi_request_workflow() {
-        let space1: Arc<KitsuneSpace> = TestVal::test_val();
-        let a1: Arc<KitsuneAgent> = TestVal::test_val();
+    async fn test_single_agent_multi_request_workflow() -> Result<(), KitsuneP2pError> {
+        let (harness, _evt) = spawn_test_harness_quic().await?;
 
-        let (p2p, mut evt) = spawn_kitsune_p2p(KitsuneP2pConfig::default())
-            .await
-            .unwrap();
-
-        let space1_clone = space1.clone();
-        let r_task = tokio::task::spawn(async move {
-            use tokio::stream::StreamExt;
-            while let Some(evt) = evt.next().await {
-                use KitsuneP2pEvent::*;
-                match evt {
-                    Call {
-                        respond,
-                        space,
-                        payload,
-                        ..
-                    } => {
-                        if space != space1_clone {
-                            panic!("unexpected space");
-                        }
-                        let payload = String::from_utf8_lossy(&payload);
-                        assert_eq!(&payload, "test-multi-request");
-                        respond.r(Ok(async move { Ok(b"echo: test-multi-request".to_vec()) }
-                            .boxed()
-                            .into()));
-                    }
-                    _ => panic!("unexpected event"),
-                }
-            }
-        });
-
-        p2p.join(space1.clone(), a1.clone()).await.unwrap();
+        let space = harness.add_space().await?;
+        let (a1, p2p) = harness.add_direct_agent("DIRECT".into()).await?;
 
         let res = p2p
             .rpc_multi(actor::RpcMulti {
-                space: space1,
+                space: space,
                 from_agent: a1.clone(),
                 // this is just a dummy value right now
                 basis: TestVal::test_val(),
@@ -274,90 +157,44 @@ mod tests {
             assert!(r.agent == a1);
         }
 
-        p2p.ghost_actor_shutdown().await.unwrap();
-        r_task.await.unwrap();
+        harness.ghost_actor_shutdown().await.unwrap();
+        Ok(())
     }
 
     #[tokio::test(threaded_scheduler)]
-    async fn test_gossip_workflow() {
-        let space1: Arc<KitsuneSpace> = TestVal::test_val();
-        let a1: Arc<KitsuneAgent> = TestVal::test_val();
+    async fn test_gossip_workflow() -> Result<(), KitsuneP2pError> {
+        let (harness, _evt) = spawn_test_harness_quic().await?;
+
+        let space = harness.add_space().await?;
+        let (a1, p2p) = harness.add_direct_agent("DIRECT".into()).await?;
+        // TODO when networking works, just add_*_agent again...
+        // but for now, we need the two agents to be on the same node:
         let a2: Arc<KitsuneAgent> = TestVal::test_val();
+        p2p.join(space.clone(), a2.clone()).await?;
 
-        let oh1: Arc<KitsuneOpHash> = TestVal::test_val();
-        let oh2: Arc<KitsuneOpHash> = TestVal::test_val();
+        let op1 = harness
+            .inject_gossip_data(a1.clone(), "agent-1-data".to_string())
+            .await?;
 
-        let (p2p, mut evt) = spawn_kitsune_p2p(KitsuneP2pConfig::default())
-            .await
-            .unwrap();
+        // TODO - This doesn't work on fake nodes
+        //        we need to actually add_*_agent to do this
+        //let op2 = harness.inject_gossip_data(a2, "agent-2-data".to_string()).await?;
 
-        let result = Arc::new(std::sync::RwLock::new((false, false)));
+        tokio::time::delay_for(std::time::Duration::from_millis(200)).await;
 
-        //let space1_clone = space1.clone();
-        let a1_clone = a1.clone();
-        //let a2_clone = a2.clone();
-        let result_clone = result.clone();
-        let r_task = tokio::task::spawn(async move {
-            use tokio::stream::StreamExt;
-            while let Some(evt) = evt.next().await {
-                use KitsuneP2pEvent::*;
-                match evt {
-                    FetchOpHashesForConstraints { respond, input, .. } => {
-                        //println!("FETCH HASHES REQ: {:#?}", input);
-                        let oh = if input.agent == a1_clone {
-                            oh1.clone()
-                        } else {
-                            oh2.clone()
-                        };
-                        respond.r(Ok(async move { Ok(vec![oh]) }.boxed().into()));
-                    }
-                    FetchOpHashData { respond, input, .. } => {
-                        //println!("FETCH HASH DATA REQ: {:#?}", input);
-                        let mut out = Vec::new();
-                        for op_hash in input.op_hashes {
-                            out.push((op_hash, vec![]));
-                        }
-                        respond.r(Ok(async move { Ok(out) }.boxed().into()));
-                    }
-                    Gossip {
-                        respond,
-                        //agent,
-                        op_hash,
-                        ..
-                    } => {
-                        //println!("GOT GOSSIP: {:?} {:?}", agent, op_hash);
-                        if op_hash == oh1 {
-                            result_clone.write().unwrap().0 = true;
-                        } else {
-                            result_clone.write().unwrap().1 = true;
-                        }
-                        respond.r(Ok(async move { Ok(()) }.boxed().into()));
-                    }
-                    _ => (),
-                }
-            }
-        });
+        let res = harness.dump_local_gossip_data(a1).await?;
+        let (op_hash, data) = res.into_iter().next().unwrap();
+        assert_eq!(op1, op_hash);
+        assert_eq!("agent-1-data", &data);
 
-        p2p.join(space1.clone(), a1.clone()).await.unwrap();
-        p2p.join(space1.clone(), a2.clone()).await.unwrap();
+        // TODO - This doesn't work on fake nodes
+        //        we need to actually add_*_agent to do this
+        //let res = harness.dump_local_gossip_data(a2).await?;
+        //let (op_hash, data) = res.into_iter().next().unwrap();
+        //assert_eq!(op2, op_hash);
+        //assert_eq!("agent-2-data", &data);
 
-        let is_ok = move || {
-            let lock = result.read().unwrap();
-            lock.0 && lock.1
-        };
-
-        for _ in 0..10 {
-            if is_ok() {
-                break;
-            }
-            tokio::time::delay_for(std::time::Duration::from_millis(10)).await;
-        }
-
-        p2p.ghost_actor_shutdown().await.unwrap();
-        r_task.await.unwrap();
-
-        if !is_ok() {
-            panic!("failed to gossip both dht op hashes");
-        }
+        harness.ghost_actor_shutdown().await.unwrap();
+        Ok(())
     }
 }
