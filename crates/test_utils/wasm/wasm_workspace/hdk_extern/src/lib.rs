9371a11755022af984e7bc7b6b9d3d62a991e864
David Meister <thedavidmeister@gmail.com>
2020-11-16 22:32:50 +0400

    lint

--- crates/test_utils/wasm/wasm_workspace/hdk_extern/src/lib.rs
+++ crates/test_utils/wasm/wasm_workspace/hdk_extern/src/lib.rs
@@ -9,6 +9,7 @@ fn foo(_: ()) -> ExternResult<TestString> {
 #[hdk_extern]
 fn bar(_: ()) -> ExternResult<TestString> {
     // It should be possible to call our extern functions just like regular functions.
+    #[allow(clippy::blacklisted_name)]
     let foo: TestString = foo(())?;
     Ok(TestString::from(format!("{}{}", foo.0, "bar")))
 }
