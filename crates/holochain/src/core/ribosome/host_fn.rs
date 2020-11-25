53b067cc1e7b258e87f6983782298b2aabf0a671
Michael Dougherty <maackle.d@gmail.com>
2020-11-25 01:10:35 -0800

    WIP HostFnApi trait

--- crates/holochain/src/core/ribosome/host_fn.rs
+++ crates/holochain/src/core/ribosome/host_fn.rs
@@ -1,22 +1,35 @@
-pub mod agent_info;
-pub mod call;
-pub mod call_remote;
-pub mod capability_claims;
-pub mod capability_grants;
-pub mod capability_info;
-pub mod create;
-pub mod create_link;
-pub mod debug;
-pub mod decrypt;
-pub mod delete;
-pub mod delete_link;
-pub mod emit_signal;
-pub mod encrypt;
-pub mod entry_type_properties;
-pub mod get;
-pub mod get_agent_activity;
-pub mod get_details;
-pub mod get_link_details;
+use super::{error::RibosomeResult, CallContext, RibosomeT};
+use std::sync::Arc;
+
+pub struct HostFnApi<Ribosome: RibosomeT> {
+    ribosome: Arc<Ribosome>,
+    call_context: Arc<CallContext>,
+}
+
+macro_rules! host_fn_api_impls {
+    ( $( fn $f:ident ( $input:ty ) -> $output:ty; )* ) => {
+        $(
+            pub mod $f;
+        )*
+
+        // TODO: impl HostFnApiT for __
+
+        impl<Ribosome: RibosomeT> HostFnApi<Ribosome> {
+            $(
+                fn $f(&self, input: $input) -> RibosomeResult<$output> {
+                    $f::$f(
+                        self.ribosome.clone(),
+                        self.call_context.clone(),
+                        input.into()
+                    ).map(|r| r.into_inner())
+                }
+            )*
+        }
+    };
+}
+
+use holochain_zome_types as z;
+
 pub mod get_links;
 pub mod hash_entry;
 pub mod property;
@@ -30,3 +43,42 @@ pub mod unreachable;
 pub mod update;
 pub mod verify_signature;
 pub mod zome_info;
+
+host_fn_api_impls! {
+    fn agent_info(()) -> z::agent_info::AgentInfo;
+    fn call(z::call::Call) -> z::ZomeCallResponse;
+    fn call_remote(z::call_remote::CallRemote) -> z::ZomeCallResponse;
+    fn capability_claims(()) -> ();
+    fn capability_grants(()) -> ();
+    fn capability_info(()) -> ();
+    fn create((z::entry_def::EntryDefId, z::entry::Entry)) -> holo_hash::HeaderHash;
+    fn create_link((holo_hash::EntryHash, holo_hash::EntryHash, z::link::LinkTag)) -> holo_hash::HeaderHash;
+    fn debug(z::debug::DebugMsg) -> ();
+    fn decrypt(()) -> ();
+    fn delete(holo_hash::HeaderHash) -> holo_hash::HeaderHash;
+    fn delete_link(holo_hash::HeaderHash) -> holo_hash::HeaderHash;
+    fn emit_signal(z::signal::AppSignal) -> ();
+    fn encrypt(()) -> ();
+    fn entry_type_properties(()) -> ();
+    fn get((holo_hash::AnyDhtHash, z::entry::GetOptions)) -> Option<z::element::Element>;
+    fn get_agent_activity((
+        holo_hash::AgentPubKey,
+        z::query::ChainQueryFilter,
+        z::query::ActivityRequest,
+    )) -> z::query::AgentActivity;
+    fn get_details((holo_hash::AnyDhtHash, z::entry::GetOptions)) -> Option<z::metadata::Details>;
+    fn get_link_details((holo_hash::EntryHash, Option<z::link::LinkTag>)) -> z::link::LinkDetails;
+    // fn get_links((holo_hash::EntryHash, Option<link::LinkTag>)) -> ;
+    // fn hash_entry() -> ;
+    // fn property() -> ;
+    // fn query() -> ;
+    // fn random_bytes() -> ;
+    // fn schedule() -> ;
+    // fn show_env() -> ;
+    // fn sign() -> ;
+    // fn sys_time() -> ;
+    // fn unreachable() -> ;
+    // fn update() -> ;
+    // fn verify_signature() -> ;
+    // fn zome_info() -> ;
+}
--- crates/zome_types/src/zome_io.rs
+++ crates/zome_types/src/zome_io.rs
@@ -2,107 +2,124 @@ use crate::*;
 use element::ElementVec;
 use holochain_serialized_bytes::prelude::*;
 
+macro_rules! wasm_io_type {
+    ( $struct:ident($arg:ty) ) => {
+        #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes, PartialEq)]
+        pub struct $struct($arg);
+
+        impl $struct {
+            pub fn new(i: $arg) -> Self {
+                Self(i)
+            }
+
+            pub fn into_inner(self) -> $arg {
+                self.0
+            }
+
+            pub fn inner_ref(&self) -> &$arg {
+                &self.0
+            }
+        }
+    };
+}
+
 /// All wasm shared I/O types need to share the same basic behaviours to cross the host/guest
 /// boundary in a predictable way.
 macro_rules! wasm_io_types {
     ( $( fn $f:ident $in_struct:ident($in_arg:ty) -> $out_struct:ident($out_arg:ty); )* ) => {
         $(
-            #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes, PartialEq)]
-            pub struct $in_struct($in_arg);
+            wasm_io_type!($in_struct($in_arg));
+            wasm_io_type!($out_struct($out_arg));
 
-            #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes, PartialEq)]
-            pub struct $out_struct($out_arg);
-
-            impl $in_struct {
-                pub fn new(i: $in_arg) -> Self {
-                    Self(i)
-                }
-
-                pub fn into_inner(self) -> $in_arg {
-                    self.0
-                }
-
-                pub fn inner_ref(&self) -> &$in_arg {
-                    &self.0
+            // Typically we only need this for input types
+            impl From<$in_arg> for $in_struct {
+                fn from(arg: $in_arg) -> Self {
+                    Self::new(arg)
                 }
             }
-
-            impl $out_struct {
-                pub fn new(i: $out_arg) -> Self {
-                    Self(i)
-                }
-
-                pub fn into_inner(self) -> $out_arg {
-                    self.0
-                }
-
-                pub fn inner_ref(&self) -> &$out_arg {
-                    &self.0
-                }
-            }
-
         )*
+
+        pub trait HostFnApiT {
+            $(
+                fn $f(_: $in_arg) -> $out_arg;
+            )*
+        }
     }
 }
+
+// Every externed function that the zome developer exposes to holochain returns `ExternOutput`.
+// The zome developer can expose callbacks in a "sparse" way based on names and the functions
+// can take different input (e.g. validation vs. hooks like init, etc.).
+// All we can say is that some SerializedBytes are being received and returned.
+// In the case of ZomeExtern functions exposed to a client, the data input/output is entirely
+// arbitrary so we can't say anything at all. In this case the happ developer must BYO
+// deserialization context to match the client, either directly or via. the HDK.
+// Note though, that _unlike_ zome externs, the host _does_ know exactly the guest should be
+// returning for callbacks, it's just that the unpacking of the return happens in two steps:
+// - first the sparse callback is triggered with SB input/output
+// - then the guest inflates the expected input or the host the expected output based on the
+//   callback flavour
+wasm_io_type!(ExternInput(SerializedBytes));
+wasm_io_type!(ExternOutput(SerializedBytes));
 
 wasm_io_types!(
     // The zome and agent info are constants specific to the current zome and chain.
     // All the information is provided by core so there is no input value.
     // These are constant for the lifetime of a zome call.
-    fn _zomeinfo ZomeInfoInput(()) -> ZomeInfoOutput(zome_info::ZomeInfo);
-    fn _agentinfo AgentInfoInput(()) -> AgentInfoOutput(agent_info::AgentInfo);
-    fn _call CallInput(call::Call) -> CallOutput(ZomeCallResponse);
+    fn zome_info ZomeInfoInput(()) -> ZomeInfoOutput(zome_info::ZomeInfo);
+    fn agent_info AgentInfoInput(()) -> AgentInfoOutput(agent_info::AgentInfo);
+    fn call CallInput(call::Call) -> CallOutput(ZomeCallResponse);
     // @todo List all the local capability claims.
-    fn _capabilityclaims CapabilityClaimsInput(()) -> CapabilityClaimsOutput(());
+    fn capability_claims CapabilityClaimsInput(()) -> CapabilityClaimsOutput(());
     // @todo List all the local capability grants.
-    fn _capabilitygrants CapabilityGrantsInput(()) -> CapabilityGrantsOutput(());
+    fn capability_grants CapabilityGrantsInput(()) -> CapabilityGrantsOutput(());
     // @todo Get the capability for the current zome call.
-    fn _capabilityinfo CapabilityInfoInput(()) -> CapabilityInfoOutput(());
+    fn capability_info CapabilityInfoInput(()) -> CapabilityInfoOutput(());
     // The EntryDefId determines how a create is handled on the host side.
     // CapGrant and CapClaim are handled natively.
     // App entries are referenced by entry defs then SerializedBytes stuffed into an Entry::App.
-    fn _create CreateInput((entry_def::EntryDefId, entry::Entry)) -> CreateOutput(holo_hash::HeaderHash);
+    fn create CreateInput((entry_def::EntryDefId, entry::Entry)) -> CreateOutput(holo_hash::HeaderHash);
     // Header hash of the newly created element.
     // @todo
-    fn _decrypt DecryptInput(()) -> DecryptOutput(());
+    fn decrypt DecryptInput(()) -> DecryptOutput(());
     // @todo
-    fn _encrypt EncryptInput(()) -> EncryptOutput(());
+    fn encrypt EncryptInput(()) -> EncryptOutput(());
     // @todo
-    fn _showenv ShowEnvInput(()) -> ShowEnvOutput(());
+    fn showenv ShowEnvInput(()) -> ShowEnvOutput(());
     // @todo
-    fn _property PropertyInput(()) -> PropertyOutput(());
+    fn property PropertyInput(()) -> PropertyOutput(());
     // Query the source chain for data.
-    fn _query QueryInput(query::ChainQueryFilter) -> QueryOutput(ElementVec);
+    fn query QueryInput(query::ChainQueryFilter) -> QueryOutput(ElementVec);
     // the length of random bytes to create
-    fn _randombytes RandomBytesInput(u32) -> RandomBytesOutput(bytes::Bytes);
+    fn random_bytes RandomBytesInput(u32) -> RandomBytesOutput(bytes::Bytes);
     // Header hash of the CreateLink element.
-    fn _deletelink DeleteLinkInput(holo_hash::HeaderHash) -> DeleteLinkOutput(holo_hash::HeaderHash);
+    fn delete_link DeleteLinkInput(holo_hash::HeaderHash) -> DeleteLinkOutput(holo_hash::HeaderHash);
     // Header hash of the DeleteLink element.
-    fn _callremote CallRemoteInput(call_remote::CallRemote) -> CallRemoteOutput(ZomeCallResponse);
+    fn call_remote CallRemoteInput(call_remote::CallRemote) -> CallRemoteOutput(ZomeCallResponse);
     // @todo
-    fn _send SendInput(()) -> SendOutput(());
+    fn send SendInput(()) -> SendOutput(());
     // Attempt to have the keystore sign some data
     // The pubkey in the input needs to be found in the keystore for this to work
-    fn _sign SignInput(crate::signature::Sign) -> SignOutput(crate::signature::Signature);
-    fn _verifysignature VerifySignatureInput(crate::signature::VerifySignature) -> VerifySignatureOutput(bool);
+    fn sign SignInput(crate::signature::Sign) -> SignOutput(crate::signature::Signature);
+    fn verify_signature VerifySignatureInput(crate::signature::VerifySignature) -> VerifySignatureOutput(bool);
     // @todo
-    fn _schedule ScheduleInput(core::time::Duration) -> ScheduleOutput(());
+    fn schedule ScheduleInput(core::time::Duration) -> ScheduleOutput(());
     // Same as CreateInput but also takes the HeaderHash of the updated element.
-    fn _update UpdateInput((entry_def::EntryDefId, entry::Entry, holo_hash::HeaderHash)) -> UpdateOutput(holo_hash::HeaderHash);
+    fn update UpdateInput((entry_def::EntryDefId, entry::Entry, holo_hash::HeaderHash)) -> UpdateOutput(holo_hash::HeaderHash);
     // Header hash of the newly committed element.
     // Emit a Signal::App to subscribers on the interface
-    fn _emitsignal EmitSignalInput(signal::AppSignal) -> EmitSignalOutput(());
+    fn emit_signal EmitSignalInput(signal::AppSignal) -> EmitSignalOutput(());
     // @todo
-    fn _delete DeleteInput(holo_hash::HeaderHash) -> DeleteOutput(holo_hash::HeaderHash);
+    fn delete DeleteInput(holo_hash::HeaderHash) -> DeleteOutput(holo_hash::HeaderHash);
     // Create a link between two entries.
-    fn _createlink CreateLinkInput((holo_hash::EntryHash, holo_hash::EntryHash, link::LinkTag)) -> CreateLinkOutput(holo_hash::HeaderHash);
+    fn create_link CreateLinkInput((holo_hash::EntryHash, holo_hash::EntryHash, link::LinkTag)) -> CreateLinkOutput(holo_hash::HeaderHash);
     // Get links by entry hash from the cascade.
-    fn _getlinks GetLinksInput((holo_hash::EntryHash, Option<link::LinkTag>)) -> GetLinksOutput(link::Links);
-    fn _getlinkdetails GetLinkDetailsInput((holo_hash::EntryHash, Option<link::LinkTag>)) -> GetLinkDetailsOutput(link::LinkDetails);
+    fn get_links GetLinksInput((holo_hash::EntryHash, Option<link::LinkTag>)) -> GetLinksOutput(link::Links);
+    fn get_link_details GetLinkDetailsInput((holo_hash::EntryHash, Option<link::LinkTag>)) -> GetLinkDetailsOutput(link::LinkDetails);
     // Attempt to get a live entry from the cascade.
-    fn _get GetInput((holo_hash::AnyDhtHash, entry::GetOptions)) -> GetOutput(Option<element::Element>);
-    fn _getdetails GetDetailsInput((holo_hash::AnyDhtHash, entry::GetOptions)) -> GetDetailsOutput(Option<metadata::Details>);
-    fn _getagentactivity GetAgentActivityInput(
+    fn get GetInput((holo_hash::AnyDhtHash, entry::GetOptions)) -> GetOutput(Option<element::Element>);
+    fn get_details GetDetailsInput((holo_hash::AnyDhtHash, entry::GetOptions)) -> GetDetailsOutput(Option<metadata::Details>);
+    fn get_agent_activity GetAgentActivityInput(
         (
             holo_hash::AgentPubKey,
             query::ChainQueryFilter,
@@ -110,30 +127,17 @@ wasm_io_types!(
         )
     ) -> GetAgentActivityOutput(query::AgentActivity);
     // @todo
-    fn _entrytypeproperties EntryTypePropertiesInput(()) -> EntryTypePropertiesOutput(());
+    fn entry_type_properties EntryTypePropertiesInput(()) -> EntryTypePropertiesOutput(());
     // Hash an entry on the host.
-    fn _hashentry HashEntryInput(entry::Entry) -> HashEntryOutput(holo_hash::EntryHash);
+    fn hash_entry HashEntryInput(entry::Entry) -> HashEntryOutput(holo_hash::EntryHash);
     // Current system time, in the opinion of the host, as a `Duration`.
-    fn _systime SysTimeInput(()) -> SysTimeOutput(core::time::Duration);
+    fn sys_time SysTimeInput(()) -> SysTimeOutput(core::time::Duration);
     // The debug host import takes a DebugMsg to output wherever the host wants to display it.
     // DebugMsg includes line numbers. so the wasm tells the host about it's own code structure.
-    fn _debug DebugInput(debug::DebugMsg) -> DebugOutput(());
+    fn debug DebugInput(debug::DebugMsg) -> DebugOutput(());
     // There's nothing to go in or out of a noop.
     // Used to "defuse" host functions when side effects are not allowed.
-    fn _unreachable UnreachableInput(()) -> UnreachableOutput(());
-    // Every externed function that the zome developer exposes to holochain returns `ExternOutput`.
-    // The zome developer can expose callbacks in a "sparse" way based on names and the functions
-    // can take different input (e.g. validation vs. hooks like init, etc.).
-    // All we can say is that some SerializedBytes are being received and returned.
-    // In the case of ZomeExtern functions exposed to a client, the data input/output is entirely
-    // arbitrary so we can't say anything at all. In this case the happ developer must BYO
-    // deserialization context to match the client, either directly or via. the HDK.
-    // Note though, that _unlike_ zome externs, the host _does_ know exactly the guest should be
-    // returning for callbacks, it's just that the unpacking of the return happens in two steps:
-    // - first the sparse callback is triggered with SB input/output
-    // - then the guest inflates the expected input or the host the expected output based on the
-    //   callback flavour
-    fn _extern ExternInput(SerializedBytes) -> ExternOutput(SerializedBytes);
+    fn unreachable UnreachableInput(()) -> UnreachableOutput(());
 );
 
 /// Response to a zome call.
