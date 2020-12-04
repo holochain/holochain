e59a9af5605386d2ad8a76526aba05da25445318
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-12-04 14:31:36 +0100

    Fixed format

--- crates/hdk/src/host_fn/call_remote.rs
+++ crates/hdk/src/host_fn/call_remote.rs
@@ -10,19 +10,19 @@ use crate::prelude::*;
 /// - cap_secret: Optional cap claim secret to allow access to the remote call.
 /// - payload: The payload to send to the remote function; receiver needs to deserialize cleanly.
 ///
-/// Response is HdkResult which can either return HdkResult::Ok with the deserialized result 
+/// Response is HdkResult which can either return HdkResult::Ok with the deserialized result
 /// of the function call, HdkError::ZomeCallNetworkError if there was a network error,
 /// or HdkError::UnauthorizedZomeCall if the provided cap grant is invalid. The Unauthorized case
 /// should always be handled gracefully because gap grants can be revoked at any time and the claim
 /// holder has no way of knowing until they provide a secret for a call.
 ///
-/// An Ok response already includes the deserialized type. Note that this type should derive from 
+/// An Ok response already includes the deserialized type. Note that this type should derive from
 /// `SerializedBytes`, and it should be the same one as the return type from the remote function call.
 ///
 ///
 /// ```ignore
 /// #[derive(SerializedBytes, Serialize, Deserialize)]
-/// struct Foo(String); 
+/// struct Foo(String);
 ///
 /// ...
 /// let foo: Foo = call_remote(bob, "foo_zome", "do_it", secret, serialized_payload)?;
