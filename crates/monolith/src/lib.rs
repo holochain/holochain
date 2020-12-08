ad0249e301377db37dd9a21b4c61d9de81da434c
Michael Dougherty <maackle.d@gmail.com>
2020-12-07 23:28:39 -0500

    Move all holochain crates into monolith crate

---
+++ crates/monolith/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod hdk;
+pub mod holochain;
+pub mod holochain_p2p;
+pub mod holochain_state;
+pub mod holochain_types;
+pub mod holochain_zome_types;
+pub mod keystore;
+pub mod wasm_test_utils;
