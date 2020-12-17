966872852e7b1f72c1ac4bcb07695e7100aa5840
Michael Dougherty <maackle.d@gmail.com>
2020-12-17 12:29:27 -0500

    Oops

--- crates/holochain/tests/multi_conductor.rs
+++ crates/holochain/tests/multi_conductor.rs
@@ -1,11 +1,19 @@
+#![cfg(feature = "test_utils")]
+
+use futures::future;
 use hdk3::prelude::*;
-use holochain::test_utils::cool::MaybeElement;
 use holochain::{
-    conductor::p2p_store::exchange_peer_info,
-    test_utils::cool::{CoolConductorBatch, CoolDnaFile},
+    conductor::{config::ConductorConfig, p2p_store::exchange_peer_info, Conductor},
+    destructure_test_cells,
 };
-use holochain_types::dna::zome::inline_zome::InlineZome;
+use holochain::{
+    destructure_test_cell_vec,
+    test_utils::test_conductor::{MaybeElement, TestAgents, TestConductorHandle},
+};
+use holochain_lmdb::test_utils::test_environments;
+use holochain_types::dna::{zome::inline_zome::InlineZome, DnaFile};
 use holochain_zome_types::element::ElementEntry;
+use kitsune_p2p::KitsuneP2pConfig;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, SerializedBytes, derive_more::From)]
 #[serde(transparent)]
@@ -28,23 +36,60 @@ fn simple_crud_zome() -> InlineZome {
         })
 }
 
+// TODO [ B-03669 ]: make much less verbose
 #[tokio::test(threaded_scheduler)]
 #[cfg(feature = "test_utils")]
 async fn multi_conductor() -> anyhow::Result<()> {
     const NUM_CONDUCTORS: usize = 3;
 
-    let conductors = CoolConductorBatch::from_standard_config(NUM_CONDUCTORS).await;
+    let conductors = future::join_all(
+        std::iter::repeat_with(|| async move {
+            let envs = test_environments();
+            let mut network = KitsuneP2pConfig::default();
+            network.transport_pool = vec![kitsune_p2p::TransportConfig::Quic {
+                bind_to: None,
+                override_host: None,
+                override_port: None,
+            }];
+            let conductor: TestConductorHandle = Conductor::builder()
+                .config(ConductorConfig {
+                    network: Some(network),
+                    ..Default::default()
+                })
+                .test(&envs)
+                .await
+                .unwrap()
+                .into();
+            (conductor, envs)
+        })
+        .take(NUM_CONDUCTORS),
+    )
+    .await;
 
-    let (dna_file, _) = CoolDnaFile::unique_from_inline_zome("zome1", simple_crud_zome())
+    let (dna_file, _) = DnaFile::unique_from_inline_zome("zome1", simple_crud_zome())
         .await
         .unwrap();
 
-    let apps = conductors.setup_app("app", &[dna_file]).await;
+    let data = future::join_all(conductors.iter().map(|(conductor, envs)| {
+        let dna_file = dna_file.clone();
+        async move {
+            let data = conductor
+                .setup_app_for_agents_with_no_membrane_proof(
+                    "app",
+                    &[TestAgents::one(envs.keystore()).await],
+                    &[dna_file.clone()],
+                )
+                .await;
+            (data, envs)
+        }
+    }))
+    .await;
 
-    let p2p_envs = conductors.iter().map(|c| c.envs().p2p()).collect();
+    let p2p_envs = data.iter().map(|(_, envs)| envs.p2p()).collect();
     exchange_peer_info(p2p_envs);
 
-    let ((alice,), (bobbo,), (_carol,)) = apps.into_tuples();
+    let (((alice,),), ((bobbo,),), ((_carol,),)) =
+        destructure_test_cell_vec!(data.into_iter().map(|(data, _)| data).collect());
 
     // Call the "create" zome fn on Alice's app
     let hash: HeaderHash = alice.call("zome1", "create", ()).await;
