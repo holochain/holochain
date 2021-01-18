1c298c2c0b8d2ce843d2cb813c492a2eea383a5d
thedavidmeister <thedavidmeister@gmail.com>
2021-01-18 07:59:42 +0400

    fix merge

--- crates/holochain/src/test_utils/cool/cool_zome.rs
+++ crates/holochain/src/test_utils/cool/cool_zome.rs
@@ -9,44 +9,6 @@ pub struct CoolZome {
 }
 
 impl CoolZome {
-    // /// Call a function as if from another Agent.
-    // /// The provenance and optional CapSecret must be provided.
-    // pub async fn call_from<I, O, F>(
-    //     &self,
-    //     provenance: AgentPubKey,
-    //     cap: Option<CapSecret>,
-    //     fn_name: F,
-    //     payload: I,
-    // ) -> O
-    // where
-    //     FunctionName: From<F>,
-    //     I: serde::Serialize,
-    //     O: serde::de::DeserializeOwned + std::fmt::Debug,
-    // {
-    //     self.handle
-    //         .call_zome_ok_flat(
-    //             &self.cell_id,
-    //             self.zome_name.clone(),
-    //             fn_name,
-    //             cap,
-    //             Some(provenance),
-    //             payload,
-    //         )
-    //         .await
-    // }
-
-    /// Call a function on this zome.
-    /// No CapGrant is provided; the authorship capability will be granted.
-    pub async fn call<I, O, F>(&self, fn_name: F, payload: I) -> O
-    where
-        FunctionName: From<F>,
-        I: serde::Serialize,
-        O: serde::de::DeserializeOwned + std::fmt::Debug,
-    {
-        self.call_from(self.cell_id.agent_pubkey().clone(), None, fn_name, payload)
-            .await
-    }
-
     /// Accessor
     pub fn cell_id(&self) -> &CellId {
         &self.cell_id
