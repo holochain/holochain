305a60510ef141af57c54e49b2b4e8d544a04bcc
thedavidmeister <thedavidmeister@gmail.com>
2021-01-28 14:41:51 +0400

    lint

--- crates/test_utils/wasm/wasm_workspace/debug/src/lib.rs
+++ crates/test_utils/wasm/wasm_workspace/debug/src/lib.rs
@@ -2,7 +2,7 @@ use hdk3::prelude::*;
 
 #[hdk_extern]
 fn debug(_: ()) -> ExternResult<()> {
-    // hdk3::debug!("debug line numbers {}", "work");
-    // hdk3::debug!("debug again");
+    hdk3::debug!("debug line numbers {}", "work");
+    hdk3::debug!("debug again");
     Ok(())
 }
