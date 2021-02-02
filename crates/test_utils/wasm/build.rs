84414b6f80d3b357368318a4898704708bb6b5cf
Eric Harris-Braun <eric@harris-braun.com>
2021-02-02 14:47:12 -0500

    Update and remove test_utils special build

--- crates/test_utils/wasm/build.rs
+++ crates/test_utils/wasm/build.rs
@@ -1,8 +1,9 @@
-use std::io::Write;
-use std::process::Stdio;
+//use std::io::Write;
+//use std::process::Stdio;
 
 fn main() {
-    let should_build = std::env::var_os("CARGO_FEATURE_BUILD").is_some();
+    return;
+/*    let should_build = std::env::var_os("CARGO_FEATURE_BUILD").is_some();
     let only_check = std::env::var_os("CARGO_FEATURE_ONLY_CHECK").is_some();
 
     if !(should_build || only_check) {
@@ -83,7 +84,7 @@ fn main() {
             output.status.success(),
             std::io::stderr().write_all(&output.stderr)
         );
-    }
+    }*/
 }
 
 /// Return the list of local path dependencies specified in the Cargo.toml
