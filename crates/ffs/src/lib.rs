a013d320caf59afa196a6e2a87372fff0aebe810
Michael Dougherty <maackle.d@gmail.com>
2021-02-19 13:00:44 -0800

    Use dunce::canonicalize

--- crates/ffs/src/lib.rs
+++ crates/ffs/src/lib.rs
@@ -17,43 +17,52 @@
 mod io_error;
 
 pub use crate::io_error::{IoError, IoResult};
+use std::path::PathBuf;
+
+fn mapper<P: AsRef<std::path::Path>>(path: P) -> impl FnOnce(std::io::Error) -> IoError {
+    move |e| IoError::new(e, path.as_ref().to_owned())
+}
 
 macro_rules! impl_ffs {
     ( $( fn $name:ident (path $(, $arg:ident : $arg_ty:ty)* ) -> $output:ty ; )* ) => {
 
         $(
             pub async fn $name<P: Clone + AsRef<std::path::Path>>(path: P $(, $arg : $arg_ty)*) -> IoResult<$output> {
-                let err_path = path.as_ref().to_owned();
-                let mapper = move |e| IoError::new(e, err_path);
 
                 #[cfg(feature = "tokio")]
-                return tokio::fs::$name(path $(, $arg)*).await.map_err(mapper);
+                return tokio::fs::$name(path.clone() $(, $arg)*).await.map_err(mapper(path));
 
                 #[cfg(not(feature = "tokio"))]
-                return std::fs::$name(path $(, $arg)*).map_err(mapper);
+                return std::fs::$name(path.clone() $(, $arg)*).map_err(mapper(path));
             }
         )*
+
+        /// Wrap dunce::canonicalize, since std::fs::canonicalize has problems for Windows
+        /// (see https://docs.rs/dunce/1.0.1/dunce/index.html)
+        pub async fn canonicalize<P: Clone + AsRef<std::path::Path>>(path: P) -> IoResult<PathBuf> {
+            dunce::canonicalize(path.clone()).map_err(mapper(path))
+        }
 
         pub mod sync {
 
             use super::*;
             $(
                 pub fn $name<P: Clone + AsRef<std::path::Path>>(path: P $(, $arg : $arg_ty)*) -> IoResult<$output> {
-                    let err_path = path.clone();
-                    let mapper = move |e| IoError::new(e, err_path.as_ref().to_owned());
-                    return std::fs::$name(path $(, $arg)*).map_err(mapper);
+                    return std::fs::$name(path.clone() $(, $arg)*).map_err(mapper(path));
                 }
             )*
+
+            pub fn canonicalize<P: Clone + AsRef<std::path::Path>>(path: P) -> IoResult<PathBuf> {
+                dunce::canonicalize(path.clone()).map_err(mapper(path))
+            }
+
         }
     };
-
-
 }
 
 impl_ffs! {
     fn create_dir(path) -> ();
     fn create_dir_all(path) -> ();
-    fn canonicalize(path) -> std::path::PathBuf;
     fn read(path) -> Vec<u8>;
     fn read_to_string(path) -> String;
     fn write(path, data: &[u8]) -> ();
