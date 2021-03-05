173641429a8152a923cd096da6844af971b0b874
thedavidmeister <thedavidmeister@gmail.com>
2021-03-05 15:18:36 +0400

    fmt

--- crates/holochain/src/core/workflow/genesis_workflow.rs
+++ crates/holochain/src/core/workflow/genesis_workflow.rs
@@ -159,11 +159,7 @@ pub mod tests {
 
             assert_matches!(
                 headers.as_slice(),
-                [
-                    Header::Create(_),
-                    Header::AgentValidationPkg(_),
-                    Header::Dna(_)
-                ]
+                [Header::Create(_), Header::AgentValidationPkg(_), Header::Dna(_)]
             );
         }
 
