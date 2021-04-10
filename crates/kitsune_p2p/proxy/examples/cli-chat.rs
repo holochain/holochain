08d0ed2c45660456fa8d79ee2ba7c06c55bcf8da
neonphog <neonphog@gmail.com>
2021-04-09 21:11:55 -0600

    chat almost working

--- crates/kitsune_p2p/proxy/examples/cli-chat.rs
+++ crates/kitsune_p2p/proxy/examples/cli-chat.rs
@@ -1,57 +1,265 @@
 use crossterm::ExecutableCommand;
+use futures::stream::{BoxStream, StreamExt};
+use kitsune_p2p_proxy::tx2::*;
+use kitsune_p2p_proxy::ProxyUrl;
+use kitsune_p2p_transport_quic::tx2::*;
+use kitsune_p2p_types::tx2::tx2_api::*;
+use kitsune_p2p_types::tx2::tx2_pool_promote::*;
+use kitsune_p2p_types::*;
 use std::io::Write;
 
-#[tokio::main(flavor = "multi_thread")]
-async fn main() {
-    crossterm::terminal::enable_raw_mode().unwrap();
+kitsune_p2p_types::write_codec_enum! {
+    codec Wire {
+        Null(0x00) {},
+        Msg(0x01) {
+            usr.0: String,
+            msg.1: String,
+        },
+    }
+}
 
-    tokio::task::spawn_blocking(|| {
-        let mut stdout = std::io::stdout();
+enum Evt {
+    Output(String),
+    Key(char),
+    Backspace,
+    Enter,
+    End,
+}
+
+fn spawn_evt() -> (tokio::sync::mpsc::Sender<String>, BoxStream<'static, Evt>) {
+    use crossterm::event::{poll, read, Event, KeyCode::*, KeyModifiers};
+    let (s_o, mut r_o) = tokio::sync::mpsc::channel(32);
+    let (s, mut r) = tokio::sync::mpsc::channel(32);
+    tokio::task::spawn_blocking(move || {
         loop {
-            if crossterm::event::poll(std::time::Duration::from_millis(500))? {
-                use crossterm::event::Event::*;
-                match crossterm::event::read()? {
-                    Key(event) => {
-                        if event
-                            .modifiers
-                            .contains(crossterm::event::KeyModifiers::CONTROL)
+            if poll(std::time::Duration::from_millis(500)).unwrap() {
+                match read().unwrap() {
+                    Event::Key(event) => {
+                        // first catch ctrl-escapes
+                        if event.modifiers.contains(KeyModifiers::CONTROL)
+                            && (event.code == Char('c') || event.code == Char('d'))
                         {
-                            if event.code == crossterm::event::KeyCode::Char('c')
-                                || event.code == crossterm::event::KeyCode::Char('c')
-                                || event.code == crossterm::event::KeyCode::Esc
-                            {
-                                stdout.execute(crossterm::cursor::MoveTo(0, 0)).unwrap();
-                                stdout
-                                    .execute(crossterm::terminal::Clear(
-                                        crossterm::terminal::ClearType::All,
-                                    ))
-                                    .unwrap();
-                                crossterm::terminal::disable_raw_mode().unwrap();
-                                println!("DONE");
-                                std::process::exit(0);
+                            if s.blocking_send(Evt::End).is_err() {
+                                return;
                             }
+                            continue;
                         }
-                        println!("{:?}", event);
+
+                        let evt = match event.code {
+                            Char(c) => Evt::Key(c),
+                            Backspace => Evt::Backspace,
+                            Enter => Evt::Enter,
+                            _ => Evt::End,
+                        };
+
+                        if s.blocking_send(evt).is_err() {
+                            return;
+                        }
                     }
-                    Mouse(event) => println!("{:?}", event),
-                    Resize(w, h) => println!("resize {}x{}", w, h),
+                    _ => (),
                 }
             }
         }
-        #[allow(unreachable_code)]
-        <crossterm::Result<()>>::Ok(())
+    });
+    let r_o = futures::stream::poll_fn(move |cx| -> std::task::Poll<Option<String>> {
+        r_o.poll_recv(cx)
+    });
+    let r_o = r_o.map(|s| Evt::Output(s)).boxed();
+    let r = futures::stream::poll_fn(move |cx| -> std::task::Poll<Option<Evt>> { r.poll_recv(cx) })
+        .boxed();
+    (s_o, futures::stream::select_all(vec![r, r_o]).boxed())
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
+    let parse_args = || {
+        let args = std::env::args().collect::<Vec<_>>();
+        if args.len() != 3 {
+            return Err(());
+        }
+        let username = args.get(1).unwrap().to_string();
+        let proxy_url = ProxyUrl::from(args.get(2).unwrap());
+        Ok((username, proxy_url))
+    };
+
+    let (username, proxy_url) = match parse_args() {
+        Err(_) => {
+            eprintln!("usage: cli-chat username proxy-url");
+            std::process::exit(127);
+        }
+        Ok(r) => r,
+    };
+
+    println!("[cli-chat: start]");
+    println!("[cli-chat: username: {}]", username);
+    println!("[cli-chat: proxy_url: {}]", proxy_url);
+
+    let conf = QuicConfig::default();
+    let f = tx2_quic_adapter(conf).await.unwrap();
+    let f = tx2_pool_promote(f, Default::default());
+    let f = tx2_proxy(f, Default::default());
+    let f = tx2_api::<Wire>(f, Default::default());
+
+    let ep = f
+        .bind(
+            "kitsune-quic://0.0.0.0:0",
+            KitsuneTimeout::from_millis(5000),
+        )
+        .await
+        .unwrap();
+
+    let ep_hnd = ep.handle().clone();
+
+    let raw_addr = ep_hnd.local_addr().unwrap();
+    println!("[cli-chat: local raw addr: {}]", raw_addr);
+
+    let _ = ep_hnd
+        .get_connection(
+            proxy_url.as_str().to_string(),
+            KitsuneTimeout::from_millis(1000 * 30),
+        )
+        .await
+        .unwrap();
+
+    let local_digest = ProxyUrl::from(raw_addr.as_str());
+    let local_digest = local_digest.digest();
+    let local_addr = ProxyUrl::new(proxy_url.as_base().as_str(), local_digest).unwrap();
+    println!("[cli-chat: local proxy addr: {}]", local_addr);
+    println!("\n--- local proxy addr - share this one ---");
+    println!("{}", local_addr);
+    println!("--- ----- ----- ---- - ----- ---- --- ---\n");
+    println!("type '/help' for a list of commands.");
+
+    crossterm::terminal::enable_raw_mode().unwrap();
+
+    let (send_output, mut evt) = spawn_evt();
+
+    let s_o_2 = send_output.clone();
+    tokio::task::spawn(async move {
+        let s_o_2 = &s_o_2;
+        ep.for_each_concurrent(32, move |evt| async move {
+            use Tx2EpEvent::*;
+            match evt {
+                IncomingRequest(Tx2EpIncomingRequest { data, respond, .. }) => {
+                    match data {
+                        Wire::Msg(Msg { usr, msg }) => {
+                            let _ = s_o_2.send(format!("{} says: {}", usr, msg)).await;
+                        }
+                        _ => (),
+                    }
+                    let _ = respond
+                        .respond(Wire::null(), KitsuneTimeout::from_millis(1000 * 5))
+                        .await;
+                }
+                _ => (),
+            }
+        })
+        .await;
     });
 
     let mut stdout = std::io::stdout();
-    stdout
-        .execute(crossterm::terminal::Clear(
-            crossterm::terminal::ClearType::All,
-        ))
-        .unwrap();
-    stdout.execute(crossterm::cursor::MoveTo(5, 5)).unwrap();
-    write!(stdout, "{}", "[---------------------------]").unwrap();
-    stdout.flush().unwrap();
-    stdout.execute(crossterm::cursor::MoveTo(8, 8)).unwrap();
+    let mut line = String::new();
+    let mut con_list = Vec::new();
 
-    tokio::time::sleep(std::time::Duration::from_secs(20)).await;
+    macro_rules! rline {
+        ($($t:tt)*) => {{
+            use crossterm::cursor::MoveToColumn;
+            use crossterm::terminal::{Clear, ClearType::*};
+            stdout.execute(Clear(CurrentLine)).unwrap();
+            stdout.execute(MoveToColumn(0)).unwrap();
+            if line.len() > 60 {
+                write!(
+                    stdout,
+                    "{}>... {}",
+                    username,
+                    line.chars().skip(line.len() - 60).collect::<String>()
+                )
+                .unwrap();
+            } else {
+                write!(stdout, "{}> {}", username, line).unwrap();
+            }
+            stdout.flush().unwrap();
+        }};
+    }
+
+    macro_rules! pline {
+        ($($t:tt)*) => {{
+            use crossterm::terminal::{Clear, ClearType::*};
+            use crossterm::cursor::{MoveToColumn};
+            stdout.execute(Clear(CurrentLine)).unwrap();
+            stdout.execute(MoveToColumn(0)).unwrap();
+            write!(stdout, $($t)*).unwrap();
+            write!(stdout, "\n").unwrap();
+        }};
+    }
+
+    rline!();
+    while let Some(evt) = evt.next().await {
+        match evt {
+            Evt::Output(o) => pline!("{}", o),
+            Evt::Key(evt) => line.push(evt),
+            Evt::Backspace => {
+                line.pop();
+            }
+            Evt::Enter => {
+                if line.as_bytes().len() >= 2 && line.as_bytes()[0] as char == '/' {
+                    pline!("{}", line);
+                    match line.as_bytes()[1] as char {
+                        'h' => {
+                            pline!("");
+                            pline!("[cli-chat commands]:");
+                            pline!("/help - this help text");
+                            pline!("/connect peer_url - connect to a remote peer");
+                            pline!("/quit | /exit - exit cli-chat");
+                            pline!("");
+                        }
+                        'e' | 'q' => break,
+                        'c' => {
+                            let (_, url) =
+                                line.split_at(line.find(char::is_whitespace).unwrap() + 1);
+                            let url = ProxyUrl::from(url);
+
+                            let con = ep_hnd
+                                .get_connection(
+                                    url.to_string(),
+                                    KitsuneTimeout::from_millis(1000 * 30),
+                                )
+                                .await
+                                .unwrap();
+
+                            con_list.push(con);
+
+                            pline!("connected to {}", url);
+                        }
+                        _ => pline!("unknown command: {}", line),
+                    }
+                    line.clear();
+                } else {
+                    let mut new_vec = Vec::new();
+                    for c in con_list.drain(..) {
+                        if c.request(
+                            &Wire::msg(username.clone(), line.clone()),
+                            KitsuneTimeout::from_millis(1000 * 5),
+                        )
+                        .await
+                        .is_ok()
+                        {
+                            new_vec.push(c);
+                        }
+                    }
+                    con_list = new_vec;
+                    send_output
+                        .send(format!("{} says: {}", username, line))
+                        .await
+                        .unwrap();
+                    line.clear();
+                }
+            }
+            Evt::End => break,
+        }
+        rline!();
+    }
+    crossterm::terminal::disable_raw_mode().unwrap();
+    println!("\n[cli-chat: done]");
+    std::process::exit(0);
 }
