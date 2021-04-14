e90d34282901e4b3ed57f4069080cd29b4c92667
neonphog <neonphog@gmail.com>
2021-04-14 17:08:01 -0600

    remove println

--- crates/kitsune_p2p/kitsune_p2p/src/spawn/actor.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/spawn/actor.rs
@@ -354,7 +354,6 @@ impl KitsuneP2pActor {
                         }
                         evt => {
                             tracing::error!("UNHANDLED EVENT: {:?}", evt);
-                            println!("UNHANDLED EVENT: {:?}", evt);
                         }
                     }
                 }
