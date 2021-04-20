077ca129275e5dca89d538d57f4e13d2e9751c4a
peeech <pj@imagine-nyc.com>
2021-04-20 04:40:03 +0200

    Update crates/holochain/src/bin/holochain/main.rs
    Co-authored-by: David Braden <neonphog@gmail.com>

--- crates/holochain/src/bin/holochain/main.rs
+++ crates/holochain/src/bin/holochain/main.rs
@@ -6,6 +6,7 @@ use holochain::conductor::Conductor;
 use holochain::conductor::ConductorHandle;
 use holochain_conductor_api::conductor::ConductorConfigError;
 use observability::Output;
+#[cfg(unix)]
 use sd_notify::{notify, NotifyState};
 use std::path::PathBuf;
 use structopt::StructOpt;
