e26c7481c08f0d823ff3bf9e3ac32d8c3f944686
Michael Dougherty <maackle.d@gmail.com>
2021-08-09 13:34:17 -0700

    Renames and comments

--- crates/kitsune_p2p/kitsune_p2p/src/gossip/sharded_gossip/tests/sharded_scenario.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/gossip/sharded_gossip/tests/sharded_scenario.rs
@@ -1,82 +1,29 @@
-use kitsune_p2p_types::{
-    dht_arc::{ArcInterval, DhtLocation},
-    Tx2Cert,
-};
+use kitsune_p2p_types::dht_arc::{ArcInterval, DhtLocation};
 use maplit::hashset;
-use observability::tracing::Instrument;
-
-use crate::*;
-use std::{
-    collections::{BTreeMap, BTreeSet, HashSet},
-    sync::Arc,
-};
-
-/// Concise representation of data held by various agents in a sharded scenario,
-/// without having to refer to explicit op hashes or locations.
-///
-/// This type is intended to be used to easily define arbitrary sharded network scenarios,
-/// to test various cases of local sync and gossip. It's expected that we'll eventually have a
-/// small library of such scenarios, defined in terms of this type.
-///
-/// See [`mock_agent_persistence`] for usage detail.
-pub struct OwnershipData {
-    /// Total number of op hashes to be generated
-    pub total_ops: usize,
-    /// Declares arcs and ownership in terms of indices into a vec of generated op hashes.
-    pub agents: Vec<OwnershipDataAgent>,
-}
-
-impl OwnershipData {
-    /// Construct `OwnershipData` from a more compact "untagged" format using
-    /// tuples instead of structs. This is intended to be the canonical constructor.
-    pub fn from_compact(total_ops: usize, v: Vec<OwnershipDataAgentCompact>) -> Self {
-        Self {
-            total_ops,
-            agents: v
-                .into_iter()
-                .map(|(agent, arc_indices, hash_indices)| OwnershipDataAgent {
-                    agent,
-                    arc_indices,
-                    hash_indices,
-                })
-                .collect(),
-        }
-    }
-}
-
-/// Declares arcs and ownership in terms of indices into a vec of generated op hashes.
-pub struct OwnershipDataAgent {
-    /// The agent in question
-    pub agent: Arc<KitsuneAgent>,
-    /// The start and end indices of the arc for this agent
-    pub arc_indices: (usize, usize),
-    /// The indices of ops to consider as owned
-    pub hash_indices: Vec<usize>,
-}
-
-/// Same as [`OwnershipDataAgent`], but using a tuple instead of a struct.
-/// It's just more compact.
-pub type OwnershipDataAgentCompact = (Arc<KitsuneAgent>, (usize, usize), Vec<usize>);
+use std::collections::{BTreeSet, HashSet};
 
 /// Abstract representation of the instantaneous state of a sharded network
-/// with multiple conductors.
+/// with multiple conductors. Useful for setting up multi-node test scenarios,
+/// and for deriving the expected final state after reaching consistency.
 ///
-/// NB: The reified form of this representation will break a rule:
+/// NB: The "reification" of this representation will break a rule:
 /// Agent and Op hashes will have manually defined locations which do NOT
-/// match the actual hash content.
+/// match the actual hash content, since it is computationally infeasible to
+/// search for hashes which match a given location.
+/// (By "reification" I mean the actual concrete Nodes which are set up.)
 ///
 /// Thus, rather than dealing with hash types directly, this representation
 /// deals only with locations.
 ///
-/// Also, note that for simplicity's sake, it's impossible to specify two ops
+/// Thus, note that for simplicity's sake, it's impossible to specify two ops
 /// at the same location, which is possible in reality, but rare, and should
 /// have no bearing on test results. (TODO: test this case separately)
-pub struct ShardedScenario<const N: usize> {
+pub struct ScenarioDef<const N: usize> {
     /// The locations of all ops held by all agents
     ops: BTreeSet<DhtLocation>,
 
     /// The "nodes" (in Holochain, "conductors") participating in this scenario
-    nodes: [ShardedScenarioNode; N],
+    nodes: [ScenarioDefNode; N],
 
     /// Specifies which other nodes are present in the peer store of each node.
     /// The array index matches the array defined in `ShardedScenario::nodes`.
@@ -87,10 +34,10 @@ pub struct ShardedScenario<const N: usize> {
     latency_matrix: LatencyMatrix<N>,
 }
 
-impl<const N: usize> ShardedScenario<N> {
+impl<const N: usize> ScenarioDef<N> {
     pub fn new<O: Copy + Into<DhtLocation>>(
         ops: &[O],
-        nodes: [ShardedScenarioNode; N],
+        nodes: [ScenarioDefNode; N],
         peer_matrix: PeerMatrix<N>,
         latency_matrix: LatencyMatrix<N>,
     ) -> Self {
@@ -105,24 +52,24 @@ impl<const N: usize> ShardedScenario<N> {
 
 /// An individual node in a sharded scenario.
 /// The only data needed is the list of local agents.
-pub struct ShardedScenarioNode(HashSet<ShardedScenarioAgent>);
+pub struct ScenarioDefNode(HashSet<ScenarioDefAgent>);
 
-impl ShardedScenarioNode {
-    pub fn new(agents: HashSet<ShardedScenarioAgent>) -> Self {
+impl ScenarioDefNode {
+    pub fn new(agents: HashSet<ScenarioDefAgent>) -> Self {
         Self(agents)
     }
 }
 
 /// An individual agent on a node in a sharded scenario
 #[derive(PartialEq, Eq, Hash)]
-pub struct ShardedScenarioAgent {
+pub struct ScenarioDefAgent {
     /// The storage arc for this agent
     arc: ArcInterval,
     /// The ops stored by this agent
     ops_held: BTreeSet<DhtLocation>,
 }
 
-impl ShardedScenarioAgent {
+impl ScenarioDefAgent {
     pub fn new<O: Copy + Into<DhtLocation>>(arc: ArcInterval, ops_held: &[O]) -> Self {
         Self {
             arc,
@@ -148,8 +95,8 @@ pub enum PeerMatrix<const N: usize> {
 /// Just construct a scenario to illustrate/experience how it's done
 #[test]
 fn constructors() {
-    use ShardedScenarioAgent as Agent;
-    use ShardedScenarioNode as Node;
+    use ScenarioDefAgent as Agent;
+    use ScenarioDefNode as Node;
     let ops: Vec<DhtLocation> = (-10..11).map(i32::into).collect();
     let nodes = [
         Node::new(hashset![
@@ -173,7 +120,7 @@ fn constructors() {
             ),
         ]),
     ];
-    let _scenario = ShardedScenario::new(
+    let _scenario = ScenarioDef::new(
         ops.as_slice(),
         nodes,
         PeerMatrix::Sparse([hashset![1], hashset![]]),
