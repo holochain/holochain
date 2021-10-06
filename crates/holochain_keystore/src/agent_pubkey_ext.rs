056af8d9639da6e1081d0bb672dc8f5d7161f614
neonphog <neonphog@gmail.com>
2021-10-06 13:25:25 -0600

    provide the serialized byte translations

--- crates/holochain_keystore/src/agent_pubkey_ext.rs
+++ crates/holochain_keystore/src/agent_pubkey_ext.rs
@@ -15,21 +15,47 @@ pub trait AgentPubKeyExt {
     where
         Self: Sized;
 
-    /// sign some arbitrary data
-    fn sign<S>(&self, keystore: &KeystoreSender, data: S) -> KeystoreApiFuture<Signature>
-    where
-        S: Serialize + std::fmt::Debug;
-
     /// sign some arbitrary raw bytes
     fn sign_raw(&self, keystore: &KeystoreSender, data: &[u8]) -> KeystoreApiFuture<Signature>;
+
+    /// verify a signature for given raw bytes with this agent public_key is valid
+    fn verify_signature_raw(&self, signature: &Signature, data: &[u8]) -> KeystoreApiFuture<bool>;
+
+    // -- provided -- //
+
+    /// sign some arbitrary data
+    fn sign<S>(&self, keystore: &KeystoreSender, input: S) -> KeystoreApiFuture<Signature>
+    where
+        S: Serialize + std::fmt::Debug,
+    {
+        use ghost_actor::dependencies::futures::future::FutureExt;
+
+        let data = match holochain_serialized_bytes::encode(&input) {
+            Err(e) => {
+                return async move { Err(e.into()) }.boxed().into();
+            }
+            Ok(data) => data,
+        };
+
+        self.sign_raw(keystore, &data)
+    }
 
     /// verify a signature for given data with this agent public_key is valid
     fn verify_signature<D>(&self, signature: &Signature, data: D) -> KeystoreApiFuture<bool>
     where
-        D: TryInto<SerializedBytes, Error = SerializedBytesError>;
+        D: TryInto<SerializedBytes, Error = SerializedBytesError>,
+    {
+        use ghost_actor::dependencies::futures::future::FutureExt;
 
-    /// verify a signature for given raw bytes with this agent public_key is valid
-    fn verify_signature_raw(&self, signature: &Signature, data: &[u8]) -> KeystoreApiFuture<bool>;
+        let data = match data.try_into() {
+            Err(e) => {
+                return async move { Err(e.into()) }.boxed().into();
+            }
+            Ok(data) => data,
+        };
+
+        self.verify_signature_raw(signature, data.bytes())
+    }
 }
 
 impl AgentPubKeyExt for holo_hash::AgentPubKey {
@@ -43,55 +69,18 @@ impl AgentPubKeyExt for holo_hash::AgentPubKey {
         MustBoxFuture::new(async move { f.await })
     }
 
-    fn sign<S>(&self, keystore: &KeystoreSender, input: S) -> KeystoreApiFuture<Signature>
-    where
-        S: Serialize + std::fmt::Debug,
-    {
+    fn sign_raw(&self, keystore: &KeystoreSender, data: &[u8]) -> KeystoreApiFuture<Signature> {
         use ghost_actor::dependencies::futures::future::FutureExt;
         let keystore = keystore.clone();
-        let maybe_data: Result<Vec<u8>, SerializedBytesError> =
-            holochain_serialized_bytes::encode(&input);
-        let key = self.clone();
+        let input = Sign::new_raw(self.clone(), data.to_vec());
         async move {
-            let data = maybe_data?;
-            let f = keystore.sign(Sign {
-                key,
-                data: serde_bytes::ByteBuf::from(data),
-            });
+            let f = keystore.sign(input);
             match tokio::time::timeout(std::time::Duration::from_secs(30), f).await {
                 Ok(r) => r,
                 Err(_) => Err(KeystoreError::Other(
                     "Keystore timeout while signing agent key".to_string(),
                 )),
             }
-        }
-        .boxed()
-        .into()
-    }
-
-    fn sign_raw(&self, keystore: &KeystoreSender, data: &[u8]) -> KeystoreApiFuture<Signature> {
-        use ghost_actor::dependencies::futures::future::FutureExt;
-        let keystore = keystore.clone();
-        let input = Sign::new_raw(self.clone(), data.to_vec());
-        async move { keystore.sign(input).await }.boxed().into()
-    }
-
-    fn verify_signature<D>(&self, signature: &Signature, data: D) -> KeystoreApiFuture<bool>
-    where
-        D: TryInto<SerializedBytes, Error = SerializedBytesError>,
-    {
-        use ghost_actor::dependencies::futures::future::FutureExt;
-
-        let pub_key: legacy_lair_api::internal::sign_ed25519::SignEd25519PubKey =
-            self.get_raw_32().to_vec().into();
-        let sig: legacy_lair_api::internal::sign_ed25519::SignEd25519Signature =
-            signature.0.to_vec().into();
-
-        let data: Result<SerializedBytes, SerializedBytesError> = data.try_into();
-
-        async move {
-            let data = Arc::new(data?.bytes().to_vec());
-            Ok(pub_key.verify(data, sig).await?)
         }
         .boxed()
         .into()
