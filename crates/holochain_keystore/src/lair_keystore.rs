9f01cab5a1cc1cad00052e5ead7f2ca39ae74956
neonphog <neonphog@gmail.com>
2021-10-12 14:04:16 -0600

    checkpoint normalizing test keystores

--- crates/holochain_keystore/src/lair_keystore.rs
+++ crates/holochain_keystore/src/lair_keystore.rs
@@ -1,7 +1,9 @@
 //! Keystore backed by legacy_lair_client.
 
 use crate::*;
+use kitsune_p2p_types::dependencies::{new_lair_api, url2};
 use legacy_lair_api::*;
+use new_lair_api::LairResult;
 
 /// Spawn a new keystore backed by legacy_lair_client.
 pub async fn spawn_lair_keystore(
@@ -17,4 +19,20 @@ pub async fn spawn_lair_keystore(
     let api = legacy_lair_client::assert_running_lair_and_connect(config, passphrase).await?;
 
     Ok(MetaLairClient::Legacy(api))
+}
+
+/// Spawn a new keystore backed by new_lair_api.
+pub async fn spawn_new_lair_keystore(
+    connection_url: url2::Url2,
+    passphrase: sodoken::BufRead,
+) -> LairResult<MetaLairClient> {
+    use new_lair_api::ipc_keystore::*;
+    let opts = IpcKeystoreClientOptions {
+        connection_url: connection_url.into(),
+        passphrase,
+        exact_client_server_version_match: true,
+        danger_unlock_without_server_validate: false,
+    };
+    let client = ipc_keystore_connect_options(opts).await?;
+    Ok(MetaLairClient::NewLair(client))
 }
