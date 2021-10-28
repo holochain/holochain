56530d88ada9d48ddc3d96fd997ea93758a76257
Michael Dougherty <maackle.d@gmail.com>
2021-10-28 12:05:10 -0700

    Mostly empty test mod

---
+++ crates/kitsune_p2p/dht_arc/src/test/arc_stability.rs
@@ -0,0 +1,16 @@
+use crate::DhtArc;
+
+#[test]
+fn a_test() {
+    // todo
+}
+
+struct EpochStats {
+    net_change: f64,
+    gross_change: f64,
+    min_redundancy: u32,
+}
+
+fn one_epoch(mut arcs: Vec<DhtArc>) -> EpochStats {
+    todo!()
+}
--- crates/kitsune_p2p/dht_arc/src/test/mod.rs
+++ crates/kitsune_p2p/dht_arc/src/test/mod.rs
@@ -1,5 +1,6 @@
 mod ascii;
 pub use ascii::ascii;
 
+mod arc_stability;
 mod intersection;
 mod union;
