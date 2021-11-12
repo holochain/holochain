0a848bed5d186f07ee01edafd9e85e6b55ba6cd9
freesig <tomrgowan@gmail.com>
2021-11-12 11:27:01 +1100

    ignore flaky test

--- crates/holochain/src/local_network_tests.rs
+++ crates/holochain/src/local_network_tests.rs
@@ -38,6 +38,7 @@ const TIMEOUT_ERROR: &'static str = "inner function \'call_create_entry_remotely
 // @todo figure out why we can't have more than 4
 #[test_case(4)]
 // #[test_case(10)]
+#[ignore = "Ignoring this test as it's flaky on CI and either needs to be redesigned or the problem debugged"]
 fn conductors_call_remote(num_conductors: usize) {
     let f = async move {
         observability::test_run().ok();
