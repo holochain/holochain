5c6a972a73c1f1898a635c0aa7c2b0aba28429ca
Michael Dougherty <maackle.d@gmail.com>
2022-02-07 15:53:45 -0800

    Make proper error out of GossipError

--- crates/kitsune_p2p/dht/src/error.rs
+++ crates/kitsune_p2p/dht/src/error.rs
@@ -1,8 +1,12 @@
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum GossipError {
+    #[error("The fundamental parameters of Op region spacetime are mismatched between nodes.")]
     TopologyMismatch,
+    #[error("System times between nodes are too far apart to be able to gossip.")]
     TimesOutOfSync,
+    #[error("Attempting to gossip with too large a discrepancy in chunk size")]
     ArqPowerDiffTooLarge,
+    #[error("Attempting to gossip with a mismatch in the common arc set")]
     ArqSetMismatchForDiff,
 }
 
