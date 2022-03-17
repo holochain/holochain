52a273e08bc00ad46db56baf1801e6eb022d3e6e
thedavidmeister <thedavidmeister@gmail.com>
2022-03-17 17:38:22 +0400

    docs and hash_external

--- crates/hdk/src/hash.rs
+++ crates/hdk/src/hash.rs
@@ -68,6 +68,33 @@ pub fn hash_header(input: Header) -> ExternResult<HeaderHash> {
     }
 }
 
+/// Hash arbitrary bytes into an `ExternalHash`.
+///
+/// External hashes have a DHT location and hash prefix like all other native
+/// holochain hashes but are NOT found/fetchable on the DHT.
+///
+/// External hashing makes no assumptions about the data that was digested to
+/// create the hash so arbitrary bytes can be passed in. Note however that the
+/// hashing is synchronous (like everything in wasm) so avoid very large data
+/// inputs.
+///
+/// It is valid to EITHER use an existing 32 byte hash/data as literal bytes
+/// for an external hash (literal+prefix, no data loss) OR digest arbitrary
+/// data into an external hash (support all data, opaque result).
+///
+/// This function facilitates the latter, if you have compatible 32 byte data
+/// consider whether `ExternalHash::from_raw_32` might better serve your use
+/// case, or retyping a native holochain hash in place:
+/// `entry_hash.retype(hash_type::External)`
+/// The benefit of NOT using `hash_external` is that if the hash exists in some
+/// external location such as an entry in some other DHT, it can be referenced.
+pub fn hash_external(input: Vec<u8>) -> ExternResult<ExternalHash> {
+    match HDK.with(|h| h.borrow().hash(HashInput::External(input)))? {
+        HashOutput::External(external_hash) => Ok(external_hash),
+        _ => unreachable!(),
+    }
+}
+
 /// Hash arbitrary bytes using BLAKE2b.
 /// This is the same algorithm used by holochain for typed hashes.
 /// Notably the output hash length is configurable.
@@ -94,6 +121,10 @@ pub fn hash_sha512(input: Vec<u8>) -> ExternResult<Vec<u8>> {
     }
 }
 
+/// Hash arbitrary bytes using keccak256.
+/// This is the same algorithm used by ethereum and other EVM compatible blockchains.
+/// It is essentially the same as sha3 256 but with a minor difference in configuration
+/// that is enough to generate different hash outputs.
 pub fn hash_keccak256(input: Vec<u8>) -> ExternResult<Vec<u8>> {
     match HDK.with(|h| h.borrow().hash(HashInput::Keccak256(input)))? {
         HashOutput::Keccak256(hash) => Ok(hash.as_ref().to_vec()),
@@ -101,6 +132,8 @@ pub fn hash_keccak256(input: Vec<u8>) -> ExternResult<Vec<u8>> {
     }
 }
 
+/// Hash arbitrary bytes using SHA3 256.
+/// This is the official NIST standard for 256 bit SHA3 hashes.
 pub fn hash_sha3(input: Vec<u8>) -> ExternResult<Vec<u8>> {
     match HDK.with(|h| h.borrow().hash(HashInput::Sha3256(input)))? {
         HashOutput::Sha3256(hash) => Ok(hash.as_ref().to_vec()),
