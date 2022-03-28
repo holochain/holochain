210524a8a0453e92f483ccc89022261d5b271409
Michael Dougherty <maackle.d@gmail.com>
2022-03-28 17:32:24 -0400

    fix fix

--- crates/kitsune_p2p/dht_arc/tests/common/mod.rs
+++ crates/kitsune_p2p/dht_arc/tests/common/mod.rs
@@ -113,7 +113,7 @@ pub fn run_one_epoch(
         let arc = peers.get_mut(i).unwrap();
         let view = strat.view(*arc, p.as_slice());
         let before = arc.length() as f64;
-        arc.update_length(&view.into());
+        arc.update_length(view);
         let after = arc.length() as f64;
         let delta = after - before;
         // dbg!(&before, &after, &delta);
