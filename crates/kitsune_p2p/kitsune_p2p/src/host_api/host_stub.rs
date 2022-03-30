d51fbdacf1d65ec7f7c3f3f503e2d1d5f0068a00
Michael Dougherty <maackle.d@gmail.com>
2022-03-30 14:45:20 -0400

    Naive attempt at fixing flaky HostStub use cases

--- crates/kitsune_p2p/kitsune_p2p/src/host_api/host_stub.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/host_api/host_stub.rs
@@ -1,10 +1,24 @@
-use crate::KitsuneHostPanicky;
+use std::sync::Arc;
+
+use kitsune_p2p_types::{bin_types::KitsuneSpace, box_fut, dht_arc::DhtArcSet};
+
+use crate::{KitsuneHostPanicky, KitsuneHostResult};
 
 /// Dummy host impl for plumbing
 pub struct HostStub;
 
 impl KitsuneHostPanicky for HostStub {
     const NAME: &'static str = "HostStub";
+
+    // XXX: shouldn't be implemented for a stub, but we have tests failing due to this
+    // being called, so let's just implement it in a naive way.
+    fn peer_extrapolated_coverage(
+        &self,
+        _space: Arc<KitsuneSpace>,
+        _dht_arc_set: DhtArcSet,
+    ) -> KitsuneHostResult<Vec<f64>> {
+        box_fut(Ok(vec![1.0]))
+    }
 }
 
 impl HostStub {
