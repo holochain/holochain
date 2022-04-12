081bdcb71bbbe38b973a5d4fe18661ba98c07ed2
Jost Schulte <jost.schulte@protonmail.com>
2022-04-12 09:05:08 -0600

    remove outdated links to ElementBuf

--- crates/holochain_state/src/lib.rs
+++ crates/holochain_state/src/lib.rs
@@ -2,11 +2,11 @@
 //! # Persisted State building blocks
 //!
 //! This crate provides a few types for working with databases. The types build upon those found in holochain_sqlite::buffer.
-//! - [`ElementBuf`](crate::prelude::ElementBuf): the union of two CasBuffers, one for Entries, one for Headers
+//! - ElementBuf the union of two CasBuffers, one for Entries, one for Headers
 //! - ChainSequenceBuf: database representing the chain sequence DB, which provides a special method for accessing the chain head
-//! - SourceChainBuf: the union of a [`ElementBuf`](crate::prelude::ElementBuf) and a [ChainSequenceBuf], which fully represents a source chain
+//! - SourceChainBuf: the union of a ElementBuf and a ChainSequenceBuf, which fully represents a source chain
 //! - MetadataBuf: (*unimplemented*) Uses a KvvBuffer to represent EAV-like relationships between CAS entries
-//! - Cascade: (*unimplemented*) Unifies two [`ElementBuf`](crate::prelude::ElementBuf) and two [MetadataBuf] references (one of each is a cache)
+//! - Cascade: (*unimplemented*) Unifies two ElementBuf and two MetadataBuf references (one of each is a cache)
 //! in order to perform the complex metadata-aware queries for getting entries and links, including CRUD resolution
 //!
 //! The follow diagram shows the composition hierarchy.
