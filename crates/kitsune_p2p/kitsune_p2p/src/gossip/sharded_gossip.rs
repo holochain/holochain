8f4c0a186bc2e71a4304cbd38e5d68c6b1ee6548
Jost Schulte <jost.schulte@protonmail.com>
2022-04-14 14:55:45 -0600

    remove intra-doc link for gossip factory fns

--- crates/kitsune_p2p/kitsune_p2p/src/gossip/sharded_gossip.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/gossip/sharded_gossip.rs
@@ -1133,12 +1133,12 @@ impl AsGossipModuleFactory for ShardedHistoricalGossipFactory {
     }
 }
 
-/// Create a recent [`GossipModuleFactory`](gossip::GossipModuleFactory)
+/// Create a recent `GossipModuleFactory`
 pub fn recent_factory(bandwidth: Arc<BandwidthThrottle>) -> GossipModuleFactory {
     GossipModuleFactory(Arc::new(ShardedRecentGossipFactory::new(bandwidth)))
 }
 
-/// Create a [`GossipModuleFactory`](gossip::GossipModuleFactory)
+/// Create a historical `GossipModuleFactory`
 pub fn historical_factory(bandwidth: Arc<BandwidthThrottle>) -> GossipModuleFactory {
     GossipModuleFactory(Arc::new(ShardedHistoricalGossipFactory::new(bandwidth)))
 }
