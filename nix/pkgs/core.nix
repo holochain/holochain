4c36d63cf67df254efc0203ccecb269055d27847
Stefan Junker <mail@stefanjunker.de>
2022-05-19 16:41:47 +0200

    ci(nix,circle-ci): configure nextest parameters and use it on circleci
    the builtin cargo test runner produces unreliable results at this point.
    by running both but only require the nextest jobs we can monitor
    reliability over time.

--- nix/pkgs/core.nix
+++ nix/pkgs/core.nix
@@ -68,7 +68,7 @@ rec {
 
     # alas, we cannot specify --features in the virtual workspace
     # run the specific slow tests in the holochain crate
-    cargo nextest ''${@:-run} ''${CARGO_TEST_ARGS:-} -p holochain --features slow_tests,test_utils,build_wasms,db-encryption --cargo-profile fast-test
+    cargo nextest ''${@:-run --test-threads=2} ''${CARGO_TEST_ARGS:-} -p holochain --features slow_tests,test_utils,build_wasms,db-encryption --cargo-profile fast-test
   '';
 
   hcSlowTestsIter = writeShellScriptBin "hc-test-slow-iter" ''
