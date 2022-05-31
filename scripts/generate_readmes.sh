396493332f482c7d3c64587d7651359885b3ddee
Jost Schulte <jost.schulte@protonmail.com>
2022-05-31 11:02:12 -0600

    ci: wip

--- scripts/generate_readmes.sh
+++ scripts/generate_readmes.sh
@@ -12,5 +12,8 @@ git diff --exit-code --quiet
 readmes_updated=$?
 if [[ "$readmes_updated" == 1 ]]; then
     echo 'READMEs have been updated, committing changes'
+    git config user.name release-ci
+    git config user.email jost.schulte@holo.host
+    git push
     git commit -am "docs(github): generated READMEs from doc comments"
 fi
