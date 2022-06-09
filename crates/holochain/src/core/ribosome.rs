117604d1d20ae232ec05cee732ede062a65ed776
Michael Dougherty <maackle.d@gmail.com>
2022-06-09 11:40:35 -0700

    Add weigh() callback
    commit 93a7639480af1a3fb961ab127f8e1f9b40075074
    Author: Michael Dougherty <maackle.d@gmail.com>
    Date:   Thu May 26 15:59:47 2022 -0700
    
        Add first pass of weigh() callback

--- crates/holochain/src/core/ribosome.rs
+++ crates/holochain/src/core/ribosome.rs
@@ -12,6 +12,10 @@ pub mod guest_callback;
 pub mod host_fn;
 pub mod real_ribosome;
 
+use self::{
+    error::RibosomeError,
+    guest_callback::genesis_self_check::{GenesisSelfCheckHostAccess, GenesisSelfCheckInvocation},
+};
 use crate::conductor::api::CellConductorApi;
 use crate::conductor::api::CellConductorReadHandle;
 use crate::conductor::api::ZomeCall;
@@ -26,7 +30,13 @@ use crate::core::ribosome::guest_callback::validate::ValidateInvocation;
 use crate::core::ribosome::guest_callback::validate::ValidateResult;
 use crate::core::ribosome::guest_callback::validation_package::ValidationPackageInvocation;
 use crate::core::ribosome::guest_callback::validation_package::ValidationPackageResult;
+use crate::core::ribosome::guest_callback::weigh::WeighHostAccess;
+use crate::core::ribosome::guest_callback::weigh::WeighInvocation;
+use crate::core::ribosome::guest_callback::weigh::WeighResult;
 use crate::core::ribosome::guest_callback::CallIterator;
+use crate::core::ribosome::guest_callback::{
+    entry_defs::EntryDefsInvocation, genesis_self_check::GenesisSelfCheckResult,
+};
 use derive_more::Constructor;
 use error::RibosomeResult;
 use guest_callback::entry_defs::EntryDefsHostAccess;
@@ -44,14 +54,6 @@ use holochain_state::host_fn_workspace::HostFnWorkspaceRead;
 use holochain_types::prelude::*;
 use mockall::automock;
 use std::iter::Iterator;
-
-use self::guest_callback::{
-    entry_defs::EntryDefsInvocation, genesis_self_check::GenesisSelfCheckResult,
-};
-use self::{
-    error::RibosomeError,
-    guest_callback::genesis_self_check::{GenesisSelfCheckHostAccess, GenesisSelfCheckInvocation},
-};
 
 #[derive(Clone)]
 pub struct CallContext {
@@ -102,6 +104,7 @@ pub enum HostContext {
     PostCommit(PostCommitHostAccess), // MAYBE: add emit_signal access here?
     Validate(ValidateHostAccess),
     ValidationPackage(ValidationPackageHostAccess),
+    Weigh(WeighHostAccess),
     ZomeCall(ZomeCallHostAccess),
 }
 
@@ -116,6 +119,7 @@ impl From<&HostContext> for HostFnAccess {
             HostContext::MigrateAgent(access) => access.into(),
             HostContext::ValidationPackage(access) => access.into(),
             HostContext::PostCommit(access) => access.into(),
+            HostContext::Weigh(access) => access.into(),
         }
     }
 }
@@ -545,6 +549,12 @@ pub trait RibosomeT: Sized + std::fmt::Debug + Send + Sync {
         access: ValidateHostAccess,
         invocation: ValidateInvocation,
     ) -> RibosomeResult<ValidateResult>;
+
+    fn run_weigh(
+        &self,
+        host_access: WeighHostAccess,
+        invocation: WeighInvocation,
+    ) -> RibosomeResult<WeighResult>;
 
     /// Runs the specified zome fn. Returns the cursor used by HDK,
     /// so that it can be passed on to source chain manager for transactional writes
--- crates/holochain_zome_types/src/rate_limit.rs
+++ crates/holochain_zome_types/src/rate_limit.rs
@@ -1,3 +1,35 @@
-//! Types for rate limiting
+//! Types for rate limiting, including the result of the `weigh` callback
+
+use crate::CallbackResult;
+use holochain_integrity_types::rate_limit::{RateBucketId, RateUnits};
+use holochain_serialized_bytes::prelude::*;
+use holochain_wasmer_common::WasmError;
 
 pub use holochain_integrity_types::rate_limit::*;
+
+/// The result of the `weigh` callback
+#[derive(Clone, PartialEq, Serialize, Deserialize, SerializedBytes, Debug)]
+pub struct WeighCallbackResult {
+    /// The ascribed bucket id
+    pub bucket_id: RateBucketId,
+    /// The ascribed weight
+    pub units: RateUnits,
+}
+
+impl CallbackResult for WeighCallbackResult {
+    fn is_definitive(&self) -> bool {
+        true
+    }
+    fn try_from_wasm_error(wasm_error: WasmError) -> Result<Self, WasmError> {
+        Err(wasm_error)
+    }
+}
+
+impl Default for WeighCallbackResult {
+    fn default() -> Self {
+        Self {
+            bucket_id: 255,
+            units: 0,
+        }
+    }
+}
