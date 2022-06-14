8979b932cdd1819ef464fb6183ea0d2c5f00636c
neonphog <neonphog@gmail.com>
2022-06-14 09:58:59 -0600

    code review & merge fixes

--- crates/holochain/src/core/ribosome/host_fn/x_salsa20_poly1305_shared_secret_create_random.rs
+++ crates/holochain/src/core/ribosome/host_fn/x_salsa20_poly1305_shared_secret_create_random.rs
@@ -3,7 +3,7 @@ use crate::core::ribosome::HostFnAccess;
 use crate::core::ribosome::RibosomeError;
 use crate::core::ribosome::RibosomeT;
 use holochain_types::prelude::*;
-use holochain_wasmer_host::prelude::WasmError;
+use holochain_wasmer_host::prelude::*;
 use ring::rand::SecureRandom;
 use std::sync::Arc;
 
@@ -11,7 +11,7 @@ pub fn x_salsa20_poly1305_shared_secret_create_random(
     _ribosome: Arc<impl RibosomeT>,
     call_context: Arc<CallContext>,
     input: Option<XSalsa20Poly1305KeyRef>,
-) -> Result<XSalsa20Poly1305KeyRef, WasmError> {
+) -> Result<XSalsa20Poly1305KeyRef, RuntimeError> {
     match HostFnAccess::from(&call_context.host_context()) {
         HostFnAccess {
             keystore: Permission::Allow,
@@ -26,7 +26,7 @@ pub fn x_salsa20_poly1305_shared_secret_create_random(
                     // this is a temp requirement until we do the
                     // actual lair integration
                     if key_ref.len() != 32 {
-                        return Err(WasmError::Host("TempErrKeyRefLen".into()));
+                        return Err(wasm_error!(WasmErrorInner::Host("TempErrKeyRefLen".into())).into());
                     }
                     Ok(key_ref)
                 }
@@ -36,19 +36,19 @@ pub fn x_salsa20_poly1305_shared_secret_create_random(
                     system_random
                         .fill(&mut key_ref)
                         .map_err(|ring_unspecified| {
-                            WasmError::Host(ring_unspecified.to_string())
+                            wasm_error!(WasmErrorInner::Host(ring_unspecified.to_string()))
                         })?;
                     Ok(key_ref.into())
                 }
             }
         }
-        _ => Err(WasmError::Host(
+        _ => Err(wasm_error!(WasmErrorInner::Host(
             RibosomeError::HostFnPermissions(
                 call_context.zome.zome_name().clone(),
                 call_context.function_name().clone(),
                 "x_salsa20_poly1305_shared_secret_create_random".into(),
             )
             .to_string(),
-        )),
+        )).into()),
     }
 }
--- crates/holochain/src/core/ribosome/host_fn/x_salsa20_poly1305_shared_secret_export.rs
+++ crates/holochain/src/core/ribosome/host_fn/x_salsa20_poly1305_shared_secret_export.rs
@@ -3,14 +3,14 @@ use crate::core::ribosome::HostFnAccess;
 use crate::core::ribosome::RibosomeError;
 use crate::core::ribosome::RibosomeT;
 use holochain_types::prelude::*;
-use holochain_wasmer_host::prelude::WasmError;
+use holochain_wasmer_host::prelude::*;
 use std::sync::Arc;
 
 pub fn x_salsa20_poly1305_shared_secret_export(
     _ribosome: Arc<impl RibosomeT>,
     call_context: Arc<CallContext>,
     input: XSalsa20Poly1305SharedSecretExport,
-) -> Result<XSalsa20Poly1305EncryptedData, WasmError> {
+) -> Result<XSalsa20Poly1305EncryptedData, RuntimeError> {
     match HostFnAccess::from(&call_context.host_context()) {
         HostFnAccess {
             keystore: Permission::Allow,
@@ -39,15 +39,15 @@ pub fn x_salsa20_poly1305_shared_secret_export(
                     cipher.to_vec(),
                 ))
             })
-            .map_err(|keystore_error| WasmError::Host(keystore_error.to_string()))
+            .map_err(|keystore_error| wasm_error!(WasmErrorInner::Host(keystore_error.to_string())).into())
         }
-        _ => Err(WasmError::Host(
+        _ => Err(wasm_error!(WasmErrorInner::Host(
             RibosomeError::HostFnPermissions(
                 call_context.zome.zome_name().clone(),
                 call_context.function_name().clone(),
                 "x_salsa20_poly1305_shared_secret_export".into(),
             )
             .to_string(),
-        )),
+        )).into()),
     }
 }
