794ce515dde8e8fc226aff6d05b950422a09cf61
Jost Schulte <jost.schulte@protonmail.com>
2022-07-13 10:00:45 -0600

    docs(link): add comments to LInkTypeFilterExt

--- crates/hdi/CHANGELOG.md
+++ crates/hdi/CHANGELOG.md
@@ -3,6 +3,7 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/). This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+- Docs: Add documentation on `LinkTypeFilterExt`. [\#1486](https://github.com/holochain/holochain/pull/1486)
 - Docs: replace occurrences of `hdk_entry_def` and `entry_def!` with `hdk_entry_helper`.
 
 ## 0.0.13
--- crates/hdk/src/link.rs
+++ crates/hdk/src/link.rs
@@ -115,12 +115,12 @@ pub fn delete_link(address: ActionHash) -> ExternResult<ActionHash> {
 
 /// Returns all links that reference a base entry hash, optionally filtered by link type and tag.
 ///
-/// Type can be filtered by providing a variant of the link types. To get links of all types, the
-/// range operator can be used `get_links(base, .., None)`. Furthermore, vectors of link types can
-/// be passed in to specify multiple types. Refer to the `get_links` function in
+/// Type can be filtered by providing a variant of the link types, or a range of them. To get links of
+/// all types, the full range operator can be used: `get_links(base, .., None)`. Furthermore, vectors of
+/// link types can be passed in to specify multiple types. Refer to the `get_links` function in
 /// ]this coordinator zome](https://github.com/holochain/holochain/blob/develop/crates/test_utils/wasm/wasm_workspace/link/src/coordinator.rs)
 /// for several examples.
-/// 
+///
 /// Tag filtering is a simple bytes prefix.
 ///
 /// e.g. if you had these links:
@@ -155,10 +155,10 @@ pub fn get_links(
 }
 
 /// Get all link creates and deletes that reference a base entry hash, optionally filtered by type or tag.
-/// 
-/// Type can be filtered by providing a variant of the link types. To get links of all types, the
-/// range operator can be used `get_links(base, .., None)`. Furthermore, vectors of link types can
-/// be passed in to specify multiple types. Refer to the `get_links` function in
+///
+/// Type can be filtered by providing a variant of the link types, or a range of them. To get links of
+/// all types, the full range operator can be used: `get_links(base, .., None)`. Furthermore, vectors of
+/// link types can be passed in to specify multiple types. Refer to the `get_links` function in
 /// ]this coordinator zome](https://github.com/holochain/holochain/blob/develop/crates/test_utils/wasm/wasm_workspace/link/src/coordinator.rs)
 /// for several examples.
 ///
