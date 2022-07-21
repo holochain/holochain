29f7cf536c030c5d79c6595f4bdbcb0c61c3b9d1
Jost Schulte <jost.schulte@protonmail.com>
2022-07-21 09:47:08 -0600

    refactor(uid)!: rename uid to network_seed (#1493)
    * refactor(uid): rename uid to network_seed
    
    * chore(all): update changelogs
    
    * docs(dna): describe network seed
    
    * docs(network_seed): add cross-links
    
    * chore(changelogs): highlight breaking change

--- crates/holochain/src/test_utils.rs
+++ crates/holochain/src/test_utils.rs
@@ -769,8 +769,11 @@ where
 }
 
 /// A fixture example dna for unit testing.
-pub fn fake_valid_dna_file(uid: &str) -> DnaFile {
-    fake_dna_zomes(uid, vec![(TestWasm::Foo.into(), TestWasm::Foo.into())])
+pub fn fake_valid_dna_file(network_seed: &str) -> DnaFile {
+    fake_dna_zomes(
+        network_seed,
+        vec![(TestWasm::Foo.into(), TestWasm::Foo.into())],
+    )
 }
 
 /// Run genesis on the source chain for testing.
