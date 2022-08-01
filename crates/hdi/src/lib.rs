f65de8212cde1c1904148a540a8ee34dfa89d14c
Jost Schulte <jost.schulte@protonmail.com>
2022-08-01 11:53:50 -0600

    add more explanatory text to OpType helper

--- crates/hdi/src/lib.rs
+++ crates/hdi/src/lib.rs
@@ -51,7 +51,10 @@
 //! which is called by the Holochain engine for each operation.
 //!
 //! There's a helper type called [`OpType`](holochain_integrity_types::OpType) available for easy
-//! access to all link and entry variants when validating an operation.
+//! access to all link and entry variants when validating an operation. In many cases, this type can 
+//! be easier to work with than the bare [`Op`](holochain_integrity_types::Op), which contains the
+//! same information as `OpType`, but the former has a flatter data structure, whereas the latter has
+//! a deeply nested structure.
 //! ```ignore
 //! let op_type: OpType<EntryTypes, LinkTypes> = op.to_type()?;
 //! match op_type {
