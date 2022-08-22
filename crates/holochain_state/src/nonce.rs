093ad0b6e658382fe7854b19ff0a8c50b75d36e7
thedavidmeister <thedavidmeister@gmail.com>
2022-08-22 23:03:15 +0400

    zome call witness nonce tests

--- crates/holochain_state/src/nonce.rs
+++ crates/holochain_state/src/nonce.rs
@@ -13,8 +13,9 @@ use rand::Rng;
 
 /// Rather arbitrary but we expire nonces after 5 mins.
 pub const FRESH_NONCE_EXPIRES_AFTER: Duration = Duration::from_secs(60 * 5);
+pub const WITNESSABLE_EXPIRY_DURATION: Duration = Duration::from_secs(600 * 5);
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum WitnessNonceResult {
     Fresh,
     Stale,
@@ -27,7 +28,8 @@ pub async fn witness_nonce(
     now: Timestamp,
     expires: Timestamp,
 ) -> DatabaseResult<WitnessNonceResult> {
-    if expires <= now {
+    // Treat expired but also very far future expiries as stale as we cannot trust the time in that case.
+    if expires <= now || expires > (now + WITNESSABLE_EXPIRY_DURATION)? {
         Ok(WitnessNonceResult::Stale)
     } else {
         db.async_commit(move |txn| {
@@ -46,11 +48,82 @@ pub async fn witness_nonce(
     }
 }
 
-pub async fn fresh_nonce(
+pub fn fresh_nonce(
     now: Timestamp,
 ) -> DatabaseResult<(IntNonce, Timestamp)> {
     // very unlikely to get a collision, we assume impossible.
     let nonce = rand::thread_rng().gen();
     let expires: Timestamp = (now + FRESH_NONCE_EXPIRES_AFTER)?;
     Ok((nonce, expires))
+}
+
+#[cfg(test)]
+pub mod test {
+    use holochain_zome_types::Timestamp;
+    use fixt::prelude::*;
+    use hdk::prelude::AgentPubKeyFixturator;
+
+    use crate::{nonce::{FRESH_NONCE_EXPIRES_AFTER, WitnessNonceResult}, prelude::test_conductor_db};
+
+    #[test]
+    fn test_fresh_nonce() {
+        let now = Timestamp::now();
+        let (nonce, expires) = super::fresh_nonce(now).unwrap();
+        let (nonce_2, expires_2) = super::fresh_nonce(now).unwrap();
+        assert!(nonce != nonce_2);
+        assert_eq!(expires, expires_2);
+        assert_eq!(expires, (now + FRESH_NONCE_EXPIRES_AFTER).unwrap());
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_witness_nonce() {
+        let db = test_conductor_db();
+        let now_0 = Timestamp::now();
+        let agent_0 = fixt!(AgentPubKey, Predictable, 0);
+        let agent_1 = fixt!(AgentPubKey, Predictable, 1);
+        let (nonce_0, expires_0) = super::fresh_nonce(now_0).unwrap();
+
+        // First witnessing should be fresh.
+        let witness_0 = super::witness_nonce(&db, agent_0.clone(), nonce_0, now_0, expires_0).await.unwrap();
+
+        assert_eq!(witness_0, WitnessNonceResult::Fresh);
+
+        // Second witnessing stale.
+        let witness_1 = super::witness_nonce(&db, agent_0.clone(), nonce_0, now_0, expires_0).await.unwrap();
+
+        assert_eq!(witness_1, WitnessNonceResult::Stale);
+
+        // Different agent is different witnessing even with same params.
+        assert_eq!(WitnessNonceResult::Fresh, super::witness_nonce(&db, agent_1, nonce_0, now_0, expires_0).await.unwrap());
+
+        // New nonce is new witnessing.
+        let now_1 = Timestamp::now();
+        let (nonce_1, expires_1) = super::fresh_nonce(now_1).unwrap();
+
+        assert_eq!(WitnessNonceResult::Fresh, super::witness_nonce(&db, agent_0.clone(), nonce_1, now_1, expires_1).await.unwrap());
+
+
+        // Past expiry is stale witnessing.
+        let past = (now_0 - std::time::Duration::from_secs(1)).unwrap();
+        let (nonce_2, _expires_2) = super::fresh_nonce(past).unwrap();
+
+        assert_eq!(WitnessNonceResult::Stale, super::witness_nonce(&db, agent_0.clone(), nonce_2, past, past).await.unwrap());
+
+        // Far future expiry is stale witnessing.
+        let future = (Timestamp::now() + std::time::Duration::from_secs(1_000_000)).unwrap();
+        let (nonce_3, expires_3) = super::fresh_nonce(future).unwrap();
+
+        assert_eq!(WitnessNonceResult::Stale, super::witness_nonce(&db, agent_0.clone(), nonce_3, now_1, expires_3).await.unwrap());
+
+        // Expired nonce can be reused.
+        let now_2 = Timestamp::now();
+        let (nonce_4, expires_4) = super::fresh_nonce(now_2).unwrap();
+
+        assert_eq!(WitnessNonceResult::Fresh, super::witness_nonce(&db, agent_0.clone(), nonce_4, now_2, expires_4).await.unwrap());
+        assert_eq!(WitnessNonceResult::Stale, super::witness_nonce(&db, agent_0.clone(), nonce_4, now_2, expires_4).await.unwrap());
+        let later = (expires_4 + std::time::Duration::from_millis(1)).unwrap();
+        let (_nonce_5, later_expires) = super::fresh_nonce(later).unwrap();
+        assert_eq!(WitnessNonceResult::Fresh, super::witness_nonce(&db, agent_0, nonce_4, later, later_expires).await.unwrap());
+
+    }
 }
