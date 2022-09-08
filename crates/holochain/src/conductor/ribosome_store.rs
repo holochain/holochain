f1d8d1d777b4ae4e749707774552e21caa7c4276
release-ci <ci@holo.host>
2022-09-08 14:22:32 -0600

    refactor(conductor): add ribosome after app update

--- crates/holochain/src/conductor/ribosome_store.rs
+++ crates/holochain/src/conductor/ribosome_store.rs
@@ -38,6 +38,10 @@ impl RibosomeStore {
         self.ribosomes.extend(ribosomes);
     }
 
+    pub fn remove_ribosome(&mut self, dna_hash: &DnaHash) {
+        self.ribosomes.remove(dna_hash);
+    }
+
     #[instrument]
     pub fn list(&self) -> Vec<DnaHash> {
         self.ribosomes.keys().cloned().collect()
