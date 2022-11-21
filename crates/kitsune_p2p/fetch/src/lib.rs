8372d48f7c4f38ab6c0dc8ddb83d007fe27bb664
Michael Dougherty <maackle.d@gmail.com>
2022-11-21 11:42:19 -0800

    Add RegionCoords

--- crates/kitsune_p2p/fetch/src/lib.rs
+++ crates/kitsune_p2p/fetch/src/lib.rs
@@ -1,26 +1,21 @@
 use std::sync::Arc;
 
-use kitsune_p2p_types::bin_types::*;
+use kitsune_p2p_types::{bin_types::*, dht::region::RegionCoords};
 
 /// Determine what should be fetched.
 #[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "type", rename_all = "camelCase")]
 pub enum FetchKey {
     /// Fetch via region.
-    // TODO - add region info
-    Region,
+    Region {
+        /// The region coordinates to fetch
+        region_coords: RegionCoords,
+    },
 
     /// Fetch via op hash.
     Op {
         /// The hash of the op to fetch.
-        op_hash: Arc<KitsuneBasis>,
-
-        /// If specified, the author of the op.
-        /// NOTE: author is addative-only. That is, an op without an author
-        /// is the same as one *with* an author, but should be updated to
-        /// include the author. It is UB to have two FetchKeys with the
-        /// same op_hash, but different authors.
-        author: Option<Arc<KitsuneAgent>>,
+        op_hash: Arc<KitsuneOpHash>,
     },
 }
 
@@ -28,8 +23,15 @@ pub enum FetchKey {
 #[derive(Debug, Clone, PartialEq, Eq, Hash, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "type", rename_all = "camelCase")]
 pub struct FetchRequest {
-    ///
+    /// Description of what to fetch.
     key: FetchKey,
+
+    /// If specified, the author of the op.
+    /// NOTE: author is additive-only. That is, an op without an author
+    /// is the same as one *with* an author, but should be updated to
+    /// include the author. It is UB to have two FetchKeys with the
+    /// same op_hash, but different authors.
+    author: Option<Arc<KitsuneAgent>>,
 
     /// Optional arguments related to fetching the data.
     _options: Option<FetchOptions>,
