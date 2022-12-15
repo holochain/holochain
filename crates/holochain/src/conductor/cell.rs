f9253de8d870230b3d2c2bf03d6ce8baa82a3971
Jost Schulte <jost.schulte@protonmail.com>
2022-12-15 08:16:54 -0600

    refactor is_enabled

--- crates/holochain/src/conductor/cell.rs
+++ crates/holochain/src/conductor/cell.rs
@@ -829,15 +829,11 @@ impl Cell {
             .get_state()
             .await
             .map_err(|e| CellError::ConductorError(Box::new(e)))?;
-        let mut is_enabled = true;
-        for (_, app) in state.enabled_apps() {
-            for (_, cell_id) in app.disabled_clone_cells() {
-                if cell_id.clone() == self.id {
-                    is_enabled = false;
-                    break;
-                }
-            }
-        }
+        let is_enabled = state
+            .enabled_apps()
+            .flat_map(|(_, app)| app.disabled_clone_cell_ids())
+            .find(|cell_id| **cell_id == self.id)
+            .is_none();
         Ok(is_enabled)
     }
 
