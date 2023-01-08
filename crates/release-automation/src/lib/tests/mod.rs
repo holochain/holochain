4b6c8ae76937167b1aa0c75345cdf5a99c926db5
Jost Schulte <jost.schulte@protonmail.com>
2023-01-07 19:17:12 -0600

    import missing structs

--- crates/release-automation/src/lib/tests/mod.rs
+++ crates/release-automation/src/lib/tests/mod.rs
@@ -3,7 +3,7 @@ pub mod workspace_mocker;
 
 use std::path::Path;
 
-use chrono::TimeZone;
+use chrono::{naive::NaiveDateTime, DateTime, TimeZone, Utc};
 use linked_hash_set::LinkedHashSet;
 
 use crate::release::{ReleaseSteps, ReleaseWorkspace};
@@ -176,7 +176,11 @@ fn parse_publish_error() {
                 package: "".to_string(),
                 version: "".to_string(),
                 location: "registry at https://crates.io".to_string(),
-                retry_after: DateTime::<Utc>::from_utc(NaiveDateTime::parse_from_str("2021-06-30 21:09:24", "%Y-%m-%d %H:%M:%S", Utc),
+                retry_after: DateTime::<Utc>::from_utc(NaiveDateTime::parse_from_str(
+                    "2021-06-30 21:09:24",
+                    "%Y-%m-%d %H:%M:%S",
+                    Utc,
+                )),
             },
         },
     ];
