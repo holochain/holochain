2e655f8090a93b3eb4373d763ce503e78c148f87
DavHau <hsngrmpf+github@gmail.com>
2023-01-16 23:59:23 +0800

    chore(nix): WIP iterate on holochain-tests

--- nix/modules/holochain.nix
+++ nix/modules/holochain.nix
@@ -44,44 +44,39 @@
     };
 
     # derivation building all dependencies
-    cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+    holochainDeps = craneLib.buildDepsOnly (commonArgs // rec {
       RUST_SODIUM_LIB_DIR = "${pkgs.libsodium}/lib";
       RUST_SODIUM_SHARED = "1";
     });
 
     # derivation with the main crates
     holochain = craneLib.buildPackage (commonArgs // {
-      inherit cargoArtifacts;
+      cargoArtifacts = holochainDeps;
       doInstallCargoArtifacts = true;
       doCheck = false;
-      NIX_DEBUG = 1;
-      # cargoExtraArgs = "--features 'build' -p holochain_wasm_test_utils";
-      # preConfigure = lib.optionalString pkgs.stdenv.isDarwin ''
-      #   export NIX_LDFLAGS="-F${pkgs.darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation $NIX_LDFLAGS"
-      # '';
-      # NIX_CFLAGS_COMPILE = [ ] ++ lib.optionals pkgs.stdenv.isDarwin [
-      #   # disable modules, otherwise we get redeclaration errors
-      #   "-fno-modules"
-      #   # link AppKit since we don't get it from modules now
-      #   "-framework"
-      #   "CoreFoundation"
-      # ];
     });
 
-    holochain-tests = craneLib.cargoNextest (commonArgs // {
-      cargoArtifacts = cargoArtifacts;
-      preBuild = ''
-        chmod -R +wx /build/source/target
+    # derivation with the main crates
+    holochain-tests = craneLib.buildPackage (commonArgs // {
+      cargoArtifacts = holochainDeps;
+      doCheck = true;
+      cargoExtraArgs = "--features build_wasms";
+    });
+
+    holochain-tests-nextest = craneLib.cargoNextest (commonArgs // {
+      cargoArtifacts = holochainDeps;
+      preCheck = ''
+        rm /build/source/target/debug/.fingerprint/holochain_wasm_test_utils-*/invoked.timestamp
+        rm /build/source/target/debug/.fingerprint/holochain_test_wasm_common-*/invoked.timestamp
       '';
-      cargoExtraArgs = "--features slow_tests,glacial_tests,test_utils,build_wasms,db-encryption";
+      # cargoExtraArgs = "--features slow_tests,glacial_tests,test_utils,build_wasms,db-encryption";
       # CARGO_PROFILE = "release";
-      # checkPhase = ''
-      #   cargo nextest run
-      #   # cargo nextest run --workspace --features slow_tests,glacial_tests,test_utils,build_wasms,db-encryption --lib --tests --cargo-profile fast-test
-      # '';
+      checkPhaseCargoCommand = ''
+        cargo nextest run --workspace --features slow_tests,glacial_tests,test_utils,build_wasms,db-encryption --lib --tests
+      '';
     });
 
   in {
-    packages = {inherit holochain holochain-tests;};
+    packages = {inherit holochain holochain-tests holochain-tests-nextest;};
   };
 }
