c4cb604a54a09cd53b50a353549583bdc84832d4
Michael Dougherty <maackle.d@gmail.com>
2023-01-19 15:09:41 -0800

    Clippy

--- crates/hc_bundle/src/init.rs
+++ crates/hc_bundle/src/init.rs
@@ -54,11 +54,7 @@ fn prompt_dna_init(root_dir: PathBuf) -> anyhow::Result<DnaBundle> {
         vec![],
         vec![],
     );
-    Ok(DnaBundle::new(
-        manifest.try_into()?,
-        vec![].into(),
-        root_dir,
-    )?)
+    Ok(DnaBundle::new(manifest.try_into()?, vec![], root_dir)?)
 }
 
 fn prompt_app_init(root_dir: PathBuf) -> anyhow::Result<AppBundle> {
