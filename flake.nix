11ad9079d5aec25462924d50def4f34bfa57d0a6
DavHau <hsngrmpf+github@gmail.com>
2023-01-20 23:43:46 +0800

    macos: build holochain for macos in CI (#1780)
    * macos: build holochain for macos in CI
    
    * chore(nix): remove unused overlay.nix file
    
    * chore(nix): fix holochain version
    
    * build.yml: add holonix-integration-test
    
    * build.yml: fix wrong args passed to nix run
    
    * build.yml: downgrade nix to 2.12.0 to fix bug
    
    * chore(nix): remote .#holochain-integration-test (moved to holonix)

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "The new, performant, and simplified version of Holochain on Rust (sometimes called Holochain RSM for Refactored State Model) ";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-filter.url = "github:numtide/nix-filter";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = ["aarch64-darwin" "x86_64-linux" "x86_64-darwin"];
+      # auto import all nix code from `./modules`
+      imports = map (m: "${./nix/modules}/${m}")
+        (builtins.attrNames (builtins.readDir ./nix/modules));
+      perSystem = { config, self', inputs', ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
