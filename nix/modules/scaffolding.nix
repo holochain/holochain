adb98444c2dcfde3dbb2e3802414c2f0d31d7d6a
DavHau <hsngrmpf+github@gmail.com>
2023-01-26 15:39:45 +0000

    feat: add crane builds for lair, launcher, scaffolding

---
+++ nix/modules/scaffolding.nix
@@ -0,0 +1,60 @@
+# Definitions can be imported from a separate file like this one
+
+{ self, inputs, lib, ... } @ flake: {
+  perSystem = { config, self', inputs', system, pkgs, ... }:
+    let
+
+      rustToolchain = config.rust.rustHolochain;
+      craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
+
+      commonArgs = {
+
+        pname = "scaffolding";
+        src = inputs.scaffolding;
+
+        CARGO_PROFILE = "release";
+
+        cargoExtraArgs = "--bin hc-scaffold";
+
+        buildInputs =
+          (with pkgs; [
+            openssl
+          ])
+          ++ (lib.optionals pkgs.stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk_11_0.frameworks; [
+              AppKit
+              CoreFoundation
+              CoreServices
+              Security
+            ])
+          );
+
+        nativeBuildInputs =
+          (with pkgs; [
+            perl
+            pkg-config
+          ])
+          ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            xcbuild
+            libiconv
+          ]);
+      };
+
+      # derivation building all dependencies
+      deps = craneLib.buildDepsOnly (commonArgs // {
+
+      });
+
+      # derivation with the main crates
+      package = craneLib.buildPackage (commonArgs // {
+        cargoArtifacts = deps;
+        doCheck = false;
+      });
+
+    in
+    {
+      packages = {
+        scaffolding = package;
+      };
+    };
+}
