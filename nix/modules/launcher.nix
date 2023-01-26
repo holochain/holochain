3e9497d4ec44b3c2a7908645004f6a27d1b0c6a7
DavHau <hsngrmpf+github@gmail.com>
2023-01-26 15:39:45 +0000

    feat: add crane builds for lair, launcher, scaffolding

---
+++ nix/modules/launcher.nix
@@ -0,0 +1,64 @@
+# Definitions can be imported from a separate file like this one
+
+{ self, inputs, lib, ... } @ flake: {
+  perSystem = { config, self', inputs', system, pkgs, ... }:
+    let
+
+      rustToolchain = config.rust.rustHolochain;
+      craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
+
+      commonArgs = {
+
+        pname = "launcher";
+        src = inputs.launcher;
+
+        CARGO_PROFILE = "release";
+
+        cargoExtraArgs = "--bin hc-launch";
+
+        buildInputs =
+          (with pkgs; [
+            openssl
+            glib
+            webkitgtk.dev
+            gdk-pixbuf
+            gtk3
+          ])
+          ++ (lib.optionals pkgs.stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk_11_0.frameworks; [
+              AppKit
+              CoreFoundation
+              CoreServices
+              Security
+            ])
+          );
+
+        nativeBuildInputs =
+          (with pkgs; [
+            perl
+            pkg-config
+          ])
+          ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            xcbuild
+            libiconv
+          ]);
+      };
+
+      # derivation building all dependencies
+      deps = craneLib.buildDepsOnly (commonArgs // {
+
+      });
+
+      # derivation with the main crates
+      package = craneLib.buildPackage (commonArgs // {
+        cargoArtifacts = deps;
+        doCheck = false;
+      });
+
+    in
+    {
+      packages = {
+        launcher = package;
+      };
+    };
+}
