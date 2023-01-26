8bf43f8dfb6149172df625421a887790113fa126
DavHau <hsngrmpf+github@gmail.com>
2023-01-26 15:36:17 +0000

    feat(nix): add inputs for holochain, lair, scaffolding, launcher

--- flake.nix
+++ flake.nix
@@ -22,6 +22,51 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # lair
+    lair = {
+      url = "github:holochain/lair";
+      flake = false;
+    };
+
+    # holochain_scaffolding_cli
+    scaffolding = {
+      url = "github:holochain/scaffolding";
+      flake = false;
+    };
+
+    # launcher
+    launcher = {
+      url = "github:holochain/launcher";
+      flake = false;
+    };
+
+    # holochain
+    holochain = {
+      # url = "github:holochain/holochain/develop";
+      url = "https://example.com";
+      flake = false;
+    };
+    holochain_v0_1_0-beta-rc_0 = {
+      url = "github:holochain/holochain/holochain-0.1.0-beta-rc.0";
+      flake = false;
+    };
+    holochain_v0_1_0-beta-rc_1 = {
+      url = "github:holochain/holochain/holochain-0.1.0-beta-rc.1";
+      flake = false;
+    };
+    holochain_v0_1_0-beta-rc_2 = {
+      url = "github:holochain/holochain/holochain-0.1.0-beta-rc.2";
+      flake = false;
+    };
+    holochain_v0_1_0-beta-rc_3 = {
+      url = "github:holochain/holochain/holochain-0.1.0-beta-rc.3";
+      flake = false;
+    };
+    holochain_v0_1_0-beta-rc_4 = {
+      url = "github:holochain/holochain/holochain-0.1.0-beta-rc.4";
+      flake = false;
+    };
   };
 
   # refer to flake-parts docs https://flake.parts/
