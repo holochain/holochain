ebd13e808edc4d6ffe6d5a7ca1bec0facf824bbf
Stefan Junker <mail@stefanjunker.de>
2023-01-30 15:57:51 +0100

    ci(circleci): migrate to github actions

--- nix/modules/scripts/default.nix
+++ nix/modules/scripts/default.nix
@@ -3,31 +3,32 @@
     # Definitions like this are entirely equivalent to the ones
     # you may have directly in flake.nix.
     packages = {
-      scripts-ci-cachix-helper =
-        let pathPrefix = lib.makeBinPath (with pkgs; [ cachix ]);
-        in pkgs.writeShellScript "scripts-ci-cachix-helper" ''
-          #! /usr/bin/env nix-shell
-          set -euo pipefail
+      scripts-ci-cachix-helper = let
+        pathPrefix = lib.makeBinPath (with pkgs; [ cachix ]);
+        nix = "nix --extra-experimental-features nix-command";
+      in pkgs.writeShellScript "scripts-ci-cachix-helper" ''
+        #! /usr/bin/env nix-shell
+        set -euo pipefail
 
-          export PATH=${pathPrefix}:$PATH
+        export PATH=${pathPrefix}:$PATH
 
-          export PATHS_PREBUILD_FILE="''${HOME}/.store-path-pre-build"
+        export PATHS_PREBUILD_FILE="''${HOME}/.store-path-pre-build"
 
-          case ''${1} in
-            setup)
-              if [[ -n ''${CACHIX_AUTH_TOKEN:-} ]]; then
-                  echo Using CACHIX_AUTH_TOKEN
-                  cachix --verbose authtoken ''${CACHIX_AUTH_TOKEN}
-              fi
-              cachix --verbose use -m user-nixconf ''${CACHIX_NAME:?}
-              nix path-info --all > "''${PATHS_PREBUILD_FILE}"
-              ;;
+        case ''${1} in
+          setup)
+            if [[ -n ''${CACHIX_AUTH_TOKEN:-} ]]; then
+                echo Using CACHIX_AUTH_TOKEN
+                cachix --verbose authtoken ''${CACHIX_AUTH_TOKEN}
+            fi
+            cachix --verbose use -m user-nixconf ''${CACHIX_NAME:?}
+            ${nix} path-info --all > "''${PATHS_PREBUILD_FILE}"
+            ;;
 
-            push)
-              comm -13 <(sort "''${PATHS_PREBUILD_FILE}" | grep -v '\.drv$') <(nix path-info --all | grep -v '\.drv$' | sort) | cachix --verbose push ''${CACHIX_NAME:?}
-              ;;
-          esac
-        '';
+          push)
+            comm -13 <(sort "''${PATHS_PREBUILD_FILE}" | grep -v '\.drv$') <(${nix} path-info --all | grep -v '\.drv$' | sort) | cachix --verbose push ''${CACHIX_NAME:?}
+            ;;
+        esac
+      '';
     };
   };
 }
