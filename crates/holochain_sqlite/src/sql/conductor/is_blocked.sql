89889ee727d2b09beb45b2533a3e28d07fab9b93
thedavidmeister <thedavidmeister@gmail.com>
2023-02-05 12:10:47 +0400

    wip on is blocked

---
+++ crates/holochain_sqlite/src/sql/conductor/is_blocked.sql
@@ -0,0 +1,7 @@
+-- simple select the matching agent
+SELECT COUNT(1)  > 0
+FROM BlockSpan
+WHERE
+  target_id = :target_id
+  AND start_ms <= :time_ms
+  AND :time_ms <= end_ms
--- crates/holochain_state/src/block.rs
+++ crates/holochain_state/src/block.rs
@@ -13,3 +13,33 @@ pub async fn unblock(db: &DbWrite<DbKindConductor>, block: Block) -> DatabaseRes
     db.async_commit(move |txn| mutations::insert_unblock(txn, block))
         .await
 }
+
+pub async fn is_blocked(db: &DbWrite<DbKindConductor>, target_id: BlockTargetId, timestamp: Timestamp) -> DatabaseResult<bool> {
+    db.with_reader(|txn| )
+}
+
+#[cfg(test)]
+mod test {
+    use crate::test_utils::test_conductor_db;
+    use holochain_types::block::Block;
+    use holochain_types::block::BlockTarget;
+    use holochain_types::block::CellBlockReason;
+    use hdk::prelude::Timestamp;
+    use holochain_types::prelude::CellIdFixturator;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn block_is_blocked() {
+        let db = test_conductor_db();
+        let start = Timestamp(10);
+        let end = Timestamp(20);
+
+        let target = BlockTarget::Cell(fixt::fixt!(CellId), CellBlockReason::BadCrypto);
+
+        super::block(&db, Block {
+            target,
+            start,
+            end,
+        }).await.unwrap();
+    }
+
+}
