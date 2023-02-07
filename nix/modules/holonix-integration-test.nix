95e16007ebaed4d8f2d6afe82e0fc1de5dbd9e32
DavHau <hsngrmpf+github@gmail.com>
2023-02-07 18:50:13 +0700

    fix(holonix-tests-integration): remove recursion
    Nix develop imitates a nix build environment outside the sandbox.
    This is only useful if we are not inside the sandbox.
    Inside the nix sandbox we can just use the shell derivation as is.

--- nix/modules/holonix-integration-test.nix
+++ nix/modules/holonix-integration-test.nix
@@ -16,37 +16,12 @@
 
     in
     {
-      packages =
-        {
-          holonix-tests-integration = pkgs.runCommand
-            "holonix-tests-integration"
-            {
-              __noChroot = pkgs.stdenv.isLinux;
-
-              requiredSystemFeatures = [ "recursive-nix" ];
-
-              nativeBuildInputs = [
-                pkgs.nix
-              ];
-              buildInputs = [
-                pkgs.cacert
-              ];
-            } ''
-
-            export HOME=$PWD/.writeable_home
-            mkdir -p $HOME
-
-            set -x
-
-            nix develop ${self}#holonix \
-              --override-input versions/holochain ${self} \
-              --extra-experimental-features "flakes nix-command" \
-              --command ${testScript}
-
-            echo this line causes ${self'.devShells.holonix} to be pre-built | tee $out
-
-            set +x
+      packages.holonix-tests-integration =
+        self'.devShells.holonix.overrideAttrs (old: {
+          buildPhase = ''
+            ${testScript}
+            touch $out
           '';
-        };
-    };
+        });
+      };
 }
