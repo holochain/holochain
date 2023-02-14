195b8e75120348bdc8379424af46a9c839512444
thedavidmeister <thedavidmeister@gmail.com>
2023-02-14 17:03:10 +0400

    lint

--- crates/holochain_sqlite/src/sql/conductor/schema/1.sql
+++ crates/holochain_sqlite/src/sql/conductor/schema/1.sql
@@ -1,14 +1,11 @@
 CREATE TABLE IF NOT EXISTS BlockSpan (
-    id INTEGER PRIMARY KEY,
-
-    target_id BLOB NOT NULL,
-    target_reason BLOB NOT NULL,
-
-    -- start and end micros
-    -- literal integer from Timestamp in rust
-    start_us INTEGER NOT NULL,
-    end_us INTEGER NOT NULL
+  id INTEGER PRIMARY KEY,
+  target_id BLOB NOT NULL,
+  target_reason BLOB NOT NULL,
+  -- start and end micros
+  -- literal integer from Timestamp in rust
+  start_us INTEGER NOT NULL,
+  end_us INTEGER NOT NULL
 );
-
 CREATE INDEX IF NOT EXISTS block_span_start_us_idx ON BlockSpan(start_us);
 CREATE INDEX IF NOT EXISTS block_span_end_us_idx ON BlockSpan(end_us);
