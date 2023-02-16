fe334e195f1b7c9f6940c5611f3f672f7595bf4d
Jost Schulte <jost.schulte@protonmail.com>
2023-02-16 14:25:59 -0600

    chore(setup-script): install cache & scaffold example app

--- install.sh
+++ install.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
-cat <<EOF
-Welcome, in the future this might be used to help set up the Holochain development environment.
-This script doesn't run any commands for you, it only displays them.
-
-EOF
-
 if ! type -f nix >/dev/null; then
 cat <<EOF
-# Install the nix utility.
+# Install the nix utility first:
 sh <(curl -L https://nixos.org/nix/install) --daemon
 EOF
 fi
 
-cat <<EOF
+echo "Setting up the binary cache..."
+sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features nix-command --extra-experimental-features flakes -- use holochain-ci -m root-nixconf && sudo pkill nix-daemon
 
-# Prepare the environment, setup the binary cache and make sure it's loaded
-export NIX_CONFIG="extra-experimental-features = nix-command flakes"
-sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix -- use holochain-ci -m root-nixconf && sudo pkill nix-daemon
-EOF
-
-cat <<EOF
-
-# This will scaffold the example project into the 'forum' directory
+echo "Scaffolding the example Holochain app..."
 nix run github:holochain/holochain#hc-scaffold -- example forum
-EOF
