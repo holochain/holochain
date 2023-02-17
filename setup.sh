220bbe409fab7b841681cefbd7a1322156e692d3
Jost Schulte <jost.schulte@protonmail.com>
2023-02-16 19:45:44 -0600

    fix typo

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 if ! command -v nix &>/dev/null; then
     echo "Nix package manager not found"
-    echo "Install Nix first, or open a new shell it it is already installed"
+    echo "Install Nix first, or open a new shell if it is already installed"
     echo
     echo "sh <(curl -L https://nixos.org/nix/install) --daemon"
     exit 1
 fi
 
+echo
 echo "Setting up binary cache for all users (requires root access)"
 sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features nix-command --extra-experimental-features flakes -- use holochain-ci -m root-nixconf && sudo pkill nix-daemon
 echo
