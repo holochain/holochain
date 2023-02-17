d49c0cd847c31e406ff7e3a92044bd17a3c3c35f
Stefan Junker <1181362+steveeJ@users.noreply.github.com>
2023-02-17 22:48:01 +0100

    setup.sh: append to nix.conf, abort on error (#1958)

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+set -ex
+
 if ! command -v nix &>/dev/null; then
     echo "Nix package manager not found"
     echo "Install Nix first, or open a new shell if it is already installed"
     echo
     echo "sh <(curl -L https://nixos.org/nix/install) --daemon"
-    exit 1
 fi
 
 echo
 echo "Setting up binary cache for all users (requires root access)"
-sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features nix-command --extra-experimental-features flakes -- use holochain-ci -m root-nixconf && sudo pkill nix-daemon
+sudo --preserve-env=PATH,NIX_CONFIG nix run nixpkgs/nixos-22.11#cachix --extra-experimental-features "nix-command flakes" -- use holochain-ci -m root-nixconf
+sudo pkill nix-daemon
 echo
 
 echo "Creating Nix user config in ~/.config/nix/nix.conf"
@@ -18,7 +20,7 @@ mkdir -p ~/.config/nix
 echo
 
 echo "Enabling additional Nix commands and Nix flakes"
-echo "experimental-features = nix-command flakes" >~/.config/nix/nix.conf
+echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 echo
 
 echo "Scaffolding the example Holochain app"
