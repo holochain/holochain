96b304d0797f6453b8ddef2b8600e0dd124205b2
Stefan Junker <1181362+steveeJ@users.noreply.github.com>
2023-02-28 08:09:39 +0100

    fix(WSL2): install nix with --no-daemon; refactor (#2017)
    * chore: add empty circleci config
    
    * fix(WSL2): install nix with --no-daemon
    
    * feat(test-holonix-setup): run on macos
    
    * chore(test-holonix-setup): define PR event types
    
    * feat(test-holonix-setup): use github access token
    
    * fix(setup.sh): tolerate failures to source
    
    * fix(setup.sh): try to source various places to find nix
    
    * fix(setup.sh): pass NIX_CONFIG via sudo
    
    * refactor(setup.sh): use run_cmd function instead of duplication
    
    * fix(test-holonix-setup): adjust for macos

--- setup.sh
+++ setup.sh
@@ -2,38 +2,52 @@
 
 set -e
 
+run_cmd() {
+    echo "$@"
+    "$@"
+}
+
 if ! command -v nix &>/dev/null; then
     echo "Nix package manager not found"
     echo "Installing Nix"
     echo
-    echo "bash <(curl -L https://nixos.org/nix/install) --daemon"
-    bash <(curl -L https://nixos.org/nix/install) --daemon
+
+    if [[ $(uname -r) == *"WSL2" ]]; then
+        run_cmd bash <(curl -L https://nixos.org/nix/install) --no-daemon
+    else
+        run_cmd bash <(curl -L https://nixos.org/nix/install) --daemon
+    fi
 
     echo
-    echo "Starting Nix daemon"
-    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh > /dev/null 2>&1
-    source /nix/var/nix/profiles/default/etc/profile.d/nix.sh > /dev/null 2>&1
+    echo "Sourcing the nix config files"
+    set +e
+    for file in \
+        ~/.profile \
+        /etc/profile \
+        /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh \
+        /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+    do
+        source $file > /dev/null 2>&1
+        if command -v nix; then break; fi
+    done
+    set -e
     echo
 fi
 
 echo "Setting up binary cache for all users (requires root access)"
-echo "sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features \"nix-command flakes\" -- use holochain-ci -m root-nixconf"
-sudo --preserve-env=PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features "nix-command flakes" -- use holochain-ci -m root-nixconf
+run_cmd sudo --preserve-env=NIX_CONFIG,PATH $(which nix) run nixpkgs/nixos-22.11#cachix --extra-experimental-features "nix-command flakes" -- use holochain-ci -m root-nixconf
 echo
 
 echo "Restarting Nix daemon"
-echo "sudo pkill nix-daemon"
-sudo pkill nix-daemon || :
+run_cmd sudo pkill nix-daemon || :
 echo
 
 echo "Creating Nix user config in ~/.config/nix/nix.conf"
-echo "mkdir -p ~/.config/nix"
-mkdir -p ~/.config/nix
+run_cmd mkdir -p ~/.config/nix
 echo
 
 echo "Enabling additional Nix commands and Nix flakes"
-echo "echo \"experimental-features = nix-command flakes\" >>~/.config/nix/nix.conf"
-echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
+run_cmd bash -c 'echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf'
 echo
 
 echo "Please close this shell and open a new one to start using Nix".
