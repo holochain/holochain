2d6cbebce6daae00df18c368325149cbc3f0d1ba
Jost Schulte <jost.schulte@protonmail.com>
2023-03-01 20:39:19 -0600

    add links to conductor api docs

--- crates/holochain_conductor_api/src/lib.rs
+++ crates/holochain_conductor_api/src/lib.rs
@@ -1,16 +1,28 @@
-//! The API interface into the holochain conductor.
+//! Interfaces to manage Holochain applications (hApps) and call their functions.
 //!
-//! The interface is split into admin and app requests and responses. Each has
-//! an associated enum [ `AdminRequest` ] and [ `AppRequest` ] that define and
-//! document available methods.
+//! The Conductor is the central component of Holochain. It exposes web sockets for clients to
+//! connect to, processes incoming requests and orchestrates data flow and persistence.
 //!
-//! The admin interface generally manages the conductor itself, such as installing apps, changing coordinators, listing
-//! dnas, cells and apps, accessing state and metric information dumps, managing
-//! agents, etc.
+//! Refer to [Holochain's architecture](https://developer.holochain.org/concepts/2_application_architecture)
+//! for more info. Read about hApp development in the
+//! [Happ Development Kit (HDK) documentation](https://docs.rs/hdk/latest/hdk).
+//! 
+//! There is a [Holochain client for JavaScript](https://github.com/holochain/holochain-client-js)
+//! and a [Rust client](https://github.com/holochain/holochain-client-rust)
+//! to connect to the Conductor.
+//!
+//! The Conductor API is split into Admin and App requests and responses. Each has
+//! an associated enum [`AdminRequest`] and [`AppRequest`] that define and
+//! document available calls.
+//!
+//! The admin interface generally manages the conductor itself, such as installing apps,
+//! listing dnas, cells and apps, accessing state and metric information dumps and managing
+//! agents.
 //!
 //! The app interface is smaller and focussed on interfacing with an app directly.
-//! Notably the app interface allows calling zome functions and subscribing to
-//! signals from the app.
+//! Notably the app interface allows calling functions exposed by the hApps'
+//! modules, called DNAs. To discover a particular hApp's structure, its app
+//! info can be requested.
 
 mod admin_interface;
 mod app_interface;
