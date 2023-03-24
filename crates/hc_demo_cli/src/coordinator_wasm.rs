ccc5323e302c21d59c1f3985fa33964cd0ef66f9
neonphog <neonphog@gmail.com>
2023-03-24 11:40:42 -0600

    working demo cli

--- crates/hc_demo_cli/src/coordinator_wasm.rs
+++ crates/hc_demo_cli/src/coordinator_wasm.rs
@@ -26,3 +26,30 @@ pub fn create_file(file: File) -> ExternResult<Record> {
     create_link(path.path_entry_hash()?, file_hash, LinkTypes::AllFiles, ())?;
     Ok(record)
 }
+
+#[hdk_extern]
+pub fn get_file(hash: ActionHash) -> ExternResult<Option<File>> {
+    let record: Record = match get(hash, GetOptions::default())? {
+        Some(r) => r,
+        None => return Ok(None),
+    };
+
+    record.entry.to_app_option().map_err(|e| wasm_error!(e))
+}
+
+#[hdk_extern]
+pub fn get_all_files(_: ()) -> ExternResult<Vec<ActionHash>> {
+    let path = Path::from("all_files");
+    let links = get_links(path.path_entry_hash()?, LinkTypes::AllFiles, None)?;
+    let get_input: Vec<GetInput> = links
+        .into_iter()
+        .map(|link| GetInput::new(ActionHash::from(link.target).into(), GetOptions::default()))
+        .collect();
+    let records = HDK.with(|hdk| hdk.borrow().get(get_input))?;
+    let hashes: Vec<ActionHash> = records
+        .into_iter()
+        .flatten()
+        .map(|r| r.action_address().clone())
+        .collect();
+    Ok(hashes)
+}
--- crates/hc_demo_cli/src/demo.rs
+++ crates/hc_demo_cli/src/demo.rs
@@ -22,10 +22,10 @@ pub const COORDINATOR_WASM: &[u8] = include_bytes!(concat!(
     "/coordinator/wasm32-unknown-unknown/release/hc_demo_cli.wasm"
 ));
 
-use std::sync::Arc;
 use holochain_types::prelude::*;
+use std::sync::Arc;
 
-/// hc_demo_cli - Self-contained demo for holochain functionality.
+/// `hc demo-cli` - Self-contained demo for holochain functionality.
 ///
 /// First, you need to save a dna file to use with the demo:
 ///
@@ -56,7 +56,7 @@ impl RunOpts {
 /// hc_demo_cli run command.
 #[derive(Debug, clap::Subcommand, serde::Serialize, serde::Deserialize)]
 pub enum RunCmd {
-    /// Run the hc_demo_cli.
+    /// Run the hc demo-cli.
     Run {
         /// The dna file path. Default "-" for stdin.
         #[arg(long, default_value = "-")]
@@ -71,12 +71,8 @@ pub enum RunCmd {
         outbox: std::path::PathBuf,
     },
 
-    /// Generate a dna file that can be used with hc_demo_cli.
+    /// Generate a dna file that can be used with hc demo-cli.
     GenDnaFile {
-        /// The network seed to be used in the generated dna file.
-        #[arg(long, default_value = "")]
-        network_seed: String,
-
         /// Filename path to write the dna file. Default "-" for stdout.
         #[arg(long, default_value = "-")]
         output: std::path::PathBuf,
@@ -87,59 +83,55 @@ pub enum RunCmd {
 pub async fn run_demo(opts: RunOpts) {
     tracing::info!(?opts);
     match opts.command {
-        Some(RunCmd::Run {
-            dna,
-            inbox,
-            outbox,
-        }) => {
+        Some(RunCmd::Run { dna, inbox, outbox }) => {
             run(dna, inbox, outbox).await;
         }
-        Some(RunCmd::GenDnaFile {
-            network_seed,
-            output,
-        }) => {
-            gen_dna_file(network_seed, output).await;
+        Some(RunCmd::GenDnaFile { output }) => {
+            gen_dna_file(output).await;
         }
         _ => unreachable!(),
     }
 }
 
-async fn gen_dna_file(network_seed: String, output: std::path::PathBuf) {
+async fn gen_dna_file(output: std::path::PathBuf) {
     let i_wasm = DnaWasmHashed::from_content(DnaWasm {
         code: Arc::new(INTEGRITY_WASM.to_vec().into_boxed_slice()),
-    }).await;
+    })
+    .await;
     let i_zome = IntegrityZomeDef::from(ZomeDef::Wasm(WasmZome::new(i_wasm.hash.clone())));
 
     let c_wasm = DnaWasmHashed::from_content(DnaWasm {
         code: Arc::new(COORDINATOR_WASM.to_vec().into_boxed_slice()),
-    }).await;
+    })
+    .await;
     let c_zome = CoordinatorZomeDef::from(ZomeDef::Wasm(WasmZome::new(c_wasm.hash.clone())));
+
+    let network_seed = rand_utf8::rand_utf8(&mut rand::thread_rng(), 32);
 
     let dna_def = DnaDefBuilder::default()
         .name("hc_demo_cli".to_string())
         .modifiers(
             DnaModifiersBuilder::default()
-                .network_seed(network_seed)
-                .origin_time(Timestamp::HOLOCHAIN_EPOCH)
+                .network_seed(network_seed.into())
+                .origin_time(Timestamp::now())
                 .build()
-                .unwrap()
+                .unwrap(),
         )
         .integrity_zomes(vec![("integrity".into(), i_zome)])
         .coordinator_zomes(vec![("coordinator".into(), c_zome)])
         .build()
         .unwrap();
 
-    let dna_file = DnaFile::new(
-        dna_def,
-        vec![i_wasm.into_content(), c_wasm.into_content()],
-    ).await;
+    let dna_file = DnaFile::new(dna_def, vec![i_wasm.into_content(), c_wasm.into_content()]).await;
 
     let dna_file: SerializedBytes = dna_file.try_into().unwrap();
     let dna_file: UnsafeBytes = dna_file.try_into().unwrap();
     let dna_file: Vec<u8> = dna_file.into();
 
     if output == std::path::PathBuf::from("-") {
-        tokio::io::AsyncWriteExt::write_all(&mut tokio::io::stdout(), &dna_file).await.unwrap();
+        tokio::io::AsyncWriteExt::write_all(&mut tokio::io::stdout(), &dna_file)
+            .await
+            .unwrap();
     } else {
         tokio::fs::write(output, &dna_file).await.unwrap();
     }
@@ -176,18 +168,19 @@ async fn run(dna: std::path::PathBuf, inbox: std::path::PathBuf, outbox: std::pa
     )
     .await;
 
-    let dna_with_role = holochain::sweettest::DnaWithRole::from((
-        "hc_demo_cli".into(),
-        dna,
-    ));
+    let dna_with_role = holochain::sweettest::DnaWithRole::from(("hc_demo_cli".into(), dna));
 
-    let app = conductor.setup_app(
-        "hc_demo_cli",
-        vec![&dna_with_role],
-    ).await.unwrap();
+    let app = conductor
+        .setup_app("hc_demo_cli", vec![&dna_with_role])
+        .await
+        .unwrap();
 
     let cell = app.cells().get(0).unwrap().clone();
     tracing::info!(?cell);
+
+    // PRINT to stdout instead of trace
+    println!("#DNA_HASH#{}#", cell.dna_hash());
+    println!("#AGENT_KEY#{}#", cell.agent_pubkey());
 
     let i_zome = cell.zome("integrity");
     tracing::info!(?i_zome);
@@ -195,11 +188,72 @@ async fn run(dna: std::path::PathBuf, inbox: std::path::PathBuf, outbox: std::pa
     tracing::info!(?c_zome);
 
     let handle = conductor.sweet_handle();
-    let create_file: Record = handle.call(&c_zome, "create_file", File {
-        desc: "yo".to_string(),
-        data: UnsafeBytes::from(vec![0_u8, 1, 2, 3]).try_into().unwrap(),
-    }).await;
-    tracing::info!(?create_file);
 
-    conductor.shutdown().await;
+    loop {
+        let mut dir = tokio::fs::read_dir(&inbox).await.unwrap();
+
+        while let Ok(Some(i)) = dir.next_entry().await {
+            if i.file_type().await.unwrap().is_file() {
+                let name = i.path();
+                let name = name.file_name().unwrap().to_string_lossy().to_string();
+                let data = tokio::fs::read(i.path()).await.unwrap();
+
+                if data.len() > 2 * 1024 * 1024 {
+                    panic!("{:?}: file too large: max 2MiB for hc demo-cli", i.path());
+                }
+
+                tracing::info!(?name, byte_count = %data.len());
+
+                let create_file: Record = handle
+                    .call(
+                        &c_zome,
+                        "create_file",
+                        File {
+                            desc: name.clone(),
+                            data: UnsafeBytes::from(data).try_into().unwrap(),
+                        },
+                    )
+                    .await;
+                tracing::info!(?create_file);
+
+                tokio::fs::remove_file(i.path()).await.unwrap();
+
+                println!("#PUBLISHED#{name}#");
+            }
+        }
+
+        let all_files: Vec<ActionHash> = handle.call(&c_zome, "get_all_files", ()).await;
+
+        for file in all_files {
+            let mut path = outbox.clone();
+            path.push(file.to_string());
+
+            if tokio::fs::metadata(&path).await.is_ok() {
+                continue;
+            }
+
+            let data: File = match handle.call(&c_zome, "get_file", file.clone()).await {
+                Some(data) => data,
+                None => {
+                    println!("#WARN#failed to fetch hash {file}, waiting 5 seconds#");
+                    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+                    continue;
+                }
+            };
+
+            let _ = tokio::fs::create_dir_all(&path).await;
+
+            path.push(&data.desc);
+
+            let bytes: UnsafeBytes = data.data.try_into().unwrap();
+            let bytes: Vec<u8> = bytes.into();
+            tokio::fs::write(&path, &bytes).await.unwrap();
+
+            println!("#FETCHED#{path:?}#");
+        }
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+
+    // conductor.shutdown().await;
 }
