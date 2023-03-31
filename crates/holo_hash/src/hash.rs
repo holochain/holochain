704c3129dff6381bbd34ff6d2c47ca4af2dedcce
Michael Dougherty <maackle.d@gmail.com>
2023-03-31 13:18:45 -0700

    Update from_raw_32 to only allow 32

--- crates/holo_hash/src/hash.rs
+++ crates/holo_hash/src/hash.rs
@@ -174,16 +174,10 @@ impl<T: HashType> HoloHash<T> {
     /// Construct a HoloHash from a 32-byte hash.
     /// The 3 prefix bytes will be added based on the provided HashType,
     /// and the 4 location bytes will be computed.
-    ///
-    /// For convenience, 36 bytes can also be passed in, in which case
-    /// the location bytes will used as provided, not computed.
     pub fn from_raw_32_and_type(mut hash: Vec<u8>, hash_type: T) -> Self {
-        if hash.len() == HOLO_HASH_CORE_LEN {
-            hash.append(&mut encode::holo_dht_location_bytes(&hash));
-        }
-
         assert_length!(HOLO_HASH_UNTYPED_LEN, &hash);
-
+        hash.append(&mut encode::holo_dht_location_bytes(&hash));
+        assert_length!(HOLO_HASH_UNTYPED_LEN, &hash);
         HoloHash::from_raw_36_and_type(hash, hash_type)
     }
 }
