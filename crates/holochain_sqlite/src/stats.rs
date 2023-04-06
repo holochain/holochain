d1559cf91b25e48e28ea6e0bd78da66887d88cb6
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-04-06 12:48:18 +0100

    Add app storage info request (#2147)
    * Add storage info request
    
    * Add a test for getting storage info for an app
    
    * Add a test for getting storage info for all apps
    
    * Describe storage info api in the changelog
    
    * Support getting the size of the cache db too
    
    * Rework storage info following the discussion on #1660
    
    * Update changelogs
    
    * Fix clippy warning
    
    * Rename blobs to include DNA information
    
    * Fix compile error from rebase

---
+++ crates/holochain_sqlite/src/stats.rs
@@ -0,0 +1,12 @@
+use crate::error::DatabaseError;
+use rusqlite::Transaction;
+
+pub fn get_size_on_disk(txn: Transaction) -> Result<usize, DatabaseError> {
+    txn.query_row("select sum(pgsize) from dbstat", (), |r| r.get(0))
+        .map_err(DatabaseError::SqliteError)
+}
+
+pub fn get_used_size(txn: Transaction) -> Result<usize, DatabaseError> {
+    txn.query_row("select sum(pgsize - unused) from dbstat", (), |r| r.get(0))
+        .map_err(DatabaseError::SqliteError)
+}
