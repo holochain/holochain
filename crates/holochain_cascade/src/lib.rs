e5e4d5065f2f9c4802e1d9326a1cd1d1adb7c8ae
Michael Dougherty <maackle.d@gmail.com>
2023-04-17 14:38:31 -0400

    Clippy

--- crates/holochain_cascade/src/lib.rs
+++ crates/holochain_cascade/src/lib.rs
@@ -1149,7 +1149,7 @@ impl MockCascade {
             })))
         });
 
-        let map = map0.clone();
+        let map = map0;
         cascade.expect_retrieve_entry().returning(move |hash, _| {
             box_fut_plain(Ok(map.share_ref(|m| {
                 m.get(&hash.into()).map(|r| {
