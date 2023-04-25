e29e8469ba63388e28ef2b1ef47e5b9837e7d967
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-04-25 17:01:15 +0100

    Don't enable unnecessary features

--- .config/test-args.nix
+++ .config/test-args.nix
@@ -1,1 +1,1 @@
-"--workspace --no-default-features --features slow_tests,glacial_tests,tx2,tx5,sweetest,build_wasms,sqlite-encrypted --lib --tests --bins"
+"--workspace --no-default-features --features slow_tests,glacial_tests,sweetest,build_wasms,sqlite-encrypted --lib --tests --bins"
