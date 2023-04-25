626dad6a1cbcf2b5833b0b88954af2f5f74f2647
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-04-25 17:26:29 +0100

    Default features cannot be excluded

--- .config/test-args.nix
+++ .config/test-args.nix
@@ -1,1 +1,1 @@
-"--workspace --no-default-features --features slow_tests,glacial_tests,sweetest,build_wasms,sqlite-encrypted --lib --tests --bins"
+"--workspace --features slow_tests,glacial_tests,sweetest,build_wasms,sqlite-encrypted --lib --tests --bins"
