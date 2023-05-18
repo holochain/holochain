a141df04cd32002a753526b04f5f40b779236d93
Michael Dougherty <maackle.d@gmail.com>
2023-05-18 16:48:05 -0700

    Fix bad hex display impl

--- crates/holo_hash/src/encode_raw.rs
+++ crates/holo_hash/src/encode_raw.rs
@@ -1,12 +1,10 @@
+use crate::bytes_to_hex;
 use crate::HashType;
 use crate::HoloHash;
 
 impl<T: HashType> std::fmt::Display for HoloHash<T> {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
-        f.write_fmt(format_args!("0x{}", bytes_to_hex))?;
-        for byte in self.get_raw_39() {
-            f.write_fmt(format_args!("{:02x}", byte))?;
-        }
+        f.write_fmt(format_args!("0x{}", bytes_to_hex(self.get_raw_39())))?;
         Ok(())
     }
 }
