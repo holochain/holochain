bea8b7c5669bb30c5b1744f6f36780e2391868da
Michael Dougherty <maackle.d@gmail.com>
2023-05-18 17:10:14 -0700

    Oops

--- crates/holo_hash/src/encode_raw.rs
+++ crates/holo_hash/src/encode_raw.rs
@@ -4,7 +4,7 @@ use crate::HoloHash;
 
 impl<T: HashType> std::fmt::Display for HoloHash<T> {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
-        f.write_fmt(format_args!("0x{}", bytes_to_hex(self.get_raw_39())))?;
+        f.write_fmt(format_args!("0x{}", bytes_to_hex(self.get_raw_39(), false)))?;
         Ok(())
     }
 }
