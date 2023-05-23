31f9fee36244e3d6ee366c67b5ef70e5e1ea9495
thedavidmeister <thedavidmeister@gmail.com>
2023-05-23 20:09:49 +0400

    tests for nonet

---
+++ crates/nonet/src/lib.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+#[tokio::test]
+pub async fn test_nonet() -> Result<()> {
+    match reqwest::get("https://www.rust-lang.org").await?.error_for_status() {
+        Ok(_response) => {
+            #[cfg(feature = "nonet")]
+            panic!("Connected to the internet with nonet feature enabled.");
+        }
+        Err(_err) => {
+            #[cfg(not(feature = "nonet"))]
+            panic!("Failed to connect to the internet without nonet feature enabled (Yes, that's a triple negative).");
+        }
+    }
+    Ok(())
+}
