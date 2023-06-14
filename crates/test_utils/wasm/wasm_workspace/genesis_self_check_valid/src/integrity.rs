a0bf45ceb6d10b2daaf5f1f40384c3b67b76e515
David Meister <thedavidmeister@gmail.com>
2023-06-14 17:38:58 +0400

    wip on dna info 2 (#2366)
    * wip on dna info 2
    
    * wip on callback versioning
    
    * wip on v2 self check
    
    * v2 of genesis self check
    
    * map genesis self check in map extern
    
    * wip on fixing tests
    
    * fix tests
    
    * docs for versoning
    
    * test dna_info_1
    
    * genesis_self_check_1 test
    
    * legacy check
    
    * test that dna info is callable in genesis self check
    
    * agent key in genesis self check 2
    
    * fix tests
    
    * changelog

--- crates/test_utils/wasm/wasm_workspace/genesis_self_check_valid/src/integrity.rs
+++ crates/test_utils/wasm/wasm_workspace/genesis_self_check_valid/src/integrity.rs
@@ -2,10 +2,10 @@ use hdi::prelude::*;
 
 #[hdk_extern]
 fn genesis_self_check(data: GenesisSelfCheckData) -> ExternResult<ValidateCallbackResult> {
-    let GenesisSelfCheckData {
-        dna_info: _,
-        membrane_proof: _,
-        agent_key: _,
+    let GenesisSelfCheckDataV2 {
+        membrane_proof: _maybe_membrane_proof,
+        agent_key: _agent_key,
     } = data;
+    let _dna_info: DnaInfoV2 = dna_info()?;
     Ok(ValidateCallbackResult::Valid)
 }
