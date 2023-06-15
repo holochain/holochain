ac345937aa3bf7eb2dbaedc0c92d9c56acab0445
= <=>
2023-06-15 12:54:12 -0600

    Add sanity check

--- crates/holochain/tests/cap_tokens/mod.rs
+++ crates/holochain/tests/cap_tokens/mod.rs
@@ -16,7 +16,6 @@ async fn alice_cant_remote_call_bobs_private_function() {
     use holochain::conductor::api::error::ConductorApiResult;
 
     holochain_trace::test_run().ok();
-    const NUM_AGENTS: usize = 30;
 
     let (dna_file, _, _) = SweetDnaFile::unique_from_test_wasms(vec![TestWasm::CapTokens]).await;
 
@@ -32,23 +31,45 @@ async fn alice_cant_remote_call_bobs_private_function() {
     let alice = cells[0].clone();
     let bob = cells[1].clone();
 
-    let alice_secret: CapSecret = conductor.call(&alice.zome(TestWasm::CapTokens), "create_cap_grant_for_private_function", ()).await;
-
     #[derive(Serialize, Deserialize, Debug, SerializedBytes)]
     pub struct RemoteCallPrivateInput {
         pub to_cell: CellId,
-        pub cap_secret: CapSecret,
+        pub maybe_cap_secret: Option<CapSecret>,
     }    
+
+    // Making a remote call to bobs cell without a secret should fail
+    let remote_call_to_bob: RemoteCallPrivateInput = RemoteCallPrivateInput {
+        to_cell: bob.cell_id().to_owned(),
+        maybe_cap_secret: None
+    };
+
+    let attempted_call_no_secret: ConductorApiResult<String> = conductor.call_fallible(&alice.zome(TestWasm::CapTokens), "remote_call_private_function", remote_call_to_bob ).await;
+
+    match attempted_call_no_secret {
+        Ok(_) => {
+            panic!("calling bobs cell remotely with no secret should fail")
+        },
+        Err(err) => match err {
+            // This is the result we expect
+            holochain::conductor::api::error::ConductorApiError::Other(_boxed_call_error) => {
+                // specifically boxed_call_error should be `CallError("Unauthorized call to private_function".to_string())` but 
+                // I wasn't sure how to assert that
+            },
+            _ => panic!("Unexpected err {}", err),
+        },
+    };
+
+    let alice_secret: CapSecret = conductor.call(&alice.zome(TestWasm::CapTokens), "create_cap_grant_for_private_function", ()).await;
 
     // Making a remote call to bobs cell using a secret alice created should fail
     let remote_call_to_bob: RemoteCallPrivateInput = RemoteCallPrivateInput {
         to_cell: bob.cell_id().to_owned(),
-        cap_secret: alice_secret
+        maybe_cap_secret: Some(alice_secret)
     };
 
-    let attempted_call: ConductorApiResult<String> = conductor.call_fallible(&alice.zome(TestWasm::CapTokens), "remote_call_private_function", remote_call_to_bob ).await;
+    let attempted_call_wrong_secret: ConductorApiResult<String> = conductor.call_fallible(&alice.zome(TestWasm::CapTokens), "remote_call_private_function", remote_call_to_bob ).await;
 
-    match attempted_call {
+    match attempted_call_wrong_secret {
         Ok(_) => {
             panic!("calling bobs cell remotely using alices secret should fail")
         },
--- crates/test_utils/wasm/wasm_workspace/cap_tokens/src/coordinator.rs
+++ crates/test_utils/wasm/wasm_workspace/cap_tokens/src/coordinator.rs
@@ -1,27 +1,5 @@
 use hdk::prelude::*;
 
-pub fn set_cap_tokens() -> ExternResult<()> {
-    let mut functions = BTreeSet::new();
-    functions.insert((zome_info()?.name, "get_cap_grant".into()));
-    functions.insert((
-        zome_info()?.name,
-        "create_cap_grant_for_private_function".into(),
-    ));
-    create_cap_grant(CapGrantEntry {
-        tag: "".into(),
-        access: ().into(),
-        functions: GrantedFunctions::Listed(functions),
-    })?;
-    Ok(())
-}
-
-#[hdk_extern]
-fn init(_: ()) -> ExternResult<InitCallbackResult> {
-    // set_cap_tokens()?;
-
-    Ok(InitCallbackResult::Pass)
-}
-
 #[hdk_extern]
 fn create_cap_grant_for_private_function(_: ()) -> ExternResult<CapSecret> {
     let cap_secret = generate_cap_secret()?;
@@ -29,6 +7,7 @@ fn create_cap_grant_for_private_function(_: ()) -> ExternResult<CapSecret> {
     let mut functions = BTreeSet::new();
     functions.insert((zome_info()?.name, "private_function".into()));
 
+    // If you comment out this call, the test behaves as expected.
     create_cap_grant(CapGrantEntry {
         tag: "".into(),
         access: cap_secret.into(),
@@ -46,22 +25,23 @@ fn private_function(_: ()) -> ExternResult<String> {
 #[derive(Serialize, Deserialize, Debug, SerializedBytes)]
 pub struct RemoteCallPrivateInput {
     pub to_cell: CellId,
-    pub cap_secret: CapSecret,
+    pub maybe_cap_secret: Option<CapSecret>,
 }
 
 #[hdk_extern]
 fn remote_call_private_function(input: RemoteCallPrivateInput) -> ExternResult<String> {
     let zome_name = zome_info()?.name;
+
     let RemoteCallPrivateInput {
         to_cell,
-        cap_secret,
+        maybe_cap_secret,
     } = input;
 
     match hdk::p2p::call_remote(
         to_cell.agent_pubkey().clone(),
         zome_name,
         FunctionName::new("private_function".to_owned()),
-        Some(cap_secret),
+        maybe_cap_secret,
         Some(()),
     )? {
         ZomeCallResponse::Ok(response) => match response.decode() {
