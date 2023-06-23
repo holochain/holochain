7b417bdc76afc510d3de4450d589ae4af85f5356
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-06-23 12:52:59 +0000

    wip locking calls for a permit

--- crates/holochain/src/conductor/conductor.rs
+++ crates/holochain/src/conductor/conductor.rs
@@ -1097,7 +1097,7 @@ mod network_impls {
                 } => {
                     use holochain_sqlite::db::AsP2pAgentStoreConExt;
                     let db = { self.p2p_agents_db(&dna_hash) };
-                    let permit = db.conn_permit().await;
+                    let permit = db.conn_permit().await?;
                     let res = tokio::task::spawn_blocking(move || {
                         let mut conn = db.with_permit(permit)?;
                         conn.p2p_gossip_query_agents(since_ms, until_ms, (*arc_set).clone())
