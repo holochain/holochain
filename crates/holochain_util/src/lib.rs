8aa4500f0a16ba31025edb311356eee9d9038dc1
Michael Dougherty <maackle.d@gmail.com>
2023-08-23 16:07:53 -0700

    Proper tokio flag

--- crates/holochain_util/src/lib.rs
+++ crates/holochain_util/src/lib.rs
@@ -1,6 +1,8 @@
 //! This crate is a collection of various utility functions that are used by the other crates in the holochain repository.
 
 pub mod ffs;
+
+#[cfg(feature = "tokio")]
 pub mod tokio_helper;
 
 #[cfg(feature = "pw")]
