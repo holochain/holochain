1dbe66483c6eff8d179749c83c87f87e3483d820
neonphog <neonphog@gmail.com>
2023-08-29 16:40:28 -0600

    test_search_remotes_covering_basis

---
+++ crates/kitsune_p2p/kitsune_p2p/src/spawn/actor/discover/test_search_remotes_covering_basis.rs
@@ -0,0 +1,95 @@
+use super::*;
+use SearchRemotesCoveringBasisLogicResult::*;
+
+async fn mk_agent_info(u: u8, covers: bool, offline: bool) -> AgentInfoSigned {
+    let arc_len = if covers { u32::MAX } else { 0 };
+
+    let url_list = if offline {
+        vec![]
+    } else {
+        vec!["wss://test".into()]
+    };
+
+    AgentInfoSigned::sign(
+        Arc::new(KitsuneSpace::new(vec![0x11; 32])),
+        Arc::new(KitsuneAgent::new(vec![u; 32])),
+        arc_len,
+        url_list,
+        0,
+        0,
+        |_| async move { Ok(Arc::new(KitsuneSignature(vec![0; 64]))) },
+    )
+    .await
+    .unwrap()
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn happy_path() {
+    let mut logic =
+        SearchRemotesCoveringBasisLogic::new(1, 1, 2, 0.into(), KitsuneTimeout::from_millis(1000));
+
+    assert!(matches!(logic.check_nodes(vec![]), ShouldWait));
+
+    let near = mk_agent_info(1, false, false).await;
+
+    assert!(matches!(logic.check_nodes(vec![near]), QueryPeers(_)));
+
+    let covers = mk_agent_info(2, true, false).await;
+
+    assert!(matches!(logic.check_nodes(vec![covers]), Success(_)));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn timeout() {
+    let mut logic =
+        SearchRemotesCoveringBasisLogic::new(1, 1, 2, 0.into(), KitsuneTimeout::from_millis(1));
+
+    tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+
+    assert!(matches!(logic.check_nodes(vec![]), Error(_)));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn respect_max_covers() {
+    let mut logic =
+        SearchRemotesCoveringBasisLogic::new(1, 1, 2, 0.into(), KitsuneTimeout::from_millis(1000));
+
+    let mut covers = Vec::new();
+    for i in 0..5 {
+        covers.push(mk_agent_info(i, true, false).await);
+    }
+
+    assert!(matches!(
+        logic.check_nodes(covers),
+        Success(results) if results.len() == 2
+    ));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn respect_max_near() {
+    let mut logic =
+        SearchRemotesCoveringBasisLogic::new(1, 1, 2, 0.into(), KitsuneTimeout::from_millis(1000));
+
+    let mut covers = Vec::new();
+    for i in 0..5 {
+        covers.push(mk_agent_info(i, false, false).await);
+    }
+
+    assert!(matches!(
+        logic.check_nodes(covers),
+        QueryPeers(results) if results.len() == 2
+    ));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn ignore_offline_nodes() {
+    let mut logic =
+        SearchRemotesCoveringBasisLogic::new(1, 1, 2, 0.into(), KitsuneTimeout::from_millis(1000));
+
+    let covers_offline = mk_agent_info(2, true, true).await;
+
+    assert!(matches!(
+        logic.check_nodes(vec![covers_offline]),
+        ShouldWait
+    ));
+}
