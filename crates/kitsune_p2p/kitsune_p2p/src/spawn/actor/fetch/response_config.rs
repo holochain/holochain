33ba58a910884903c9c33110f35d5c7bbf4dcb2f
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-08-29 17:49:03 +0100

    Add response config tests

--- crates/kitsune_p2p/kitsune_p2p/src/spawn/actor/fetch/response_config.rs
+++ crates/kitsune_p2p/kitsune_p2p/src/spawn/actor/fetch/response_config.rs
@@ -45,3 +45,86 @@ impl kitsune_p2p_fetch::FetchResponseConfig for FetchResponseConfig {
         });
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::spawn::actor::fetch::FetchResponseConfig as RealFetchResponseConfig;
+    use crate::spawn::meta_net::{MetaNetCon, MetaNetConTest};
+    use kitsune_p2p_fetch::test_utils::test_space;
+    use kitsune_p2p_fetch::{FetchResponseConfig, FetchResponseGuard};
+    use kitsune_p2p_types::bin_types::KitsuneOpData;
+    use kitsune_p2p_types::config::tuning_params_struct::KitsuneP2pTuningParams;
+    use kitsune_p2p_types::dependencies::lair_keystore_api::dependencies::parking_lot::lock_api::RwLock;
+    use std::sync::Arc;
+    use std::time::Duration;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn responds_by_doing_notify() {
+        let config = RealFetchResponseConfig::new(Arc::new(KitsuneP2pTuningParams::default()));
+
+        let (s, _r) = tokio::sync::oneshot::channel();
+
+        let connection_state = Arc::new(RwLock::new(MetaNetConTest::default()));
+        config.respond(
+            test_space(1),
+            (
+                MetaNetCon::Test {
+                    state: connection_state.clone(),
+                },
+                "".to_string(),
+                None,
+            ),
+            FetchResponseGuard::new(s),
+            Arc::new(KitsuneOpData(vec![1, 2, 3])),
+        );
+
+        tokio::time::timeout(Duration::from_millis(100), {
+            let connection_state = connection_state.clone();
+            async move {
+                while connection_state.read().notify_call_count < 1 {
+                    tokio::time::sleep(Duration::from_millis(1)).await;
+                }
+            }
+        })
+        .await
+        .unwrap();
+
+        assert_eq!(1, connection_state.read().notify_call_count);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn handles_error_during_notify() {
+        let config = RealFetchResponseConfig::new(Arc::new(KitsuneP2pTuningParams::default()));
+
+        let (s, _r) = tokio::sync::oneshot::channel();
+
+        let connection_state = Arc::new(RwLock::new(MetaNetConTest::default()));
+        connection_state.write().notify_succeed = false;
+
+        config.respond(
+            test_space(1),
+            (
+                MetaNetCon::Test {
+                    state: connection_state.clone(),
+                },
+                "".to_string(),
+                None,
+            ),
+            FetchResponseGuard::new(s),
+            Arc::new(KitsuneOpData(vec![1, 2, 3])),
+        );
+
+        tokio::time::timeout(Duration::from_millis(100), {
+            let connection_state = connection_state.clone();
+            async move {
+                while connection_state.read().notify_call_count < 1 {
+                    tokio::time::sleep(Duration::from_millis(1)).await;
+                }
+            }
+        })
+        .await
+        .unwrap();
+
+        assert_eq!(1, connection_state.read().notify_call_count);
+    }
+}
