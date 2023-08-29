f20f96d783410f4538c443365ef73dbc1c1c2b54
Stefan Junker <mail@stefanjunker.de>
2023-08-29 16:13:02 +0200

    chore(nix/modules/rust): remove unused apple_sdk

--- nix/modules/rust.nix
+++ nix/modules/rust.nix
@@ -133,15 +133,6 @@
           type = "derivation";
           orig = pkg;
         };
-
-        apple_sdk =
-          lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin
-            (
-              # aarch64 only uses 11.0 and x86_64 mixes them
-              if system == "x86_64-darwin"
-              then pkgs.darwin.apple_sdk_10_12
-              else pkgs.darwin.apple_sdk_11_0
-            );
       };
     };
 }
