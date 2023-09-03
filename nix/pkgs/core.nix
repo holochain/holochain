17b8d990aa7c6d59a6ffce282e6ce1bde8ec9e64
Stefan Junker <mail@stefanjunker.de>
2023-09-03 11:30:11 +0200

    Revert "chore(cargo): use dev profile instead of fast-test"
    This reverts commit 85f228eadd301db9dbbf27cc6e7f8e7102a6482f.

--- nix/pkgs/core.nix
+++ nix/pkgs/core.nix
@@ -25,7 +25,7 @@ rec {
 
     # run all the non-slow cargo tests
     cargo build --features 'build' -p holochain_wasm_test_utils
-    cargo test ''${CARGO_TEST_ARGS:-} --workspace --features slow_tests,glacial_tests,chc,test_utils,build_wasms,sqlite-encrypted --lib --tests --profile dev ''${1-} -- --nocapture
+    cargo test ''${CARGO_TEST_ARGS:-} --workspace --features slow_tests,glacial_tests,chc,test_utils,build_wasms,sqlite-encrypted --lib --tests --profile fast-test ''${1-} -- --nocapture
   '';
 
   hcStandardTestsNextest = writeShellScriptBin "hc-test-standard-nextest" ''
