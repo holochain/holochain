99dbdf98ee4c27880946db2a028192f6bc5f8536
Michael Dougherty <maackle.d@gmail.com>
2023-09-11 12:53:39 -0700

    Bump serde

---
+++ scripts/cargo-update-all.sh
@@ -0,0 +1,3 @@
+cargo update --manifest-path Cargo.toml $@
+cargo update --manifest-path crates/release-automation/Cargo.toml $@
+cargo update --manifest-path crates/test_utils/wasm/wasm_workspace/Cargo.toml $@
