49f2ec131ab97394abab14b7b00c7ca11bafbd62
Stefan Junker <mail@stefanjunker.de>
2023-09-15 21:27:58 +0200

    fixup! feat(script/update-flakes): rewrite to github URL

--- nix/modules/devShells.nix
+++ nix/modules/devShells.nix
@@ -1,8 +1,32 @@
-{ inputs, self, lib, ... }: {
+{ self, lib, ... }: {
   perSystem = { config, self', inputs', pkgs, ... }:
+    let
+      holonixPackages = with self'.packages; [ holochain lair-keystore hc-launch hc-scaffold ];
+      versionsFileText = builtins.concatStringsSep "\n"
+        (
+          builtins.map
+            (package: ''
+              echo ${package.pname} \($(${package}/bin/${package.pname} -V)\): ${package.src.rev or "na"}'')
+            holonixPackages
+        );
+      hn-introspect =
+        pkgs.writeShellScriptBin "hn-introspect" versionsFileText;
+    in
     {
+      packages = {
+        inherit hn-introspect;
+      };
+
       devShells = {
         default = self'.devShells.holonix;
+        holonix = pkgs.mkShell {
+          inputsFrom = [ self'.devShells.rustDev ];
+          packages = holonixPackages ++ [ hn-introspect ];
+          shellHook = ''
+            echo Holochain development shell spawned. Type 'exit' to leave.
+            export PS1='\n\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
+          '';
+        };
 
         holochainBinaries = pkgs.mkShell {
           inputsFrom = [ self'.devShells.rustDev ];
