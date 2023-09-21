5e76e718d1badd64708fbf399d5d2288045ef457
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-09-21 16:23:10 +0000

    Update weekly to 0.3.0-beta-dev.18

--- versions/weekly/flake.nix
+++ versions/weekly/flake.nix
@@ -2,7 +2,7 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.3.0-beta-dev.16";
+        url = "github:holochain/holochain/holochain-0.3.0-beta-dev.18";
         flake = false;
       };
 
