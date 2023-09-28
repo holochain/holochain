c4fa05dc2557594aae262d9e50fd1248f04f2f12
Matt Gabrenya <matt@buildyourweb.app>
2023-09-28 07:50:48 -0700

    revert: accidental deletion of type

--- crates/holochain_integrity_types/src/lib.rs
+++ crates/holochain_integrity_types/src/lib.rs
@@ -49,6 +49,39 @@ pub mod dependencies {
     pub use ::subtle;
 }
 
+/// A utility trait for associating a data enum
+/// with a unit enum that has the same variants.
+pub trait UnitEnum {
+    /// An enum with the same variants as the implementor
+    /// but without any data.
+    type Unit: core::fmt::Debug
+        + Clone
+        + Copy
+        + PartialEq
+        + Eq
+        + PartialOrd
+        + Ord
+        + core::hash::Hash;
+
+    /// Turn this type into it's unit enum.
+    fn to_unit(&self) -> Self::Unit;
+
+    /// Iterate over the unit variants.
+    fn unit_iter() -> Box<dyn Iterator<Item = Self::Unit>>;
+}
+
+/// Needed as a base case for ignoring types.
+impl UnitEnum for () {
+    type Unit = ();
+
+    fn to_unit(&self) -> Self::Unit {}
+
+    fn unit_iter() -> Box<dyn Iterator<Item = Self::Unit>> {
+        Box::new([].into_iter())
+    }
+}
+
+
 /// A full UnitEnum, or just the unit type of that UnitEnum
 #[derive(Clone, Debug)]
 pub enum UnitEnumEither<E: UnitEnum> {
