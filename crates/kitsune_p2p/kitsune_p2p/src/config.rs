82e77209888fa67d400b5e90c82ba22304f41a0f
Matt Gabrenya <matt@buildyourweb.app>
2023-09-28 09:36:38 -0700

    chore: remove unused kitsune_p2p::config variables BOOTSTRAP_SERVICE_DEFAULT and BOOTSTRAP_STATIC_DEV

--- crates/kitsune_p2p/kitsune_p2p/src/config.rs
+++
@@ -1,7 +0,0 @@
-// TODO - FIXME - holochain bootstrap should not be encoded in kitsune
-/// The default production bootstrap service url.
-pub const BOOTSTRAP_SERVICE_DEFAULT: &str = "https://bootstrap-staging.holo.host";
-
-// TODO - FIXME - holochain bootstrap should not be encoded in kitsune
-/// The default development bootstrap service url.
-pub const BOOTSTRAP_SERVICE_DEV: &str = "https://bootstrap-dev.holohost.workers.dev";
