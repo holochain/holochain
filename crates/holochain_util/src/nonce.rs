c7d0f339a8a484a80ec2e1716c899083541e0b3b
Matt Gabrenya <matt@buildyourweb.app>
2023-09-27 19:22:22 -0700

    fix: use imports

--- crates/holochain_util/src/nonce.rs
+++ crates/holochain_util/src/nonce.rs
@@ -1,9 +1,11 @@
 use getrandom;
+use holochain_zome_types::zome_io::Nonce256Bits;
+use holochain_zome_types::Timestamp;
 
 pub fn fresh_nonce(now: Timestamp) -> Result<(Nonce256Bits, Timestamp)> {
-  let mut bytes = [0; 32];
-  getrandom::getrandom(&mut bytes)?;
-  let nonce = Nonce256Bits::from(bytes);
-  let expires: Timestamp = (now + FRESH_NONCE_EXPIRES_AFTER)?;
-  Ok((nonce, expires))
+    let mut bytes = [0; 32];
+    getrandom::getrandom(&mut bytes)?;
+    let nonce = Nonce256Bits::from(bytes);
+    let expires: Timestamp = (now + FRESH_NONCE_EXPIRES_AFTER)?;
+    Ok((nonce, expires))
 }
