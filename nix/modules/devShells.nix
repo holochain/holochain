5bd10ac7c76e2db8e6e780ffd15ecc037c215542
Stefan Junker <mail@stefanjunker.de>
2023-10-03 21:23:57 +0200

    fixup! feat(holonix): add `override` and propagate `holochainOverrides` with example

--- nix/modules/devShells.nix
+++ nix/modules/devShells.nix
@@ -34,7 +34,9 @@
               export PS1='\n\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
             '';
           })
-          { };
+          {
+            holochainOverrides = { };
+          };
 
         holochainBinaries = pkgs.mkShell {
           inputsFrom = [ self'.devShells.rustDev ];
