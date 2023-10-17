98737e1165b80c76014a749faeceefa4b2ac49d3
Michael Dougherty <maackle.d@gmail.com>
2023-10-17 09:52:43 -0700

    WIP

---
+++ crates/hc-sleuth/src/lib.rs
@@ -0,0 +1,250 @@
+use holochain_state::prelude::*;
+use kitsune_p2p::gossip::sharded_gossip::GossipType;
+
+#[cfg(test)]
+mod tests;
+
+pub fn action_report(dbs: &Dbs, hash: ActionHash, expected_status: ItemStatus) -> ActionReport {
+    todo!()
+}
+
+pub struct OpInfo {
+    validation_status: Option<ValidationStatus>,
+    when_integrated: Option<Timestamp>,
+}
+
+fn op_info(txn: &mut Transaction) -> anyhow::Result<Option<OpInfo>>  {
+    let sql = "
+    SELECT 
+        DhtOp.validation_status,
+        DhtOp.when_integrated
+    FROM DhtOp
+    JOIN Action On DhtOp.action_hash = Action.hash
+    WHERE DhtOp.type IN (:create_type, :delete_type, :update_type)
+    AND DhtOp.basis_hash = :action_hash
+    AND DhtOp.when_integrated IS NOT NULL
+    AND DhtOp.validation_status IS NOT NULL
+    ";
+    Ok(txn.prepare(sql)?.query_row((), |row: &Row| {
+        // let action =
+        //     from_blob::<SignedAction>(row.get(row.as_ref().column_index("action_blob")?)?)?;
+        // let op_type = row.get(row.as_ref().column_index("dht_type")?)?;
+        let validation_status = row.get(row.as_ref().column_index("DhtOp.validation_status")?)?;
+        let when_integrated = row.get(row.as_ref().column_index("DhtOp.when_integrated")?)?;
+        Ok(OpInfo {validation_status, when_integrated})
+    }).optional()?)
+}
+
+pub fn op_report(
+    subject: &Dbs,
+    peers: &[Dbs],
+    hash: DhtOpHash,
+    expected_status: ItemStatus,
+) -> anyhow::Result<OpReport> {
+    match expected_status {
+        ItemStatus::Unseen => {
+            subject.
+        }
+        ItemStatus::Exists => todo!(),
+        ItemStatus::Integrated => todo!(),
+    }
+}
+
+pub struct Dbs {
+    pub authored: TestDb<DbKindAuthored>,
+    pub cache: TestDb<DbKindCache>,
+    pub dht: TestDb<DbKindDht>,
+    pub peers: TestDb<DbKindP2pAgents>,
+    pub metrics: TestDb<DbKindP2pMetrics>,
+}
+
+impl Dbs {
+    pub async fn as_authority<R>(&self, f: impl Fn(&mut Transaction) -> anyhow::Result<Option<R>>) -> anyhow::Result<Option<R>> {
+        if let Some(r) = self.authored.read_async(f).await {
+            Ok(Some(r))
+        } else {
+            todo!()
+        }
+    }
+}
+
+#[cfg(feature = "test_utils")]
+impl Dbs {
+    pub fn test() -> Self {
+        Self {
+            authored: test_authored_db(),
+            cache: test_cache_db(),
+            dht: test_dht_db(),
+            peers: test_p2p_agents_db(),
+            metrics: test_p2p_metrics_db(),
+        }
+    }
+}
+
+/// The primary significant states an item can be in from a node's perspective
+pub enum ItemStatus {
+    /// The item has never been encountered by the agent in any form:
+    /// the hash has not been published or gossiped to this node, and this node
+    /// has never attempted to fetch
+    Unseen,
+    /// The item exists either in the cache via a `get`, or is pending integration.
+    /// The significance of this is that it will be available for `must_get_*` calls.
+    Exists,
+    /// The item is fully integrated
+    Integrated,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Step {
+    Integrated(IntegrationStep),
+    Cached(Cached),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum IntegrationStep {
+    Propagated(Propagation),
+    Fetched(Fetch),
+    SysValidated(SysVal),
+    AppValidated(AppVal),
+    Integrated(Integrated),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Cached {
+    timestamp: Timestamp,
+    from: AgentPubKey,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Gossip {
+    timestamp: Timestamp,
+    gossip_type: GossipType,
+    from: AgentPubKey,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Publish {
+    timestamp: Timestamp,
+    from: AgentPubKey,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Propagation {
+    Gossip(Gossip),
+    Publish(Publish),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Blocker<Hash> {
+    Get(GetBlocker),
+    Integration(AuthorityBlocker<Hash>),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum AuthorityBlocker<Hash> {
+    Propagation(PropagationBlocker),
+    Fetch(FetchBlocker),
+    SysVal(SysValBlocker<Hash>),
+    AppVal(AppValBlocker<Hash>),
+    Integration(IntegrationBlocker),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PropagationBlocker {
+    /// Gossip or publishing is not hooked up / the workflows are not running
+    WorkflowNotRunning,
+    // No peers have integrated this item, so nobody can send it to me
+    NoPeersHaveIntegrated,
+    /// There are peers who hold this item but they are not visible to me
+    InaccessablePeers {
+        authorities: Vec<AgentPubKey>,
+    },
+    /// There are agents who hold this item and I know about them, but I have not yet talked to them
+    StillNoPropagation {
+        authorities: Vec<AgentPubKey>,
+    },
+    /// Other
+    Other,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum GossipBlocker {
+    /// Peers holding this item talk to me but they don't send me this op,
+    /// maybe because they think I have it
+    PeersWithholding,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum FetchBlocker {
+    /// I can't connect to the source peer
+    SourceInaccessible,
+    /// The FetchPool is not retrying fetching after failing / the op hash is somehow stuck in the pool
+    FetchPoolMalfunction,
+    /// There is a loop or some degenerate case where other items fill up the fetch pool before my op hash can be processed
+    FetchPoolStarvation,
+    /// Other
+    Other,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum SysValBlocker<Hash> {
+    /// The sys validation workflow is not running properly and validation is not being attempted
+    WorkflowNotRunning,
+    /// Dependencies are missing, and we recursively report on those dependencies
+    MissingDeps(Vec<(Hash, Blocker<Hash>)>),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum AppValBlocker<Hash> {
+    /// The app validation workflow is not running properly and validation is not being attempted
+    WorkflowNotRunning,
+    /// Wasm can't be called for some reason
+    WasmFailure,
+    /// Dependencies are missing, and we recursively report on those dependencies
+    MissingDeps(Vec<(Hash, Blocker<Hash>)>),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum IntegrationBlocker {
+    /// The app validation workflow is not running properly and validation is not being attempted
+    WorkflowNotRunning,
+}
+
+/// Reasons why a `get` might fail
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum GetBlocker {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Fetch {
+    timestamp: Timestamp,
+    from: AgentPubKey,
+    propagation: Propagation,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SysVal {
+    timestamp: Timestamp,
+    outcome: ValidationStatus,
+    fetch: Fetch,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct AppVal {
+    timestamp: Timestamp,
+    sys_validation: SysVal,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Integrated {
+    timestamp: Timestamp,
+    app_validation: AppVal,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ActionReport {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum OpReport {
+    Pass { step: Option<Step> },
+    Fail { step: Option<Step> },
+}
---
+++ crates/hc-sleuth/src/tests.rs
@@ -0,0 +1,28 @@
+use arbitrary::Arbitrary;
+use holochain_state::mutations;
+
+use crate::*;
+
+#[test]
+fn all_integrated() {
+    let mut u = unstructured_noise();
+    let alice = Dbs::test();
+    let bobbo = Dbs::test();
+    let carol = Dbs::test();
+
+    let op = DhtOpHashed::arbitrary(&mut u).unwrap();
+    let op1 = op.clone();
+    let op2 = op.clone();
+
+    bobbo.authored.test_write(move |txn| {
+        mutations::insert_op(txn, &op1).unwrap();
+    });
+    carol.dht.test_write(move |txn| {
+        mutations::insert_op(txn, &op2).unwrap();
+        mutations::set_when_integrated(txn, &op2.hash, Timestamp::now()).unwrap();
+    });
+
+    let report = op_report(&alice, &[bobbo, carol], op.hash, ItemStatus::Integrated).unwrap();
+
+    assert_eq!(report, OpReport::Fail { step: None });
+}
