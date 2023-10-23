8580bc7cdc7dcf555c2c3c6b681b77cd95c26cc5
Michael Dougherty <maackle.d@gmail.com>
2023-10-23 13:25:15 -0700

    Use graph-easy if possible

--- crates/aitia/src/lib.rs
+++ crates/aitia/src/lib.rs
@@ -1,32 +1,41 @@
 // TODO: remove
 #![allow(warnings)]
 
-use std::{collections::HashMap, fmt::Display, hash::Hash};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{Debug, Display},
+    hash::Hash,
+    io::{Read, Write},
+};
 
 use petgraph::{
     data::{Build, FromElements},
     prelude::{DiGraph, DiGraphMap},
 };
 
-// #[macro_use]
-// mod cause;
-// mod fact;
-// pub use cause::*;
-// pub use fact::*;
-
-// #[macro_use]
-// pub(crate) mod report;
-// pub use report::*;
-
-// #[cfg(test)]
-// pub mod test_fact;
-
-// #[derive(Default)]
-// pub struct Context;
-
 pub type Tree<T> = petgraph::graph::DiGraph<Cause<T>, ()>;
 
-pub type Table<T> = HashMap<Cause<T>, Option<Check<T>>>;
+#[derive(derive_more::Deref, derive_more::DerefMut, derive_more::From, derive_more::Into)]
+pub struct Table<T: Fact>(TableMap<T>);
+
+pub type TableMap<T> = HashMap<Cause<T>, Option<Check<T>>>;
+pub type TableMapRef<'a, T> = HashMap<&'a Cause<T>, Option<&'a Check<T>>>;
+
+impl<T: Fact> Default for Table<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl<T: Fact> Debug for Table<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = f.debug_struct("Table");
+        for (k, v) in self.iter() {
+            s.field(&format!("{:?}", k), v);
+        }
+        s.finish()
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Check<T: Fact> {
@@ -41,34 +50,23 @@ impl<T: Fact> Check<T> {
             Check::Fail(cs) => cs.as_slice(),
         }
     }
+
+    pub fn is_pass(&self) -> bool {
+        matches!(self, Check::Pass)
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub enum Cause<T> {
-    Any(Vec<Cause<T>>),
-    Every(Vec<Cause<T>>),
     Fact(T),
-}
-
-impl<F: Fact> Cause<F> {
-    pub fn check(&self, ctx: &F::Context) -> bool {
-        match self {
-            Cause::Any(cs) => cs.iter().any(|c| c.check(ctx)),
-            Cause::Every(cs) => cs.iter().all(|c| c.check(ctx)),
-            Cause::Fact(f) => f.check(ctx),
-        }
-    }
-
-    pub fn table(&self, ctx: &F::Context) -> Table<F> {
-        let mut table = Table::new();
-        traverse(self, ctx, &mut table, 1024);
-        table
-    }
+    Any(Vec<Cause<T>>),
+    // Every(Vec<Cause<T>>),
 }
 
 impl<T: Fact> std::fmt::Debug for Cause<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Cause::Fact(fact) => f.write_str(&fact.explain()),
             Cause::Any(cs) => f.write_fmt(format_args!(
                 "Any({})",
                 cs.into_iter()
@@ -76,89 +74,106 @@ impl<T: Fact> std::fmt::Debug for Cause<T> {
                     .collect::<Vec<_>>()
                     .join(", ")
             )),
-            Cause::Every(cs) => f.write_fmt(format_args!(
-                "Every({})",
-                cs.into_iter()
-                    .map(|c| format!("{:?}", c))
-                    .collect::<Vec<_>>()
-                    .join(", ")
-            )),
-            Cause::Fact(fact) => f.write_str(&fact.explain()),
+            // Cause::Every(cs) => f.write_fmt(format_args!(
+            //     "Every({})",
+            //     cs.into_iter()
+            //         .map(|c| format!("{:?}", c))
+            //         .collect::<Vec<_>>()
+            //         .join(", ")
+            // )),
         }
     }
 }
 
-fn traverse<F: Fact>(
-    current: &Cause<F>,
-    ctx: &F::Context,
-    table: &mut Table<F>,
-    iter: u32,
-) -> Option<Traversal> {
-    dbg!(current);
-    if iter == 0 {
-        panic!("max iters reached");
-    }
+impl<F: Fact> Cause<F> {
+    #[tracing::instrument(skip(ctx, table))]
+    pub fn traverse(&self, ctx: &F::Context, table: &mut Table<F>) -> Option<Check<F>> {
+        tracing::trace!("enter");
+        match table.get(self) {
+            None => {
+                tracing::trace!("marked visited");
+                // Mark this node as visited but undetermined in case the traversal leads to a loop
+                table.insert(self.clone(), None);
+            }
+            Some(None) => {
+                tracing::trace!("loop encountered");
+                // We're currently processing a traversal that started from this cause.
+                // Not even sure if this is even valid, but in any case
+                // we certainly can't say anything about this traversal.
+                return None;
+            }
+            Some(Some(check)) => {
+                tracing::trace!("return cached: {:?}", check);
+                return Some(check.clone());
+            }
+        }
 
-    if table.contains_key(&current) {
-        // Prevent loops
-        return Some(Traversal::Loop);
-    } else {
-        table.insert(current.clone(), None);
-    }
-
-    match &current {
-        Cause::Fact(f) => {
-            if f.check(ctx) {
-                Some(Traversal::Pass)
-            } else {
-                // If the check fails and there is no cause,
-                // then completely throw away this whole branch,
-                // since the branch is not rooted in a passing fact.
-                let cause = f.cause(ctx)?;
-                let t = traverse(&cause, ctx, table, iter - 1)?;
-                if t != Traversal::Pass {
-                    let old = table.insert(current.clone(), Some(Check::Fail(vec![cause])));
-                    assert_eq!(old, Some(None));
+        let check = match self {
+            Cause::Fact(f) => {
+                if f.check(ctx) {
+                    tracing::trace!("fact pass");
+                    Check::Pass
+                } else {
+                    if let Some(cause) = f.cause(ctx) {
+                        tracing::trace!("fact fail with cause, traversing");
+                        let check = cause.traverse(ctx, table)?;
+                        tracing::trace!("traversal done, check: {:?}", check);
+                        Check::Fail(vec![cause])
+                    } else {
+                        tracing::trace!("fact fail with no cause, terminating");
+                        return None;
+                    }
                 }
-                Some(Traversal::Fail)
             }
-        }
-        Cause::Any(cs) => {
-            // XXX: the traversal could be short-circuited if any pass
-            let ts: Vec<_> = cs
-                .iter()
-                .filter_map(|c| traverse(c, ctx, table, iter - 1))
-                .collect();
-            dbg!(cs);
-            if ts.is_empty() {
-                None
-            } else if ts.iter().any(|t| *t == Traversal::Pass) {
-                Some(Traversal::Pass)
-            } else {
-                table.insert(current.clone(), Some(Check::Fail(cs.to_vec())));
-                Some(Traversal::Fail)
+            Cause::Any(cs) => {
+                let checks: Vec<_> = cs
+                    .iter()
+                    .filter_map(|c| Some((c.clone(), c.traverse(ctx, table)?)))
+                    .collect();
+                tracing::trace!("Any. checks: {:?}", checks);
+                if checks.is_empty() {
+                    // All loops
+                    tracing::debug!("All loops");
+                    return None;
+                }
+                let num_checks = checks.len();
+                let fails: Vec<_> = checks
+                    .into_iter()
+                    .filter_map(|(cause, check)| (!check.is_pass()).then_some(cause))
+                    .collect();
+                tracing::trace!("Any. fails: {:?}", fails);
+                if fails.len() < num_checks {
+                    Check::Pass
+                } else {
+                    Check::Fail(fails)
+                }
             }
-        }
-        Cause::Every(cs) => {
-            unimplemented!();
-            // let ts: Vec<_> = cs
-            //     .iter()
-            //     .filter_map(|c| Some((c, traverse(c, ctx, table, iter - 1)?)))
-            //     .collect();
-            // if ts.is_empty() {
-            //     None
-            // } else if ts.iter().all(|(_, t)| *t == Traversal::Pass) {
-            //     Some(Traversal::Pass)
-            // } else {
-            //     let cs = ts
-            //         .into_iter()
-            //         .filter_map(|(c, t)| (t == Traversal::Fail).then_some(c.clone()))
-            //         .collect();
-            //     table.insert(current.clone(), cs);
-            //     Some(Traversal::Fail)
-            // }
-        }
+        };
+        table.insert(self.clone(), Some(check.clone()));
+        tracing::trace!("exit. check: {:?}", check);
+        Some(check)
     }
+
+    pub fn table(&self, ctx: &F::Context) -> Table<F> {
+        let mut table = Table::default();
+        self.traverse(ctx, &mut table);
+        table
+    }
+}
+
+pub fn prune<'a, 'b: 'a, T: Fact + Eq + Hash>(
+    table: &'a TableMap<T>,
+    start: &'b Cause<T>,
+) -> HashMap<&'a Cause<T>, &'a [Cause<T>]> {
+    let mut sub = HashMap::<&Cause<T>, &[Cause<T>]>::new();
+    let mut to_add = vec![start];
+
+    while let Some(next) = to_add.pop() {
+        let causes = table[&next].as_ref().map(|c| c.causes()).unwrap_or(&[]);
+        to_add.extend(causes.iter());
+        sub.insert(next, causes);
+    }
+    sub
 }
 
 pub fn graph<'a, 'b: 'a, T: Fact + Eq + Hash>(
@@ -167,16 +182,7 @@ pub fn graph<'a, 'b: 'a, T: Fact + Eq + Hash>(
 ) -> DiGraph<&'a Cause<T>, ()> {
     let mut g = DiGraph::new();
 
-    let mut sub = HashMap::<&Cause<T>, &[Cause<T>]>::new();
-    let mut to_add = vec![start];
-
-    while let Some(next) = to_add.pop() {
-        let causes = table[&next].as_ref().map(|c| c.causes()).unwrap_or(&[]);
-        // if let Some(causes) = table[&next].as_ref().map(|c| c.causes()) {
-        to_add.extend(causes.iter());
-        sub.insert(next, causes);
-        // }
-    }
+    let sub = prune(&**table, start);
 
     let rows: Vec<_> = sub.iter().collect();
     let mut nodemap = HashMap::new();
@@ -199,7 +205,6 @@ pub fn graph<'a, 'b: 'a, T: Fact + Eq + Hash>(
 enum Traversal {
     Pass,
     Fail,
-    Loop,
 }
 
 pub trait Fact: Sized + Clone + Eq + std::fmt::Debug + std::hash::Hash {
@@ -210,10 +215,23 @@ pub trait Fact: Sized + Clone + Eq + std::fmt::Debug + std::hash::Hash {
     fn explain(&self) -> String;
 }
 
+pub fn graph_easy(dot: &str) -> anyhow::Result<String> {
+    let process = std::process::Command::new("graph-easy")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()?;
+
+    process.stdin.unwrap().write_all(dot.as_bytes()).unwrap();
+    let mut s = String::new();
+    process.stdout.unwrap().read_to_string(&mut s).unwrap();
+
+    Ok(s)
+}
+
 #[cfg(test)]
 mod tests {
 
-    use std::sync::Arc;
+    use std::{process::Stdio, sync::Arc};
 
     use super::*;
 
@@ -287,20 +305,22 @@ mod tests {
 
     #[test]
     fn one() {
+        holochain_trace::test_run().ok().unwrap();
+
         let fatma_store = Cause::Fact(Step {
             which: false,
             stage: Stage::Store,
         });
 
         let checks: StepValues = Box::new(|step: &Step| match (step.which, step.stage) {
-            // (true, Stage::Create) => true,
+            (true, Stage::Create) => true,
             // (false, Stage::Create) => true,
 
             // this leads to Trudy Create in the graph, which is wrong
             // TODO: revisit pruning branches that terminate with false, including loops
             //       basically we only want to create edges on the way back up from either
             //       a None, or a Loop detection
-            (true, Stage::ReceiveA) => true,
+            // (true, Stage::ReceiveA) => true,
 
             // TODO: if all are false, then that doesn't work either
             _ => false,
@@ -318,12 +338,23 @@ mod tests {
             // (false, Stage::SendB) => todo!(),
         });
 
-        let t = fatma_store.table(&checks);
-        let g = graph(&t, &fatma_store);
+        let table = fatma_store.table(&checks);
+        let sub = prune(&table, &fatma_store);
 
-        println!(
+        println!("TABLE\n{:#?}", table);
+        println!("SUBTABLE\n{:#?}", sub);
+
+        let g = graph(&table, &fatma_store);
+
+        let dot = format!(
             "{:?}",
-            petgraph::dot::Dot::with_config(&g, &[petgraph::dot::Config::EdgeNoLabel])
-        )
+            petgraph::dot::Dot::with_config(&g, &[petgraph::dot::Config::EdgeNoLabel],)
+        );
+
+        if let Ok(graph) = graph_easy(&dot) {
+            println!("`graph-easy` output:\n{}", graph);
+        } else {
+            println!("`graph-easy` not installed. Original dot output: {}", dot);
+        }
     }
 }
