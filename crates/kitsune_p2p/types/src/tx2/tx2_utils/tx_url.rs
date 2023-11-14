cd9c6aae0fe651cdf65169e294126bc98be44e98
Michael Dougherty <maackle.d@gmail.com>
2023-11-14 12:46:55 -0800

    Fix fuzzing impls

--- crates/kitsune_p2p/types/src/tx2/tx2_utils/tx_url.rs
+++ crates/kitsune_p2p/types/src/tx2/tx2_utils/tx_url.rs
@@ -97,7 +97,9 @@ impl TryFrom<&str> for TxUrl {
 #[cfg(feature = "fuzzing")]
 impl<'a> arbitrary::Arbitrary<'a> for TxUrl {
     fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
-        String::arbitrary(u).map(Into::into)
+        // XXX: not the best implementation, it doesn't exercise other protocols
+        let s = String::arbitrary(u)?;
+        Ok(TxUrl::from_str_panicking(&format!("http://{s}")))
     }
 }
 
@@ -109,9 +111,10 @@ impl proptest::arbitrary::Arbitrary for TxUrl {
     fn arbitrary_with((): Self::Parameters) -> Self::Strategy {
         use proptest::strategy::Strategy;
 
+        // XXX: not the best implementation, it doesn't exercise other protocols
         proptest::string::string_regex(r"http://\w+")
             .unwrap()
-            .prop_map(TxUrl::from)
+            .prop_map(|s| TxUrl::from_str_panicking(&s))
             .boxed()
     }
 }
