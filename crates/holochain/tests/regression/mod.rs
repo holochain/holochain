1a7fdac761650bef7710a4ad802d07c2ce0abe64
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-12-06 20:38:53 +0000

    Review changes

--- crates/holochain/tests/regression/mod.rs
+++ crates/holochain/tests/regression/mod.rs
@@ -23,7 +23,7 @@ async fn zome_with_no_entry_types_does_not_prevent_deletes() {
     let created: ActionHash = conductor.call(&cell.zome(TestWasm::Crd.coordinator_zome_name()), "create", ()).await;
 
     let _: ActionHash = conductor
-        .call(&cell.zome("crd"), "delete_via_hash", created)
+        .call(&cell.zome(TestWasm::Crd.coordinator_zome_name()), "delete_via_hash", created)
         .await;
 }
 
@@ -47,9 +47,9 @@ async fn zome_with_no_link_types_does_not_prevent_delete_links() {
         .unwrap()
         .into_tuple();
 
-    let created: ActionHash = conductor.call(&cell.zome("link"), "create_link", ()).await;
+    let created: ActionHash = conductor.call(&cell.zome(TestWasm::Link.coordinator_zome_name()), "create_link", ()).await;
 
     let _: ActionHash = conductor
-        .call(&cell.zome("link"), "delete_link", created)
+        .call(&cell.zome(TestWasm::Link.coordinator_zome_name()), "delete_link", created)
         .await;
 }
