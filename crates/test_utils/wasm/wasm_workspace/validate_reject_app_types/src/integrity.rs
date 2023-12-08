fa80c8465449750d2df98777506e82c9fe34608a
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-12-08 11:42:05 +0000

    Hide Timestamp::now() from hdk (#3116)
    * WIP hide Timestamp::now in hdk
    
    * Fix availability
    
    * Tidy up
    
    * Allow feature when optional dep enabled
    
    * Remove testing change

--- crates/test_utils/wasm/wasm_workspace/validate_reject_app_types/src/integrity.rs
+++ crates/test_utils/wasm/wasm_workspace/validate_reject_app_types/src/integrity.rs
@@ -5,13 +5,9 @@ fn validate(op: Op) -> ExternResult<ValidateCallbackResult> {
     match op.flattened::<(), ()>()? {
         FlatOp::StoreRecord(store_record) => match store_record {
             OpRecord::DeleteEntry {
-                original_action_hash,
-                action,
                 ..
             } => Ok(ValidateCallbackResult::Invalid("This zome does not define any entry types".to_string())),
             OpRecord::DeleteLink {
-                original_action_hash,
-                action,
                 ..
             } => Ok(ValidateCallbackResult::Invalid("This zome does not define any link types".to_string())),
             _ => Ok(ValidateCallbackResult::Valid),
