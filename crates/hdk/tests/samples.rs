824d95897eb1386e3157b480820f5a424a0bb571
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-12-08 11:08:16 +0000

    Skip if fixtures enabled...

--- crates/hdk/tests/samples.rs
+++ crates/hdk/tests/samples.rs
@@ -1,4 +1,5 @@
 #[test]
+#[cfg_attr(feature = "fixturators", ignore)] // Turns on `full-dna-def` in holochain_zome_types which turns on `kitsune_p2p_timestamp/now`.
 fn samples() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/samples/*.rs");
