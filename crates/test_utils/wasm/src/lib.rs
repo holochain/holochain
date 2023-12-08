e84bd1ff72f12dad12a6bc97d103a095ee9a1451
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-12-08 15:33:04 +0000

    [BACKPORT 0.2] theta bugfix batch (#3121)
    * Fix clone only running genesis (#3094)
    
    * Fix clone only running genesis
    
    * Format
    
    * Reject unknown fields in DNA manfests (#3105)
    
    * Reject unknown fields in DNA manfests
    
    * Format tests
    
    * Update crates/holochain_types/CHANGELOG.md
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Filter validation callbacks (#3107)
    
    * Start test
    
    * Make the test fail like it should
    
    * Tests and a fix
    
    * Format
    
    * update reference to issue
    
    * Update crates/holochain/tests/regression/mod.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Review changes
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Hide Timestamp::now() from hdk (#3116)
    
    * WIP hide Timestamp::now in hdk
    
    * Fix availability
    
    * Tidy up
    
    * Allow feature when optional dep enabled
    
    * Remove testing change
    
    * Remove changelog entry for Holochain that wasn't backported
    
    * Fixup imports
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>

--- crates/test_utils/wasm/src/lib.rs
+++ crates/test_utils/wasm/src/lib.rs
@@ -37,6 +37,7 @@ pub enum TestWasm {
     GenesisSelfCheckValidV1,
     GenesisSelfCheckInvalid,
     GenesisSelfCheckValid,
+    GenesisSelfCheckRequiresProperties,
     HashPath,
     HdkExtern,
     InitFail,
@@ -63,6 +64,7 @@ pub enum TestWasm {
     ValidateCreateLinkInvalid,
     ValidateValid,
     ValidateCreateLinkValid,
+    ValidateRejectAppTypes,
     WhoAmI,
     ZomeInfo,
 }
@@ -136,6 +138,9 @@ impl From<TestWasm> for ZomeName {
             TestWasm::GenesisSelfCheckValidV1 => "genesis_self_check_1",
             TestWasm::GenesisSelfCheckInvalid => "genesis_self_check_invalid",
             TestWasm::GenesisSelfCheckValid => "genesis_self_check_valid",
+            TestWasm::GenesisSelfCheckRequiresProperties => {
+                "genesis_self_check_requires_properties"
+            }
             TestWasm::HashPath => "hash_path",
             TestWasm::HdkExtern => "hdk_extern",
             TestWasm::InitFail => "init_fail",
@@ -162,6 +167,7 @@ impl From<TestWasm> for ZomeName {
             TestWasm::ValidateCreateLinkInvalid => "validate_link_add_invalid",
             TestWasm::ValidateValid => "validate_valid",
             TestWasm::ValidateCreateLinkValid => "validate_link_add_valid",
+            TestWasm::ValidateRejectAppTypes => "validate_reject_app_types",
             TestWasm::WhoAmI => "whoami",
             TestWasm::ZomeInfo => "zome_info",
         })
@@ -212,6 +218,9 @@ impl From<TestWasm> for PathBuf {
             TestWasm::GenesisSelfCheckValid => {
                 "wasm32-unknown-unknown/release/test_wasm_genesis_self_check_valid.wasm"
             }
+            TestWasm::GenesisSelfCheckRequiresProperties => {
+                "wasm32-unknown-unknown/release/test_wasm_genesis_self_check_requires_properties.wasm"
+            }
             TestWasm::HashPath => "wasm32-unknown-unknown/release/test_wasm_hash_path.wasm",
             TestWasm::HdkExtern => "wasm32-unknown-unknown/release/test_wasm_hdk_extern.wasm",
             TestWasm::InitFail => "wasm32-unknown-unknown/release/test_wasm_init_fail.wasm",
@@ -261,6 +270,9 @@ impl From<TestWasm> for PathBuf {
             }
             TestWasm::ValidateCreateLinkValid => {
                 "wasm32-unknown-unknown/release/test_wasm_validate_link_add_valid.wasm"
+            }
+            TestWasm::ValidateRejectAppTypes => {
+                "wasm32-unknown-unknown/release/test_wasm_validate_reject_app_types.wasm"
             }
             TestWasm::WhoAmI => "wasm32-unknown-unknown/release/test_wasm_whoami.wasm",
             TestWasm::ZomeInfo => "wasm32-unknown-unknown/release/test_wasm_zome_info.wasm",
