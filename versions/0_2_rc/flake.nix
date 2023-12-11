f3af7085dce497dfe881d22c1d7fbe9fbe86e0c8
ThetaSinner <ThetaSinner@users.noreply.github.com>
2023-12-11 18:58:24 +0000

    Bump 0.2-rc version to 0.2.4-rc.0

--- versions/0_2_rc/flake.nix
+++ versions/0_2_rc/flake.nix
@@ -2,7 +2,7 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.2.3-rc.1";
+        url = "github:holochain/holochain/holochain-0.2.4-rc.0";
         flake = false;
       };
 
