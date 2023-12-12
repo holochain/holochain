1dac136eaf91f60089c2b8d357bb4d2e247f0e28
Michael Dougherty <maackle.d@gmail.com>
2023-12-12 14:07:24 -0800

    Some changes

--- crates/holochain/src/conductor/conductor.rs
+++ crates/holochain/src/conductor/conductor.rs
@@ -1307,7 +1307,6 @@ mod app_impls {
     use super::*;
 
     impl Conductor {
-        #[cfg(feature = "test_utils")]
         // FIXME: can rewrite now in terms of `get_install_app_payload_from_dnas`
         pub(crate) async fn install_app_legacy(
             self: Arc<Self>,
