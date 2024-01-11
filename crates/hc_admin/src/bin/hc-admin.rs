572e5aec30bfd45fc3f53351b0817501e6803bfc
Michael Dougherty <maackle.d@gmail.com>
2024-01-10 17:14:47 -0800

    Revert "New crate hc_admin"
    This reverts commit a4f83216a0b47e4e6b3335b54635d391fe0099c3.

--- crates/hc_admin/src/bin/hc-admin.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-use holochain_cli_admin::HcAdmin;
-
-/// Main `hc-admin` executable entrypoint.
-#[tokio::main]
-pub async fn main() -> anyhow::Result<()> {
-    HcAdmin::parse().run().await
-}
--- crates/hc_admin/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod cli;
-mod error;
-
-pub use cli::HcAdmin;
