3612671eaa6900eb420354a293f105e8070330f5
Jost Schulte <jost.schulte@protonmail.com>
2024-01-15 12:43:40 -0600

    docs: deprecate wasmer_types

--- crates/holochain_types/src/wasmer_types.rs
+++ crates/holochain_types/src/wasmer_types.rs
@@ -1,5 +1,7 @@
 //! These types manage the consumption and configuration of the `wasmer` library
 //! in terms of middleware (metering), Target, Module, and Store.
+//!
+//! Note that this module is deprecated. Use https://github.com/holochain/holochain-wasmer/blob/develop/crates/host/src/module.rs#L135-L185 instead.
 
 use std::str::FromStr;
 use std::sync::Arc;
@@ -17,10 +19,12 @@ pub const WASM_METERING_LIMIT: u64 = 100_000_000_000;
 #[cfg(test)]
 /// ten mega ops.
 /// We don't want tests to run forever, and it can take several minutes for 100 giga ops to run.
+#[deprecated]
 pub const WASM_METERING_LIMIT: u64 = 10_000_000;
 
 /// Generate a Cranelift type (1 of 3 possible types) wasm compiler
 /// with Metering (use limits) in place.
+#[deprecated]
 pub fn cranelift() -> Cranelift {
     let cost_function = |_operator: &wasmparser::Operator| -> u64 { 1 };
     // @todo 100 giga-ops is totally arbitrary cutoff so we probably
@@ -32,6 +36,7 @@ pub fn cranelift() -> Cranelift {
 }
 
 /// Configuration of a Target for wasmer for iOS
+#[deprecated]
 pub fn wasmer_ios_target() -> Target {
     // use what I see in
     // platform ios headless example
@@ -42,6 +47,7 @@ pub fn wasmer_ios_target() -> Target {
 }
 
 /// Take WASM binary and prepare a wasmer Module suitable for iOS
+#[deprecated]
 pub fn build_ios_module(wasm: &[u8]) -> Result<Module, CompileError> {
     info!(
         "Found wasm and was instructed to serialize it for ios in wasmer format, doing so now..."
@@ -54,6 +60,7 @@ pub fn build_ios_module(wasm: &[u8]) -> Result<Module, CompileError> {
 
 /// Generate a headless Dylib Store suitable for iOS.
 /// Useful for re-building an iOS Module from a preserialized WASM Module.
+#[deprecated]
 pub fn ios_dylib_headless_store() -> Store {
     let engine = Engine::headless();
     Store::new(engine)
