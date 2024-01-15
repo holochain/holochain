3cb1c896bf6fb2b925ccf0bb2f10011125c8cb07
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-01-15 15:12:43 +0000

    [BACKPORT 0.2] Thetasinner Jan 2024 backports (#3215)
    * More sys validation tests (#3130)
    
    * Add tests
    
    * Assert actual error messages + correct one test
    
    * Add more tests
    
    * register deleted by tests
    
    * deleted entry action tests
    
    * Delete link tests
    
    * Fix check for delete link add address
    
    * Format
    
    * Update changelog
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow/validate_op_tests.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow/validate_op_tests.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Improve error assertions
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Reduce log level for queue consumer (#3146)
    
    * Reduce log level for queue consumer
    
    * Up to debug
    
    * Improve conductor output on IO errors (#3147)
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Sys validation workflow docs (#3124)
    
    * Docs for the sys validation workflow
    
    * More docs
    
    * Format docs
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Update crates/holochain/src/core/workflow/sys_validation_workflow.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Review changes
    
    * Add docs for delete link
    
    * Fix tests broken or made flaky by avp check
    
    * Stop checking previous action for store record or entry
    
    * Changelog for prev action change
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Eliminate txn open when hitting p2p agent cache (#3181)
    
    * Eliminate txn open when hitting p2p agent cache
    
    * Format
    
    * Fix imports
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>

--- crates/holochain/src/core/sys_validate.rs
+++ crates/holochain/src/core/sys_validate.rs
@@ -236,7 +236,14 @@ pub fn check_agent_validation_pkg_predecessor(
     let maybe_error = match (prev_action, action) {
         (
             Action::AgentValidationPkg(AgentValidationPkg { .. }),
-            Action::Create(Create { .. }) | Action::Update(Update { .. }),
+            Action::Create(Create {
+                entry_type: EntryType::AgentPubKey,
+                ..
+            })
+            | Action::Update(Update {
+                entry_type: EntryType::AgentPubKey,
+                ..
+            }),
         ) => None,
         (Action::AgentValidationPkg(AgentValidationPkg { .. }), _) => Some(
             "Every AgentValidationPkg must be followed by a Create or Update for an AgentPubKey",
