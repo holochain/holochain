d1f20b6dbbda9cc9ff674dc09baf1f136becc551
Michael Dougherty <maackle.d@gmail.com>
2024-01-17 10:46:10 -0800

    Oops

--- crates/holochain/src/conductor/state.rs
+++ crates/holochain/src/conductor/state.rs
@@ -38,7 +38,7 @@ pub struct ConductorState {
     #[serde(default)]
     installed_apps: InstalledAppMap,
     /// List of interfaces any UI can use to access zome functions.
-    #[serde(default, skip)]
+    #[serde(default)]
     pub(crate) app_interfaces: HashMap<AppInterfaceId, AppInterfaceConfig>,
 }
 
