68f7781443e4b4fc9ee6da793928b7d368bddee3
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-01-17 19:10:10 +0000

    Clippy

--- crates/holochain_sqlite/src/db/access.rs
+++ crates/holochain_sqlite/src/db/access.rs
@@ -409,7 +409,7 @@ impl<Kind: DbKindT + Send + Sync + 'static> DbWrite<Kind> {
         path: &Path,
         sync_level: DbSyncLevel,
     ) -> rusqlite::Result<Option<PathBuf>> {
-        Connection::open(&path)
+        Connection::open(path)
             // For some reason calling pragma_update is necessary to prove the database file is valid.
             .and_then(|mut c| {
                 initialize_connection(&mut c, sync_level)?;
