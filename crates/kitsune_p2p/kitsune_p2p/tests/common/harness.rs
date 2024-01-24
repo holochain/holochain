5e1417b79cb91b31702cd0ba81d6424df2bc31c1
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-01-24 13:33:49 +0000

    More kitsune tests (#3233)
    * Add more tests for Kitsune
    
    * Fix tests
    
    * Format and tidy log messaging
    
    * Tidy up

--- crates/kitsune_p2p/kitsune_p2p/tests/common/harness.rs
+++ crates/kitsune_p2p/kitsune_p2p/tests/common/harness.rs
@@ -1,23 +1,49 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
+use super::{test_keystore, RecordedKitsuneP2pEvent, TestHost, TestHostOp, TestLegacyHost};
 use kitsune_p2p::{
     actor::KitsuneP2p, event::KitsuneP2pEventReceiver, spawn_kitsune_p2p, HostApi, KitsuneP2pResult,
 };
-use kitsune_p2p_types::{config::KitsuneP2pConfig, tls::TlsConfig};
+use kitsune_p2p_types::{
+    agent_info::AgentInfoSigned,
+    config::{tuning_params_struct, KitsuneP2pConfig},
+    tls::TlsConfig,
+    KAgent,
+};
+use parking_lot::RwLock;
 use tokio::task::AbortHandle;
 
 pub struct KitsuneTestHarness {
+    name: String,
     config: KitsuneP2pConfig,
     tls_config: kitsune_p2p_types::tls::TlsConfig,
     host_api: HostApi,
+    legacy_host_api: TestLegacyHost,
+    agent_store: Arc<RwLock<Vec<AgentInfoSigned>>>,
+    op_store: Arc<RwLock<Vec<TestHostOp>>>,
 }
 
 impl KitsuneTestHarness {
-    pub async fn try_new(host_api: HostApi) -> KitsuneP2pResult<Self> {
+    pub async fn try_new(name: &str) -> KitsuneP2pResult<Self> {
+        let keystore = test_keystore();
+        let agent_store = Arc::new(RwLock::new(Vec::new()));
+        let op_store = Arc::new(RwLock::new(Vec::new()));
+
+        // Unpack the keystore, since we need to pass it to the host_api
+        let keystore = Arc::try_unwrap(keystore).unwrap().into_inner();
+
+        let host_api =
+            Arc::new(TestHost::new(keystore.clone(), agent_store.clone(), op_store.clone()).await);
+        let legacy_host_api = TestLegacyHost::new(keystore);
+
         Ok(Self {
+            name: name.to_string(),
             config: Default::default(),
             tls_config: TlsConfig::new_ephemeral().await?,
             host_api,
+            legacy_host_api,
+            agent_store,
+            op_store,
         })
     }
 
@@ -37,18 +63,65 @@ impl KitsuneTestHarness {
         self
     }
 
-    pub async fn spawn(
+    pub fn update_tuning_params(
+        mut self,
+        f: impl Fn(
+            tuning_params_struct::KitsuneP2pTuningParams,
+        ) -> tuning_params_struct::KitsuneP2pTuningParams,
+    ) -> Self {
+        let new_config: KitsuneP2pConfig = self.config.tune(f);
+        self.config = new_config;
+        self
+    }
+
+    pub async fn spawn(&mut self) -> KitsuneP2pResult<ghost_actor::GhostSender<KitsuneP2p>> {
+        let (sender, receiver) = self.spawn_without_legacy_host(self.name.clone()).await?;
+
+        self.start_legacy_host(vec![receiver]).await;
+
+        Ok(sender)
+    }
+
+    pub async fn spawn_without_legacy_host(
         &mut self,
+        name: String,
     ) -> KitsuneP2pResult<(
         ghost_actor::GhostSender<KitsuneP2p>,
         KitsuneP2pEventReceiver,
     )> {
-        spawn_kitsune_p2p(
-            self.config.clone(),
-            self.tls_config.clone(),
-            self.host_api.clone(),
-        )
-        .await
+        let mut config = self.config.clone();
+        config.tracing_scope = Some(name);
+
+        let (sender, receiver) =
+            spawn_kitsune_p2p(config, self.tls_config.clone(), self.host_api.clone()).await?;
+
+        Ok((sender, receiver))
+    }
+
+    pub async fn start_legacy_host(&mut self, receivers: Vec<KitsuneP2pEventReceiver>) {
+        self.legacy_host_api
+            .start(self.agent_store.clone(), self.op_store.clone(), receivers)
+            .await;
+    }
+
+    pub async fn create_agent(&mut self) -> KAgent {
+        self.legacy_host_api.create_agent().await
+    }
+
+    pub fn agent_store(&self) -> Arc<parking_lot::RwLock<Vec<AgentInfoSigned>>> {
+        self.agent_store.clone()
+    }
+
+    pub fn op_store(&self) -> Arc<parking_lot::RwLock<Vec<TestHostOp>>> {
+        self.op_store.clone()
+    }
+
+    pub async fn drain_legacy_host_events(&mut self) -> Vec<RecordedKitsuneP2pEvent> {
+        self.legacy_host_api.drain_events().await
+    }
+
+    pub fn duplicate_ops_received_count(&self) -> u32 {
+        self.legacy_host_api.duplicate_ops_received_count()
     }
 }
 
