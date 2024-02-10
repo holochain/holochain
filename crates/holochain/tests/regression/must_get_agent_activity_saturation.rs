c35b598531e817f949cc01b196783d43138077af
Jost Schulte <jost.schulte@protonmail.com>
2024-02-09 19:18:22 -0600

    does this fix it?

--- crates/holochain/tests/regression/must_get_agent_activity_saturation.rs
+++ crates/holochain/tests/regression/must_get_agent_activity_saturation.rs
@@ -40,7 +40,7 @@ async fn must_get_agent_activity_saturation() {
     }
 
     // let conductors catch up
-    tokio::time::sleep(Duration::from_secs(60)).await;
+    tokio::time::sleep(Duration::from_secs(100)).await;
 
     let record: Option<Record> = conductors[1]
         .call(
