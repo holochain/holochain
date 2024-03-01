8a50b543fadb9104ad023cb3d2673b05d482f679
Michael Dougherty <maackle.d@gmail.com>
2024-02-29 17:50:57 -0800

    Fix duplicate wasm imports

--- crates/hc_demo_cli/src/lib.rs
+++ crates/hc_demo_cli/src/lib.rs
@@ -4,18 +4,17 @@
 //! on downstream projects such as launcher, dev hub, or the app store.
 //! Run `hc demo-cli help` to get started.
 
-mod coordinator_wasm;
-mod demo;
-mod integrity_wasm;
-
 cfg_if::cfg_if! {
     if #[cfg(feature = "build_demo")] {
+        mod demo;
         pub use demo::*;
         pub const BUILD_MODE: &str = "build_demo";
     } else if #[cfg(feature = "build_integrity_wasm")] {
+        mod integrity_wasm;
         pub use integrity_wasm::*;
         pub const BUILD_MODE: &str = "build_integrity_wasm";
     } else if #[cfg(feature = "build_coordinator_wasm")] {
+        mod coordinator_wasm;
         pub use coordinator_wasm::*;
         pub const BUILD_MODE: &str = "build_coordinator_wasm";
     }
