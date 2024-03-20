bff572f018e73c243dadc0393b8dc03b11cc806b
Michael Dougherty <maackle.d@gmail.com>
2024-03-20 15:15:17 -0700

    Arq -> Arc

--- crates/hc_cli_util/src/lib.rs
+++ crates/hc_cli_util/src/lib.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
-use kitsune_p2p_dht::{arq::ArqSet, spacetime::Topology, ArqBounds, ArqStrat};
-use kitsune_p2p_dht_arc::{DhtArcRange, DhtArcSet};
+use kitsune_p2p_dht::{arq::ArqSet, spacetime::Topology, Arq, ArqBounds, ArqStrat};
+use kitsune_p2p_dht_arc::{DhtArc, DhtArcRange, DhtArcSet};
 
 /// Utility functions for holochain debugging and diagnostics.
 #[derive(Debug, Parser)]
@@ -13,7 +13,17 @@ pub struct Util {
 
 #[derive(Debug, Subcommand)]
 pub enum UtilSubCommand {
-    Arq { values: Vec<u32> },
+    Arq {
+        values: Vec<u32>,
+
+        /// Calculate the approximate arq
+        #[arg(long, short, group = "precision")]
+        approx: bool,
+
+        /// Only do exact calculations, no rounding or approximation
+        #[arg(long, short, group = "precision")]
+        exact: bool,
+    },
 }
 
 impl Util {
@@ -21,21 +31,35 @@ impl Util {
         let topo = Topology::standard_epoch_full();
         let strat = ArqStrat::default();
         match self.subcommand {
-            UtilSubCommand::Arq { values } => {
+            UtilSubCommand::Arq {
+                values,
+                approx,
+                exact,
+            } => {
                 if values.len() == 2 {
                     let start = values[0];
                     let end = values[1];
-                    let arc = DhtArcRange::from_bounds(start, end);
-                    let arcset = DhtArcSet::from_interval(arc);
-                    if let Some(arqset) = ArqSet::from_dht_arc_set_exact(&topo, &strat, &arcset) {
-                        println!("{:?}", arqset);
+                    let arc = DhtArc::from_bounds(start, end);
+                    let arq = if approx {
+                        Some(Arq::from_dht_arc_approximate(&topo, &strat, &arc))
+                    } else if exact {
+                        todo!()
+                    } else {
+                        panic!("Must set exactly one of --approx or --exact")
+                    };
+                    if let Some(arq) = arq {
+                        println!("{:?}", arq);
                     } else {
                         println!("No can do.");
                     }
                 } else if values.len() == 3 {
-                    let power = values[0];
+                    let power = values[0] as u8;
                     let start = values[1];
                     let count = values[2];
+
+                    let arq = ArqBounds::new(power, start.into(), count.into());
+                    let arc = arq.to_dht_arc_range(&topo);
+                    println!("{:?}", arc);
                 } else {
                     panic!("Usage: hc util arq START END  -or-  hc util arq POWER START COUNT")
                 }
