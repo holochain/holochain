b30420da262d16a6f094ebf86c349871018bafbe
Jost Schulte <jost.schulte@protonmail.com>
2024-04-01 20:45:11 -0600

    test: mock network in all unit tests

--- crates/holochain/src/core/workflow/app_validation_workflow/unit_tests.rs
+++ crates/holochain/src/core/workflow/app_validation_workflow/unit_tests.rs
@@ -6,21 +6,18 @@ use crate::{
             ZomesToInvoke,
         },
         workflow::app_validation_workflow::{
-            put_integrated, run_validation_callback, Outcome, ValidationDependencies,
+            run_validation_callback, Outcome, ValidationDependencies,
         },
     },
     fixt::MetaLairClientFixturator,
     sweettest::{SweetDnaFile, SweetInlineZomes},
-    test_utils::{test_network, test_network_with_events},
 };
 use fixt::fixt;
-use holo_hash::{hash_type::AnyDht, AgentPubKey, AnyDhtHash, DhtOpHash, HashableContentExtSync};
-use holochain_cascade::test_utils::MockNetwork;
+use holo_hash::{AgentPubKey, HashableContentExtSync};
 use holochain_keystore::{test_keystore, MetaLairClient};
 use holochain_p2p::{event::HolochainP2pEvent, HolochainP2pDnaFixturator, MockHolochainP2pDnaT};
 use holochain_state::{host_fn_workspace::HostFnWorkspaceRead, mutations::insert_op};
 use holochain_types::{
-    action::WireDelete,
     chain::MustGetAgentActivityResponse,
     dht_op::{DhtOp, DhtOpHashed, WireOps},
     dna::DnaFile,
@@ -47,6 +44,7 @@ use holochain_zome_types::{
 use kitsune_p2p_types::ok_fut;
 use matches::assert_matches;
 use parking_lot::{Mutex, RwLock};
+use rusqlite::params;
 use std::{
     sync::{
         atomic::{AtomicI8, Ordering},
@@ -136,7 +134,6 @@ async fn validation_callback_awaiting_deps_hashes() {
 
     let zomes = SweetInlineZomes::new(vec![], 0).integrity_function("validate", {
         move |api, op: Op| {
-            println!("op {op:?}");
             if let Op::RegisterDelete(RegisterDelete {
                 original_action, ..
             }) = op
@@ -199,7 +196,7 @@ async fn validation_callback_awaiting_deps_hashes() {
     assert_matches!(outcome, Outcome::AwaitingDeps(hashes) if hashes == vec![create_action.clone().to_hash().into()]);
 
     // await while missing record is being fetched in background task
-    tokio::time::sleep(Duration::from_millis(500)).await;
+    tokio::time::sleep(Duration::from_millis(10)).await;
 
     // app validation outcome should be accepted, now that the missing record
     // has been fetched
@@ -254,6 +251,9 @@ async fn validation_callback_awaiting_deps_agent_activity() {
         ..
     } = TestCase::new(zomes).await;
 
+    println!("alice {alice:?}");
+    println!("bob {bob:?}");
+
     let action = Action::Dna(Dna {
         author: bob.clone(),
         timestamp: Timestamp::now(),
@@ -297,7 +297,7 @@ async fn validation_callback_awaiting_deps_agent_activity() {
     assert_matches!(outcome, Outcome::AwaitingDeps(hashes) if hashes == vec![action.author().to_owned().into()]);
 
     // await while bob's chain is being fetched in background task
-    tokio::time::sleep(Duration::from_millis(500)).await;
+    tokio::time::sleep(Duration::from_millis(10)).await;
 
     // app validation outcome should be accepted, now that bob's missing agent
     // activity is available in alice's cache
@@ -318,7 +318,7 @@ async fn validation_callback_awaiting_deps_agent_activity() {
 // test scenario every agent is an authority and are expected to hold data
 // they are an authority of
 #[tokio::test(flavor = "multi_thread")]
-async fn validation_callback_prevent_multiple_identical_fetches() {
+async fn validation_callback_prevent_multiple_identical_hash_fetches() {
     holochain_trace::test_run().unwrap();
 
     let zomes = SweetInlineZomes::new(vec![], 0).integrity_function("validate", {
@@ -354,58 +354,31 @@ async fn validation_callback_prevent_multiple_identical_fetches() {
         ..
     } = TestCase::new(zomes).await;
 
-    // handle only Get events
-    let filter_events = |evt: &_| match evt {
-        holochain_p2p::event::HolochainP2pEvent::Get { .. } => true,
-        _ => false,
-    };
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
-    let network = Arc::new(
-        test_network_with_events(
-            Some(dna_file.dna_hash().clone()),
-            Some(alice.clone()),
-            filter_events,
-            tx,
-        )
-        .await
-        .dna_network(),
-    );
-
+    let action_to_be_fetched = create_action_signed_hashed;
     let times_same_hash_is_fetched = Arc::new(AtomicI8::new(0));
 
-    // respond to Get request with requested action
-    tokio::spawn({
-        let times_fetched = Arc::clone(&times_same_hash_is_fetched);
-        async move {
-            let action_hash = create_action.clone().to_hash();
-            while let Some(evt) = rx.recv().await {
-                if let HolochainP2pEvent::Get {
-                    dht_hash, respond, ..
-                } = evt
-                {
-                    assert_eq!(dht_hash, action_hash.clone().into());
-
-                    respond.r(ok_fut(Ok(WireOps::Record(WireRecordOps {
-                        action: Some(Judged::new(
-                            create_action_signed_hashed.clone().into(),
-                            ValidationStatus::Valid,
-                        )),
-                        deletes: vec![Judged::new(
-                            WireDelete {
-                                delete: delete.clone(),
-                                signature: delete_action_signed_hashed.signature.clone(),
-                            },
-                            ValidationStatus::Valid,
-                        )],
-                        updates: vec![],
-                        entry: None,
-                    }))));
-
-                    times_fetched.fetch_add(1, Ordering::Relaxed);
-                }
-            }
+    // mock network that returns the requested create action and increments
+    // counter of identical hash fetches
+    let mut network = MockHolochainP2pDnaT::new();
+    network.expect_get().returning({
+        let times_same_hash_is_fetched = times_same_hash_is_fetched.clone();
+        move |hash, actor| {
+            println!("ending up here");
+            if hash == action_to_be_fetched.as_hash().clone().into() {
+                times_same_hash_is_fetched.fetch_add(1, Ordering::Relaxed);
+            };
+            Ok(vec![WireOps::Record(WireRecordOps {
+                action: Some(Judged::new(
+                    action_to_be_fetched.clone().into(),
+                    ValidationStatus::Valid,
+                )),
+                deletes: vec![],
+                updates: vec![],
+                entry: None,
+            })])
         }
     });
+    let network = Arc::new(network);
 
     let fetched_dependencies = Arc::new(Mutex::new(ValidationDependencies::new()));
 
@@ -424,10 +397,10 @@ async fn validation_callback_prevent_multiple_identical_fetches() {
         network,
         fetched_dependencies.clone(),
     );
-    let outcomes = futures::future::join_all([validate_1, validate_2]).await;
+    futures::future::join_all([validate_1, validate_2]).await;
 
     // await while missing records are being fetched in background task
-    tokio::time::sleep(Duration::from_millis(500)).await;
+    tokio::time::sleep(Duration::from_millis(10)).await;
 
     assert_eq!(times_same_hash_is_fetched.load(Ordering::Relaxed), 1);
     // after successfully fetching dependencies, the set should be empty
@@ -436,128 +409,102 @@ async fn validation_callback_prevent_multiple_identical_fetches() {
 
 #[tokio::test(flavor = "multi_thread")]
 async fn validation_callback_prevent_multiple_identical_agent_activity_fetches() {
-    // holochain_trace::test_run().unwrap();
+    holochain_trace::test_run().unwrap();
 
-    // let action = fixt!(Action);
-    // let action_signed_hashed = SignedActionHashed::new_unchecked(action.clone(), fixt!(Signature));
-    // let action_op = Op::RegisterAgentActivity(RegisterAgentActivity {
-    //     action: action_signed_hashed.clone(),
-    //     cached_entry: None,
-    // });
+    let zomes = SweetInlineZomes::new(vec![], 0).integrity_function("validate", {
+        move |api, op: Op| {
+            println!("op {op:?}");
+            if let Op::RegisterDelete(RegisterDelete {
+                original_action, ..
+            }) = op
+            {
+                let result = api.must_get_agent_activity(MustGetAgentActivityInput {
+                    author: original_action.author().clone(),
+                    chain_filter: ChainFilter::new(original_action.clone().to_hash()),
+                });
+                if result.is_ok() {
+                    Ok(ValidateCallbackResult::Valid)
+                } else {
+                    Ok(ValidateCallbackResult::UnresolvedDependencies(
+                        UnresolvedDependencies::AgentActivity(
+                            original_action.author().clone(),
+                            ChainFilter::new(original_action.clone().to_hash()),
+                        ),
+                    ))
+                }
+            } else {
+                unreachable!()
+            }
+        }
+    });
 
-    // let zomes = SweetInlineZomes::new(vec![], 0).integrity_function("validate", {
-    //     let action_hash = action.clone().to_hash();
-    //     move |api, op: Op| {
-    //         if let Op::RegisterAgentActivity(RegisterAgentActivity { action, .. }) = op {
-    //             let result = api.must_get_action(MustGetActionInput(action.as_hash().to_owned()));
-    //             if result.is_ok() {
-    //                 Ok(ValidateCallbackResult::Valid)
-    //             } else {
-    //                 Ok(ValidateCallbackResult::UnresolvedDependencies(
-    //                     UnresolvedDependencies::Hashes(vec![action_hash.clone().into()]),
-    //                 ))
-    //             }
-    //         } else {
-    //             Ok(ValidateCallbackResult::Valid)
-    //         }
-    //     }
-    // });
+    let TestCase {
+        dna_file,
+        integrity_zomes,
+        test_space,
+        ribosome,
+        alice,
+        bob,
+        create_action_signed_hashed,
+        delete_action_op,
+        workspace,
+        ..
+    } = TestCase::new(zomes).await;
 
-    // let TestCase {
-    //     dna_file,
-    //     integrity_zomes,
-    //     ribosome,
-    //     test_space,
-    //     ..
-    // } = TestCase::new(zomes).await;
+    let action_to_be_fetched = create_action_signed_hashed;
+    let times_same_hash_is_fetched = Arc::new(AtomicI8::new(0));
 
-    // let alice = fixt!(AgentPubKey);
+    // mock network with alice not being an authority of bob's action
+    let mut network = MockHolochainP2pDnaT::new();
+    network.expect_authority_for_hash().returning(|_| Ok(false));
+    // return single action as requested chain
+    network.expect_must_get_agent_activity().returning({
+        let times_same_hash_is_fetched = times_same_hash_is_fetched.clone();
+        move |author, filter| {
+            assert_eq!(author, alice);
+            assert_eq!(&filter.chain_top, action_to_be_fetched.as_hash());
 
-    // let zomes_to_invoke = ZomesToInvoke::OneIntegrity(integrity_zomes[0].clone());
-    // let invocation = ValidateInvocation::new(zomes_to_invoke, &action_op).unwrap();
-    // let workspace_read = get_workspace_read(&test_space, &alice, dna_file.dna_def()).await;
+            times_same_hash_is_fetched
+                .clone()
+                .fetch_add(1, Ordering::Relaxed);
+            Ok(vec![MustGetAgentActivityResponse::Activity(vec![
+                RegisterAgentActivity {
+                    action: action_to_be_fetched.clone(),
+                    cached_entry: None,
+                },
+            ])])
+        }
+    });
+    let network = Arc::new(network);
+    let zomes_to_invoke = ZomesToInvoke::OneIntegrity(integrity_zomes[0].clone());
+    let invocation = ValidateInvocation::new(zomes_to_invoke, &delete_action_op).unwrap();
 
-    // // handle only Get events
-    // let mut network = MockHolochainP2pDnaT::new();
-    // network.expect_get().returning(|holo_hash, actor| {
-    //     println!("hello {holo_hash:?} actor {actor:?}");
-    //     Ok(vec![])
-    // });
-    // let network = Arc::new(network);
-    // // let filter_events = |evt: &_| match evt {
-    // //     holochain_p2p::event::HolochainP2pEvent::Get { .. } => true,
-    // //     _ => false,
-    // // };
-    // // let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let fetched_dependencies = Arc::new(Mutex::new(ValidationDependencies::new()));
 
-    // // let network = test_network_with_events(
-    // //     Some(dna_file.dna_hash().clone()),
-    // //     Some(agent_key.clone()),
-    // //     filter_events,
-    // //     tx,
-    // // )
-    // // .await;
+    // run two op validations that depend on the same record in parallel
+    let validate_1 = run_validation_callback(
+        invocation.clone(),
+        &ribosome,
+        workspace.clone(),
+        network.clone(),
+        fetched_dependencies.clone(),
+    );
+    let validate_2 = run_validation_callback(
+        invocation,
+        &ribosome,
+        workspace,
+        network,
+        fetched_dependencies.clone(),
+    );
+    futures::future::join_all([validate_1, validate_2]).await;
 
-    // let times_same_hash_is_fetched = Arc::new(AtomicI8::new(0));
+    // await while missing records are being fetched in background task
+    tokio::time::sleep(Duration::from_millis(20)).await;
 
-    // // respond to Get request with requested action
-    // // tokio::spawn({
-    // //     let action_hash = action.clone().to_hash();
-    // //     let action_hash_32 = action_hash.get_raw_32().to_vec();
-    // //     let times_fetched = Arc::clone(&times_same_hash_is_fetched);
-    // //     async move {
-    // //         while let Some(evt) = rx.recv().await {
-    // //             if let HolochainP2pEvent::Get {
-    // //                 dht_hash, respond, ..
-    // //             } = evt
-    // //             {
-    // //                 assert_eq!(dht_hash.get_raw_32().to_vec(), action_hash_32);
-
-    // //                 respond.r(ok_fut(Ok(WireOps::Record(WireRecordOps {
-    // //                     action: Some(Judged::new(
-    // //                         action_signed_hashed.clone().into(),
-    // //                         ValidationStatus::Valid,
-    // //                     )),
-    // //                     deletes: vec![],
-    // //                     updates: vec![],
-    // //                     entry: None,
-    // //                 }))));
-
-    // //                 times_fetched.fetch_add(1, Ordering::Relaxed);
-    // //             }
-    // //         }
-    // //     }
-    // // });
-
-    // let fetched_dependencies = Arc::new(Mutex::new(HashSet::new()));
-
-    // // run two op validations that depend on the same record in parallel
-    // let validate_1 = run_validation_callback_inner(
-    //     invocation.clone(),
-    //     &ribosome,
-    //     workspace_read.clone(),
-    //     network.clone(),
-    //     fetched_dependencies.clone(),
-    // );
-    // let validate_2 = run_validation_callback_inner(
-    //     invocation.clone(),
-    //     &ribosome,
-    //     workspace_read.clone(),
-    //     network.clone(),
-    //     fetched_dependencies.clone(),
-    // );
-    // let outcomes = futures::future::join_all([validate_1, validate_2]).await;
-
-    // // await while missing records are being fetched in background task
-    // tokio::time::sleep(Duration::from_millis(500)).await;
-
-    // assert_eq!(times_same_hash_is_fetched.load(Ordering::Relaxed), 1);
-    // // after successfully fetching dependencies, the set should be empty
-    // assert_eq!(fetched_dependencies.lock().missing_hashes.len(), 0);
+    assert_eq!(times_same_hash_is_fetched.load(Ordering::Relaxed), 1);
+    // after successfully fetching dependencies, the set should be empty
+    assert_eq!(fetched_dependencies.lock().missing_hashes.len(), 0);
 }
-
-#[tokio::test(flavor = "multi_thread")]
-async fn hashes_missing_for_op_is_updated_with_unresolved_deps() {}
 
 // test case with alice and bob, a create by alice and a delete by bob that
 // references alice's create
@@ -570,6 +517,7 @@ struct TestCase {
     bob: AgentPubKey,
     workspace: HostFnWorkspaceRead,
     create_action: Action,
+    create_action_op: Op,
     create_action_signed_hashed: SignedActionHashed,
     delete: Delete,
     delete_action_op: Op,
@@ -607,12 +555,18 @@ impl TestCase {
         // a create by alice
         let mut create = fixt!(Create);
         create.author = alice.clone();
+        create.action_seq = 5;
         let create_action = Action::Create(create.clone());
         let create_action_signed_hashed =
             SignedHashed::new_unchecked(create_action.clone(), fixt!(Signature));
+        let create_action_op = Op::RegisterAgentActivity(RegisterAgentActivity {
+            action: create_action_signed_hashed.clone(),
+            cached_entry: None,
+        });
         // a delete by bob that references alice's create
         let mut delete = fixt!(Delete);
         delete.author = bob.clone();
+        delete.action_seq = 6;
         delete.deletes_address = create_action.clone().to_hash();
         let delete_action = Action::Delete(delete.clone());
         let delete_action_signed_hashed =
@@ -633,6 +587,7 @@ impl TestCase {
             bob,
             workspace,
             create_action,
+            create_action_op,
             create_action_signed_hashed,
             delete,
             delete_action_op,
