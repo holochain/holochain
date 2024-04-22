1436157405e2ab28eddee35d3337c8d8a3d5ec41
Stefan Junker <1181362+steveej@users.noreply.github.com>
2024-04-22 17:40:15 +0200

    feat(ci): partially re-enable x86_64-darwin (#3695)
    * feat(ci): re-enable x86_64-darwin
    
    * chore(nix/goWrapper): use go 1.21
    
    * only run the unit tests on x86_64-darwin
    
    holonix integration tests are not working yet

--- nix/modules/go.nix
+++ nix/modules/go.nix
@@ -13,17 +13,20 @@
     }: {
       packages = {
         goWrapper =
+          let
+            go = pkgs.go_1_21;
+          in
           # there is interference only in this specific case, we assemble a go derivationt that not propagate anything but still has everything available required for our specific use-case
-          #
-          # the wrapper inherits preconfigured environment variables from the
-          # derivation that depends on the propagating go
+            #
+            # the wrapper inherits preconfigured environment variables from the
+            # derivation that depends on the propagating go
           if pkgs.stdenv.isDarwin && pkgs.system == "x86_64-darwin" then
             pkgs.darwin.apple_sdk_11_0.stdenv.mkDerivation
               {
                 name = "go";
 
-                nativeBuildInputs = with pkgs; [
-                  makeBinaryWrapper
+                nativeBuildInputs = [
+                  pkgs.makeBinaryWrapper
                   go
                 ];
 
@@ -66,7 +69,7 @@
                     )}
                 '';
               }
-          else pkgs.go;
+          else go;
       };
     };
 }
