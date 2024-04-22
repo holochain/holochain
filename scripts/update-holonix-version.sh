60c5418811f7fc462b3fe000dc2967f2ece92098
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-04-22 12:57:06 +0100

    Fix `-e` vs `-E` in `sed` call

--- scripts/update-holonix-version.sh
+++ scripts/update-holonix-version.sh
@@ -16,19 +16,19 @@ SEP_COUNT=$(echo "$VERSION_STR" | tr -d -c '_' | awk '{ print length; }')
 SEARCH_PATTERN="no-tag"
 if [ "$SEP_COUNT" == "1" ]; then
     # Matched format 0_X
-    SEARCH_PATTERN=$(echo "$VERSION_STR" | awk -F_ 'BEGIN { OFS="" } {print "^holochain-", $1, ".", $2, ".[0-9]+$"}')
+    SEARCH_PATTERN=$(echo "$VERSION_STR" | awk -F _ 'BEGIN { OFS="" } {print "^holochain-", $1, ".", $2, ".[0-9]+$"}')
 elif [ "$SEP_COUNT" == "2" ] && [[ "$VERSION_STR" =~ rc$ ]]; then
     # Matched format 0_X_rc
-  SEARCH_PATTERN=$(echo "$VERSION_STR" | awk -F_ 'BEGIN { OFS="" } {print "^holochain-", $1, ".", $2, ".[0-9]+-rc.[0-9]+$"}')
+  SEARCH_PATTERN=$(echo "$VERSION_STR" | awk -F _ 'BEGIN { OFS="" } {print "^holochain-", $1, ".", $2, ".[0-9]+-rc.[0-9]+$"}')
 elif [ "$VERSION_STR" == "weekly" ]; then
   # Special case, weekly tracks the latest pre-release version
-  SEARCH_PATTERN="holochain-[0-9]+.[0-9]+.[0-9]+-beta-dev.[0-9]+"
+  SEARCH_PATTERN="^holochain-[0-9]+.[0-9]+.[0-9]+-beta-dev.[0-9]+$"
 else
     echo "Invalid version format: $VERSION_STR"
     exit 1
 fi
 
-echo "$SEARCH_PATTERN"
+echo "Looking for tags matching pattern: $SEARCH_PATTERN"
 
 LATEST_MATCHING_TAG=$(git tag --list --sort=version:refname | { grep -E "$SEARCH_PATTERN" || printf ""; } | tail -n 1)
 
@@ -39,6 +39,4 @@ fi
 
 echo "Latest matching tag: $LATEST_MATCHING_TAG"
 
-sed -i -e "s#holochain/holochain/holochain-.*\"#holochain/holochain/$LATEST_MATCHING_TAG\"#" "./versions/$VERSION_STR/flake.nix"
-
-# nix run .#scripts-repo-flake-update "$VERSION_STR"
+sed --in-place --regexp-extended "s#holochain/holochain/holochain-.*\"#holochain/holochain/$LATEST_MATCHING_TAG\"#" "./versions/$VERSION_STR/flake.nix"
