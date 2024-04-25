267d366f33be25bfaeb3c9fdf3446bbee5069118
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-04-25 19:42:56 +0100

    Disable tests that are broken on macos

--- crates/holochain/tests/new_lair/mod.rs
+++ crates/holochain/tests/new_lair/mod.rs
@@ -17,6 +17,7 @@ use super::test_utils::*;
 const ADMIN_PORT: u16 = 12909;
 
 #[tokio::test(flavor = "multi_thread")]
+#[cfg_attr(target_os = "macos", ignore = "processes fail to launch on macos, broken!")]
 async fn test_new_lair_conductor_integration() {
     // working temp dir
     let tmp = tempfile::tempdir().unwrap();
