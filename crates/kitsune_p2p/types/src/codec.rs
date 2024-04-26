9f9960f1368facb0b29fd49ba43168adec65a45d
Jost Schulte <jost.schulte@protonmail.com>
2024-04-26 07:17:23 -0600

    the fix the clip

--- crates/kitsune_p2p/types/src/codec.rs
+++ crates/kitsune_p2p/types/src/codec.rs
@@ -7,8 +7,7 @@ where
     W: std::io::Write,
     S: serde::Serialize,
 {
-    let mut se = rmp_serde::encode::Serializer::new(write)
-        .with_struct_map();
+    let mut se = rmp_serde::encode::Serializer::new(write).with_struct_map();
     item.serialize(&mut se)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     Ok(())
