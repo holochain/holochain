0ae1d716ba7fbd0328c76d09ea4a2de7bba5d08d
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-04-30 10:00:37 +0100

    Simplify script entry

--- scripts/format-toml.sh
+++ scripts/format-toml.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz
-#! nix-shell -i bash --pure
-#! nix-shell -p bash taplo
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz -i bash --pure --packages taplo
 
 # Usage to format: ./scripts/format-toml.sh
 # Usage to check: ./scripts/format-toml.sh --check
