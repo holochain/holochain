d456d25bdf23b1a6448b42daffa5247cf8219ee0
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-05-13 13:07:49 +0100

    Remove crate2nix and upgrade Rust

--- nix/modules/scaffolding.nix
+++ nix/modules/scaffolding.nix
@@ -5,7 +5,7 @@
     let
       rustToolchain = config.rustHelper.mkRust {
         track = "stable";
-        version = "1.75.0";
+        version = "1.78.0";
       };
       craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
 
