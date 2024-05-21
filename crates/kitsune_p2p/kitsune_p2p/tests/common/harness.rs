b921c8b39d29312e90dc50946aee1cc4c1b8c8b3
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-05-21 16:48:35 +0100

    Try to make Kitsune functional tests less flaky

--- crates/kitsune_p2p/kitsune_p2p/tests/common/harness.rs
+++ crates/kitsune_p2p/kitsune_p2p/tests/common/harness.rs
@@ -14,6 +14,7 @@ use kitsune_p2p_types::{
 };
 use parking_lot::RwLock;
 use tokio::task::AbortHandle;
+use kitsune_p2p_bootstrap::BootstrapShutdown;
 
 pub struct KitsuneTestHarness {
     name: String,
@@ -149,19 +150,45 @@ impl KitsuneTestHarness {
     }
 }
 
-pub async fn start_bootstrap() -> (SocketAddr, AbortHandle) {
+pub struct TestBootstrapHandle {
+    shutdown_cb: Option<BootstrapShutdown>,
+    abort_handle: AbortHandle,
+}
+
+impl TestBootstrapHandle {
+    fn new(shutdown_cb: BootstrapShutdown, abort_handle: AbortHandle) -> Self {
+        Self {
+            shutdown_cb: Some(shutdown_cb),
+            abort_handle,
+        }
+    }
+
+    pub fn abort(&mut self) {
+        if let Some(shutdown_cb) = self.shutdown_cb.take() {
+            shutdown_cb();
+        }
+        self.abort_handle.abort();
+    }
+}
+
+impl Drop for TestBootstrapHandle {
+    fn drop(&mut self) {
+        self.abort();
+    }
+}
+
+pub async fn start_bootstrap() -> (SocketAddr, TestBootstrapHandle) {
     let (bs_driver, bs_addr, shutdown) =
         kitsune_p2p_bootstrap::run("127.0.0.1:0".parse::<SocketAddr>().unwrap(), vec![])
             .await
             .expect("Could not start bootstrap server");
 
     let abort_handle = tokio::spawn(async move {
-        let _shutdown_cb = shutdown;
         bs_driver.await;
     })
     .abort_handle();
 
-    (bs_addr, abort_handle)
+    (bs_addr, TestBootstrapHandle::new(shutdown, abort_handle))
 }
 
 pub async fn start_signal_srv() -> (SocketAddr, tx5_signal_srv::SrvHnd) {
