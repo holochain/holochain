4e16556eed7100a75d2153b9a9438c6414fea872
Jost Schulte <jost.schulte@protonmail.com>
2024-06-03 10:44:08 -0600

    revert some stuff

--- nix/modules/launcher.nix
+++ nix/modules/launcher.nix
@@ -19,69 +19,83 @@
 
       commonArgs = {
         pname = "hc-launch";
-        src = inputs.launcher;
-        cargoExtraArgs = "--bin hc-launch";
+        src = flake.config.reconciledInputs.launcher;
 
         # We are asking Crane to build a binary from the workspace and that's the only way we can build it because
         # the workspace defines the dependencies, so we can't just build the member crate. But then we need to tell
         # Crate what the version is, so we look it up directly from the member's Cargo.toml.
         version = crateInfo.version;
 
-        buildInputs = [
-          pkgs.perl
-        ]
-        ++ (pkgs.lib.optionals pkgs.stdenv.isLinux
-          [
-            pkgs.glib
-            pkgs.go
-            pkgs.webkitgtk.dev
-          ])
-        ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
-          [
-            apple_sdk.frameworks.AppKit
-            apple_sdk.frameworks.WebKit
+        CARGO_PROFILE = "release";
 
-            (if pkgs.system == "x86_64-darwin" then
-              pkgs.darwin.apple_sdk_11_0.stdenv.mkDerivation
-                {
-                  name = "go";
-                  nativeBuildInputs = with pkgs; [
-                    makeBinaryWrapper
-                    go
-                  ];
-                  dontBuild = true;
-                  dontUnpack = true;
-                  installPhase = ''
-                    makeWrapper ${pkgs.go}/bin/go $out/bin/go
-                  '';
-                }
-            else pkgs.go)
-          ]
+        cargoExtraArgs = "--bin hc-launch";
+
+        buildInputs = (with pkgs; [
+          openssl
+
+          # this is required for glib-networking
+          glib
+        ])
+        ++ (lib.optionals pkgs.stdenv.isLinux
+          (with pkgs; [
+            webkitgtk.dev
+            gdk-pixbuf
+            gtk3
+          ]))
+        ++ lib.optionals pkgs.stdenv.isDarwin
+          (with apple_sdk.frameworks; [
+            AppKit
+            CoreFoundation
+            CoreServices
+            Security
+            IOKit
+            WebKit
+          ])
         ;
 
-        nativeBuildInputs = (
-          if pkgs.stdenv.isLinux then [ pkgs.pkg-config ]
-          else [ ]
-        );
+        nativeBuildInputs = (with pkgs; [
+          perl
+          pkg-config
+
+          # currently needed to build tx5
+          self'.packages.goWrapper
+        ])
+        ++ (lib.optionals pkgs.stdenv.isLinux
+          (with pkgs; [
+            wrapGAppsHook
+          ]))
+        ++ (lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.xcbuild
+          pkgs.libiconv
+        ])
+        ;
 
         doCheck = false;
       };
 
       # derivation building all dependencies
-      deps = craneLib.buildDepsOnly
-        (commonArgs // { });
+      deps = craneLib.buildDepsOnly (commonArgs // { });
 
       # derivation with the main crates
-      hc-launch = craneLib.buildPackage
-        (commonArgs // {
-          cargoArtifacts = deps;
+      package = craneLib.buildPackage (commonArgs // {
+        cargoArtifacts = deps;
 
-          stdenv =
-            if pkgs.stdenv.isDarwin then
-              pkgs.overrideSDK pkgs.stdenv "11.0"
-            else
-              pkgs.stdenv;
-        });
+        nativeBuildInputs = commonArgs.nativeBuildInputs ++ [
+          pkgs.makeBinaryWrapper
+        ];
+
+        preFixup = ''
+          gappsWrapperArgs+=(
+            --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+          )
+
+          # without this the DevTools will just display an unparsed HTML file (see https://github.com/tauri-apps/tauri/issues/5711#issuecomment-1336409601)
+          gappsWrapperArgs+=(
+            --prefix XDG_DATA_DIRS : "${pkgs.shared-mime-info}/share"
+          )
+        '';
+      });
+
     in
     {
       packages = {
