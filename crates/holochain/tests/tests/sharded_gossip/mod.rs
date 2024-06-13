bfde9926087008861c8110d1be5c006539e8fb89
Michael Dougherty <maackle.d@gmail.com>
2024-06-13 09:51:39 -0700

    Disable hc_sleuth logging for now

--- crates/holochain/tests/tests/sharded_gossip/mod.rs
+++ crates/holochain/tests/tests/sharded_gossip/mod.rs
@@ -558,7 +558,8 @@ async fn test_gossip_startup() {
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "macos", ignore = "flaky")]
 async fn three_way_gossip_recent() {
-    hc_sleuth::init_subscriber();
+    // hc_sleuth::init_subscriber();
+    holochain_trace::test_run();
     let config = TestConfig {
         publish: false,
         recent: true,
@@ -574,7 +575,8 @@ async fn three_way_gossip_recent() {
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "macos", ignore = "flaky")]
 async fn three_way_gossip_historical() {
-    hc_sleuth::init_subscriber();
+    // hc_sleuth::init_subscriber();
+    holochain_trace::test_run();
     let config = TestConfig {
         publish: false,
         recent: false,
