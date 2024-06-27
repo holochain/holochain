80b95bff9cfaa1fa901d306b65dc16bb54e009d9
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-06-27 12:03:46 +0100

    Wrap expression

--- nix/modules/devShells.nix
+++ nix/modules/devShells.nix
@@ -6,7 +6,7 @@
           (holochain.override holochainOverrides)
           lair-keystore
           hc-scaffold
-        ] ++ (lib.optionals system != "x86_64-darwin" [hc-launch]);
+        ] ++ (lib.optionals (system != "x86_64-darwin") [ hc-launch ]);
       versionsFileText = builtins.concatStringsSep "\n"
         (
           builtins.map
