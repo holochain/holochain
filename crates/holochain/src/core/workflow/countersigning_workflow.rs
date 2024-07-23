c1c763ba1b6966dabb263f09b6c4737a8435f878
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-23 17:24:08 +0100

    Log hash at end of session

--- crates/holochain/src/core/workflow/countersigning_workflow.rs
+++ crates/holochain/src/core/workflow/countersigning_workflow.rs
@@ -325,7 +325,7 @@ pub(crate) async fn countersigning_success(
         }
 
         authored_db.read_async(|txn| -> DatabaseResult<()> {
-            tracing::info!("At completion of session, the lock table contains the following locks: {:?}", get_locks(&txn));
+            tracing::info!("At completion of session {:?}, the lock table contains the following locks: {:?}", session.preflight_request.app_entry_hash, get_locks(&txn));
             Ok(())
         }).await?;
         // Signal to the UI.
