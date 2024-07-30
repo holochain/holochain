4ef1ca7181f60cdc430b24c6c24b49225f9bc26b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-30 11:41:36 +0100

    Fix 0.3 Holonix

--- versions/weekly/flake.nix
+++ versions/weekly/flake.nix
@@ -2,18 +2,18 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.3.0-beta-dev.48";
+        url = "github:holochain/holochain/holochain-0.3.1";
         flake = false;
       };
 
       lair = {
-        url = "github:holochain/lair/lair_keystore-v0.4.4";
+        url = "github:holochain/lair/lair_keystore-v0.4.5";
         flake = false;
       };
 
       # holochain_cli_launch
       launcher = {
-        url = "github:holochain/launcher/holochain-weekly";
+        url = "github:holochain/hc-launch/holochain-weekly";
         flake = false;
       };
 
