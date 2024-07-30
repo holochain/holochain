d6ff4ebb7f834a5a8cbfe41459e54c1de8f94318
ThetaSinner <6267702+ThetaSinner@users.noreply.github.com>
2024-07-30 17:20:12 +0000

    [create-pull-request] automated change

--- versions/0_3/flake.nix
+++ versions/0_3/flake.nix
@@ -2,7 +2,7 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.3.1";
+        url = "github:holochain/holochain/holochain-0.3.2";
         flake = false;
       };
 
