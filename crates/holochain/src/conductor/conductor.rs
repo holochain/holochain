36ab4aa4e6165af1df56980c7e233ff16a11e316
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-08-16 12:05:33 +0100

    Move pre-flight acceptance into the workflow to allow state to be updated

--- crates/holochain/src/conductor/conductor.rs
+++ crates/holochain/src/conductor/conductor.rs
@@ -3122,6 +3122,7 @@ mod accessor_impls {
     }
 }
 
+/// Methods related to app authentication tokens
 mod authenticate_token_impls {
     use super::*;
     use holochain_conductor_api::{
@@ -3173,6 +3174,31 @@ mod authenticate_token_impls {
             self.app_auth_token_store.share_mut(|app_connection_auth| {
                 app_connection_auth.authenticate_token(token, app_id)
             })
+        }
+    }
+}
+
+/// Methods for bridging from host calls to workflows for countersigning
+mod countersigning_impls {
+    use super::*;
+    use crate::core::workflow;
+
+    impl Conductor {
+        /// Accept a countersigning session
+        pub(crate) async fn accept_countersigning_session(
+            &self,
+            cell_id: CellId,
+            request: PreflightRequest,
+        ) -> ConductorResult<PreflightRequestAcceptance> {
+            Ok(
+                workflow::countersigning_workflow::accept_countersigning_request(
+                    self.spaces.get_or_create_space(cell_id.dna_hash())?,
+                    self.keystore.clone(),
+                    cell_id.agent_pubkey().clone(),
+                    request,
+                )
+                    .await?,
+            )
         }
     }
 }
@@ -3584,7 +3610,7 @@ impl Conductor {
         Ok(installed_clone_cell)
     }
 
-    /// Print the current setup in a machine readable way
+    /// Print the current setup in a machine-readable way
     fn print_setup(&self) {
         use std::fmt::Write;
         let mut out = String::new();
