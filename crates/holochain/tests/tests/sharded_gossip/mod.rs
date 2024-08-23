355ea3d56a157268291cf170ec3344fd8274c220
David Braden <neonphog@gmail.com>
2024-08-23 15:54:48 -0600

    sqlcipher docs (#4207)
    * sqlcipher docs & attempt at windows ci fix
    
    * ignore flaky windows tests

--- crates/holochain/tests/tests/sharded_gossip/mod.rs
+++ crates/holochain/tests/tests/sharded_gossip/mod.rs
@@ -639,6 +639,7 @@ async fn test_gossip_startup() {
 #[cfg(feature = "slow_tests")]
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "macos", ignore = "flaky")]
+#[cfg_attr(target_os = "windows", ignore = "flaky")]
 async fn three_way_gossip_recent() {
     // hc_sleuth::init_subscriber();
     holochain_trace::test_run();
@@ -657,6 +658,7 @@ async fn three_way_gossip_recent() {
 #[cfg(feature = "slow_tests")]
 #[tokio::test(flavor = "multi_thread")]
 #[cfg_attr(target_os = "macos", ignore = "flaky")]
+#[cfg_attr(target_os = "windows", ignore = "flaky")]
 async fn three_way_gossip_historical() {
     // hc_sleuth::init_subscriber();
     holochain_trace::test_run();
