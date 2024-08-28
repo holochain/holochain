66c3a4c0ce606b61df0fb621ee5e0fa657fe5320
Matt Gabrenya <matt@buildyourweb.app>
2024-08-28 12:27:26 -0700

    feat: bump holochain-wasmer, bump wasmer / hc-wasmer deps to mirror

--- crates/holochain/src/lib.rs
+++ crates/holochain/src/lib.rs
@@ -65,3 +65,7 @@ compile_error!(
 
 #[cfg(all(not(feature = "wasmer_sys"), not(feature = "wasmer_wamr"),))]
 compile_error!("One of: `wasmer_sys`, `wasmer_wamr` features must be enabled. Please, pick one.");
+
+// Temporarily include a fork of wasmer from the git branch 'wamr', until it is officially released in wasmer v5
+#[cfg(feature = "wasmer_wamr")]
+extern crate hc_wasmer as wasmer;
