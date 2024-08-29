e35ee2a82b0fc7e8d15eec7d83b89bd7e9907eb4
Matt Gabrenya <matt@buildyourweb.app>
2024-08-29 08:16:03 -0700

    fix: missing imports

--- crates/hc_bundle/src/packing/wasmer_wamr.rs
+++ crates/hc_bundle/src/packing/wasmer_wamr.rs
@@ -1,5 +1,9 @@
+use crate::error::HcBundleError;
+use mr_bundle::{Bundle, Manifest};
+use std::path::Path;
+
 pub(super) async fn build_preserialized_wasm<M: Manifest>(
-    _target_path: &PathBuf,
+    _target_path: &Path,
     _bundle: &Bundle<M>,
 ) -> Result<(), HcBundleError> {
     unimplemented!("The feature flag 'wasmer_sys' must be enabled to support compiling wasm");
