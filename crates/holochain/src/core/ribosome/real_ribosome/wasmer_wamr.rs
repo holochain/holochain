89033ecda02ce677c91c2e35988a8b82f46afde4
Matt Gabrenya <matt@buildyourweb.app>
2024-09-01 10:33:32 -0700

    chore: unused params

--- crates/holochain/src/core/ribosome/real_ribosome/wasmer_wamr.rs
+++ crates/holochain/src/core/ribosome/real_ribosome/wasmer_wamr.rs
@@ -9,9 +9,9 @@ pub fn preserialized_module(_path: &PathBuf) -> RibosomeResult<Arc<Module>> {
 }
 
 // Metering is not supported in wasmer_wamr feature. This is a no-op.
-pub fn reset_metering_points(instance_with_store: Arc<InstanceWithStore>) {}
+pub fn reset_metering_points(_instance_with_store: Arc<InstanceWithStore>) {}
 
 // Metering is not support in wasmer_wamr feature. This is a no-op.
-pub fn get_used_metering_points(instance_with_store: Arc<InstanceWithStore>) -> u64 {
+pub fn get_used_metering_points(_instance_with_store: Arc<InstanceWithStore>) -> u64 {
     0
 }
