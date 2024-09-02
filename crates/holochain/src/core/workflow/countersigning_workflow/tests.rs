e6ca0ba8df01d277a60b454ff688a244f4cf5537
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-09-02 18:45:37 +0100

    Rename SessionState to CountersigningSessionState

---
+++ crates/holochain/src/core/workflow/countersigning_workflow/tests.rs
@@ -0,0 +1,15 @@
+
+#[tokio::test(flavor = "multi_thread")]
+async fn accept_countersigning_session() {
+    // TODO write a test
+}
+
+struct TestHarness {
+
+}
+
+impl TestHarness {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
