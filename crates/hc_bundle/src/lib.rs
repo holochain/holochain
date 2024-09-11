eccace2dbcf2f3195fc3500a4bedbd30575fbf1b
Matt Gabrenya <matt@buildyourweb.app>
2024-09-11 12:22:16 -0700

    revert: remove unused wasmer_wamr flag from hc_bundle

--- crates/hc_bundle/src/lib.rs
+++ crates/hc_bundle/src/lib.rs
@@ -8,7 +8,3 @@ pub use cli::{
     get_web_app_name, web_app_pack_recursive, HcAppBundle, HcDnaBundle, HcWebAppBundle,
 };
 pub use packing::{pack, unpack, unpack_raw};
-
-// Temporarily include a fork of wasmer from the git branch 'wamr', until it is officially released in wasmer v5
-#[cfg(feature = "wasmer_wamr")]
-extern crate hc_wasmer as wasmer;
