f19e953ab65d27936d245b4f46daf138f77ae380
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-09-13 10:52:33 +0100

    Secret records and unfortunate consequences

--- crates/holochain/tests/tests/countersigning.rs
+++ crates/holochain/tests/tests/countersigning.rs
@@ -297,6 +297,141 @@ async fn retry_countersigning_commit_on_missing_deps() {
     wait_for_completion(bob_rx, preflight_request.app_entry_hash).await;
 }
 
+#[tokio::test(flavor = "multi_thread")]
+#[cfg_attr(target_os = "windows", ignore = "flaky")]
+async fn bad_chc_not_respect_chain_lock() {
+    holochain_trace::test_run();
+
+    let mut config = SweetConductorConfig::rendezvous(true);
+    config.chc_url = Some(url2::Url2::parse(
+        holochain::conductor::chc::CHC_LOCAL_MAGIC_URL,
+    ));
+    let mut conductors = SweetConductorBatch::from_config_rendezvous(2, config).await;
+
+    let (dna, _, _) = SweetDnaFile::unique_from_test_wasms(vec![TestWasm::CounterSigning]).await;
+    let apps = conductors.setup_app("app", &[dna]).await.unwrap();
+    let cells = apps.cells_flattened();
+    let alice = &cells[0];
+    let bob = &cells[1];
+
+    // Need an initialised source chain for countersigning, so commit anything
+    let alice_zome = alice.zome(TestWasm::CounterSigning);
+    let _: ActionHash = conductors[0]
+        .call_fallible(&alice_zome, "create_a_thing", ())
+        .await
+        .unwrap();
+    let bob_zome = bob.zome(TestWasm::CounterSigning);
+    let _: ActionHash = conductors[1]
+        .call_fallible(&bob_zome, "create_a_thing", ())
+        .await
+        .unwrap();
+
+    await_consistency(30, vec![alice, bob]).await.unwrap();
+
+    // Need chain head for each other, so get agent activity before starting a session
+    let _: AgentActivity = conductors[0]
+        .call_fallible(
+            &alice_zome,
+            "get_agent_activity",
+            GetAgentActivityInput {
+                agent_pubkey: bob.agent_pubkey().clone(),
+                chain_query_filter: ChainQueryFilter::new(),
+                activity_request: ActivityRequest::Full,
+            },
+        )
+        .await
+        .unwrap();
+    let _: AgentActivity = conductors[1]
+        .call_fallible(
+            &bob_zome,
+            "get_agent_activity",
+            GetAgentActivityInput {
+                agent_pubkey: alice.agent_pubkey().clone(),
+                chain_query_filter: ChainQueryFilter::new(),
+                activity_request: ActivityRequest::Full,
+            },
+        )
+        .await
+        .unwrap();
+
+    // Set up the session and accept it for both agents
+    let preflight_request: PreflightRequest = conductors[0]
+        .call_fallible(
+            &alice_zome,
+            "generate_countersigning_preflight_request_fast",
+            vec![
+                (alice.agent_pubkey().clone(), vec![Role(0)]),
+                (bob.agent_pubkey().clone(), vec![]),
+            ],
+        )
+        .await
+        .unwrap();
+    let alice_acceptance: PreflightRequestAcceptance = conductors[0]
+        .call_fallible(
+            &alice_zome,
+            "accept_countersigning_preflight_request",
+            preflight_request.clone(),
+        )
+        .await
+        .unwrap();
+    let alice_response =
+        if let PreflightRequestAcceptance::Accepted(ref response) = alice_acceptance {
+            response
+        } else {
+            unreachable!();
+        };
+    let bob_acceptance: PreflightRequestAcceptance = conductors[1]
+        .call_fallible(
+            &bob_zome,
+            "accept_countersigning_preflight_request",
+            preflight_request.clone(),
+        )
+        .await
+        .unwrap();
+    let bob_response = if let PreflightRequestAcceptance::Accepted(ref response) = bob_acceptance {
+        response
+    } else {
+        unreachable!();
+    };
+
+    // Alice's chain is locked, this should error
+    conductors[0]
+        .call_fallible::<_, ActionHash>(&alice_zome, "create_a_thing", ())
+        .await
+        .unwrap_err();
+
+    // Sorry Alice, you're done!
+    let (_, _): (ActionHash, EntryHash) = conductors[0]
+        .call_fallible(
+            &alice_zome,
+            "create_a_countersigned_thing_with_entry_hash",
+            vec![alice_response.clone(), bob_response.clone()],
+        )
+        .await
+        .unwrap();
+
+    // Alice's chain is locked, this should error
+    conductors[0]
+        .call_fallible::<_, ActionHash>(&alice_zome, "create_a_thing", ())
+        .await
+        .unwrap_err();
+
+    let (_, _): (ActionHash, EntryHash) = conductors[1]
+        .call_fallible(
+            &bob_zome,
+            "create_a_countersigned_thing_with_entry_hash",
+            vec![alice_response.clone(), bob_response.clone()],
+        )
+        .await
+        .unwrap();
+
+    let alice_rx = conductors[0].subscribe_to_app_signals("app".into());
+    let bob_rx = conductors[1].subscribe_to_app_signals("app".into());
+
+    wait_for_completion(alice_rx, preflight_request.app_entry_hash.clone()).await;
+    wait_for_completion(bob_rx, preflight_request.app_entry_hash).await;
+}
+
 async fn wait_for_completion(mut signal_rx: Receiver<Signal>, expected_hash: EntryHash) {
     let signal = tokio::time::timeout(std::time::Duration::from_secs(30), signal_rx.recv())
         .await
