4e13ae1f96d66c06d4e89dc9e9054bfe4cb3c057
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-09-21 00:56:53 +0100

    Countersigning workflow (#4188)
    * Database changes for locking
    
    * Fix locking logic
    
    * Check fixes
    
    * Add test for noisy signatures regression
    
    * Format
    
    * Add single lock test
    
    * Update changelog
    
    * Split witnessing workflow out of the countersigning workflow
    
    * Move pre-flight acceptance into the workflow to allow state to be updated
    
    * Rewire triggers and prepare countersigning workflow
    
    * Workflow triggering
    
    * Simplify chain locks and remove automatic expiry
    
    * Refactor success flow
    
    * Debug test failures
    
    * Format
    
    * Fix tests
    
    * Clear sessions that time out without making a commit
    
    * Format
    
    * Implement authority queries and abandon
    
    * Format
    
    * Signal abandoned countersigning sessions
    
    * Resolve and retry sessions in a bad state
    
    * Publish when the session completed without us
    
    * Static
    
    * Fix race conditions in signal emission
    
    * Add failing test to debug recovery
    
    * Update unknown state
    
    * Make session abandon work
    
    * Add completed session recovery test and fix the refresh logic to make it work
    
    * Split up the workflow and fix warnings
    
    * Rename SessionState to CountersigningSessionState
    
    * Set up testing for the countersigning workflow
    
    * Rough success test
    
    * Tidy up test code into reusable fixture
    
    * Add abandon recovery test
    
    * Add complete recovery test
    
    * Lint
    
    * Test stay in unknown state
    
    * Avoid duplicate triggers
    
    * Less unused now there are tests
    
    * Time out if signatures not received and add more tests
    
    * Fix session timeout test
    
    * Avoid missing signals in recovery test
    
    * Stabilise sig bundle noise test
    
    * Update crates/holochain/src/core/workflow/countersigning_workflow/complete.rs
    
    Co-authored-by: David Braden <neonphog@gmail.com>
    
    * Update crates/holochain/src/core/workflow/countersigning_workflow/incomplete.rs
    
    Co-authored-by: David Braden <neonphog@gmail.com>
    
    * Update crates/holochain/src/core/workflow/countersigning_workflow/incomplete.rs
    
    Co-authored-by: David Braden <neonphog@gmail.com>
    
    * Update crates/holochain/src/core/workflow/countersigning_workflow/refresh.rs
    
    Co-authored-by: David Braden <neonphog@gmail.com>
    
    * Add new test for restart after commit that failed validation
    
    * Resolve TODO by removing unused code
    
    * Remove pointless `get_agent_activity` calls in countersigning test
    
    * Format
    
    * Update crates/holochain/src/core/ribosome/host_fn/accept_countersigning_preflight_request.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Prefer `is_expired_at` over `is_expired`
    
    * Update changelog
    
    * Add more logging for easier debugging
    
    * Update crates/holochain/src/core/workflow/call_zome_workflow.rs
    
    Co-authored-by: Michael Dougherty <maackle.d@gmail.com>
    
    * Format
    
    * Delay CHC push until countersigning publish
    
    * Fix countersigning tests
    
    * Fix removal logic
    
    * 4159 countersigning workflow rework debug3 (#4257)
    
    * Print more information
    
    * Better count
    
    * Tidy
    
    * Only remove if matches fingerprint
    
    * Always check current session against expected
    
    * Add a test to verify changes
    
    * Tidy
    
    * Format toml
    
    * 4159 countersigning workflow rework debug4 (#4259)
    
    * Log publish record
    
    * Move workflow to cell, from dna
    
    * Downgrade to debug log
    
    * Fix tests, a performance issue and add review notes
    
    * Fix confusing log message
    
    * Prevent CHC push when the chain is locked
    
    * Clippy
    
    * Allow functions to be scheduled when the chain is locked
    
    * Try resolve on timeout
    
    * Rework state machine flows to handle running resolution after a timeout
    
    ---------
    
    Co-authored-by: David Braden <neonphog@gmail.com>
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    Co-authored-by: Michael Dougherty <maackle.d@gmail.com>

--- crates/holochain/src/conductor/cell.rs
+++ crates/holochain/src/conductor/cell.rs
@@ -32,17 +32,17 @@ use holochain_types::db_cache::DhtDbQueryCache;
 
 use crate::conductor::api::CellConductorApi;
 use crate::conductor::cell::error::CellResult;
-use crate::core::queue_consumer::spawn_queue_consumer_tasks;
 use crate::core::queue_consumer::InitialQueueTriggers;
 use crate::core::queue_consumer::QueueTriggers;
+use crate::core::queue_consumer::{spawn_queue_consumer_tasks, TriggerSender};
 use crate::core::ribosome::guest_callback::init::InitResult;
 use crate::core::ribosome::real_ribosome::RealRibosome;
 use crate::core::ribosome::ZomeCallInvocation;
 use crate::core::workflow::call_zome_workflow;
 use crate::core::workflow::countersigning_workflow::countersigning_success;
-use crate::core::workflow::countersigning_workflow::incoming_countersigning;
 use crate::core::workflow::genesis_workflow::genesis_workflow;
 use crate::core::workflow::initialize_zomes_workflow;
+use crate::core::workflow::witnessing_workflow::receive_incoming_countersigning_ops;
 use crate::core::workflow::CallZomeWorkflowArgs;
 use crate::core::workflow::GenesisWorkflowArgs;
 use crate::core::workflow::GenesisWorkspace;
@@ -141,7 +141,8 @@ impl Cell {
                 &space,
                 conductor_handle.clone(),
             )
-            .await?;
+            .await
+            .map_err(Box::new)?;
 
             Ok((
                 Self {
@@ -585,7 +586,7 @@ impl Cell {
         Ok(())
     }
 
-    #[cfg_attr(feature = "instrument", tracing::instrument(skip(self, message)))]
+    #[cfg_attr(feature = "instrument", tracing::instrument(skip_all))]
     /// we are receiving a response from a countersigning authority
     async fn handle_countersigning_session_negotiation(
         &self,
@@ -600,25 +601,24 @@ impl Cell {
                         (hash, op)
                     })
                     .collect();
-                incoming_countersigning(
+                receive_incoming_countersigning_ops(
                     ops,
-                    &self.space.countersigning_workspace,
-                    self.queue_triggers.countersigning.clone(),
+                    &self.space.witnessing_workspace,
+                    self.queue_triggers.witnessing.clone(),
                 )
                 .map_err(Box::new)?;
                 Ok(())
             }
             CountersigningSessionNegotiationMessage::AuthorityResponse(signed_actions) => {
-                Ok(countersigning_success(
+                countersigning_success(
                     self.space.clone(),
-                    &self.holochain_p2p_cell,
                     self.id.agent_pubkey().clone(),
                     signed_actions,
-                    self.queue_triggers.clone(),
-                    self.signal_tx.clone(),
+                    self.queue_triggers.countersigning.clone(),
                 )
-                .await
-                .map_err(Box::new)?)
+                .await;
+
+                Ok(())
             }
         }
     }
@@ -955,6 +955,7 @@ impl Cell {
             args,
             self.queue_triggers.publish_dht_ops.clone(),
             self.queue_triggers.integrate_dht_ops.clone(),
+            self.queue_triggers.countersigning.clone(),
         )
         .await
         .map_err(Box::new)?)
@@ -1080,6 +1081,10 @@ impl Cell {
         self.queue_triggers
             .publish_dht_ops
             .trigger(&"publish_authored_ops");
+    }
+
+    pub(crate) fn countersigning_trigger(&self) -> TriggerSender {
+        self.queue_triggers.countersigning.clone()
     }
 
     #[cfg(any(test, feature = "test_utils"))]
