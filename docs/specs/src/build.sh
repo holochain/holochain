005414b44185226c7a15ab35ff1f0e3ea317e510
Eric Harris-Braun <eric@harris-braun.com>
2024-09-20 12:01:19 -0400

    add players of ludos

--- docs/specs/src/build.sh
+++ docs/specs/src/build.sh
@@ -2,3 +2,6 @@
 sed -e '$s/$/\n/' -s hwp_*.md > hwp.md
 pandoc -L diagram-1.0.0/diagram.lua --extract-media media/ -f markdown -t latex hwp.md --template ./pandoc-template.latex > hwp.tex
 pdflatex --shell-escape hwp.tex
+pdflatex --shell-escape hwp_alpha.tex
+pandoc -f markdown -t latex holochain-players-of-ludos.md --template ./pandoc-template.latex > holochain-players-of-ludos.tex
+pdflatex --shell-escape holochain-players-of-ludos.tex
