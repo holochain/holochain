da3c8484a9ddc302f3b7fef4e44bd294d69b3f49
abe-njama <62102332+abe-njama@users.noreply.github.com>
2024-10-02 06:49:57 +0000

    [create-pull-request] automated change

--- versions/weekly/flake.nix
+++ versions/weekly/flake.nix
@@ -2,7 +2,7 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.4.0-dev.27";
+        url = "github:holochain/holochain/holochain-0.4.0-dev.28";
         flake = false;
       };
 
