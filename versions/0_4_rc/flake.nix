d2c5bd1f9a2926dfad7e2f1fe82c3abb8abbab60
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-05 00:18:15 +0100

    Add `0_4_rc` Holonix version

---
+++ versions/0_4_rc/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs =
+    {
+      holochain = {
+        url = "github:holochain/holochain/holochain-0.4.0-rc.0";
+        flake = false;
+      };
+
+      lair = {
+        url = "github:holochain/lair/lair_keystore-v0.5.2";
+        flake = false;
+      };
+
+      # holochain_cli_launch
+      launcher = {
+        url = "github:holochain/hc-launch/holochain-weekly";
+        flake = false;
+      };
+
+      # holochain_scaffolding_cli
+      scaffolding = {
+        url = "github:holochain/scaffolding/holochain-weekly";
+        flake = false;
+      };
+    };
+
+  outputs = { ... }: { };
+}
