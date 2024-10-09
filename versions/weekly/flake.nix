d4fadd89159ad201509e3bc5ec86b25c10f6feac
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-09 20:31:26 +0100

    Update Holonix weekly

--- versions/weekly/flake.nix
+++ versions/weekly/flake.nix
@@ -2,7 +2,7 @@
   inputs =
     {
       holochain = {
-        url = "github:holochain/holochain/holochain-0.4.0-dev.28";
+        url = "github:holochain/holochain/holochain-0.5.0-dev.0";
         flake = false;
       };
 
