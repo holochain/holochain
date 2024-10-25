90dbc5c65fb3261de6dc3931da2a0cb3d1817e99
Matt Gabrenya <matt@buildyourweb.app>
2024-10-25 10:31:23 -0400

    feat(hc): override RUST_LOG to display holochain_cli_bundle warnings if not already set

--- crates/hc/src/bin/hc.rs
+++ crates/hc/src/bin/hc.rs
@@ -3,9 +3,12 @@ use holochain_cli as hc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    if std::env::var_os("RUST_LOG").is_some() {
-        holochain_trace::init_fmt(holochain_trace::Output::Log).ok();
+    // This is necessary to ensure deprecation warnings in holochain_cli_bundle are displayed by default
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "holochain_cli_bundle=warn");
     }
+    holochain_trace::init_fmt(holochain_trace::Output::Log).ok();
+
     let cli = hc::Cli::parse();
     cli.subcommand.run().await
 }
