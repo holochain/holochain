2bec168031282ef0595fb27e649fa79f35efc12a
Michael Dougherty <maackle.d@gmail.com>
2024-10-31 10:29:00 -0700

    Some more docs and cleanup (#4366)
    * Add some fields to the DB; add disposable event sourcing experiment (#4265)
    
    * WIP new Event
    
    * Basic correspondence between DB rows and Authored events
    
    * Add Fetched event and db migration for 2 new timestamps
    
    * Single pass over events
    
    * Alternate migration. (discard.)
    
    * Revert "Alternate migration. (discard.)"
    
    This reverts commit 0fe778069243d53063b85625eabbec4eecaf8069.
    
    * Stable roundtrip Events->DB->Events
    
    * Do away with in-memory op cache
    
    * Remove fallacious notion that authored data has validation events
    
    * Add validation receipt time to db
    
    * Docs
    
    * Remove comments
    
    * Clippy
    
    * Fix import
    
    * Add 3.sql
    
    * Fix some wrong Cargo.toml keys
    
    * Apply suggestions from code review
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * remove event source module
    
    * Update crates/holochain_types/src/dht_op.rs
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    Co-authored-by: neonphog <neonphog@gmail.com>
    
    * change 0_4_rc flake.nix to point to holochain-0.4 branch of hc-launch (#4327)
    
    * change 0_4_rc flake.nix to point to holochain-0.4 branch of hc-launch
    
    * chore(flakes): update versions/0_4_rc
    
    ---------
    
    Co-authored-by: Holochain Release Automation <hra+gh@holochain.org>
    
    * Use holochain-0.4 tag for 0_4_rc flake.nix (#4328)
    
    * Remove 4 crates that only @maackle used (#4324)
    
    Remove 4 crates that only @maackle ever used
    
    * Update Holonix weekly (#4329)
    
    * Include `installed_at` in InstalledApp & AppInfo, sort ListApps by installed_at desc (#4306)
    
    * feat: save installed_at timestamp in InstalledApp and include in AppInfo, sort ListApps by installed_at desc
    
    * doc: changelog
    
    * doc: include in docs
    
    * refactor: pass installed_at as parameter
    
    * feature gate new_legacy
    
    * chore: fmt
    
    * fix: sort apps by installed at
    
    * chore: fmt
    
    * test: fix assertion
    
    * chore: grammer
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Fix: a bad return value from a callback crashes the conductor (#4315)
    
    * test: add tests for calling a valid init
    
    * test: add tests for calling init with invalid return type
    
    * test: add tests for calling init with invalid parameters
    
    * feat: invalid callback return types returns RibosomeError
    
    * feat: invalid callback parameters returns RibosomeError
    
    * feat: WASM runtime error caused by invalid params returns RibosomeError
    
    * test: add tests for calling validate with valid declaration
    
    * test: add integrity test for validate with invalid return type
    
    * test: add integrity test for validate with invalid parameters
    
    ---------
    
    Co-authored-by: ThetaSinner <ThetaSinner@users.noreply.github.com>
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    
    * Module docs for task manager
    
    * Move TaskManagerClient to top
    
    * Remove duplicate graft_records_onto_source_chain
    
    * Fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Jost Schulte <28270981+jost-s@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jost Schulte <jost.schulte@protonmail.com>
    Co-authored-by: neonphog <neonphog@gmail.com>
    Co-authored-by: matthme <36768177+matthme@users.noreply.github.com>
    Co-authored-by: Holochain Release Automation <hra+gh@holochain.org>
    Co-authored-by: Collins Muriuki <hello@collinsmuriuki.xyz>
    Co-authored-by: ThetaSinner <ThetaSinner@users.noreply.github.com>
    Co-authored-by: Matt Gabrenya <matt@buildyourweb.app>
    Co-authored-by: Callum Dunster <cdunster@users.noreply.github.com>
    Co-authored-by: Jost Schulte <28270981+jost-s@users.noreply.github.com>

--- crates/holochain/src/conductor/manager/mod.rs
+++ crates/holochain/src/conductor/manager/mod.rs
@@ -1,9 +1,30 @@
-//! We want to have control over certain long running
-//! tasks that we care about.
-//! If a task that is added to the task manager ends
-//! then a reaction can be set.
-//! An example would be a websocket closes with an error
-//! and you want to restart it.
+//! Holochain Task Manager
+//!
+//! The TaskManager is used to manage long running tasks that are critical to the
+//! operation of the conductor.
+//!
+//! Tasks added to the manager can be in one of three groups:
+//!
+//! - Conductor: Tasks which are associated with the conductor as a whole
+//! - Dna: Tasks which are associated with a particular DNA
+//! - Cell: Tasks which are associated with a particular Cell
+//!
+//! The outcome of a task in a group can affect the other tasks in its group.
+//! Tasks which are critical to the operation of its group level will cause
+//! the other tasks in that group to be stopped.
+//!
+//! For instance, the tasks which run the workflows for a cell are critical
+//! to the cell's functioning, so if any of these tasks fail, then the cell
+//! is no longer able to function. Task failure is a signal that the cell
+//! needs to be shut down, so the task manager takes the steps necessary to
+//! accomplish that:
+//!
+//! 1. Stop all other tasks related to the cell, so they don't continue in the background.
+//! 2. Pause or disable any apps which depend on the cell, because the app cannot
+//!     function without the proper functioning of that cell.
+//!
+//! See [`crate::conductor::Conductor::reconcile_app_status_with_cell_status`] for more details
+//! on how app status is related to cell status.
 
 mod error;
 pub use error::*;
@@ -19,6 +40,165 @@ use tokio_stream::StreamExt;
 use tracing::*;
 
 use super::ConductorHandle;
+
+/// The main interface for interacting with a task manager.
+/// Contains functions for adding tasks to groups, stopping task groups,
+/// and shutting down the task manager.
+#[derive(Clone)]
+pub struct TaskManagerClient {
+    tm: Arc<Mutex<Option<task_motel::TaskManager<TaskGroup, TaskOutcome>>>>,
+}
+
+impl TaskManagerClient {
+    /// Construct the TaskManager and the outcome channel receiver
+    pub fn new(tx: OutcomeSender, scope: String) -> Self {
+        let span = tracing::info_span!("managed task", scope = scope);
+        let tm = task_motel::TaskManager::new_instrumented(span, tx, |g| match g {
+            TaskGroup::Conductor => None,
+            TaskGroup::Dna(_) => Some(TaskGroup::Conductor),
+            TaskGroup::Cell(cell_id) => Some(TaskGroup::Dna(Arc::new(cell_id.dna_hash().clone()))),
+        });
+        Self {
+            tm: Arc::new(Mutex::new(Some(tm))),
+        }
+    }
+
+    /// Stop all managed tasks and await their completion.
+    pub fn stop_all_tasks(&self) -> ShutdownHandle {
+        if let Some(tm) = self.tm.lock().as_mut() {
+            tokio::spawn(tm.stop_group(&TaskGroup::Conductor))
+        } else {
+            tracing::warn!("Tried to shutdown task manager while it's already shutting down");
+            tokio::spawn(async move {})
+        }
+    }
+
+    /// Stop all tasks associated with a Cell and await their completion.
+    #[cfg_attr(feature = "instrument", tracing::instrument(skip(self)))]
+    pub fn stop_cell_tasks(&self, cell_id: CellId) -> ShutdownHandle {
+        if let Some(tm) = self.tm.lock().as_mut() {
+            tokio::spawn(tm.stop_group(&TaskGroup::Cell(cell_id)).in_current_span())
+        } else {
+            tracing::warn!("Tried to shutdown cell's tasks while they're already shutting down");
+            tokio::spawn(async move {})
+        }
+    }
+
+    /// Stop all tasks and prevent any new tasks from being added to the manager.
+    /// Returns a future to await completion of all tasks.
+    pub fn shutdown(&mut self) -> ShutdownHandle {
+        if let Some(mut tm) = self.tm.lock().take() {
+            tokio::spawn(tm.stop_group(&TaskGroup::Conductor))
+        } else {
+            // already shutting down
+            tokio::spawn(async move {})
+        }
+    }
+
+    /// Add a conductor-level task whose outcome is ignored.
+    pub fn add_conductor_task_ignored<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        f: impl FnOnce() -> Fut + Send + 'static,
+    ) {
+        self.add_conductor_task(name, TaskKind::Ignore, move |stop| async move {
+            tokio::select! {
+                _ = stop => (),
+                _ = f() => (),
+            }
+            Ok(())
+        })
+    }
+
+    /// Add a conductor-level task which will cause the conductor to shut down if it fails
+    pub fn add_conductor_task_unrecoverable<
+        Fut: Future<Output = ManagedTaskResult> + Send + 'static,
+    >(
+        &self,
+        name: &str,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        self.add_conductor_task(name, TaskKind::Unrecoverable, f)
+    }
+
+    /// Add a DNA-level task which will cause all cells under that DNA to be disabled if
+    /// the task fails
+    pub fn add_dna_task_critical<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        dna_hash: Arc<DnaHash>,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        self.add_dna_task(name, TaskKind::DnaCritical(dna_hash.clone()), dna_hash, f)
+    }
+
+    /// Add a Cell-level task whose outcome is ignored
+    pub fn add_cell_task_ignored<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        cell_id: CellId,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        self.add_cell_task(name, TaskKind::Ignore, cell_id, f)
+    }
+
+    /// Add a Cell-level task which will cause that to be disabled if the task fails
+    pub fn add_cell_task_critical<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        cell_id: CellId,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        self.add_cell_task(name, TaskKind::CellCritical(cell_id.clone()), cell_id, f)
+    }
+
+    fn add_conductor_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        task_kind: TaskKind,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        let name = name.to_string();
+        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
+        self.add_task(TaskGroup::Conductor, f)
+    }
+
+    fn add_dna_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        task_kind: TaskKind,
+        dna_hash: Arc<DnaHash>,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        let name = name.to_string();
+        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
+        self.add_task(TaskGroup::Dna(dna_hash), f)
+    }
+
+    fn add_cell_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
+        &self,
+        name: &str,
+        task_kind: TaskKind,
+        cell_id: CellId,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        let name = name.to_string();
+        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
+        self.add_task(TaskGroup::Cell(cell_id), f)
+    }
+
+    fn add_task<Fut: Future<Output = TaskOutcome> + Send + 'static>(
+        &self,
+        group: TaskGroup,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
+    ) {
+        if let Some(tm) = self.tm.lock().as_mut() {
+            tm.add_task(group, f)
+        } else {
+            tracing::warn!("Tried to add task while task manager is shutting down.");
+        }
+    }
+}
 
 /// The "kind" of a managed task determines how the Result from the task's
 /// completion will be handled.
@@ -54,7 +234,7 @@ pub enum TaskOutcome {
 /// Spawn a task which performs some action after each task has completed,
 /// as recieved by the outcome channel produced by the task manager.
 #[cfg_attr(feature = "instrument", tracing::instrument(skip_all))]
-pub fn spawn_task_outcome_handler(
+pub(crate) fn spawn_task_outcome_handler(
     conductor: ConductorHandle,
     mut outcomes: OutcomeReceiver,
 ) -> JoinHandle<TaskManagerResult> {
@@ -265,164 +445,6 @@ pub enum TaskGroup {
 pub type OutcomeSender = futures::channel::mpsc::Sender<(TaskGroup, TaskOutcome)>;
 /// Channel receiver for task outcomes
 pub type OutcomeReceiver = futures::channel::mpsc::Receiver<(TaskGroup, TaskOutcome)>;
-
-/// A collection of channels and handles used by the Conductor to talk to the
-/// TaskManager task
-#[derive(Clone)]
-pub struct TaskManagerClient {
-    tm: Arc<Mutex<Option<task_motel::TaskManager<TaskGroup, TaskOutcome>>>>,
-}
-
-impl TaskManagerClient {
-    /// Construct the TaskManager and the outcome channel receiver
-    pub fn new(tx: OutcomeSender, scope: String) -> Self {
-        let span = tracing::info_span!("managed task", scope = scope);
-        let tm = task_motel::TaskManager::new_instrumented(span, tx, |g| match g {
-            TaskGroup::Conductor => None,
-            TaskGroup::Dna(_) => Some(TaskGroup::Conductor),
-            TaskGroup::Cell(cell_id) => Some(TaskGroup::Dna(Arc::new(cell_id.dna_hash().clone()))),
-        });
-        Self {
-            tm: Arc::new(Mutex::new(Some(tm))),
-        }
-    }
-
-    /// Stop all tasks and await their completion.
-    pub fn stop_all_tasks(&self) -> ShutdownHandle {
-        if let Some(tm) = self.tm.lock().as_mut() {
-            tokio::spawn(tm.stop_group(&TaskGroup::Conductor))
-        } else {
-            tracing::warn!("Tried to shutdown task manager while it's already shutting down");
-            tokio::spawn(async move {})
-        }
-    }
-
-    /// Stop all tasks for a Cell and await their completion.
-    #[cfg_attr(feature = "instrument", tracing::instrument(skip(self)))]
-    pub fn stop_cell_tasks(&self, cell_id: CellId) -> ShutdownHandle {
-        if let Some(tm) = self.tm.lock().as_mut() {
-            tokio::spawn(tm.stop_group(&TaskGroup::Cell(cell_id)).in_current_span())
-        } else {
-            tracing::warn!("Tried to shutdown cell's tasks while they're already shutting down");
-            tokio::spawn(async move {})
-        }
-    }
-
-    /// Stop all tasks and return a future to await their completion,
-    /// and prevent any new tasks from being added to the manager.
-    pub fn shutdown(&mut self) -> ShutdownHandle {
-        if let Some(mut tm) = self.tm.lock().take() {
-            tokio::spawn(tm.stop_group(&TaskGroup::Conductor))
-        } else {
-            // already shutting down
-            tokio::spawn(async move {})
-        }
-    }
-
-    /// Add a conductor-level task whose outcome is ignored.
-    pub fn add_conductor_task_ignored<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        f: impl FnOnce() -> Fut + Send + 'static,
-    ) {
-        self.add_conductor_task(name, TaskKind::Ignore, move |stop| async move {
-            tokio::select! {
-                _ = stop => (),
-                _ = f() => (),
-            }
-            Ok(())
-        })
-    }
-
-    /// Add a conductor-level task which will cause the conductor to shut down if it fails
-    pub fn add_conductor_task_unrecoverable<
-        Fut: Future<Output = ManagedTaskResult> + Send + 'static,
-    >(
-        &self,
-        name: &str,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        self.add_conductor_task(name, TaskKind::Unrecoverable, f)
-    }
-
-    /// Add a DNA-level task which will cause all cells under that DNA to be disabled if
-    /// the task fails
-    pub fn add_dna_task_critical<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        dna_hash: Arc<DnaHash>,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        self.add_dna_task(name, TaskKind::DnaCritical(dna_hash.clone()), dna_hash, f)
-    }
-
-    /// Add a Cell-level task whose outcome is ignored
-    pub fn add_cell_task_ignored<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        cell_id: CellId,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        self.add_cell_task(name, TaskKind::Ignore, cell_id, f)
-    }
-
-    /// Add a Cell-level task which will cause that to be disabled if the task fails
-    pub fn add_cell_task_critical<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        cell_id: CellId,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        self.add_cell_task(name, TaskKind::CellCritical(cell_id.clone()), cell_id, f)
-    }
-
-    fn add_conductor_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        task_kind: TaskKind,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        let name = name.to_string();
-        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
-        self.add_task(TaskGroup::Conductor, f)
-    }
-
-    fn add_dna_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        task_kind: TaskKind,
-        dna_hash: Arc<DnaHash>,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        let name = name.to_string();
-        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
-        self.add_task(TaskGroup::Dna(dna_hash), f)
-    }
-
-    fn add_cell_task<Fut: Future<Output = ManagedTaskResult> + Send + 'static>(
-        &self,
-        name: &str,
-        task_kind: TaskKind,
-        cell_id: CellId,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        let name = name.to_string();
-        let f = move |stop| f(stop).map(move |t| produce_task_outcome(&task_kind, t, name));
-        self.add_task(TaskGroup::Cell(cell_id), f)
-    }
-
-    fn add_task<Fut: Future<Output = TaskOutcome> + Send + 'static>(
-        &self,
-        group: TaskGroup,
-        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
-    ) {
-        if let Some(tm) = self.tm.lock().as_mut() {
-            tm.add_task(group, f)
-        } else {
-            tracing::warn!("Tried to add task while task manager is shutting down.");
-        }
-    }
-}
 
 /// A future which awaits the completion of all managed tasks
 pub type ShutdownHandle = JoinHandle<()>;
