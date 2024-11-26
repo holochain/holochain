d7a807b27edcefdb840ac73f8dac9a49beb155e4
Callum Dunster <cdunster@users.noreply.github.com>
2024-11-26 12:54:06 +0100

    chore: remove cargo update for release-automation
    The cargo update script was added to surface semver-incompatible changes
    in dependencies before users find them. However, release-automation
    isn't used externally and often causes the job to fail.

--- nix/modules/scripts.nix
+++ nix/modules/scripts.nix
@@ -169,8 +169,6 @@
 
             # Update the Holochain project Cargo.lock
             cargo update --manifest-path Cargo.toml
-            # Update the release-automation crate's Cargo.lock
-            cargo update --manifest-path crates/release-automation/Cargo.toml
             # Update the WASM workspace Cargo.lock
             cargo update --manifest-path crates/test_utils/wasm/wasm_workspace/Cargo.toml
           '';
