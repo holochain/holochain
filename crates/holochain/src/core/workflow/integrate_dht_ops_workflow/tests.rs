3d05fa7e0ff21ac24c2a6c87b84004282c4a1a68
Callum Dunster <cdunster@users.noreply.github.com>
2025-01-22 17:12:04 +0100

    test: add test for RegisterAgentActivity op for Create action

--- crates/holochain/src/core/workflow/integrate_dht_ops_workflow/tests.rs
+++ crates/holochain/src/core/workflow/integrate_dht_ops_workflow/tests.rs
@@ -645,6 +645,49 @@ fn register_agent_activity_open_chain(mut a: TestData) -> (Vec<Db>, Vec<Db>, &'s
 }
 
 #[allow(unused)] // Wrong detection by Clippy, due to unusual calling pattern
+fn register_agent_activity_create(mut a: TestData) -> (Vec<Db>, Vec<Db>, &'static str) {
+    // Previous op to depend on
+    let mut prev_create_action = fixt!(Create);
+    prev_create_action.author = a.original_action.author().clone();
+    prev_create_action.action_seq = 10;
+    prev_create_action.entry_type = EntryType::App(AppEntryDef {
+        entry_index: 0.into(),
+        zome_index: 0.into(),
+        visibility: EntryVisibility::Public,
+    });
+    let previous_action = Action::Create(prev_create_action.clone());
+    let previous_op: DhtOp =
+        ChainOp::RegisterAgentActivity(fixt!(Signature), Action::Create(prev_create_action)).into();
+    let previous_op_hash = DhtOpHash::with_data_sync(&previous_op);
+    let previous_op_hashed = DhtOpHashed::from_content_sync(previous_op.clone());
+
+    // Op to integrate
+    let mut create_action = fixt!(Create);
+    create_action.author = previous_action.author().clone();
+    create_action.action_seq = previous_action.action_seq() + 1;
+    create_action.prev_action = previous_action.to_hash();
+    create_action.timestamp = Timestamp::now();
+    let new_dht_op: DhtOp =
+        ChainOp::RegisterAgentActivity(fixt!(Signature), Action::Create(create_action)).into();
+    let new_dht_op_hash = DhtOpHash::with_data_sync(&new_dht_op);
+    let new_dht_op_hashed = DhtOpHashed::from_content_sync(new_dht_op.clone());
+
+    let pre_state = vec![
+        Db::Integrated(previous_op.clone()),
+        Db::IntQueue(new_dht_op.clone()),
+    ];
+    let expect = vec![
+        Db::Integrated(previous_op.clone()),
+        Db::Integrated(new_dht_op.clone()),
+    ];
+    (
+        pre_state,
+        expect,
+        "register agent activity for create action",
+    )
+}
+
+#[allow(unused)] // Wrong detection by Clippy, due to unusual calling pattern
 fn register_updated_record(a: TestData) -> (Vec<Db>, Vec<Db>, &'static str) {
     let original_op = ChainOp::StoreRecord(
         a.signature.clone(),
@@ -796,6 +839,7 @@ async fn test_ops_state() {
         register_agent_activity_delete_link,
         register_agent_activity_close_chain,
         register_agent_activity_open_chain,
+        register_agent_activity_create,
         register_replaced_by_for_entry,
         register_updated_record,
         register_deleted_by,
