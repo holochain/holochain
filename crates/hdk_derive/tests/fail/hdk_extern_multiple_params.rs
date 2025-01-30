d3e533cb4384b0323a6cb59a894854029bc6d584
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-01-30 10:47:53 +0300

    Remove out of scope testcase

--- crates/hdk_derive/tests/fail/hdk_extern_multiple_params.rs
+++
@@ -1,9 +0,0 @@
-use hdk::{map_extern, map_extern_infallible};
-use hdk_derive::hdk_extern;
-
-#[hdk_extern] //~ ERROR hdk_extern functions must take a single parameter or none
-fn multiple_params(a: String, b: i32) -> ExternResult<String> {
-    Ok(format!("{}{}", a, b))
-}
-
-fn main() {}
