8c2032aa484dc64aee3008e9985f911c8355a52f
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-02-14 14:59:58 +0000

    feat: Implement slice hash logic for the Holochain op store

--- crates/holochain_sqlite/src/sql/dht/ops_by_id.sql
+++ crates/holochain_sqlite/src/sql/dht/ops_by_id.sql
@@ -1,14 +1,13 @@
 SELECT
-    DhtOp.hash,
-    DhtOp.type,
-    Action.blob AS action_blob,
-    Action.author AS author,
-    Entry.blob AS entry_blob
+  DhtOp.hash,
+  DhtOp.type,
+  Action.blob AS action_blob,
+  Action.author AS author,
+  Entry.blob AS entry_blob
 FROM
-    DhtOp
-        JOIN Action ON DhtOp.action_hash = Action.hash
-        LEFT JOIN Entry ON Action.entry_hash = Entry.hash
+  DhtOp
+  JOIN Action ON DhtOp.action_hash = Action.hash
+  LEFT JOIN Entry ON Action.entry_hash = Entry.hash
 WHERE
-    DhtOp.hash in rarray(:hashes)
-    AND DhtOp.when_integrated IS NOT NULL
-    AND DhtOp.withhold_publish IS NULL
+  DhtOp.hash in rarray(:hashes)
+  AND DhtOp.when_integrated IS NOT NULL
