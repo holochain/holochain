565d6fa9dfe3ff04e6be80b3c273c7228155d38d
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-02 01:01:51 +0000

    fix: Fix storage info for empty databases

--- crates/holochain_sqlite/src/stats.rs
+++ crates/holochain_sqlite/src/stats.rs
@@ -2,11 +2,19 @@ use crate::error::DatabaseError;
 use rusqlite::Transaction;
 
 pub fn get_size_on_disk(txn: Transaction) -> Result<usize, DatabaseError> {
-    txn.query_row("select sum(pgsize) from dbstat", (), |r| r.get(0))
-        .map_err(DatabaseError::SqliteError)
+    Ok(txn
+        .query_row("select sum(pgsize) from dbstat", (), |r| {
+            r.get::<_, Option<usize>>(0)
+        })
+        .map_err(DatabaseError::SqliteError)?
+        .unwrap_or_default())
 }
 
 pub fn get_used_size(txn: Transaction) -> Result<usize, DatabaseError> {
-    txn.query_row("select sum(pgsize - unused) from dbstat", (), |r| r.get(0))
-        .map_err(DatabaseError::SqliteError)
+    Ok(txn
+        .query_row("select sum(pgsize - unused) from dbstat", (), |r| {
+            r.get::<_, Option<usize>>(0)
+        })
+        .map_err(DatabaseError::SqliteError)?
+        .unwrap_or_default())
 }
