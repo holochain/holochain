bc71ba7545f50ab69dc71498977aac639d509060
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-03 02:06:44 +0000

    feat: Expose test keystore with password

--- crates/holochain_keystore/src/lib.rs
+++ crates/holochain_keystore/src/lib.rs
@@ -6,15 +6,21 @@
 //!
 //! # Examples
 //!
-//! ```
+//! ```rust,no_run
 //! use holo_hash::AgentPubKey;
+//! use std::path::Path;
+//! use std::path::PathBuf;
 //! use holochain_keystore::*;
+//! use holochain_keystore::lair_keystore::*;
 //! use holochain_serialized_bytes::prelude::*;
 //!
 //! #[tokio::main(flavor = "multi_thread")]
 //! async fn main() {
 //!     tokio::task::spawn(async move {
-//!         let keystore = holochain_keystore::spawn_test_keystore().await.unwrap();
+//!         let mut passphrase = sodoken::BufWrite::new_mem_locked(32).unwrap();
+//!         passphrase.write_lock().copy_from_slice(b"passphrase");
+//!
+//!         let keystore = spawn_lair_keystore_in_proc(&PathBuf::from("/"), passphrase.to_read()).await.unwrap();
 //!         let agent_pubkey = AgentPubKey::new_random(&keystore).await.unwrap();
 //!
 //!         #[derive(Debug, serde::Serialize, serde::Deserialize, SerializedBytes)]
@@ -51,20 +57,54 @@ pub use test_keystore::*;
 pub mod crude_mock_keystore;
 
 /// Construct a simple in-memory in-process keystore.
+///
+/// # Examples
+///
+/// ```
+/// use holo_hash::AgentPubKey;
+/// use holochain_keystore::*;
+/// use holochain_serialized_bytes::prelude::*;
+///
+/// #[tokio::main(flavor = "multi_thread")]
+/// async fn main() {
+///     tokio::task::spawn(async move {
+///         let keystore = spawn_mem_keystore().await.unwrap();
+///         let agent_pubkey = AgentPubKey::new_random(&keystore).await.unwrap();
+///
+///         #[derive(Debug, serde::Serialize, serde::Deserialize, SerializedBytes)]
+///         struct MyData(Vec<u8>);
+///
+///         let my_data_1 = MyData(b"signature test data 1".to_vec());
+///
+///         let signature = agent_pubkey.sign(&keystore, &my_data_1).await.unwrap();
+///
+///         assert!(agent_pubkey.verify_signature(&signature, &my_data_1).await.unwrap());
+///     }).await.unwrap();
+/// }
+/// ```
+#[cfg(feature = "test_utils")]
 pub async fn spawn_mem_keystore() -> LairResult<MetaLairClient> {
-    use kitsune_p2p_types::dependencies::lair_keystore_api;
-    use lair_keystore_api::prelude::*;
-    use std::sync::Arc;
-
     // in-memory secure random passphrase
     let passphrase = sodoken::BufWrite::new_mem_locked(32)?;
     sodoken::random::bytes_buf(passphrase.clone()).await?;
+
+    spawn_mem_keystore_with_passphrase(passphrase.to_read()).await
+}
+
+/// Construct a simple in-memory in-process keystore with the given passphrase.
+#[cfg(feature = "test_utils")]
+pub async fn spawn_mem_keystore_with_passphrase(
+    passphrase: sodoken::BufRead,
+) -> LairResult<MetaLairClient> {
+    use kitsune_p2p_types::dependencies::lair_keystore_api;
+    use lair_keystore_api::prelude::*;
+    use std::sync::Arc;
 
     // in-mem / in-proc config
     let config = Arc::new(
         PwHashLimits::Minimum
             .with_exec(|| {
-                lair_keystore_api::config::LairServerConfigInner::new("/", passphrase.to_read())
+                lair_keystore_api::config::LairServerConfigInner::new("/", passphrase.clone())
             })
             .await?,
     );
@@ -73,7 +113,7 @@ pub async fn spawn_mem_keystore() -> LairResult<MetaLairClient> {
     let keystore = lair_keystore_api::in_proc_keystore::InProcKeystore::new(
         config,
         lair_keystore_api::mem_store::create_mem_store_factory(),
-        passphrase.to_read(),
+        passphrase,
     )
     .await?;
 
