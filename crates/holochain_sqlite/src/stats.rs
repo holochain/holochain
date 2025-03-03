f24fee0479b6562daf083e27d4cd0022487badb4
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-03 11:50:01 +0000

    chore: Use optional

--- crates/holochain_sqlite/src/stats.rs
+++ crates/holochain_sqlite/src/stats.rs
@@ -1,3 +1,4 @@
+use rusqlite::OptionalExtension;
 use crate::{
     db::{DbKindT, Txn},
     error::DatabaseError,
@@ -5,18 +6,16 @@ use crate::{
 
 pub fn get_size_on_disk<K: DbKindT>(txn: &Txn<K>) -> Result<usize, DatabaseError> {
     Ok(txn
-        .query_row("select sum(pgsize) from dbstat", (), |r| {
-            r.get::<_, Option<usize>>(0)
-        })
+        .query_row("select sum(pgsize) from dbstat", (), |r| r.get(0))
+        .optional()
         .map_err(DatabaseError::SqliteError)?
         .unwrap_or_default())
 }
 
 pub fn get_used_size<K: DbKindT>(txn: &Txn<K>) -> Result<usize, DatabaseError> {
     Ok(txn
-        .query_row("select sum(pgsize - unused) from dbstat", (), |r| {
-            r.get::<_, Option<usize>>(0)
-        })
+        .query_row("select sum(pgsize - unused) from dbstat", (), |r| r.get(0))
+        .optional()
         .map_err(DatabaseError::SqliteError)?
         .unwrap_or_default())
 }
