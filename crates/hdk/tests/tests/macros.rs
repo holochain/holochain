9841fcace1e15f25a3b3c0360de57baa2c7679b8
Collins Muriuki <hello@collinsmuriuki.xyz>
2025-03-18 18:43:42 +0300

    Fix failing tests

--- crates/hdk/tests/tests/macros.rs
+++ crates/hdk/tests/tests/macros.rs
@@ -2,5 +2,5 @@
 #[ignore = "this is a long-running test that slows down ci - execute this manually"]
 fn macros() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/macros/*.rs");
+    t.compile_fail("tests/tests/macros/*.rs");
 }
