6a6cc8937b6962271fe7171b797f2444fe3b6329
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-19 16:49:52 +0000

    chore: Use semaphore instead of lock

--- crates/hc_sandbox/tests/cli.rs
+++ crates/hc_sandbox/tests/cli.rs
@@ -16,7 +16,7 @@ use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 use std::process::Stdio;
 use std::str::FromStr;
-use std::sync::{Arc, OnceLock};
+use std::sync::Arc;
 use std::time::Duration;
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::process::{Child, Command};
@@ -108,11 +108,8 @@ async fn check_timeout<T>(response: impl Future<Output = WebsocketResult<T>>) ->
 }
 
 async fn package_fixture_if_not_packaged() {
-    static PACKAGE_LOCK: OnceLock<tokio::sync::Mutex<()>> = OnceLock::new();
-    let _lock = PACKAGE_LOCK
-        .get_or_init(|| tokio::sync::Mutex::new(()))
-        .lock()
-        .await;
+    static PACKAGE_SEMAPHORE: tokio::sync::Semaphore = tokio::sync::Semaphore::const_new(1);
+    let _lock = PACKAGE_SEMAPHORE.acquire().await.unwrap();
 
     if PathBuf::from("tests/fixtures/my-app/my-fixture-app.happ").exists()
         && PathBuf::from("tests/fixtures/my-app-deferred/my-fixture-app-deferred.happ").exists()
