f0f1bfce953130090e9147a52a1490fa1d337d89
Matthias <tuttiacc@hotmail.com>
2025-04-03 15:47:13 +0200

    fix dna path

--- crates/hc_bundle/tests/test_cli.rs
+++ crates/hc_bundle/tests/test_cli.rs
@@ -286,7 +286,7 @@ async fn test_multi_integrity() {
             .unwrap()
     };
 
-    let (dna, _) = pack_dna("tests/fixtures/my-app/dnas/dna5").await;
+    let (dna, _) = pack_dna("tests/fixtures/my-app/dnas/dna6").await;
 
     // The actual wasm hashes of the fake zomes.
     let wasm_hash = WasmHash::from_raw_39(vec![
