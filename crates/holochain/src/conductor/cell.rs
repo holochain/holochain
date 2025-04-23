3d504988f730f6e0af26b45a344e9ca2c318cd96
guillem.cordoba <guillem.cordoba@gmail.com>
2025-04-23 13:14:30 +0200

    Fixed countersigning trigger

--- crates/holochain/src/conductor/cell.rs
+++ crates/holochain/src/conductor/cell.rs
@@ -33,6 +33,7 @@ use crate::conductor::cell::error::CellResult;
 use crate::core::queue_consumer::spawn_queue_consumer_tasks;
 use crate::core::queue_consumer::InitialQueueTriggers;
 use crate::core::queue_consumer::QueueTriggers;
+use crate::core::queue_consumer::TriggerSender;
 use crate::core::ribosome::guest_callback::init::InitResult;
 use crate::core::ribosome::real_ribosome::RealRibosome;
 use crate::core::ribosome::ZomeCallInvocation;
@@ -923,6 +924,10 @@ impl Cell {
         self.queue_triggers
             .publish_dht_ops
             .trigger(&"publish_authored_ops");
+    }
+
+    pub(crate) fn countersigning_trigger(&self) -> TriggerSender {
+        self.queue_triggers.countersigning.clone()
     }
 
     #[cfg(any(test, feature = "test_utils"))]
