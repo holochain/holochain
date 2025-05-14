af905769ef941e922306877712d764d09d91bd4a
mattyg <matt@buildyourweb.app>
2025-05-14 07:20:06 -0700

    chore: url schema
    Co-authored-by: David Braden <neonphog@gmail.com>

--- crates/holochain_conductor_api/src/config/conductor.rs
+++ crates/holochain_conductor_api/src/config/conductor.rs
@@ -33,7 +33,7 @@
 //!   ## to what you can see here:
 //!   webrtc_config: {
 //!     "iceServers": [
-//!       { "urls": ["stun://stun.l.google.com:19302"] }
+//!       { "urls": ["stun:stun.l.google.com:19302"] }
 //!     ]
 //!   }
 //! "#;
