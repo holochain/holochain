c6609f68fe2ecdd8e0f5f7b9a04bae4a440712fa
Eric Harris-Braun <eric@harris-braun.com>
2025-05-23 11:07:36 -0400

    cleanup

--- crates/client/tests/common/mod.rs
+++ crates/client/tests/common/mod.rs
@@ -4,9 +4,11 @@ use kitsune2_test_utils::agent::AgentBuilder;
 use std::sync::Arc;
 
 pub fn make_agent(space: &SpaceId) -> String {
-    let mut builder = AgentBuilder::default();
-    let local_agent: DynLocalAgent = Arc::new(Ed25519LocalAgent::default());
-    builder.space = Some(space.clone());
-    let info = builder.build(local_agent);
-    info.encode().unwrap()
+    AgentBuilder {
+        space: Some(space.clone()),
+        ..Default::default()
+    }
+    .build(Arc::new(Ed25519LocalAgent::default()) as DynLocalAgent)
+    .encode()
+    .unwrap()
 }
