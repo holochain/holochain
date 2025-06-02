b9b719f3538b9062a2b6f717b3d6624e04bebba8
Callum Dunster <cdunster@users.noreply.github.com>
2025-06-02 18:26:08 +0200

    test: parallel must_get_agent_activity with unresponsive peers

--- crates/holochain_p2p/tests/tests/node_messaging.rs
+++ crates/holochain_p2p/tests/tests/node_messaging.rs
@@ -968,6 +968,49 @@ async fn test_must_get_agent_activity() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn test_must_get_agent_activity_with_unresponsive_agents() {
+    let dna_hash = DnaHash::from_raw_36(vec![0; 36]);
+    let space = dna_hash.to_k2_space();
+    let handler = Arc::new(Handler::default());
+    let unresponsive_handler = Arc::new(UnresponsiveHandler);
+
+    let (_agent1, hc1, _) = spawn_test(dna_hash.clone(), handler.clone()).await;
+    let (_agent2, hc2, _) = spawn_test(dna_hash.clone(), unresponsive_handler.clone()).await;
+    let (_agent3, hc3, _) = spawn_test(dna_hash.clone(), unresponsive_handler).await;
+    let (_agent4, hc4, _) = spawn_test(dna_hash.clone(), handler).await;
+
+    hc1.test_set_full_arcs(space.clone()).await;
+    hc2.test_set_full_arcs(space.clone()).await;
+    hc3.test_set_full_arcs(space.clone()).await;
+    hc4.test_set_full_arcs(space.clone()).await;
+
+    tokio::time::timeout(std::time::Duration::from_secs(20), async {
+        loop {
+            tokio::time::sleep(std::time::Duration::from_millis(1)).await;
+
+            // If we get a response at all then at least one peer completed the request
+            if hc1
+                .must_get_agent_activity(
+                    dna_hash.clone(),
+                    AgentPubKey::from_raw_36(vec![2; 36]),
+                    ChainFilter {
+                        chain_top: ActionHash::from_raw_36(vec![3; 36]),
+                        filters: ChainFilters::ToGenesis,
+                        include_cached_entries: false,
+                    },
+                )
+                .await
+                .is_ok()
+            {
+                return;
+            }
+        }
+    })
+    .await
+    .unwrap();
+}
+
+#[tokio::test(flavor = "multi_thread")]
 async fn test_validation_receipts() {
     let dna_hash = DnaHash::from_raw_36(vec![0; 36]);
     let handler = Arc::new(Handler::default());
