cc03f4e45c1996faba2bfed5955806efe7a84a5a
Matthias <tuttiacc@hotmail.com>
2025-06-24 20:47:28 +0200

    feat: app and admin API endpoints to get peer meta info

--- crates/holochain_sqlite/src/helpers.rs
+++ crates/holochain_sqlite/src/helpers.rs
@@ -1,3 +1,26 @@
+use bytes::Bytes;
+use rusqlite::{
+    types::{FromSql, FromSqlResult, ToSqlOutput, ValueRef},
+    ToSql,
+};
+
+/// A convenient struct to handle Bytes in SQL queries
+pub struct BytesSql(pub Bytes);
+
+impl ToSql for BytesSql {
+    #[inline]
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        Ok(ToSqlOutput::from(&self.0[..]))
+    }
+}
+
+impl FromSql for BytesSql {
+    #[inline]
+    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
+        Ok(BytesSql(Bytes::copy_from_slice(value.as_blob()?)))
+    }
+}
+
 /// A shrinkwrapped type with a Drop impl provided as a simple closure
 #[derive(shrinkwraprs::Shrinkwrap)]
 #[shrinkwrap(mutable, unsafe_ignore_visibility)]
