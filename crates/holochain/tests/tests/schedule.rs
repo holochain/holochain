338a7896001239372a271328c0f7b5e0596c9eac
ddd-mtl <doute.d@gmail.com>
2025-08-13 13:11:10 -0400

    add schedule_ephemeral_error test case ; have tests failing

--- crates/holochain/tests/tests/schedule.rs
+++ crates/holochain/tests/tests/schedule.rs
@@ -5,24 +5,25 @@ use holochain_sqlite::error::DatabaseError;
 use holochain_state::schedule::fn_is_scheduled;
 use holochain_types::inline_zome::InlineZomeSet;
 use holochain_types::signal::Signal;
-use holochain_zome_types::prelude::Schedule::Persisted;
+use holochain_zome_types::prelude::Schedule::{Ephemeral, Persisted};
 use holochain_zome_types::prelude::{Schedule, ScheduledFn};
 use holochain_zome_types::signal::AppSignal;
+use std::time::Duration;
 use tokio::sync::broadcast;
 use tokio::time::error::Elapsed;
 
 /// Test schedule ephemeral fn
 /// Assuming a schedular interval of 100ms
 #[tokio::test(flavor = "multi_thread")]
-async fn schedule_ephemeral() {
+async fn schedule_ephemeral_ok() {
     holochain_trace::test_run();
 
     // Start with a duration of 3ms and decrease by 1ms each time it's called.
     let zome = create_schedule_zome(|api, input| {
         let _ = api.emit_signal(AppSignal::new(ExternIO::encode(input.clone()).unwrap()));
         let ms: u64 = match input {
-            Some(Schedule::Ephemeral(duration)) => duration.as_millis() as u64,
             None => 3,
+            Some(Schedule::Ephemeral(duration)) => duration.as_millis() as u64,
             _ => panic!("Expected Ephemeral Schedule"),
         };
         match ms {
@@ -64,10 +65,50 @@ async fn schedule_ephemeral() {
     assert!(!is_scheduled(&host_fn_caller, &cell).await);
 }
 
+/// Test schedule ephemeral function which gives an error
+/// Assuming a schedular interval of 100ms
+#[tokio::test(flavor = "multi_thread")]
+async fn schedule_ephemeral_error() {
+    holochain_trace::test_run();
+
+    // Start with a crontab that triggers every second, then have it return an error.
+    let zome = create_schedule_zome(|api, input| {
+        let _ = api.emit_signal(AppSignal::new(ExternIO::encode(input.clone()).unwrap()));
+        match input {
+            None => Ok(Some(Ephemeral(Duration::from_secs(1)))),
+            _ => Err(holochain::prelude::InlineZomeError::TestError(
+                "Intentional error".to_string(),
+            )),
+        }
+    });
+
+    let dna = SweetDnaFile::unique_from_inline_zomes(zome).await;
+    let mut conductor = SweetConductor::from_standard_config().await;
+    let app = conductor.setup_app("app", &[dna.0.clone()]).await.unwrap();
+    let cell = app.into_cells()[0].clone();
+    let mut app_signal = conductor.subscribe_to_app_signals("app".into());
+    let host_fn_caller =
+        HostFnCaller::create_for_zome(cell.cell_id(), &conductor.raw_handle(), &dna.0, 0).await;
+
+    // Start test: schedule function
+    conductor
+        .call::<(), ()>(&cell.zome("coordinator"), "start", ())
+        .await;
+
+    // Scheduled function is first called with None input.
+    assert_eq!(None, wait_for_signal(&mut app_signal).await.unwrap());
+    // Scheduled function is called with input from previous output.
+    assert!(wait_for_signal(&mut app_signal).await.unwrap().is_some());
+    // Should be unscheduled
+    assert!(wait_for_signal(&mut app_signal).await.is_err());
+    assert!(!is_scheduled(&host_fn_caller, &cell).await);
+}
+
 /// Test persisted schedule with invalid crontab output
 /// Assuming a schedular interval of 100ms
 #[tokio::test(flavor = "multi_thread")]
-async fn schedule_bad_crontab() {
+#[ignore = "Test not passing ; awaiting bug fix"]
+async fn schedule_persisted_bad_crontab() {
     holochain_trace::test_run();
 
     // Start with a valid crontab then set an invalid one.
@@ -81,9 +122,11 @@ async fn schedule_bad_crontab() {
 
     let dna = SweetDnaFile::unique_from_inline_zomes(zome).await;
     let mut conductor = SweetConductor::from_standard_config().await;
-    let app = conductor.setup_app("app", &[dna.0]).await.unwrap();
+    let app = conductor.setup_app("app", &[dna.0.clone()]).await.unwrap();
     let cell = app.into_cells()[0].clone();
     let mut app_signal = conductor.subscribe_to_app_signals("app".into());
+    let host_fn_caller =
+        HostFnCaller::create_for_zome(cell.cell_id(), &conductor.raw_handle(), &dna.0, 0).await;
 
     conductor
         .call::<(), ()>(&cell.zome("coordinator"), "start", ())
@@ -95,14 +138,16 @@ async fn schedule_bad_crontab() {
     // Scheduled function is then called with Some input
     assert!(wait_for_signal(&mut app_signal).await.unwrap().is_some());
 
-    // On bad crontab scheduled function keeps its previous schedule
-    assert!(wait_for_signal(&mut app_signal).await.unwrap().is_some());
+    // On bad crontab scheduled function should unschedule
+    assert!(wait_for_signal(&mut app_signal).await.is_err());
+    assert!(!is_scheduled(&host_fn_caller, &cell).await);
 }
 
 /// Test schedule persisted function with changing crontab
 /// Assuming a schedular interval of 100ms
 #[tokio::test(flavor = "multi_thread")]
-async fn schedule_crontab_changing() {
+#[ignore = "Test not passing ; awaiting bug fix"]
+async fn schedule_persisted_ok() {
     holochain_trace::test_run();
 
     // Start with a crontab that triggers every 3 secs, then decrease frequency by 1 sec
@@ -110,8 +155,8 @@ async fn schedule_crontab_changing() {
     let zome = create_schedule_zome(|api, input| {
         let _ = api.emit_signal(AppSignal::new(ExternIO::encode(input.clone()).unwrap()));
         let cron = match input {
-            Some(Schedule::Persisted(str)) => str,
             None => "*/3 * * * * * *".to_string(),
+            Some(Schedule::Persisted(str)) => str,
             _ => panic!("Expected Persisted Schedule"),
         };
         let n: usize = cron.chars().nth(2).unwrap().to_digit(10).unwrap() as usize;
@@ -129,6 +174,8 @@ async fn schedule_crontab_changing() {
     let app = conductor.setup_app("app", &[dna.0.clone()]).await.unwrap();
     let cell = app.into_cells()[0].clone();
     let mut app_signal = conductor.subscribe_to_app_signals("app".into());
+    let host_fn_caller =
+        HostFnCaller::create_for_zome(cell.cell_id(), &conductor.raw_handle(), &dna.0, 0).await;
 
     // Start test: schedule function
     conductor
@@ -147,18 +194,16 @@ async fn schedule_crontab_changing() {
         Some(Schedule::Persisted("*/1 * * * * * *".to_string())),
         wait_for_signal(&mut app_signal).await.unwrap()
     );
-    // Scheduled function is called with input from previous output.
-    // TODO: Fix unschedule bug. Should be unscheduled instead.
-    assert_eq!(
-        Some(Schedule::Persisted("*/1 * * * * * *".to_string())),
-        wait_for_signal(&mut app_signal).await.unwrap()
-    );
+    // Should be unscheduled
+    assert!(wait_for_signal(&mut app_signal).await.is_err());
+    assert!(!is_scheduled(&host_fn_caller, &cell).await);
 }
 
 /// Test schedule persisted function which gives an error
 /// Assuming a schedular interval of 100ms
 #[tokio::test(flavor = "multi_thread")]
-async fn schedule_error() {
+#[ignore = "Test not passing ; awaiting bug fix"]
+async fn schedule_persisted_error() {
     holochain_trace::test_run();
 
     // Start with a crontab that triggers every second, then have it return an error.
@@ -177,6 +222,8 @@ async fn schedule_error() {
     let app = conductor.setup_app("app", &[dna.0.clone()]).await.unwrap();
     let cell = app.into_cells()[0].clone();
     let mut app_signal = conductor.subscribe_to_app_signals("app".into());
+    let host_fn_caller =
+        HostFnCaller::create_for_zome(cell.cell_id(), &conductor.raw_handle(), &dna.0, 0).await;
 
     // Start test: schedule function
     conductor
@@ -190,17 +237,15 @@ async fn schedule_error() {
         Some(Schedule::Persisted("*/1 * * * * * *".to_string())),
         wait_for_signal(&mut app_signal).await.unwrap()
     );
-    // Scheduled function is called with input from previous output.
-    assert_eq!(
-        Some(Schedule::Persisted("*/1 * * * * * *".to_string())),
-        wait_for_signal(&mut app_signal).await.unwrap()
-    );
+    // Should be unscheduled
+    assert!(wait_for_signal(&mut app_signal).await.is_err());
+    assert!(!is_scheduled(&host_fn_caller, &cell).await);
 }
 
 /// Test schedule persisted fn with no next crontab schedule
 /// Assuming a schedular interval of 100ms
 #[tokio::test(flavor = "multi_thread")]
-async fn schedule_crontab_end() {
+async fn schedule_persisted_crontab_end() {
     holochain_trace::test_run();
 
     // Start with an outdated crontab that has no next schedule.
