7002f3b5e89731929708da3ef877cc73c9ed1244
Eric Harris-Braun <eric@harris-braun.com>
2025-08-27 15:05:18 -0400

    fix: WIP add test for dumping network metrics for clone cell

--- crates/holochain/src/conductor/tests/network_info.rs
+++ crates/holochain/src/conductor/tests/network_info.rs
@@ -1,7 +1,10 @@
 use crate::sweettest::*;
+use hdk::prelude::DnaModifiersOpt;
+use holochain_types::app::CreateCloneCellPayload;
 use holochain_types::network::Kitsune2NetworkMetricsRequest;
 use holochain_types::prelude::InstalledAppId;
 use holochain_wasm_test_utils::TestWasm;
+use crate::prelude::fake_dna_hash;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn network_metrics() {
@@ -31,4 +34,42 @@ async fn network_metrics() {
 
     let network_metrics = &network_metrics[dna.dna_hash()];
     assert_eq!(network_metrics.gossip_state_summary.peer_meta.len(), 2);
+
+    let fake_dna = fake_dna_hash(1);
+    let response = conductors[0]
+    .dump_network_metrics_for_app(
+        &app_id,
+        Kitsune2NetworkMetricsRequest {
+            dna_hash: Some(fake_dna),
+            include_dht_summary: false,
+        },
+    )
+    .await;
+    assert!(response.is_err());
+
+    // Create a disabled clone cell for app1
+    let clone_cell = conductors[0]
+        .create_clone_cell(
+            &app_id,
+            CreateCloneCellPayload {
+                role_name: dna.dna_hash().to_string(),
+                modifiers: DnaModifiersOpt::none().with_network_seed("test_seed".to_string()),
+                membrane_proof: None,
+                name: Some("disabled_clone".into()),
+            },
+        )
+        .await
+        .unwrap();
+
+    let response = conductors[0]
+    .dump_network_metrics_for_app(
+        &app_id,
+        Kitsune2NetworkMetricsRequest {
+            dna_hash: Some(clone_cell.cell_id.dna_hash().clone()),
+            include_dht_summary: false,
+        },
+    )
+    .await;
+    assert!(!response.is_err());
+
 }
