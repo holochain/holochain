0c5e199b921ef649b2b2e3d63b0829aa2d3cb550
Eric Harris-Braun <eric@harris-braun.com>
2025-08-29 16:54:02 -0400

    fix: backport network_metrics clone-cell fix

--- crates/holochain/src/conductor/tests/network_info.rs
+++ crates/holochain/src/conductor/tests/network_info.rs
@@ -1,8 +1,11 @@
+use crate::prelude::fake_dna_hash;
+use crate::sweettest::*;
+use hdk::prelude::CloneCellId;
+use hdk::prelude::DnaModifiersOpt;
+use holochain_types::app::{CreateCloneCellPayload, EnableCloneCellPayload};
 use holochain_types::network::Kitsune2NetworkMetricsRequest;
 use holochain_types::prelude::InstalledAppId;
 use holochain_wasm_test_utils::TestWasm;
-
-use crate::sweettest::*;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn network_metrics() {
@@ -10,7 +13,7 @@ async fn network_metrics() {
 
     let (dna, _, _) = SweetDnaFile::unique_from_test_wasms(vec![TestWasm::Create]).await;
     let number_of_peers = 3;
-    let config = SweetConductorConfig::standard().no_dpki();
+    let config = SweetConductorConfig::standard();
     let mut conductors = SweetConductorBatch::from_config(number_of_peers, config).await;
     let app_id: InstalledAppId = "app".into();
     conductors.setup_app(&app_id, &[dna.clone()]).await.unwrap();
@@ -32,4 +35,50 @@ async fn network_metrics() {
 
     let network_metrics = &network_metrics[dna.dna_hash()];
     assert_eq!(network_metrics.gossip_state_summary.peer_meta.len(), 2);
+
+    let fake_dna = fake_dna_hash(1);
+    let response = conductors[0]
+        .dump_network_metrics_for_app(
+            &app_id,
+            Kitsune2NetworkMetricsRequest {
+                dna_hash: Some(fake_dna),
+                include_dht_summary: false,
+            },
+        )
+        .await;
+    assert!(response.is_err());
+
+    // Create a disabled clone cell for app1
+    let clone_cell = conductors[0]
+        .create_clone_cell(
+            &app_id,
+            CreateCloneCellPayload {
+                role_name: dna.dna_hash().to_string(),
+                modifiers: DnaModifiersOpt::none().with_network_seed("test_seed".to_string()),
+                membrane_proof: None,
+                name: Some("disabled_clone".into()),
+            },
+        )
+        .await
+        .unwrap();
+
+    let clone_cell_id = CloneCellId::CloneId(clone_cell.clone_id);
+    let response = conductors[0]
+        .clone()
+        .enable_clone_cell(&app_id, &EnableCloneCellPayload { clone_cell_id })
+        .await;
+    assert!(response.is_ok());
+
+    let response = conductors[0]
+        .dump_network_metrics_for_app(
+            &app_id,
+            Kitsune2NetworkMetricsRequest {
+                dna_hash: Some(clone_cell.cell_id.dna_hash().clone()),
+                include_dht_summary: false,
+            },
+        )
+        .await;
+    assert!(response
+        .unwrap()
+        .contains_key(clone_cell.cell_id.dna_hash()));
 }
