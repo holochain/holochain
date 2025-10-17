6ec53729a74f407c7cc051b1f5b5217873bc5879
Jost <jost-s@users.noreply.github.com>
2025-10-17 17:10:22 -0600

    test: randomize validation statuses in get_op_by_type tests

--- crates/holochain_cascade/tests/tests/fetch_op_by_type.rs
+++ crates/holochain_cascade/tests/tests/fetch_op_by_type.rs
@@ -1,53 +1,31 @@
 use ::fixt::fixt;
-use holo_hash::{
-    fixt::{ActionHashFixturator, DnaHashFixturator},
-    HashableContentExtSync,
-};
-use holochain_cascade::{authority, CascadeImpl};
+use holo_hash::{fixt::DnaHashFixturator, AgentPubKey, DnaHash, HashableContentExtSync};
+use holochain_cascade::{authority, test_utils::create_test_chain_op, CascadeImpl};
 use holochain_keystore::test_keystore;
 use holochain_p2p::{
-    event::MockHcP2pHandler, spawn_holochain_p2p, HolochainP2pConfig, HolochainP2pDna,
+    actor::DynHcP2p, event::MockHcP2pHandler, spawn_holochain_p2p, HolochainP2pConfig,
+    HolochainP2pDna,
 };
 use holochain_state::prelude::{
-    insert_op_dht, set_validation_status, test_cache_db, test_conductor_db, test_dht_db,
+    insert_op_dht, set_validation_status, set_when_integrated, test_cache_db, test_conductor_db,
+    test_dht_db,
 };
 use holochain_types::{
     db::{DbKindDht, DbKindPeerMetaStore, DbWrite},
-    dht_op::{ChainOp, DhtOp, DhtOpHashed},
-    fixt::{ActionFixturator, CreateFixturator, EntryFixturator, SignatureFixturator},
-    prelude::{Judged, SignedAction, ValidationStatus},
+    dht_op::{DhtOp, DhtOpHashed},
+    prelude::{Judged, Timestamp, ValidationStatus},
 };
-use holochain_zome_types::{op::ChainOpType, Action};
+use holochain_zome_types::op::ChainOpType;
+use rand::seq::IteratorRandom;
 use std::{sync::Arc, time::Duration};
 
 #[tokio::test(flavor = "multi_thread")]
-async fn t() {
+async fn fetch_op_by_type_over_network() {
     holochain_trace::test_run();
     let dna_hash = fixt!(DnaHash);
-    let alice_keystore = test_keystore();
+
+    // Set up Alice as the serving node.
     let alice_dht = test_dht_db();
-    let alice_cache = test_cache_db();
-    let alice_op_db = DbWrite::test_in_mem(DbKindDht(Arc::new(dna_hash.clone()))).unwrap();
-    let alice_peer_meta_db =
-        DbWrite::test_in_mem(DbKindPeerMetaStore(Arc::new(dna_hash.clone()))).unwrap();
-    let alice_network = spawn_holochain_p2p(
-        HolochainP2pConfig {
-            get_conductor_db: Arc::new(|| Box::pin(async { test_conductor_db().to_db() })),
-            get_db_op_store: Arc::new(move |_| {
-                let op_db = alice_op_db.clone();
-                Box::pin(async move { Ok(op_db) })
-            }),
-            get_db_peer_meta: Arc::new(move |_| {
-                let peer_meta_db = alice_peer_meta_db.clone();
-                Box::pin(async move { Ok(peer_meta_db) })
-            }),
-            k2_test_builder: true,
-            ..Default::default()
-        },
-        alice_keystore.clone(),
-    )
-    .await
-    .unwrap();
     let mut alice_handler = MockHcP2pHandler::new();
     let alice_dht_2 = alice_dht.clone();
     alice_handler
@@ -62,56 +40,24 @@ async fn t() {
                 )
             })
         });
-    alice_network
-        .register_handler(Arc::new(alice_handler))
-        .await
-        .unwrap();
-    let alice = alice_keystore.new_sign_keypair_random().await.unwrap();
-    alice_network
-        .join(dna_hash.clone(), alice.clone(), None)
-        .await
-        .unwrap();
+    let TestCase {
+        network: alice_network,
+        agent: alice,
+    } = TestCase::new(&dna_hash, alice_handler).await;
+
     // Alice should respond to all get requests.
     alice_network
         .test_set_full_arcs(dna_hash.to_k2_space())
         .await;
-    let alice_network = Arc::new(HolochainP2pDna::new(alice_network, dna_hash.clone(), None));
-    let alice_cascade = CascadeImpl::empty()
-        .with_dht(alice_dht.to_db().into())
-        .with_network(alice_network, alice_cache.to_db());
 
-    let bob_keystore = test_keystore();
+    // Set up Bob as the requesting node.
     let bob_cache = test_cache_db();
-    let bob_op_db = DbWrite::test_in_mem(DbKindDht(Arc::new(dna_hash.clone()))).unwrap();
-    let bob_peer_meta_db =
-        DbWrite::test_in_mem(DbKindPeerMetaStore(Arc::new(dna_hash.clone()))).unwrap();
-    let bob_network = spawn_holochain_p2p(
-        HolochainP2pConfig {
-            get_conductor_db: Arc::new(|| Box::pin(async { test_conductor_db().to_db() })),
-            get_db_op_store: Arc::new(move |_| {
-                let op_db = bob_op_db.clone();
-                Box::pin(async move { Ok(op_db) })
-            }),
-            get_db_peer_meta: Arc::new(move |_| {
-                let peer_meta_db = bob_peer_meta_db.clone();
-                Box::pin(async move { Ok(peer_meta_db) })
-            }),
-            k2_test_builder: true,
-            ..Default::default()
-        },
-        bob_keystore.clone(),
-    )
-    .await
-    .unwrap();
-    bob_network
-        .register_handler(Arc::new(MockHcP2pHandler::new()))
-        .await
-        .unwrap();
-    let bob = bob_keystore.new_sign_keypair_random().await.unwrap();
-    bob_network
-        .join(dna_hash.clone(), bob.clone(), None)
-        .await
-        .unwrap();
+    let TestCase {
+        network: bob_network,
+        ..
+    } = TestCase::new(&dna_hash, MockHcP2pHandler::new()).await;
+
+    // Wait for Bob to see Alice in his peer store.
     tokio::time::timeout(Duration::from_secs(1), async {
         loop {
             if bob_network
@@ -125,53 +71,113 @@ async fn t() {
             {
                 break;
             }
-            tokio::time::sleep(Duration::from_millis(10)).await;
+            tokio::time::sleep(Duration::from_millis(100)).await;
         }
     })
     .await
     .unwrap();
+
+    // Set up Bob's cascade.
     let bob_network = Arc::new(HolochainP2pDna::new(bob_network, dna_hash.clone(), None));
     let bob_cascade = CascadeImpl::empty()
         .with_cache(bob_cache.to_db())
         .with_network(bob_network, bob_cache.to_db());
 
-    let action_hash = fixt!(ActionHash);
-    let maybe_chain_op = bob_cascade
-        .fetch_op_by_type(action_hash, ChainOpType::RegisterAgentActivity)
-        .await
-        .unwrap();
-    assert_eq!(maybe_chain_op, None);
+    // Test all ChainOpType variants
+    let op_types = vec![
+        ChainOpType::StoreRecord,
+        ChainOpType::StoreEntry,
+        ChainOpType::RegisterAgentActivity,
+        ChainOpType::RegisterUpdatedContent,
+        ChainOpType::RegisterUpdatedRecord,
+        ChainOpType::RegisterDeletedBy,
+        ChainOpType::RegisterDeletedEntryAction,
+        ChainOpType::RegisterAddLink,
+        ChainOpType::RegisterRemoveLink,
+    ];
 
-    let mut create = fixt!(Create);
-    let entry = fixt!(Entry);
-    create.entry_hash = entry.to_hash();
-    let action = SignedAction::new(Action::Create(create), fixt!(Signature));
-    let action_hash = action.to_hash();
-    let chain_op =
-        ChainOp::from_type(ChainOpType::RegisterAgentActivity, action, Some(entry)).unwrap();
-    // let validation_status = ValidationStatus::Valid;
+    for op_type in op_types {
+        let chain_op = create_test_chain_op(op_type);
+        let action_hash = chain_op.action().to_hash();
 
-    // Insert op into Alice's DHT db.
-    alice_dht.test_write({
-        let chain_op = chain_op.clone();
-        move |txn| {
-            insert_op_dht(
-                txn,
-                &DhtOpHashed::from_content_sync(DhtOp::ChainOp(Box::new(chain_op))),
-                0,
-                None,
-            )
+        // Use a random validation status for each op type.
+        let validation_statuses = vec![
+            ValidationStatus::Abandoned,
+            ValidationStatus::Rejected,
+            ValidationStatus::Valid,
+        ];
+        let validation_status = validation_statuses
+            .iter()
+            .choose(&mut rand::rng())
+            .unwrap()
+            .clone();
+
+        let maybe_chain_op = bob_cascade
+            .fetch_op_by_type(action_hash.clone(), chain_op.get_type())
+            .await
             .unwrap();
-            set_validation_status(txn, hash, status)
-        }
-    });
+        assert_eq!(maybe_chain_op, None);
 
-    let maybe_chain_op = bob_cascade
-        .fetch_op_by_type(action_hash, ChainOpType::RegisterAgentActivity)
+        // Insert op into Alice's DHT db.
+        alice_dht.test_write({
+            let chain_op = chain_op.clone();
+            move |txn| {
+                let dht_op = DhtOpHashed::from_content_sync(DhtOp::ChainOp(Box::new(chain_op)));
+                insert_op_dht(txn, &dht_op, 0, None).unwrap();
+                set_validation_status(txn, &dht_op.hash, validation_status).unwrap();
+                set_when_integrated(txn, &dht_op.hash, Timestamp::now()).unwrap();
+            }
+        });
+
+        let maybe_chain_op = bob_cascade
+            .fetch_op_by_type(action_hash, chain_op.get_type())
+            .await
+            .unwrap();
+        assert_eq!(
+            maybe_chain_op,
+            Some(Judged::new(chain_op, validation_status))
+        );
+    }
+}
+
+struct TestCase {
+    network: DynHcP2p,
+    agent: AgentPubKey,
+}
+
+impl TestCase {
+    async fn new(dna_hash: &DnaHash, p2p_handler: MockHcP2pHandler) -> Self {
+        let keystore = test_keystore();
+        let op_db = DbWrite::test_in_mem(DbKindDht(Arc::new(dna_hash.clone()))).unwrap();
+        let peer_meta_db =
+            DbWrite::test_in_mem(DbKindPeerMetaStore(Arc::new(dna_hash.clone()))).unwrap();
+        let network = spawn_holochain_p2p(
+            HolochainP2pConfig {
+                get_conductor_db: Arc::new(|| Box::pin(async { test_conductor_db().to_db() })),
+                get_db_op_store: Arc::new(move |_| {
+                    let op_db = op_db.clone();
+                    Box::pin(async move { Ok(op_db) })
+                }),
+                get_db_peer_meta: Arc::new(move |_| {
+                    let peer_meta_db = peer_meta_db.clone();
+                    Box::pin(async move { Ok(peer_meta_db) })
+                }),
+                k2_test_builder: true,
+                ..Default::default()
+            },
+            keystore.clone(),
+        )
         .await
         .unwrap();
-    assert_eq!(
-        maybe_chain_op,
-        Some(Judged::new(chain_op, ValidationStatus::Valid))
-    );
+        network
+            .register_handler(Arc::new(p2p_handler))
+            .await
+            .unwrap();
+        let agent = keystore.new_sign_keypair_random().await.unwrap();
+        network
+            .join(dna_hash.clone(), agent.clone(), None)
+            .await
+            .unwrap();
+        Self { network, agent }
+    }
 }
