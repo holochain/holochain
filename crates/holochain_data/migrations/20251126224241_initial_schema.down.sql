ea7a44e340afb865b25a9bb31beb0eb5a3e932c7
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-12-03 11:33:16 +0000

    refactor: consolidate conductor schema into initial migration
    Since we're pre-0.7.0 release, breaking changes are acceptable.
    Merged the conductor schema migration into the initial migration
    to maintain a single migration file for development.

--- crates/holochain_data/migrations/20251126224241_initial_schema.down.sql
+++ crates/holochain_data/migrations/20251126224241_initial_schema.down.sql
@@ -8,3 +8,10 @@ DROP TABLE IF EXISTS IntegrityZome;
 DROP TABLE IF EXISTS EntryDef;
 DROP TABLE IF EXISTS DnaDef;
 DROP TABLE IF EXISTS Wasm;
+-- Revert Conductor database schema
+DROP TABLE IF EXISTS SignalSubscription;
+DROP TABLE IF EXISTS AppInterface;
+DROP TABLE IF EXISTS CloneCell;
+DROP TABLE IF EXISTS AppRole;
+DROP TABLE IF EXISTS InstalledApp;
+DROP TABLE IF EXISTS Conductor;
