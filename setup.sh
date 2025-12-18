0567dc5109c714014da074ff79fe871d85ff7f49
Christian Visintin <christian.visintin@veeso.dev>
2025-12-18 10:25:58 +0100

    test: bash scripts

--- setup.sh
+++ setup.sh
@@ -4,45 +4,7 @@ set -e
 
 export NIX_INSTALLER_URL=${NIX_INSTALLER_URL:-https://releases.nixos.org/nix/nix-2.25.4/install}
 
-run_cmd() {
-    echo "$@"
-    "$@"
-}
-
-append_nix_experimental_features() {
-    local features
-    features="$@"
-    # check if nix.conf exists
-    if [ -f ~/.config/nix/nix.conf ]; then
-        # case when nix.conf exists
-        # check if experimental-features line exists
-        if grep -q "^experimental-features" ~/.config/nix/nix.conf; then
-            # case when experimental-features line exists
-            # get current features
-            features_list=$(grep "^experimental-features" ~/.config/nix/nix.conf | cut -d '=' -f 2 | sed 's/^ *//')
-            # append new features if not already present
-            for feature in $features; do
-                if ! echo " $features_list " | grep -q " $feature "; then
-                    features_list="$features_list $feature"
-                fi
-            done
-            # update nix.conf with new features
-            # we need to use -i.bak for compatibility with both GNU sed and BSD sed (macOS)
-            run_cmd sed -i.bak "s|^experimental-features = .*|experimental-features = $features_list|" ~/.config/nix/nix.conf
-            # so we can remove the backup file
-            run_cmd rm ~/.config/nix/nix.conf.bak
-        else
-            # case when experimental-features line does not exist
-            # add new line
-            run_cmd bash -c "echo 'experimental-features = $features' >>~/.config/nix/nix.conf"
-        fi
-    else
-        # case when nix.conf does not exist
-        # create nix.conf with features
-        run_cmd mkdir -p ~/.config/nix
-        run_cmd bash -c "echo 'experimental-features = $features' >>~/.config/nix/nix.conf"
-    fi
-}
+source "$(dirname "$0")/src/setup_functions.sh"
 
 if ! command -v nix &>/dev/null; then
     echo "Nix package manager not found"
