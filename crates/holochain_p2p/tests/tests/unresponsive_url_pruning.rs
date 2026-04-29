538c69571ef520de4ea0b56c98b96359b55003de
Callum Dunster <cdunster@users.noreply.github.com>
2026-04-29 13:35:56 +0200

    test: fix flaky tests that dropped temp dir too early
    Dropping the temporary directory caused the dir and all the database
    files to be deleted, causing the tests to fail.

--- crates/holochain_p2p/tests/tests/unresponsive_url_pruning.rs
+++ crates/holochain_p2p/tests/tests/unresponsive_url_pruning.rs
@@ -19,6 +19,7 @@ async fn urls_are_pruned_at_an_interval() {
     let TestCase {
         peer_meta_store,
         db_peer_meta,
+        _dir,
         ..
     } = TestCase::spawn().await;
 
@@ -61,6 +62,7 @@ async fn urls_are_pruned_when_updated_agent_info_available() {
         lair_client,
         peer_meta_store,
         db_peer_meta,
+        _dir,
     } = TestCase::spawn().await;
 
     let earliest_timestamp = Timestamp::from_micros(Timestamp::now().as_micros() - 10_000_000); // 10s ago
@@ -207,6 +209,7 @@ struct TestCase {
     lair_client: MetaLairClient,
     peer_meta_store: DynPeerMetaStore,
     db_peer_meta: holochain_data::DbWrite<PeerMetaStoreKind>,
+    _dir: tempfile::TempDir,
 }
 
 impl TestCase {
@@ -286,6 +289,7 @@ impl TestCase {
             lair_client,
             peer_meta_store,
             db_peer_meta,
+            _dir: dir,
         }
     }
 }
