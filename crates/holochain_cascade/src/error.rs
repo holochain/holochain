575caed9d51aac551b814117f594d63fa60f49eb
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-07-14 10:21:32 +0100

    refactor: Cleanup cascade unused code, unnecessary clippy allows and outdated documentation

--- crates/holochain_cascade/src/error.rs
+++ crates/holochain_cascade/src/error.rs
@@ -1,22 +1,11 @@
 #![allow(missing_docs)]
 
-use holo_hash::{ActionHash, AnyDhtHash};
 use holochain_p2p::HolochainP2pError;
-use holochain_serialized_bytes::SerializedBytesError;
-use holochain_state::source_chain::SourceChainError;
 use holochain_types::prelude::*;
-use holochain_zome_types::action::conversions::WrongActionError;
 use thiserror::Error;
-use tokio::task::JoinError;
 
 #[derive(Error, Debug)]
 pub enum CascadeError {
-    #[error(transparent)]
-    ActionError(#[from] ActionError),
-
-    #[error("Expected this Action to contain an Entry: {0}")]
-    EntryMissing(ActionHash),
-
     #[error(transparent)]
     DhtOpError(#[from] DhtOpError),
 
@@ -27,22 +16,7 @@ pub enum CascadeError {
     InvalidInput(String),
 
     #[error(transparent)]
-    JoinError(#[from] JoinError),
-
-    #[error(transparent)]
-    SourceChainError(#[from] SourceChainError),
-
-    #[error(transparent)]
     NetworkError(#[from] HolochainP2pError),
-
-    #[error(transparent)]
-    SerializedBytesError(#[from] SerializedBytesError),
-
-    #[error(transparent)]
-    WrongActionError(#[from] WrongActionError),
-
-    #[error("Cell is an authority for is missing or incorrect: {0}")]
-    AuthorityDataError(#[from] AuthorityDataError),
 
     #[error(transparent)]
     QueryError(#[from] holochain_state::query::StateQueryError),
@@ -50,37 +24,8 @@ pub enum CascadeError {
     #[error(transparent)]
     StateMutationError(#[from] holochain_state::mutations::StateMutationError),
 
-    #[error(transparent)]
-    SyncScratchError(#[from] holochain_state::scratch::SyncScratchError),
-
     #[error("Network not initialized")]
     NetworkNotInitialized,
 }
 
 pub type CascadeResult<T> = Result<T, CascadeError>;
-
-#[derive(Error, Debug)]
-pub enum AuthorityDataError {
-    // #[error(transparent)]
-    // DhtOpConvertError(#[from] DhtOpConvertError),
-    #[error(transparent)]
-    WrongActionError(#[from] WrongActionError),
-    #[error(transparent)]
-    ActionError(#[from] ActionError),
-    #[error("Missing record data: {0:?}")]
-    MissingData(String),
-    #[error("Missing metadata: {0:?}")]
-    MissingMetadata(String),
-}
-
-impl AuthorityDataError {
-    pub fn missing_data<T: std::fmt::Debug>(data: T) -> CascadeError {
-        Self::MissingData(format!("Missing action {data:?}")).into()
-    }
-    pub fn missing_data_entry<T: std::fmt::Debug>(data: T) -> CascadeError {
-        Self::MissingData(format!("Missing entry for action {data:?}")).into()
-    }
-    pub fn missing_metadata<T: std::fmt::Debug>(data: T) -> CascadeError {
-        Self::MissingMetadata(format!("{data:?}")).into()
-    }
-}
